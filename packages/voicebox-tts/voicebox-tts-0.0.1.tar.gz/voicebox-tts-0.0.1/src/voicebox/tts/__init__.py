a6e5d10281dd8389d4b711ffb876b4d7fcf37d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 15:04:20 +0000

    Add package voicebox-tts/0.0.1/voicebox-tts-0.0.1.tar.gz

---
+++ packages/voicebox-tts/voicebox-tts-0.0.1.tar.gz/voicebox-tts-0.0.1/src/voicebox/tts/__init__.py
@@ -0,0 +1,26 @@
+from voicebox.tts.tts import TTS, FallbackTTS, RetryTTS
+
+try:
+    from voicebox.tts.amazonpolly import AmazonPolly
+except ImportError:
+    pass
+
+from voicebox.tts.cache import CachedTTS, PrerecordedTTS
+from voicebox.tts.espeakng import ESpeakConfig, ESpeakNG
+
+try:
+    from voicebox.tts.elevenlabs import ElevenLabs
+except ImportError:
+    pass
+
+try:
+    from voicebox.tts.googlecloudtts import GoogleCloudTTS
+except ImportError:
+    pass
+
+try:
+    from voicebox.tts.gtts import gTTS
+except ImportError:
+    pass
+
+from voicebox.tts.picotts import PicoTTS
