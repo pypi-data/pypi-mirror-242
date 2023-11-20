5631d4d80f2292b4f6914c06aca71fed9b37e499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 14:03:36 +0000

    Add package inputselect/0.8.1/inputselect-0.8.1.tar.gz

---
+++ packages/inputselect/inputselect-0.8.1.tar.gz/inputselect-0.8.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="inputselect",
+    version="0.8.1",
+    packages=find_packages(),
+    install_requires=[
+        "keyboard>=0.6.0"
+    ],
+    author="ItchyPython3759"
+)
