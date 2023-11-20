8d78b5ce89a0af4c7f13a63580e1bd14a5498889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 15:04:05 +0000

    Add package notus-scanner/22.6.1/notus_scanner-22.6.1.tar.gz

---
+++ packages/notus-scanner/notus_scanner-22.6.1.tar.gz/notus_scanner-22.6.1/notus/scanner/loader/loader.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2021-2023 Greenbone AG
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+import logging
+from typing import Optional
+
+from ..models.packages.package import PackageAdvisories
+
+logger = logging.getLogger(__name__)
+
+
+class AdvisoriesLoader:
+    def load_package_advisories(
+        self, operating_system: str
+    ) -> Optional[PackageAdvisories]:
+        raise NotImplementedError()
