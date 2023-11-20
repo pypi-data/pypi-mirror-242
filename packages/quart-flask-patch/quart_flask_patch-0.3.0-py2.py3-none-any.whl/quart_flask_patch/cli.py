c3478bf9449651becd91f5f369bc0d1dbfc6437b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 05:03:36 +0000

    Add package quart-flask-patch/0.3.0/quart_flask_patch-0.3.0-py2.py3-none-any.whl

---
+++ packages/quart-flask-patch/quart_flask_patch-0.3.0-py2.py3-none-any.whl/quart_flask_patch/cli.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from quart.cli import (  # noqa: F401
+    AppGroup,
+    click,
+    QuartGroup,
+    run_command,
+    ScriptInfo,
+    shell_command,
+    with_appcontext,
+)
+
+FlaskGroup = QuartGroup
