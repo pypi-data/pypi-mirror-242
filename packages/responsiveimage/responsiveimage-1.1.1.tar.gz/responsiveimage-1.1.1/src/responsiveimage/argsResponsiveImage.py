eccb106abe98e1d5a3708a1112b02d21d6b14b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 03:05:27 +0000

    Add package responsiveimage/1.1.1/responsiveimage-1.1.1.tar.gz

---
+++ packages/responsiveimage/responsiveimage-1.1.1.tar.gz/responsiveimage-1.1.1/src/responsiveimage/argsResponsiveImage.py
@@ -0,0 +1,81 @@
+'''
+MIT License
+Copyright (c) 2023 Pascal Brand
+
+Extends argparse
+'''
+
+import argparse
+
+
+def _check_arg_errors(argsparsed: argparse.Namespace) -> argparse.Namespace:
+  # update argsparsed, and check for errors
+  if (argsparsed.size is not None) and (argsparsed.height is not None):
+    raise RuntimeError("--size and --height cannot be both set")
+
+  if (argsparsed.size is None) and (argsparsed.height is None):
+    argsparsed.size = '1920'
+
+  if (argsparsed.size is not None):
+    transform = argsparsed.size.split(',')
+  elif (argsparsed.height is not None):
+    transform = argsparsed.height.split(',')
+  else:
+    raise RuntimeError("Developer issue")
+
+  if argsparsed.add_name is None:
+    if len(transform) == 1:
+      argsparsed.add_name = ''
+    else:
+      argsparsed.add_name = '-' + ',-'.join(transform)
+  else:
+    # check length of add_name is same as length of transform
+    if argsparsed.add_name.count(',') != len(transform)-1:
+      raise RuntimeError("--size (or --height) and --add-name must have the same number of elements")
+
+  if argsparsed.crop is not None:
+    values = argsparsed.crop.split(',')
+    if len(values) != 4:
+      raise RuntimeError("--crop must be equal to a,b,c,d (4 values)")
+    for index, _ in enumerate(values):
+      values[index] = int(values[index])
+    argsparsed.crop = values
+
+  return argsparsed
+
+class argsResponsiveImage():
+  '''
+  extends argparse
+  '''
+  def __init__(self, argsparsed: argparse.Namespace, nb: int) -> None:
+    argsparsed = _check_arg_errors(argsparsed)
+
+    self.args = argsparsed
+    self.nb = nb
+    self.parameters = {   # TODO: add a commandline option to update it dynamically
+      'jpg': {
+        'quality': 80,
+        'progressive': True,
+        'subsampling': '4:2:0',
+        },
+      'webp': {
+        'quality': 80,
+      }
+    }
+
+  def inc(self) -> None:
+    '''
+    increment the number of processed images
+    '''
+    if self.nb is not None:
+      self.nb = self.nb + 1
+
+  def print(self, filename: str, processed: bool) -> None:
+    '''
+    verbose the number of processed images
+    '''
+    if self.nb is not None:
+      if (processed):
+        print('  + ' + str(self.nb) + ' ' + filename)
+      else:
+        print('  - ' + str(self.nb) + ' ' + filename)
---
+++ packages/responsiveimage/responsiveimage-1.1.1.tar.gz/responsiveimage-1.1.1/src/responsiveimage/mp4.py
@@ -0,0 +1,98 @@
+'''
+MIT License
+Copyright (c) 2023 Pascal Brand
+
+mp4 utility functions:
+- save
+'''
+
+import os
+import shutil
+import subprocess
+import tempfile
+from . import argsResponsiveImage
+
+def responsive(args: argsResponsiveImage.argsResponsiveImage, filename: str) -> None:
+  '''
+  process mp4 file filename (only the filename, without src_dir)
+
+  if args.args.mp4_as_gif:
+  - create animated gif and webp
+
+  otherwise
+  - rescale mp4 (TODO: right now 1024)
+  '''
+
+  args.inc()
+
+  srcFullFilename = os.path.join(args.args.src_dir, filename)
+  dstFullFilename = os.path.join(args.args.dst_dir, filename)
+
+  if args.args.mp4_as_gif:
+    (dstName, _) = os.path.splitext(dstFullFilename)
+
+    filename_gif = dstName +'.gif'
+    filename_webp = dstName +'.webp'
+
+    if (not args.args.force) and (os.path.isfile(filename_gif)) and (os.path.isfile(filename_webp)):
+      args.print(filename, False)
+      return
+
+    args.print(filename, True)
+
+    # mp4 -> gif: https://superuser.com/questions/556029/how-do-i-convert-a-video-to-gif-using-ffmpeg-with-reasonable-quality
+    palette = tempfile.gettempdir() + '/palette.png'
+    filters='fps=2,scale=-1:-1:flags=lanczos'
+
+    subprocess.call([
+      'ffmpeg',
+      '-y',
+      '-i', srcFullFilename,
+      '-vf', filters+',palettegen',
+      '-y', palette ])
+
+    subprocess.call([
+      'ffmpeg',
+      '-y',
+      '-i', srcFullFilename,
+      '-i', palette,
+      '-lavfi', filters + ' [x]; [x][1:v] paletteuse',
+      '-y',
+      '-loop', '0',
+      '-compression_level', '6',
+      filename_gif])
+
+    # mp4 -> webp
+    subprocess.call([
+      'ffmpeg',
+      '-y',
+      '-i', srcFullFilename,
+      '-i', palette,
+      '-lavfi', filters + ' [x]; [x][1:v] paletteuse',
+      '-y',
+      '-loop', '0',
+      '-compression_level', '6',
+      filename_webp])
+
+  else:
+
+    if (not args.args.force) and (os.path.isfile(dstFullFilename)):
+      args.print(filename, False)
+      return
+    args.print(filename, True)
+
+    # -vf option: https://trac.ffmpeg.org/wiki/Scaling
+    # pad: cf. https://stackoverflow.com/questions/20847674/ffmpeg-libx264-height-not-divisible-by-2
+    subprocess.call([
+      'ffmpeg',
+      '-y',
+      '-i', srcFullFilename,
+      '-map_metadata', '0',   # copy video media properties - keep this option right after the -i option
+      '-vf', 'scale=w=\'min(1024,iw)\':h=\'min(1024,ih)\':force_original_aspect_ratio=decrease,pad=ceil(iw/2)*2:ceil(ih/2)*2',   # todo: scale
+      dstFullFilename,
+      '-loglevel', 'repeat+level+verbose',
+      # '-loglevel', 'quiet'
+      ])
+
+    # update timestamp
+    shutil.copystat(srcFullFilename, dstFullFilename)
