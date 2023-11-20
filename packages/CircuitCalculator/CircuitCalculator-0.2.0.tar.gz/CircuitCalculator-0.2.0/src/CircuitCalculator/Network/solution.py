22bbc7105d18d34a681b5d4b19844db0c9b96712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 15:04:02 +0000

    Add package CircuitCalculator/0.2.0/CircuitCalculator-0.2.0.tar.gz

---
+++ packages/CircuitCalculator/CircuitCalculator-0.2.0.tar.gz/CircuitCalculator-0.2.0/src/CircuitCalculator/Network/solution.py
@@ -0,0 +1,11 @@
+from .network import Network
+from typing import Callable, Protocol
+
+class NetworkSolution(Protocol):
+    def get_voltage(self, branch_id: str) -> complex: ...
+
+    def get_current(self, branch_id: str) -> complex: ...
+
+    def get_power(self, branch_id: str) -> complex: ...
+
+NetworkSolver = Callable[[Network], NetworkSolution]
