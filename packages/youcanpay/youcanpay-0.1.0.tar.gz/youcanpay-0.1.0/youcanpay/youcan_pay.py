7ac32d563b0d5b3bb59bc995d1708814e788a35e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 04:03:39 +0000

    Add package youcanpay/0.1.0/youcanpay-0.1.0.tar.gz

---
+++ packages/youcanpay/youcanpay-0.1.0.tar.gz/youcanpay-0.1.0/youcanpay/youcan_pay.py
@@ -0,0 +1,29 @@
+from youcanpay.adapters.adapter_picker import HttpAdapterPicker
+from youcanpay.api.endpoints import KeysEndpoint, TokenEndpoint
+from youcanpay.api.service import APIService
+
+
+class YouCanPay:
+    def __init__(self, api_service: APIService):
+        self.api_service = api_service
+        self.init_endpoints(api_service)
+
+    def init_endpoints(self, api_service: APIService):
+        self.token = TokenEndpoint(api_service)
+        self.keys = KeysEndpoint(api_service)
+
+    def use_keys(self, private_key: str, public_key: str):
+        self.api_service.use_keys(private_key, public_key)
+        self.init_endpoints(self.api_service)
+        return self
+
+    @classmethod
+    def instance(cls):
+        return cls(APIService(HttpAdapterPicker()))
+
+    @classmethod
+    def enable_sandbox_mode(cls):
+        APIService.is_sandbox_mode = True
+
+    def check_keys(self, private_key: str, public_key: str):
+        return self.keys.check(private_key=private_key, public_key=public_key)
