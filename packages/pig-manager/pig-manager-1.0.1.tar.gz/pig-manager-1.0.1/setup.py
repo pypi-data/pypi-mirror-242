34d0216880db40f052b547dc68b61344b0d7ef87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 04:03:42 +0000

    Add package pig-manager/1.0.1/pig-manager-1.0.1.tar.gz

---
+++ packages/pig-manager/pig-manager-1.0.1.tar.gz/pig-manager-1.0.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+from io import open
+from os import path
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+with open(path.join(HERE, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if ('git+' not in x) and (not x.startswith('#')) and (not x.startswith('-'))]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs if 'git+' not in x]
+
+setup(
+    name='pig-manager',
+    version='1.0.1',
+    description='Pretty prints Apache Pig outputs',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/MinatoNamikaze02/pig-pretty-printer', 
+    author='Arjun S',
+    author_email='arjunsreedar26@gmail.com',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=find_packages(),
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    entry_points={
+        'console_scripts': [
+            'pig_pretty_printer=scripts.__main__:main',
+        ],
+    },
+)
