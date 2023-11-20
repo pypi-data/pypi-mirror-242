773c0e78d0361642b35d8cce3d831ecaf6c23aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 15:04:34 +0000

    Add package pulumi-foo/1.0.1/pulumi_foo-1.0.1.tar.gz

---
+++ packages/pulumi-foo/pulumi_foo-1.0.1.tar.gz/pulumi_foo-1.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pulumi_foo', #package name
+      version='1.0.1',
+      description='Python Package By Predator_97',
+      author='Predator_97',
+      license='MIT',
+      zip_safe=False
+      )
