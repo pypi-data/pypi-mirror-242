69e08818da4251a2c5a1108bdd3c7e53597060f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 03:05:34 +0000

    Add package vp4onnx/0.1.1/vp4onnx-0.1.1-py3-none-any.whl

---
+++ packages/vp4onnx/vp4onnx-0.1.1-py3-none-any.whl/vp4onnx/app/predicts/det_YoloX_Lite.py
@@ -0,0 +1,7 @@
+from PIL import Image
+from vp4onnx.app.predicts import det_YoloX
+import onnxruntime as rt
+
+
+def predict(session:rt.InferenceSession, img_width:int, img_height:int, image:Image):
+    return det_YoloX.predict(session, img_width, img_height, image)
---
+++ packages/vp4onnx/vp4onnx-0.1.1-py3-none-any.whl/vp4onnx/version.py
@@ -0,0 +1,4 @@
+__title__ = 'Visual Prediction for onnx'
+__version__ = '0.1.1'
+
+__all__ = ['__version__']
