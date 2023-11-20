5f06924cd5406a6a1581504596345efb64614873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 15:03:57 +0000

    Add package alr-transformer/0.0.1/alr_transformer-0.0.1.tar.gz

---
+++ packages/alr-transformer/alr_transformer-0.0.1.tar.gz/alr_transformer-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['alr_transformer']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['einops', 'torch', 'zetascale']
+
+setup_kwargs = {
+    'name': 'alr-transformer',
+    'version': '0.0.1',
+    'description': 'Paper - Pytorch',
+    'long_description': '[![Multi-Modality](agorabanner.png)](https://discord.gg/qUtxnK2NMf)\n\n# ALR Transformer\n\n',
+    'author': 'Kye Gomez',
+    'author_email': 'kye@apac.ai',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/kyegomez/ShallowFF',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
