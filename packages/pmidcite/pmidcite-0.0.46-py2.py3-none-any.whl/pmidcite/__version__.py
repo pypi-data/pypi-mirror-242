7504d6b489b0c99ac1d0fc2c88f9e2f88df7b0cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 03:05:27 +0000

    Add package pmidcite/0.0.46/pmidcite-0.0.46-py2.py3-none-any.whl

---
+++ packages/pmidcite/pmidcite-0.0.46-py2.py3-none-any.whl/pmidcite/__version__.py
@@ -0,0 +1,7 @@
+"""Version of pmidcite project"""
+
+__copyright__ = 'Copyright (C) 2019-present, DV Klopfenstein, PhD. All rights reserved'
+__author__ = 'DV Klopfenstein, PhD'
+__version__ = '0.0.46'
+
+# Copyright (C) 2019-present, DV Klopfenstein, PhD. All rights reserved
