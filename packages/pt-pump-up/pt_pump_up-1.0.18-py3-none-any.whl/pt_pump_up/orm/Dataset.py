c9a312366192d8eb0b633b69e80da8a2d45cdf14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 03:04:37 +0000

    Add package pt-pump-up/1.0.18/pt_pump_up-1.0.18-py3-none-any.whl

---
+++ packages/pt-pump-up/pt_pump_up-1.0.18-py3-none-any.whl/pt_pump_up/orm/Dataset.py
@@ -0,0 +1,32 @@
+from typing import Optional, List
+from beanie import Document, Link
+from enum import Enum
+from pydantic import BaseModel
+from pt_pump_up.orm.License import License
+from pt_pump_up.orm.DatasetStats import DatasetStats
+from pt_pump_up.orm.Author import Author
+from pt_pump_up.orm.Conference import Conference
+
+
+class Status(Enum):
+    BROKEN_LINK = 1
+    READY = 2
+
+
+class Hrefs(BaseModel):
+    link_source: str
+    link_hf: Optional[str] = None
+    doi: Optional[str] = None
+
+
+class Dataset(Document):
+    name: str
+    language_stats: List[DatasetStats]
+    conference: Optional[Link[Conference]] = None
+    hrefs: Hrefs
+    year: int
+    # Status is an Enum
+    status: Status
+    overall_dataset_stats: Optional[DatasetStats] = None
+    authors: List[Link[Author]]
+    license: Optional[Link[License]] = None
