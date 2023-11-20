d66799ee15841c8fd8fb5c8acc9d3dd473ba7e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 15:03:25 +0000

    Add package RefractDQ/1.0.13/RefractDQ-1.0.13.tar.gz

---
+++ packages/RefractDQ/RefractDQ-1.0.13.tar.gz/RefractDQ-1.0.13/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "RefractDQ",
+    version = "1.0.13",
+    packages = find_packages(),
+    install_requires = [],
+    include_package_data=True
+)
