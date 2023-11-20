1a53cbda34ac2f40dc765bd8291a5f1468d79320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 14:03:50 +0000

    Add package pyrobox/0.9.1/pyrobox-0.9.1.tar.gz

---
+++ packages/pyrobox/pyrobox-0.9.1.tar.gz/pyrobox-0.9.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    package_dir={"pyrobox": "src",},
+    install_requires=[
+        'natsort',
+        "send2trash",
+    ],
+    entry_points='''
+        [console_scripts]
+        pyrobox=pyrobox:server.run
+    ''',
+
+    
+)
