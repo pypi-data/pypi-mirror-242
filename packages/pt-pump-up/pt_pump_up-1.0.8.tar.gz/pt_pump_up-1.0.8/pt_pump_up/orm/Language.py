f762d480e9ac97c7160f58f0f44d6db81663b1f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 03:04:18 +0000

    Add package pt-pump-up/1.0.8/pt_pump_up-1.0.8.tar.gz

---
+++ packages/pt-pump-up/pt_pump_up-1.0.8.tar.gz/pt_pump_up-1.0.8/pt_pump_up/orm/Language.py
@@ -0,0 +1,11 @@
+from beanie import Document
+from typing import Optional
+from pt_pump_up.orm.DatasetStats import DatasetStats
+
+
+# TODO: Integrate Python Library for ISO 639-1
+
+
+class Language(Document):
+    name: str
+    iso_code: str
