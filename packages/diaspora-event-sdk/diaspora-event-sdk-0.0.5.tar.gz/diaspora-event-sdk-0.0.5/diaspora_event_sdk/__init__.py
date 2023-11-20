aeeca8f9a5db2ad10c7dfad6caffad34bf0909d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 14:03:23 +0000

    Add package diaspora-event-sdk/0.0.5/diaspora-event-sdk-0.0.5.tar.gz

---
+++ packages/diaspora-event-sdk/diaspora-event-sdk-0.0.5.tar.gz/diaspora-event-sdk-0.0.5/diaspora_event_sdk/__init__.py
@@ -0,0 +1,14 @@
+""" Diaspora Event Fabric: Resilience-enabling services for science from HPC to edge.
+
+"""
+from diaspora_event_sdk.version import __version__ as _version
+
+__author__ = "The Diaspora Event Team"
+__version__ = _version
+
+from diaspora_event_sdk.sdk.client import Client  # Globus client
+from diaspora_event_sdk.sdk.kafka_client import KafkaProducer, KafkaConsumer, KafkaAdmin, NewTopic
+
+
+__all__ = ("Client", "KafkaProducer",
+           "KafkaConsumer", "KafkaAdmin", "NewTopic")
