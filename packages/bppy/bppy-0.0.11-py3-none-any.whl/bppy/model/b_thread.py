954e0a12dee15dea3de934684bd1d2279f6d1890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 16:03:33 +0000

    Add package bppy/0.0.11/bppy-0.0.11-py3-none-any.whl

---
+++ packages/bppy/bppy-0.0.11-py3-none-any.whl/bppy/model/b_thread.py
@@ -0,0 +1,40 @@
+from copy import copy
+from bppy.model.sync_statement import sync, choice
+from warnings import warn
+
+
+def thread(func):
+    """
+    A decorator to wrap bthread generator with, in order to handle data transmission and bthread termination.
+    """
+    def wrapper(*args):
+        while True:
+            m = None
+            f = func(*args)
+            while True:
+                try:
+                    e = f.send(m)
+                    if type(e) == dict:
+                        warn("using dict for statements is deprecated, use bppy.model.sync_statement.sync instead.")
+                        e = sync(**e)
+                    if isinstance(e, sync):
+                        local_vars = {var:val for var, val in copy(f.gi_frame.f_locals).items()}
+                        e["locals"] = copy(local_vars)
+                    elif isinstance(e, choice):
+                        pass
+                    else:
+                        raise TypeError("bthread must yield a bppy.model.sync_statement object")
+                    m = yield e
+                    if m is None:
+                        break
+                except (KeyError, StopIteration):
+                    m = yield None
+                    break
+    return wrapper
+
+
+def b_thread(func):
+    warn("the b_thread decorator is deprecated, use thread instead.")
+    return thread(func)
+
+
