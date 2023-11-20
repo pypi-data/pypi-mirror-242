9b7b7a100e29aa224005203d1b34208370830b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 03:05:41 +0000

    Add package pmidcite/0.0.47/pmidcite-0.0.47-py2.py3-none-any.whl

---
+++ packages/pmidcite/pmidcite-0.0.47-py2.py3-none-any.whl/pmidcite/icite/api.py
@@ -0,0 +1,198 @@
+"""Given PubMed IDs (PMIDs), download NIH citation data and write it to a Python module"""
+# https://icite.od.nih.gov/api
+
+__copyright__ = "Copyright (C) 2019-present, DV Klopfenstein, PhD. All rights reserved."
+__author__ = "DV Klopfenstein, PhD"
+
+## from timeit import default_timer
+from collections import OrderedDict
+
+import traceback
+import requests
+
+from pmidcite.icite.utils import split_list
+## from tests.prt_hms import prt_hms
+
+
+class NIHiCiteAPI:
+    """Given a PubMed ID (PMID), return a list of publications which cite it from NIH's iCite"""
+
+    opt_keys = {
+        # Number of publications to return. The maximum allowed is 1000.
+        'limit'
+        # Only return publications with a PMID greater than this
+        # Example: /api/pubs?offset=23456789&limit=10&format=csv
+        'offset'
+        # Only return publications from the given year.
+        'year'
+        # Only return publications with the given PubMed IDs.
+        # Separate multiple IDs with commas to request up to 1000 at a time.
+        # If this parameter is provided, all other parameters are ignored.
+        'pmids'
+        # only return publications with the given fields.
+        # Separate multiple fields with commas (no space).
+        # Field names are very specific and listed in Response example below.
+        # No fl param will return all fields.
+        # Example: /api/pubs?pmids=28968381,28324054,23843509&fl=pmid,year,title,apt
+        'fl'
+        # return csv (comma separated value) by specifying format=csv rather than the default JSON.
+        'format'}
+
+    #           https://icite.od.nih.gov/api
+    url_base = 'https://icite.od.nih.gov/api/pubs'
+
+    flds_yes_no = {'is_research_article', 'is_clinical', 'provisional'}
+    yes_no = {'Yes':True, 'No':False}
+
+    def __init__(self, **kws):
+        self.kws = {k:v for k, v in kws.items() if k in self.opt_keys}
+        self.msgs = []
+
+    def dnld_nihdict(self, pmid):
+        """Download NIH citation data for one researcher-spedified PMID. Return a corrected json"""
+        rsp_json = self._send_request('{URL}/{PMID}'.format(URL=self.url_base, PMID=pmid))
+        return self._adjust_jsondct(rsp_json) if rsp_json else None
+
+    def dnld_nihdicts(self, pmids):
+        """Download a list of NIH citation data for given PMIDs"""
+        return self._dnld_gtmax(pmids) if len(pmids) > 1000 else self._dnld_ltmax(pmids)
+
+    def _dnld_gtmax(self, pmids):
+        """Run iCite on given PubMed IDs"""
+        nih_dicts_all = []
+        max_limit = 1000
+        pmid_list_all = pmids if isinstance(pmids, list) else list(pmids)
+        num_total = len(pmids)
+        # The NIH-OCC allows for a maximum of 1,000 PMIDs to be downloaded at once
+        for pmid_list_cur in split_list(pmid_list_all, max_limit):
+            nih_dicts_cur = self._dnld_ltmax(pmid_list_cur)
+            if nih_dicts_cur:
+                nih_dicts_all.extend(nih_dicts_cur)
+            # pylint: disable=line-too-long
+            print('NIH citation data downloaded: {N:,} of {P:,}'.format(N=len(nih_dicts_all), P=num_total))
+        return nih_dicts_all
+
+    def _dnld_ltmax(self, pmids):
+        """Download NIH citation data using a request using their API"""
+        ## tic = default_timer()
+        req_nihocc = '{URL}?pmids={PMIDS}'.format(
+            URL=self.url_base,
+            PMIDS=','.join(str(p) for p in pmids))
+        ## tic = prt_hms(tic, "Create request")
+        # Note: rsp_json['data'] returned from NIH not in same order as requested
+        rsp_json = self._send_request(req_nihocc)
+        ## tic = prt_hms(tic, "Send request. Get response")
+        if rsp_json is not None:
+            # Adjust the jsons downloaded for NIH citation data
+            nih_dicts = []
+            s_adjust_jsondct = self._adjust_jsondct
+            pmids_downloaded = set()
+            for nih_json_dct in rsp_json['data']:
+                pmids_downloaded.add(nih_json_dct['pmid'])
+                nih_dicts.append(s_adjust_jsondct(nih_json_dct))
+            ## tic = prt_hms(tic, "Adjust reponse")
+            # Report PMIDs that did not have NIH citation data downloaded
+            pmids_missing = set(pmids).difference(pmids_downloaded)
+            if pmids_missing:
+                self._warn_missing(pmids_missing)
+            return nih_dicts
+        self._warn_missing(pmids)
+        return None
+
+    @staticmethod
+    def _warn_missing(pmids):
+        """Warn that NIH citation data was not downloaded for pmids"""
+        print("**WARNING: {N:,} NIH CITATION DATA NOT DOWNLOADED FOR PMIDs: {PMIDs}".format(
+            N=len(pmids), PMIDs=' '.join(str(s) for s in sorted(pmids))))
+
+    def _send_request(self, cmd):
+        """Send the request to iCite"""
+        try:
+            rsp = requests.get(cmd)
+            if rsp.status_code == 200:
+                return rsp.json()
+            self._prt_errmsg(self._err_msg(rsp))
+            return None
+        except requests.exceptions.ConnectionError as errobj:
+            self._prt_errmsg('**ERROR: ConnectionError = {ERR}\n'.format(ERR=str(errobj)))
+            return None
+        except:
+            traceback.print_exc()
+            raise RuntimeError('**ERROR DOWNLOADING {CMD}'.format(CMD=cmd))
+
+    def _prt_errmsg(self, errmsg):
+        """Print the error and add the error to the list of API messages"""
+        print(errmsg)
+        self.msgs.append(errmsg)
+
+    @staticmethod
+    def _err_msg(rsp):
+        """Get error message if an NIH iCite request failed"""
+        ## print('1 RRRRRRRRRRRRRRRRRRRRRR', dir(rsp))
+        ## print('2 RRRRRRRRRRRRRRRRRRRRRR', rsp.status_code)
+        ## print('3 RRRRRRRRRRRRRRRRRRRRRR', rsp.reason)
+        ## print('4 RRRRRRRRRRRRRRRRRRRRRR', rsp.content)
+        ## print('5 RRRRRRRRRRRRRRRRRRRRRR', rsp.text)
+        ## #print('3 RRRRRRRRRRRRRRRRRRRRRR', rsp.json())
+        ## print('6 RRRRRRRRRRRRRRRRRRRRRR', rsp.url)
+        ## if rsp.json() is not None:
+        ##     txt =' '.join('{K}({V})'.format(K=k, V=v) for k, v in sorted(rsp.json().items()))
+        return '{CODE} {REASON} URL[{N}]: {URL}'.format(
+            CODE=rsp.status_code,
+            REASON=rsp.reason,
+            N=len(rsp.url),
+            URL=rsp.url)
+            #TEXT=rsp.text)
+
+    def _adjust_jsondct(self, json_dct):
+        """Adjust values in the json dict"""
+        dct = {}
+        if json_dct['title'] is not None:
+            title = json_dct['title'].strip()
+            if '"' in title:
+                title = title.replace('"', "'")
+            if "\n" in title:
+                title = title.replace('\n', " ")
+            dct['title'] = title
+        if json_dct['authors'] is not None:
+            authors = json_dct['authors']
+            if authors == '':
+                dct['authors'] = []
+            else:
+                dct['authors'] = json_dct['authors'].split(', ')
+        yes_no = self.yes_no
+        dct['is_research_article'] = yes_no[json_dct['is_research_article']]
+        dct['is_clinical'] = yes_no[json_dct['is_clinical']]
+        dct['provisional'] = yes_no[json_dct['provisional']]
+        lst = []
+        lists = {'authors', 'cited_by_clin', 'cited_by', 'references'}
+        for key, val in json_dct.items():
+            if key in dct:
+                lst.append((key, dct[key]))
+            elif key in lists:
+                lst.append((key, [] if val is None else val))
+            else:
+                lst.append((key, val))
+        return OrderedDict(lst)
+
+    @staticmethod
+    def prt_dct(dct, prt):
+        """Print NIH iCite data as a dict"""
+        prt.write('"""Write data downloaded for NIH iCite data"""\n\n')
+        prt.write('# pylint: disable=line-too-long\n')
+        prt.write('ICITE = {\n')
+        str_val = {'title', 'journal', 'doi', 'last_modified'}
+        for key, val in dct.items():
+            if key == 'authors':
+                prt.write("    '{K}': {V},\n".format(K=key, V=val))
+                #prt.write("    '{K}': {AUTHORS},\n".format(
+                #    K=key,
+                #    AUTHORS='\n'.join(['"{AU}"'.format(AU=a) for a in val])))
+            elif key not in str_val:
+                prt.write("    '{K}': {V},\n".format(K=key, V=val))
+            else:
+                prt.write('''    '{K}': """{V}""",\n'''.format(K=key, V=val))
+        prt.write('}\n')
+
+
+# Copyright (C) 2019-present DV Klopfenstein, PhD. All rights reserved.
---
+++ packages/pmidcite/pmidcite-0.0.47-py2.py3-none-any.whl/pmidcite/icite/paper.py
@@ -0,0 +1,142 @@
+"""Holds NIH iCite data for one PubMed ID (PMID)"""
+
+__copyright__ = "Copyright (C) 2019-present, DV Klopfenstein, PhD. All rights reserved."
+__author__ = "DV Klopfenstein, PhD"
+
+from sys import stdout
+
+
+def sortby_year(obj):
+    """Sort lists of iCite items"""
+    return [-1*obj.dct['year'], -1*obj.dct['nih_percentile']]
+
+def sortby_cite(obj):
+    """Sort lists of iCite items"""
+    return [-1*obj.dct['citation_count'], -1*obj.dct['year']]
+
+def sortby_nih_group(obj):
+    """Sort lists of iCite items"""
+    dct = obj.dct
+    return [-1*dct['nih_group'], -1*dct['year'], -1*dct['nih_perc'],
+            -1*dct['citation_count'] + -1*dct['num_clin'],
+            -1*dct['num_refs'],
+            -1*dct['pmid']]
+
+
+# pylint: disable=too-many-instance-attributes
+class NIHiCitePaper:
+    """Holds NIH iCite data for one PubMed ID (PMID)"""
+
+    sortby_dct = {
+        'nih_group': sortby_nih_group,
+        'cite': sortby_cite,
+        'year': sortby_year,
+    }
+
+    def __init__(self, pmid, pmid2icite, header=None, pmid2note=None):
+        self.pmid = pmid
+        self.pmid2icite = pmid2icite
+        ## print('PPPPPPPP NIHiCitePaper({}, len(pmid2icite)={})'.format(pmid, len(pmid2icite)))
+        self.hdr = header  # A header to print before a paper
+        # A short pmid2note to print at end of cite line
+        self.pmid2note = {} if pmid2note is None else pmid2note
+        self.icite = pmid2icite.get(pmid)
+        # Sets of NIHiCiteEntrys
+        self.cited_by = self._init_pmids('cited_by')
+        self.cited_by_clin = self._init_pmids('cited_by_clin')
+        self.references = self._init_pmids('references')
+
+    def get_icite(self):
+        """Get NIHiCiteEntry for this paper"""
+        return self.pmid2icite[self.pmid]
+
+    def str_line(self):
+        """Return a string summarizing the the paper described herein"""
+        txt = str(self.icite)
+        if not self.pmid2note:
+            return txt
+        if self.pmid in self.pmid2note:
+            return '{TXT} {NOTE}'.format(TXT=txt, NOTE=self.pmid2note[self.pmid])
+        return txt
+
+    def prt_top(self, prt=stdout):
+        """Print TOP paper"""
+        prt.write('TOP {iCite}\n'.format(iCite=self.str_line()))
+
+    @staticmethod
+    def prt_keys(prt=stdout):
+        """Print paper keys"""
+        prt.write('    TOP: The paper requested by the researcher\n')
+        prt.write('    CIT: A paper that cited TOP\n')
+        prt.write('    CLI: A clinical paper that cited TOP\n')
+        prt.write("    REF: A paper referenced in the TOP paper's bibliography\n")
+
+    def prt_summary(self, prt=stdout, sortby_cites='nih_group', sortby_refs='nih_group'):
+        """Print summary of paper"""
+        if self.hdr:
+            prt.write('NAME: {NAME}\n'.format(NAME=self.hdr))
+        prt.write('TOP {iCite}\n'.format(iCite=self.str_line()))
+        # Citations by clinical papers
+        if self.cited_by_clin:
+            prt.write('Cited by {N} Clinical papers:\n'.format(N=len(self.cited_by_clin)))
+        self._prt_list(self.cited_by_clin, 'CLI', prt, sortby_cites)
+        # Citations
+        if self.cited_by:
+            prt.write('{N} of {M} citations downloaded:\n'.format(
+                N=len(self.cited_by),
+                M=self.icite.dct['citation_count']))
+        self._prt_list(self.cited_by, 'CIT', prt, sortby_cites)
+        # References
+        if self.references:
+            prt.write('{N} of {M} References downloaded:\n'.format(
+                N=len(self.references),
+                M=self.icite.dct['num_refs']))
+            self._prt_list(self.references, 'REF', prt, sortby_refs)
+
+    def get_sorted(self, icites, sortby=None):
+        """Get citations or references, sorted"""
+        if sortby is None:
+            return icites
+        if sortby in self.sortby_dct:
+            sortby = self.sortby_dct[sortby]
+        return sorted(icites, key=sortby)
+
+    def _prt_list(self, icites, desc, prt, sortby=None):
+        """Print list of NIH iCites in summary format"""
+        if sortby is not None:
+            icites = self.get_sorted(icites, sortby)
+        if self.pmid2note:
+            s_pmid2note = self.pmid2note
+            for icite in icites:
+                if icite.pmid in s_pmid2note:
+                    prt.write('{DESC} {iCite} {note}\n'.format(
+                        DESC=desc, iCite=str(icite), note=s_pmid2note[icite.pmid]))
+                else:
+                    prt.write('{DESC} {iCite}\n'.format(DESC=desc, iCite=str(icite)))
+            return
+        for icite in icites:
+            prt.write('{DESC} {iCite}\n'.format(DESC=desc, iCite=str(icite)))
+
+    def _init_pmids(self, name):
+        """Load citation/reference PMIDs, if the 'top' paper has NIH iCite data"""
+        if self.icite is None:
+            return []
+        s_pmid2icite = self.pmid2icite
+        return set(s_pmid2icite[pmid] for pmid in self.icite.dct[name] if pmid in s_pmid2icite)
+
+    def __str__(self):
+        """Get the line containing data downloaded from NIH iCite for only the featured paper"""
+        return self.str_line()
+
+    ## TBD:
+    ## def __eq__(self, rhs):
+    ##     if self.pmid != rhs.pmid:
+    ##         return False
+    ##     if self.cited_by != rhs.cited_by:
+    ##         print('LHS:', self.cited_by)
+    ##         print('RHS:', rhs.cited_by)
+    ##         return False
+    ##     return True
+
+
+# Copyright (C) 2019-present DV Klopfenstein, PhD. All rights reserved.
