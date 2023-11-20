a201fa3b758deec33c909b3b3e79901b1c14d333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 16:03:43 +0000

    Add package cheltuieli/0.1.12/cheltuieli-0.1.12.tar.gz

---
+++ packages/cheltuieli/cheltuieli-0.1.12.tar.gz/cheltuieli-0.1.12/src/cheltuieli/control.py
@@ -0,0 +1,33 @@
+import chelt_plan
+import masina
+from mysqlquerys import connect
+from datetime import date
+import time
+
+
+def main():
+    script_start_time = time.time()
+    selectedStartDate = date(2023, 11, 20)
+    selectedEndDate = date(2023, 11, 30)
+
+    ini_file = r"D:\Python\MySQL\mysqlquerys\src\mysqlquerys\local.ini"
+    ini_file = r"D:\Python\MySQL\mysqlquerys\src\mysqlquerys\heroku.ini"
+
+    app = chelt_plan.CheltuieliPlanificate(ini_file)
+    app.get_all_sql_vals()
+    print('FINISH')
+    # app.prepareTablePlan('EC', selectedStartDate, selectedEndDate)
+    # print(app.expenses)
+
+    # app = masina.Masina(ini_file)
+    # # app.prepareTablePlan('EC', selectedStartDate, selectedEndDate)
+    # print(app.default_interval)
+    # print(app.db_pass)
+
+    scrip_end_time = time.time()
+    duration = scrip_end_time - script_start_time
+    duration = time.strftime("%H:%M:%S", time.gmtime(duration))
+    print('run time: {}'.format(duration))
+
+if __name__ == '__main__':
+    main()
