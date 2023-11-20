488f1813daf4b317b2a61918e6a06f4ed5cba285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 03:05:57 +0000

    Add package moeysaklaksd/0.1/moeysaklaksd-0.1.tar.gz

---
+++ packages/moeysaklaksd/moeysaklaksd-0.1.tar.gz/moeysaklaksd-0.1/moeysaklaksd/__init__.py
@@ -0,0 +1,1 @@
+from .worldtime import get_current_utc_time
---
+++ packages/moeysaklaksd/moeysaklaksd-0.1.tar.gz/moeysaklaksd-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='moeysaklaksd',
+    version='0.1',
+    description='fethcer',
+    author='Lexy',
+    packages=['moeysaklaksd'],
+    install_requires=[
+        'requests',
+    ],
+)
