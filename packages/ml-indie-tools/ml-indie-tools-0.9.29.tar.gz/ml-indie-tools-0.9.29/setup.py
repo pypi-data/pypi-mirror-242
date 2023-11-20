5334d186370b44c2e9c3b7e4cd216c26a64d6c31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 03:04:11 +0000

    Add package ml-indie-tools/0.9.29/ml-indie-tools-0.9.29.tar.gz

---
+++ packages/ml-indie-tools/ml-indie-tools-0.9.29.tar.gz/ml-indie-tools-0.9.29/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ml-indie-tools",
+    version="0.9.29",
+    author="Dominik Schlösser",
+    author_email="dominik.schloesser@gmail.com",
+    description="A collection of tools for low-resource indie machine learning development",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/domschl/ml-indie-tools",
+    project_urls={"Bug Tracker": "https://github.com/domschl/ml-indie-tools/issues"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+)
