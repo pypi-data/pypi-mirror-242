b93f2f454605c5b8b45ffdda55039f9350bb682f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 15:04:37 +0000

    Add package zetascale/0.8.6/zetascale-0.8.6-py3-none-any.whl

---
+++ packages/zetascale/zetascale-0.8.6-py3-none-any.whl/zeta/nn/__init__.py
@@ -0,0 +1,18 @@
+# Attention
+# from zeta.nn.attention import *
+from zeta.nn import attention
+
+# architecture
+import zeta.structs as architecture
+
+# embeddings
+# from zeta.nn.embeddings import *
+from zeta.nn import embeddings
+
+# modules
+# from zeta.nn.modules import *
+from zeta.nn import modules
+
+# biases
+# from zeta.nn.biases import *
+from zeta.nn import biases
