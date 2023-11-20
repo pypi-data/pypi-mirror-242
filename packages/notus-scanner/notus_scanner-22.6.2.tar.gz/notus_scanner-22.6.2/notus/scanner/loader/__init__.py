f32f3858fc1bf1a4ed89ee7d38f85696620ef939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 15:03:14 +0000

    Add package notus-scanner/22.6.2/notus_scanner-22.6.2.tar.gz

---
+++ packages/notus-scanner/notus_scanner-22.6.2.tar.gz/notus_scanner-22.6.2/notus/scanner/loader/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2021-2023 Greenbone AG
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from .loader import AdvisoriesLoader
+from .json import JSONAdvisoriesLoader
