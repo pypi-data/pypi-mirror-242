76a4fe1049ef045a40ec1a7d8d2d233bd132b6e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 03:04:32 +0000

    Add package pt-pump-up/1.0.12/pt_pump_up-1.0.12.tar.gz

---
+++ packages/pt-pump-up/pt_pump_up-1.0.12.tar.gz/pt_pump_up-1.0.12/pt_pump_up/orm/DatasetStats.py
@@ -0,0 +1,11 @@
+from beanie import Document
+from typing import Optional
+from pt_pump_up.orm.Language import Language
+from beanie import Link
+
+
+class DatasetStats(Document):
+    language: Link[Language]
+    number_documents: Optional[int] = -1
+    number_tokens: Optional[int] = -1
+    number_chars: Optional[int] = -1
