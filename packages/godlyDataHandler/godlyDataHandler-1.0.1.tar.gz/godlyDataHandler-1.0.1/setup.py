6cec48a1cf9b95b4cee891c100e148fd0b7d0173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 04:03:24 +0000

    Add package godlyDataHandler/1.0.1/godlyDataHandler-1.0.1.tar.gz

---
+++ packages/godlyDataHandler/godlyDataHandler-1.0.1.tar.gz/godlyDataHandler-1.0.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name='godlyDataHandler',
+    version='1.0.1',
+    py_modules=['godlyDataHandler'],
+    install_requires=[
+        # Empty for no external dependencies
+    ],
+    entry_points='''
+        [console_scripts]
+        your_script_name=your_script_name:main
+    ''',
+)
+
+
+#python3 setup.py sdist bdist_wheel
+#twine upload dist/*
+#twine pass: ?v)g_8%+XUtS&#*
+"""
+PyPI recovery codes
+5e241cd83ad2c140
+09421de4d65731b1
+f18a3919c695b867
+581a5fc0c04bc00c
+a7f165f23acc9e50
+062d45cf319b3e05
+1e380d0da13c1b82
+1a408008d36cb6bf
+"""
+"""
+username: __token__
+api token:
+pypi-AgEIcHlwaS5vcmcCJGE3ZDNmMTUzLWExNWEtNDM4ZS1iOGQ2LThlODhkMDNhODg2MQACKlszLCJiNDc0OTZlNC1iNGM0LTQ2YWQtYmFmZS02ZDE2ODNhMzAxZGIiXQAABiAn8cEBER6e5-icHFJ7j7EHn30A77fCoNyDW-wIMMgIrw
+"""
