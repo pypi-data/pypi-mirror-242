c0bda0687c659dbbebf12f6b637da28ae423ed8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 04:03:52 +0000

    Add package minishell-visualizer/0.0.2/minishell_visualizer-0.0.2.tar.gz

---
+++ packages/minishell-visualizer/minishell_visualizer-0.0.2.tar.gz/minishell_visualizer-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='minishell_visualizer',
+    version='0.0.2',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'minishell_visualizer = minishell_visualizer.__main__:main'
+        ]
+    },
+    install_requires=[
+        # Add any dependencies your package needs
+    ],
+    # Other details like author, description, etc.
+    author='Axel_chab',
+    description='Simple minishell visualizer',
+    # More details can be added
+)
