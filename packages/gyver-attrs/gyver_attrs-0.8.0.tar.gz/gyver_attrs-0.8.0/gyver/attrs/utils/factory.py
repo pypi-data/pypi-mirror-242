3d711a06fac4cf507f731a8e7e8387d126fdd15d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 15:04:36 +0000

    Add package gyver-attrs/0.8.0/gyver_attrs-0.8.0.tar.gz

---
+++ packages/gyver-attrs/gyver_attrs-0.8.0.tar.gz/gyver_attrs-0.8.0/gyver/attrs/utils/factory.py
@@ -0,0 +1,28 @@
+import typing
+
+from typing_extensions import ParamSpec
+
+P = ParamSpec("P")
+T = typing.TypeVar("T")
+
+
+class Factory(typing.Generic[P, T]):
+    __slots__ = ("func",)
+
+    def __init__(self, func: typing.Callable[P, T]) -> None:
+        self.func = func
+
+    def __call__(self, *args: P.args, **kwds: P.kwargs) -> T:
+        return self.func(*args, **kwds)
+
+    @property
+    def __name__(self):
+        return self.func.__name__
+
+
+def mark_factory(func: typing.Callable[P, T]) -> Factory[P, T]:
+    return Factory(func)
+
+
+def is_factory_marked(obj: typing.Any) -> bool:
+    return isinstance(obj, Factory)
