56833679b3c7940cef1840f040c7f6a6f8d18f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 03:05:32 +0000

    Add package openllm-core/0.4.16/openllm_core-0.4.16.tar.gz

---
+++ packages/openllm-core/openllm_core-0.4.16.tar.gz/openllm_core-0.4.16/src/openllm_core/config/configuration_mistral.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+import typing as t
+
+import openllm_core
+from openllm_core.prompts import PromptTemplate
+
+# https://docs.mistral.ai/usage/guardrailing/
+DEFAULT_SYSTEM_MESSAGE = """Always assist with care, respect, and truth. Respond with utmost utility yet securely. Avoid harmful, unethical, prejudiced, or negative content. Ensure replies promote fairness and positivity."""
+SINST_KEY, EINST_KEY, BOS_TOKEN, EOS_TOKEN = '[INST]', '[/INST]', '<s>', '</s>'
+DEFAULT_PROMPT_TEMPLATE = """{start_key}{start_inst} {system_message} {instruction} {end_inst}\n""".format(
+  start_inst=SINST_KEY,
+  end_inst=EINST_KEY,
+  start_key=BOS_TOKEN,
+  system_message='{system_message}',
+  instruction='{instruction}',
+)
+
+
+class MistralConfig(openllm_core.LLMConfig):
+  """Mistral-7B-v0.1 is a small, yet powerful model adaptable to many use-cases. It is released under Apache 2.0 licence, and it's easy to deploy on any cloud
+
+  It currently contains a foundation model and a instruct-finetuned model.
+
+  Refer to [Mistral's HuggingFace page](https://huggingface.co/docs/transformers/main/model_doc/mistral)
+  for more information.
+  """
+
+  __config__ = {
+    'name_type': 'lowercase',
+    'url': 'https://mistral.ai',
+    'architecture': 'MistralForCausalLM',
+    'add_generation_prompt': True,
+    'default_id': 'mistralai/Mistral-7B-Instruct-v0.1',
+    'serialisation': 'safetensors',
+    'backend': ('pt', 'vllm'),
+    # NOTE: see https://docs.mistral.ai/usage/guardrailing/
+    # and https://docs.mistral.ai/llm/mistral-instruct-v0.1
+    'model_ids': [
+      'HuggingFaceH4/zephyr-7b-alpha',
+      'HuggingFaceH4/zephyr-7b-beta',
+      'mistralai/Mistral-7B-Instruct-v0.1',
+      'mistralai/Mistral-7B-v0.1',
+    ],
+    'fine_tune_strategies': (
+      {'adapter_type': 'lora', 'r': 64, 'lora_alpha': 16, 'lora_dropout': 0.1, 'bias': 'none'},
+    ),
+  }
+
+  class GenerationConfig:
+    max_new_tokens: int = 256
+    temperature: float = 0.7
+    top_p: float = 0.95
+    top_k: int = 40
+
+  class SamplingParams:
+    best_of: int = 1
+    presence_penalty: float = 0.5
+
+  @property
+  def default_prompt_template(self) -> str:
+    return DEFAULT_PROMPT_TEMPLATE
+
+  @property
+  def default_system_message(self) -> str:
+    return DEFAULT_SYSTEM_MESSAGE
+
+  def sanitize_parameters(
+    self,
+    prompt: str,
+    prompt_template: PromptTemplate | str | None = None,
+    system_message: str | None = None,
+    top_k: int | None = None,
+    top_p: float | None = None,
+    temperature: float | None = None,
+    max_new_tokens: int | None = None,
+    **attrs: t.Any,
+  ) -> tuple[str, dict[str, t.Any], dict[str, t.Any]]:
+    system_message = DEFAULT_SYSTEM_MESSAGE if system_message is None else system_message
+    if prompt_template is None:
+      prompt_template = DEFAULT_PROMPT_TEMPLATE
+    elif isinstance(prompt_template, str):
+      prompt_template = PromptTemplate(template=prompt_template)
+
+    return (
+      prompt_template.with_options(system_message=system_message).format(instruction=prompt),
+      {'max_new_tokens': max_new_tokens, 'temperature': temperature, 'top_p': top_p, 'top_k': top_k},
+      {},
+    )
---
+++ packages/openllm-core/openllm_core-0.4.16.tar.gz/openllm_core-0.4.16/src/openllm_core/config/configuration_mpt.py
@@ -0,0 +1,112 @@
+from __future__ import annotations
+import typing as t
+
+import openllm_core
+from openllm_core.prompts import PromptTemplate, process_prompt
+from openllm_core.utils import dantic
+
+MPTPromptType = t.Literal['default', 'instruct', 'chat', 'storywriter']
+
+INSTRUCTION_KEY, RESPONSE_KEY, END_KEY = '### Instruction:', '### Response:', '### End'
+INTRO_BLURB = (
+  'Below is an instruction that describes a task. Write a response that appropriately completes the request.'
+)
+# NOTE: This is the prompt that is used for generating responses using an already
+# trained model.  It ends with the response key, where the job of the model is to provide
+# the completion that follows it (i.e. the response itself).
+_chat_prompt, _default_prompt, _instruct_prompt = (
+  """{instruction}""",
+  """{instruction}""",
+  """{intro}
+{instruction_key}
+{instruction}
+{response_key}
+""".format(intro=INTRO_BLURB, instruction_key=INSTRUCTION_KEY, instruction='{instruction}', response_key=RESPONSE_KEY),
+)
+PROMPT_MAPPING = {
+  'default': _default_prompt,
+  'instruct': _instruct_prompt,
+  'storywriter': _default_prompt,
+  'chat': _chat_prompt,
+}
+
+
+def _get_prompt(model_type: str) -> str:
+  return PROMPT_MAPPING[model_type]
+
+
+DEFAULT_PROMPT_TEMPLATE = _get_prompt
+
+
+class MPTConfig(openllm_core.LLMConfig):
+  """MPT is a decoder-style transformer pretrained from scratch on English text and code.
+
+  This model was trained by [MosaicML](https://www.mosaicml.com/).
+
+  ``openllm.MPT`` encapsulate a family of MPT variants that is publicly available
+  on HuggingFace. Refers [HuggingFace's MosaicML page](https://huggingface.co/mosaicml)
+  for more details on specific models.
+  """
+
+  __config__ = {
+    'name_type': 'lowercase',
+    'trust_remote_code': True,
+    'url': 'https://huggingface.co/mosaicml',
+    'timeout': int(36e6),
+    'requirements': ['triton'],
+    'architecture': 'MPTForCausalLM',
+    # NOTE: See https://huggingface.co/TheBloke/mpt-30B-chat-GGML/discussions/4
+    'default_id': 'mosaicml/mpt-7b-instruct',
+    'model_ids': [
+      'mosaicml/mpt-7b',
+      'mosaicml/mpt-7b-instruct',
+      'mosaicml/mpt-7b-chat',
+      'mosaicml/mpt-7b-storywriter',
+      'mosaicml/mpt-30b',
+      'mosaicml/mpt-30b-instruct',
+      'mosaicml/mpt-30b-chat',
+    ],
+  }
+  prompt_type: MPTPromptType = dantic.Field(
+    '"default"',
+    description='Given prompt type for running MPT. Default will be inferred from model name if pretrained.',
+  )
+  max_sequence_length: int = dantic.Field(
+    2048,
+    description='Max sequence length to run MPT with. Note that MPT is trained ith sequence length of 2048, but with [ALiBi](https://arxiv.org/abs/2108.12409) it can set up to 4096 (for 7b models) and 16384 (for 30b models)',
+  )
+
+  class GenerationConfig:
+    max_new_tokens: int = 128
+    temperature: float = 0
+    top_p: float = 0.8
+
+  def sanitize_parameters(
+    self,
+    prompt: str,
+    prompt_template: PromptTemplate | str | None = None,
+    system_message: str | None = None,
+    max_new_tokens: int | None = None,
+    temperature: float | None = None,
+    top_p: float | None = None,
+    prompt_type: MPTPromptType | None = None,
+    use_default_prompt_template: bool = True,
+    **attrs: t.Any,
+  ) -> tuple[str, dict[str, t.Any], dict[str, t.Any]]:
+    _template = None
+    if use_default_prompt_template:
+      if prompt_type is None:
+        if 'instruct' in self.model_id:
+          prompt_type = 'instruct'
+        elif 'storywriter' in self.model_id:
+          prompt_type = 'storywriter'
+        elif 'chat' in self.model_id:
+          prompt_type = 'chat'
+        else:
+          prompt_type = 'default'
+      _template = DEFAULT_PROMPT_TEMPLATE(prompt_type)
+    return (
+      process_prompt(prompt, _template, use_default_prompt_template),
+      {'max_new_tokens': max_new_tokens, 'temperature': temperature, 'top_p': top_p},
+      {},
+    )
