55500a844b73bb63d2c94912d183cd1028cbddeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 05:03:26 +0000

    Add package okmesopy/1.1.4/okmesopy-1.1.4.tar.gz

---
+++ packages/okmesopy/okmesopy-1.1.4.tar.gz/okmesopy-1.1.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='okmesopy',
+      version='1.1.4',
+      description='Tools to download and manipulate data from OK Mesonet',
+      url='https://github.com/bjrubinstein/okmesopy',
+      author='Ben Rubinstein',
+      author_email='brubinst@hawk.iit.edu',
+      license='GNU',
+      packages=['okmesopy'],
+      install_requires=['numpy',
+                        'pandas',
+                        'pyshp',
+                        'pyproj',
+                        'geopy',
+                        'missingno',
+                        ],
+      keywords=['Mesonet','climatology','meteorology','hydrology'],
+      zip_safe=False)
