1e8472d5e8c42a5d0aa82c590d0ab3723ec67449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 14:03:38 +0000

    Add package slushie/0.2.4/slushie-0.2.4.tar.gz

---
+++ packages/slushie/slushie-0.2.4.tar.gz/slushie-0.2.4/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='slushie',
+    version='0.2.4',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    
+    # Dependencies
+    install_requires=[
+        # Add your dependencies here (e.g., 'numpy>=1.18.5')
+    ],
+    
+    # Metadata
+    author='Salvador Aleguas',
+    author_email='salvadoraleguas@example.com',  # Add your email here
+    description='Slushie: A Python library for relative path manipulation.',
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    keywords='path sys-path manipulation python-path module-path',
+    url='https://github.com/saleguas/slushie',
+    
+    # Specifying python requires
+    python_requires='>=3',
+    
+    # Classifier (Optional)
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
