4e9f56588fa0ce5334bd4cee9ccf5854037bc856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 14:03:22 +0000

    Add package denmune/1.17.6/denmune-1.17.6.tar.gz

---
+++ packages/denmune/denmune-1.17.6.tar.gz/denmune-1.17.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+install_requires=[
+    'ngt==2.0.4',
+    'numpy==1.23.5',
+    'pandas==1.5.3',
+    'matplotlib==3.7.1',
+    'scikit-learn==1.2.2',
+    'seaborn==0.12.2',
+    'anytree==2.8.0',
+    'treelib==1.6.1',
+    ]
+
+)
