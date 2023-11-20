e9d8349f5f5a38ea926acba9075d708aa6fb1c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 15:04:20 +0000

    Add package cogn/1.0/cogn-1.0.tar.gz

---
+++ packages/cogn/cogn-1.0.tar.gz/cogn-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="cogn",
+      version="1.0",
+      description="package with cognitive codes",
+      author="Black_shark",
+      packages=['cogn'],
+      install_requires=[])
