f080d6ce2137679827aad291f969b8cde0bd8aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 14:03:35 +0000

    Add package ctidb/0.0.14/ctidb-0.0.14.tar.gz

---
+++ packages/ctidb/ctidb-0.0.14.tar.gz/ctidb-0.0.14/ctidb/__init__.py
@@ -0,0 +1,12 @@
+# pylint:disable=C0111
+from .reader import CCtiReader, MODE_AUTO, MODE_MEMORY
+
+__all__ = [
+    "CCtiReader",
+	"MODE_AUTO",
+	"MODE_MEMORY"
+]
+
+__version__ = "0.0.14"
+__author__ = "AISpera"
+__license__ = "Apache License, Version 2.0"
