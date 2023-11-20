9fe1e69d9ab1a4e29707962bf9f0365176a83f2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 14:03:57 +0000

    Add package delpinos-rabbitmq-api/0.0.1.dev3/delpinos_rabbitmq_api-0.0.1.dev3-py3-none-any.whl

---
+++ packages/delpinos-rabbitmq-api/delpinos_rabbitmq_api-0.0.1.dev3-py3-none-any.whl/delpinos/rabbitmq/api/config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# pylint: disable=C0114,C0116
+
+from typing import Any, Dict
+from delpinos.core.functions.dict_function import dicts_merge
+from delpinos.api.core.config import config as core_config
+from api.configs.rabbitmq_connection import config as rabbitmq_connection
+
+
+config: Dict[str, Any] = dicts_merge(
+    core_config,
+    rabbitmq_connection,
+)
