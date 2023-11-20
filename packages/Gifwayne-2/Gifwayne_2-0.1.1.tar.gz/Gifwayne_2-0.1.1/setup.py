3394922eead13f6e0f586d11d07afe3fde7322fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 04:03:41 +0000

    Add package Gifwayne-2/0.1.1/Gifwayne_2-0.1.1.tar.gz

---
+++ packages/Gifwayne-2/Gifwayne_2-0.1.1.tar.gz/Gifwayne_2-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Gifwayne_2',
+    version = '0.1.1',
+    author = 'Authentique',
+    author_email = 'berlinpierre007@gmail.com',
+    packages= find_packages(),
+)
