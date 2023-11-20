4fc66424803523e7ac1b8fc50d04865449990c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 14:03:58 +0000

    Add package django-sync-env/0.2.2/django_sync_env-0.2.2.tar.gz

---
+++ packages/django-sync-env/django_sync_env-0.2.2.tar.gz/django_sync_env-0.2.2/src/django_sync_env/notifications.py
@@ -0,0 +1,29 @@
+import logging
+from slack import WebClient
+from slack.errors import SlackApiError
+from django_sync_env import settings
+
+
+
+class SyncEnvNotifications:
+    enabled = False
+    client = None
+    
+    def __init__(self):
+        if settings.SYNC_ENV_NOTIFICATION_CONFIG:        
+            self.enabled = True
+            slack_token = settings.SYNC_ENV_NOTIFICATION_CONFIG.get("SLACK_API_TOKEN", None)
+            self.client = WebClient(token=slack_token)
+
+    def send_slack_message(self, text):        
+        try:
+            response = self.client.chat_postMessage(
+                channel=settings.SYNC_ENV_NOTIFICATION_CONFIG.get("SLACK_CHANNEL_ID"),
+                text=text
+            )
+            logging.info(response)
+            return response
+        except SlackApiError as e:
+            # You will get a SlackApiError if "ok" is False
+            logging.error(e.response["error"])  # str like 'invalid_auth', 'channel_not_found'
+            return None        
