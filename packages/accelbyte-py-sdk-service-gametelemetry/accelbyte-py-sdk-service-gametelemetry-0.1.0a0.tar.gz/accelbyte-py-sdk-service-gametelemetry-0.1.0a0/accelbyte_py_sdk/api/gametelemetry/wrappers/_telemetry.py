3121895de01a94029af32e35c8d85d43f1f74ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 15:04:17 +0000

    Add package accelbyte-py-sdk-service-gametelemetry/0.1.0a0/accelbyte-py-sdk-service-gametelemetry-0.1.0a0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-gametelemetry/accelbyte-py-sdk-service-gametelemetry-0.1.0a0.tar.gz/accelbyte-py-sdk-service-gametelemetry-0.1.0a0/accelbyte_py_sdk/api/gametelemetry/wrappers/_telemetry.py
@@ -0,0 +1,172 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: wrapper.j2
+
+# pylint: disable=duplicate-code
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-public-methods
+# pylint: disable=too-many-return-statements
+# pylint: disable=too-many-statements
+# pylint: disable=unused-import
+
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from accelbyte_py_sdk.core import HeaderStr
+from accelbyte_py_sdk.core import get_namespace as get_services_namespace
+from accelbyte_py_sdk.core import run_request
+from accelbyte_py_sdk.core import run_request_async
+from accelbyte_py_sdk.core import same_doc_as
+
+from ..models import HTTPValidationError
+
+from ..operations.telemetry import (
+    GetEventsGameTelemetryV1AdminNamespacesNamespaceEventsGet,
+)
+
+
+@same_doc_as(GetEventsGameTelemetryV1AdminNamespacesNamespaceEventsGet)
+def get_events_game_telemetry_v1_admin_namespaces_namespace_events_get(
+    end_time: Optional[str] = None,
+    event_id: Optional[str] = None,
+    event_name: Optional[str] = None,
+    event_payload: Optional[str] = None,
+    limit: Optional[int] = None,
+    offset: Optional[int] = None,
+    start_time: Optional[str] = None,
+    namespace: Optional[str] = None,
+    x_additional_headers: Optional[Dict[str, str]] = None,
+    **kwargs
+):
+    """Get Events (get_events_game_telemetry_v1_admin_namespaces__namespace__events_get)
+
+
+    Properties:
+        url: /game-telemetry/v1/admin/namespaces/{namespace}/events
+
+        method: GET
+
+        tags: ["telemetry"]
+
+        consumes: []
+
+        produces: ["application/json"]
+
+        securities: [COOKIE_AUTH] or [BEARER_AUTH]
+
+        namespace: (namespace) REQUIRED str in path
+
+        end_time: (endTime) OPTIONAL str in query
+
+        event_id: (eventId) OPTIONAL str in query
+
+        event_name: (eventName) OPTIONAL str in query
+
+        event_payload: (eventPayload) OPTIONAL str in query
+
+        limit: (limit) OPTIONAL int in query
+
+        offset: (offset) OPTIONAL int in query
+
+        start_time: (startTime) OPTIONAL str in query
+
+    Responses:
+        200: OK - (Successful Response)
+
+        422: Unprocessable Entity - HTTPValidationError (Validation Error)
+    """
+    if namespace is None:
+        namespace, error = get_services_namespace()
+        if error:
+            return None, error
+    request = GetEventsGameTelemetryV1AdminNamespacesNamespaceEventsGet.create(
+        end_time=end_time,
+        event_id=event_id,
+        event_name=event_name,
+        event_payload=event_payload,
+        limit=limit,
+        offset=offset,
+        start_time=start_time,
+        namespace=namespace,
+    )
+    return run_request(request, additional_headers=x_additional_headers, **kwargs)
+
+
+@same_doc_as(GetEventsGameTelemetryV1AdminNamespacesNamespaceEventsGet)
+async def get_events_game_telemetry_v1_admin_namespaces_namespace_events_get_async(
+    end_time: Optional[str] = None,
+    event_id: Optional[str] = None,
+    event_name: Optional[str] = None,
+    event_payload: Optional[str] = None,
+    limit: Optional[int] = None,
+    offset: Optional[int] = None,
+    start_time: Optional[str] = None,
+    namespace: Optional[str] = None,
+    x_additional_headers: Optional[Dict[str, str]] = None,
+    **kwargs
+):
+    """Get Events (get_events_game_telemetry_v1_admin_namespaces__namespace__events_get)
+
+
+    Properties:
+        url: /game-telemetry/v1/admin/namespaces/{namespace}/events
+
+        method: GET
+
+        tags: ["telemetry"]
+
+        consumes: []
+
+        produces: ["application/json"]
+
+        securities: [COOKIE_AUTH] or [BEARER_AUTH]
+
+        namespace: (namespace) REQUIRED str in path
+
+        end_time: (endTime) OPTIONAL str in query
+
+        event_id: (eventId) OPTIONAL str in query
+
+        event_name: (eventName) OPTIONAL str in query
+
+        event_payload: (eventPayload) OPTIONAL str in query
+
+        limit: (limit) OPTIONAL int in query
+
+        offset: (offset) OPTIONAL int in query
+
+        start_time: (startTime) OPTIONAL str in query
+
+    Responses:
+        200: OK - (Successful Response)
+
+        422: Unprocessable Entity - HTTPValidationError (Validation Error)
+    """
+    if namespace is None:
+        namespace, error = get_services_namespace()
+        if error:
+            return None, error
+    request = GetEventsGameTelemetryV1AdminNamespacesNamespaceEventsGet.create(
+        end_time=end_time,
+        event_id=event_id,
+        event_name=event_name,
+        event_payload=event_payload,
+        limit=limit,
+        offset=offset,
+        start_time=start_time,
+        namespace=namespace,
+    )
+    return await run_request_async(
+        request, additional_headers=x_additional_headers, **kwargs
+    )
