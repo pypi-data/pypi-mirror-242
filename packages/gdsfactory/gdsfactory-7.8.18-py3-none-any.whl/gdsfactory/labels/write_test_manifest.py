0ae25c7ae3651aa9b13c275d05f3de65cacb1dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 15:04:19 +0000

    Add package gdsfactory/7.8.18/gdsfactory-7.8.18-py3-none-any.whl

---
+++ packages/gdsfactory/gdsfactory-7.8.18-py3-none-any.whl/gdsfactory/labels/write_test_manifest.py
@@ -0,0 +1,103 @@
+"""Converts CSV of test site labels into a CSV test manifest."""
+
+import json
+import pathlib
+from functools import partial
+
+import pandas as pd
+
+import gdsfactory as gf
+from gdsfactory.samples.sample_reticle import sample_reticle
+
+marker = partial(gf.components.rectangle, layer="TEXT", centered=True, size=(10, 10))
+
+
+def write_test_manifest(
+    csvpath: str | pathlib.Path,
+    gdspath: str | pathlib.Path | None = None,
+    marker_optical=marker,
+    marker_electrical=marker,
+) -> pd.DataFrame:
+    """Converts CSV of test site labels into a CSV test manifest.
+
+    Args:
+        csvpath: path to CSV file with test site labels.
+        gdspath: path to GDS file with test site labels.
+        marker_optical: marker to use for test site labels.
+        marker_electrical: marker to use for test site labels.
+    """
+    df_in = pd.read_csv(csvpath)
+
+    # Initialize an empty list to collect the rows
+    rows = []
+    columns = [
+        "cell",
+        "device",
+        "port_type",
+        "port_names",
+        "x",
+        "y",
+        "measurement",
+        "measurement_settings",
+        "doe",
+        "analysis",
+        "analysis_settings",
+        "cell_settings",
+    ]
+    if gdspath:
+        c = gf.Component()
+        _ = c << gf.import_gds(gdspath)
+        marker_optical = marker_optical()
+        marker_electrical = marker_electrical()
+
+    for _, label in df_in.iterrows():
+        x, y, _orientation = label.x, label.y, label.rotation
+        text = label.text
+        d = json.loads(text)
+
+        if gdspath:
+            if d["port_type"] == "optical":
+                ref = c << marker_optical
+                ref.x = x
+                ref.y = y
+            elif d["port_type"] == "electrical":
+                ref = c << marker_electrical
+                ref.x = x
+                ref.y = y
+
+        row = [
+            d["name"],
+            d["name"] + f"_{int(x)}_{int(y)}",
+            d["port_type"],
+            d["port_names"],
+            x,
+            y,
+            d["measurement"],
+            d["measurement_settings"],
+            d["doe"],
+            d["analysis"],
+            d["analysis_settings"],
+            d["cell_settings"],
+        ]
+        rows.append(row)
+
+    if gdspath:
+        c.show()
+
+    return pd.DataFrame(
+        rows,
+        columns=columns,
+    )
+
+
+if __name__ == "__main__":
+    c = sample_reticle(grid=False)
+    # c = c.mirror()
+    c.show(show_ports=False)
+    gdspath = c.write_gds("sample_reticle.gds")
+    csvpath = gf.labels.write_labels.write_labels_gdstk(
+        gdspath, prefixes=("{",), layer_label="TEXT"
+    )
+    df = write_test_manifest(csvpath, gdspath=gdspath)
+    df.to_csv("test_manifest.csv")
+    print(df)
---
+++ packages/gdsfactory/gdsfactory-7.8.18-py3-none-any.whl/gdsfactory/samples/sample_reticle.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+
+import pandas as pd
+
+import gdsfactory as gf
+
+
+def sample_reticle(grid: bool = True, **kwargs) -> gf.Component:
+    """Returns MZI with TE grating couplers.
+
+    Args:
+        grid: if True returns a grid of components.
+        kwargs: passed to pack or grid.
+
+    """
+    test_info_mzi_heaters = dict(
+        doe="mzis_heaters",
+        analysis="mzi_heater",
+        measurement="optical_loopback4_heater_sweep",
+    )
+    test_info_ring_heaters = dict(
+        doe="ring_heaters",
+        analysis="ring_heater",
+        measurement="optical_loopback2_heater_sweep",
+    )
+
+    mzis = [
+        gf.components.mzi2x2_2x2_phase_shifter(length_x=lengths)
+        for lengths in [100, 200, 300]
+    ]
+    rings = [
+        gf.components.ring_single_heater(length_x=length_x) for length_x in [10, 20, 30]
+    ]
+
+    spirals_sc = [
+        gf.components.spiral_inner_io_fiber_array(
+            length=length,
+            info=dict(
+                doe="spirals_sc",
+                measurement="optical_loopback4",
+                analysis="optical_loopback4_spirals",
+            ),
+            decorator=gf.labels.add_label_json,
+        )
+        for length in [20e3, 40e3, 60e3]
+    ]
+
+    mzis_te = [
+        gf.components.add_fiber_array_optical_south_electrical_north(
+            mzi,
+            electrical_port_names=["top_l_e2", "top_r_e2"],
+            decorator=gf.labels.add_label_json,
+            info=test_info_mzi_heaters,
+        )
+        for mzi in mzis
+    ]
+    rings_te = [
+        gf.components.add_fiber_array_optical_south_electrical_north(
+            ring,
+            electrical_port_names=["l_e2", "r_e2"],
+            decorator=gf.labels.add_label_json,
+            info=test_info_ring_heaters,
+        )
+        for ring in rings
+    ]
+
+    components = mzis_te + rings_te + spirals_sc
+
+    if grid:
+        return gf.grid(components, **kwargs)
+    c = gf.pack(components, **kwargs)
+    if len(c) > 1:
+        raise ValueError(f"failed to pack into single group. Made {len(c)} groups.")
+    return c[0]
+
+
+if __name__ == "__main__":
+    c = sample_reticle(grid=False)
+    gdspath = c.write_gds("mask.gds", with_metadata=True)
+    csvpath = gf.labels.write_labels.write_labels_gdstk(
+        gdspath, prefixes=["{"], layer_label="TEXT"
+    )
+
+    df = pd.read_csv(csvpath)
+    print(df)
+    c.show(show_ports=False)
