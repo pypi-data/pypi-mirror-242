9724b9026094cfd382d5a3f38615965a94ae9fe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 03:04:32 +0000

    Add package mousechd-napari/0.0.1a5/mousechd_napari-0.0.1a5-py3-none-any.whl

---
+++ packages/mousechd-napari/mousechd_napari-0.0.1a5-py3-none-any.whl/mousechd_napari/assets/__init__.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from mousechd.utils.tools import download_file, BASE_URI
+
+urls = {
+    "thumbnail.png": f"{BASE_URI}/thumbnail.png",
+    "busy_mouse.gif": f"{BASE_URI}/busy_mouse.gif",
+    "transturbo.npy": f"{BASE_URI}/transturbo.npy"
+}
+
+def download_assets():
+    savedir = Path(__file__).parent
+    for fname, url in urls.items():
+        fname = Path(fname)
+        download_file(url, fname, cache_dir=savedir)
+        
