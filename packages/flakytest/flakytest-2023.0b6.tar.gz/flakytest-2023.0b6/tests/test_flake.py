c958fe9fdcb286674b096d2191be1233112851f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 03:04:50 +0000

    Add package flakytest/2023.0b6/flakytest-2023.0b6.tar.gz

---
+++ packages/flakytest/flakytest-2023.0b6.tar.gz/flakytest-2023.0b6/tests/test_flake.py
@@ -0,0 +1,36 @@
+import random
+from unittest import TestCase
+
+import pytest
+
+
+class TestUnittest(TestCase):
+    def set_up(self):
+        pass
+
+    def test_always_pass(self):
+        self.assertTrue(True)
+
+    def test_always_fail(self):
+        self.assertTrue(False)
+
+    def test_flaky(self):
+        self.assertTrue(random.randint(0, 1) < 0.5)
+
+
+def test_always_pass():
+    assert True
+
+
+def test_always_fail():
+    print("test captured stdout")
+    assert False
+
+
+@pytest.mark.skip()
+def test_skipped():
+    assert False
+
+
+def test_flaky():
+    assert random.randint(0, 1) < 0.5
