4815f7aacbdc24e5fa1494507d6f61a477755f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 15:04:19 +0000

    Add package citation-crawler/1.3.5.1/citation_crawler-1.3.5.1.tar.gz

---
+++ packages/citation-crawler/citation_crawler-1.3.5.1.tar.gz/citation_crawler-1.3.5.1/citation_crawler/summarizers/neo4j.py
@@ -0,0 +1,81 @@
+import logging
+import re
+from typing import AsyncIterable
+from citation_crawler import Author, Summarizer, Paper
+
+from neo4j import Session
+
+'''Use with dblp-crawler'''
+
+logger = logging.getLogger("graph")
+
+
+def add_paper(tx, paper: Paper):
+    n4jset = "MERGE (p:Publication {title_hash: $title_hash}) "\
+        "SET p.title=$title, p.year=$year"
+    if paper.doi():
+        n4jset += ", p.doi=$doi"
+    if paper.dblp_id():
+        n4jset += ", p.dblp_key=$dblp_id"
+    if paper.paperId():
+        n4jset += ", p.paperId=$paperId"
+    tx.run(n4jset,
+           title_hash=paper.title_hash(),
+           title=paper.title(),
+           year=paper.year(),
+           paperId=paper.paperId(),
+           dblp_id=paper.dblp_id(),
+           doi=paper.doi())
+
+
+def add_reference(tx, a: Paper, b: Paper):
+    tx.run("MERGE (a:Publication {title_hash: $a}) "
+           "MERGE (b:Publication {title_hash: $b}) "
+           "MERGE (a)-[:CITE]->(b)",
+           a=a.title_hash(), b=b.title_hash())
+
+
+def match_corrlated_authors(tx, paper: Paper):
+    nodes = []
+    for record in tx.run("MATCH (a:Person)-[:WRITE]->(p:Publication {title_hash: $title_hash}) return a",
+                         title_hash=paper.title_hash()):
+        for value in record.values():
+            nodes.append({**dict(value), "element_id": value.element_id})
+    return nodes
+
+
+def match_authors_kv(tx, k, v):
+    nodes = []
+    for record in tx.run("MATCH (a:Person {%s: $value}) RETURN a" % k,
+                         value=v):
+        for value in record.values():
+            nodes.append({**dict(value), "element_id": value.element_id})
+    return nodes
+
+
+class Neo4jSummarizer(Summarizer):
+    def __init__(self, session: Session, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.session = session
+
+    async def write_paper(self, paper) -> None:
+        self.session.execute_write(add_paper, paper)
+
+    async def write_reference(self, paper, reference) -> None:
+        self.session.execute_write(add_reference, paper, reference)
+
+    async def get_corrlated_authors(self, paper: Paper) -> AsyncIterable[Author]:
+        authors = set()
+        for author in self.session.execute_read(match_corrlated_authors, paper):
+            if author["element_id"] not in authors:
+                yield author
+                authors.add(author["element_id"])
+        async for k, v in paper.authors_kv():
+            for author in self.session.execute_read(match_authors_kv, k, v):
+                if author["element_id"] not in authors:
+                    yield author
+                    authors.add(author["element_id"])
+
+    async def write_author(self, paper: Paper, author: Author) -> None:
+        print(await paper.__dict__())
+        print(author)
