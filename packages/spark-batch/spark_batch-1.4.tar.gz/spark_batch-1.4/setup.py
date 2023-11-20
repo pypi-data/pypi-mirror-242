d6f5f9c8ef493d40bd0b1f46679edf8124ba0822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 03:04:44 +0000

    Add package spark-batch/1.4/spark_batch-1.4.tar.gz

---
+++ packages/spark-batch/spark_batch-1.4.tar.gz/spark_batch-1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="spark_batch",
+    version="1.4",
+    description="spark_delta_batch for bronze > silve > gold > mart auto",
+    author="GunSik Choi",
+    author_email="cgshome@gmail.com", 
+    packages=find_packages(where="src", exclude=["spark_extension"]),
+    package_dir={"":"src"}, 
+    install_requires=[
+        "pyyaml",
+        "psycopg2-binary",
+        "delta-spark",
+        "boto3",
+        "cryptography"
+    ],
+)
