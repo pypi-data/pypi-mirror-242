a425ae4d9e1fc942eb452f1156bc7ddd2e65b5ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 15:04:08 +0000

    Add package GauDLutils/0.2.5/GauDLutils-0.2.5.tar.gz

---
+++ packages/GauDLutils/GauDLutils-0.2.5.tar.gz/GauDLutils-0.2.5/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+Setup script.
+
+Authors: gauthierli(lwklxh@163.com)
+Date:    2023/09/27 11:12:47
+"""
+
+import setuptools
+
+setuptools.setup()
+
