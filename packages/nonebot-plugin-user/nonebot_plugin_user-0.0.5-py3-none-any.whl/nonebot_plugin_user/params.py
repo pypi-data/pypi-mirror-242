041990ad75e4fcfaabf230e7f71cd10fda8b135d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 14:03:55 +0000

    Add package nonebot-plugin-user/0.0.5/nonebot_plugin_user-0.0.5-py3-none-any.whl

---
+++ packages/nonebot-plugin-user/nonebot_plugin_user-0.0.5-py3-none-any.whl/nonebot_plugin_user/params.py
@@ -0,0 +1,32 @@
+from nonebot import logger
+from nonebot.params import Depends
+from nonebot_plugin_session import Session, SessionLevel, extract_session
+
+from . import utils
+from .models import UserSession
+
+
+async def get_or_create_user(session: Session = Depends(extract_session)):
+    """获取一个用户，如果不存在则创建"""
+    if (
+        session.platform == "unknown"
+        or session.level == SessionLevel.LEVEL0
+        or not session.id1
+    ):
+        logger.debug(f"用户相关功能暂不支持当前平台：{session.platform}-{session.level}-{session.id1}")
+        return
+
+    user = await utils.get_or_create_user(
+        session.id1,
+        session.platform,
+        f"{session.platform}-{session.id1}",
+    )
+
+    return user
+
+
+async def get_user_session(session: Session = Depends(extract_session)):
+    """获取用户会话"""
+    user = await get_or_create_user(session)
+    if user:
+        return UserSession(session, user)
