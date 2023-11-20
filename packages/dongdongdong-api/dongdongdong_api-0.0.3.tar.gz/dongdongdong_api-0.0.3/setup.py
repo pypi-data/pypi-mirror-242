7ba96dae0f152a39497aef67e85dc8d88ae52a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 14:03:50 +0000

    Add package dongdongdong-api/0.0.3/dongdongdong_api-0.0.3.tar.gz

---
+++ packages/dongdongdong-api/dongdongdong_api-0.0.3.tar.gz/dongdongdong_api-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name                = 'dongdongdong_api',
+    version             = '0.0.3',
+    description         = 'Process signin and signup',
+    author              = 'dongjae',
+    author_email        = 'sdhsdj6450@gmail.com',
+    install_requires    =  ["bcrypt", "python-dotenv", "PyJWT"],
+    packages            = find_packages(exclude = []),
+    url                 = 'https://github.com/dongdongjae/user_package',
+    python_requires     = '>=3',
+    package_data        = {},
+    zip_safe            = False,
+    classifiers         = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
