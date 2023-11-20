33a6e697e0a0741fb434384a6164cfbe837c9e7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-20 05:03:30 +0000

    Add package colorAw/0.0.1/colorAw-0.0.1.tar.gz

---
+++ packages/colorAw/colorAw-0.0.1.tar.gz/colorAw-0.0.1/colorAw/__init__.py
@@ -0,0 +1,5 @@
+from .color import Color
+
+
+__version__ = "0.0.1"
+__author__ = "MohammadRezaFirouzi"
---
+++ packages/colorAw/colorAw-0.0.1.tar.gz/colorAw-0.0.1/colorAw/color.py
@@ -0,0 +1,29 @@
+class Color:
+    black = "\033[0;30m"
+    red = "\033[0;31m"
+    green = "\033[0;32m"
+    yellow = "\033[;33m"
+    blue = "\033[0;34m"
+    purple = "\033[0;35m"
+    cyan = "\033[0;36m"
+    white = "\033[0;37m"
+
+    class bold:
+        bold_black = "\033[1;30m"
+        bold_red = "\033[1;31m"
+        bold_green = "\033[1;32m"
+        bold_yellow = "\033[1;33m"
+        bold_blue = "\033[0;34m"
+        bold_purple = "\033[1;35m"
+        bold_cyan = "\033[1;36m"
+        bold_white = "\033[1;37m"
+
+    class background():
+        black = "\033[40m"
+        red = "\033[41m"
+        green = "\033[42m"
+        yellow = "\033[43m"
+        blue = "\033[44m"
+        purple = "\033[45m"
+        cyan = "\033[46m"
+        white = "\033[47m"
