969f3c7fda6f0cf6c0d676d6a2549e7b7a066562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 05:03:48 +0000

    Add package influxable/1.4.1/influxable-1.4.1.tar.gz

---
+++ packages/influxable/influxable-1.4.1.tar.gz/influxable-1.4.1/influxable/settings.py
@@ -0,0 +1,7 @@
+import os
+
+INFLUXDB_URL = os.getenv('INFLUXDB_URL', 'http://localhost:8086')
+INFLUXDB_USER = os.getenv('INFLUXDB_USER', 'admin')
+INFLUXDB_PASSWORD = os.getenv('INFLUXDB_PASSWORD', 'changeme')
+INFLUXDB_DATABASE_NAME = os.getenv('INFLUXDB_DATABASE_NAME', 'default')
+INFLUXDB_AUTH_TOKEN = os.getenv('INFLUXDB_AUTH_TOKEN', '')
