9f69a18ecaf1e7e7b6a211dc549896d7f5456441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 15:04:07 +0000

    Add package discorpy/1.6.0/discorpy-1.6.0.tar.gz

---
+++ packages/discorpy/discorpy-1.6.0.tar.gz/discorpy-1.6.0/setup.py
@@ -0,0 +1,60 @@
+import pathlib
+import setuptools
+import sys
+
+py_ver = sys.version.split(".")[:2]
+python_version = py_ver[0] + "." + py_ver[1]
+
+if python_version == "3.8":
+    dependencies = [
+        "scipy<1.10",
+        "pywavelets<1.5",
+        "contourpy<1.2",
+        "h5py<3.10",
+        "pillow",
+        "matplotlib<3.8",
+        "scikit-image<0.19",
+        "numpy<1.21",
+    ]
+else:
+    dependencies = [
+        "scikit-image",
+        "scipy",
+        "h5py",
+        "pillow",
+        "matplotlib",
+        "numpy"
+    ]
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setuptools.setup(
+    name="discorpy",
+    version="1.6.0",
+    author="Nghia Vo",
+    author_email="nvo@bnl.gov",
+    description='Correction for radial distortion and perspective distortion '
+                'in Python',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    keywords=['Distortion correction', 'Tomography', 'Radial lens distortion',
+              'Camera calibration', 'Perspective distortion'],
+    url="https://github.com/DiamondLightSource/discorpy",
+    download_url='https://github.com/DiamondLightSource/discorpy.git',
+    license="Apache 2.0",
+    platforms="Any",
+    packages=setuptools.find_packages(include=["discorpy", "discorpy.*"],
+                                      exclude=['test*', 'doc*', 'data*',
+                                               'example*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Topic :: Scientific/Engineering :: Image Processing"
+    ],
+    install_requires=dependencies,
+    python_requires='>=3.8',
+)
