ccb54833037169872d05dabd0cfb57f97cd6f473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 16:03:53 +0000

    Add package pollinations.ai/0.0.3/pollinations.ai-0.0.3-py3-none-any.whl

---
+++ packages/pollinations.ai/pollinations.ai-0.0.3-py3-none-any.whl/pollinations/__init__.py
@@ -0,0 +1,39 @@
+from .abc import samples
+import random
+
+class Sample:
+  @property
+  def prompt(self) -> str:
+    return random.choice(samples)
+
+  def batch(self, size: int=10, *args, **kwargs) -> list:
+    return random.choices(samples, k=size)
+
+def main(*args, **kwargs) -> str:
+  pollinations_ai_info: str = """
+  [[ pollinations.ai ]]
+
+  Architect:
+    pollinations
+      file: __init__.py
+
+      folders:
+        - abc
+        - ai
+        - ext
+
+      [[ ai usage ]]
+      ```python
+
+      import pollinations.ai as ai
+
+      ImageAi = ai.Image()
+      ImageAi.generate(
+          ai.sample()
+      )
+      
+      ImageAi.save()
+      ```
+  """
+  print(pollinations_ai_info)
+  return pollinations_ai_info
