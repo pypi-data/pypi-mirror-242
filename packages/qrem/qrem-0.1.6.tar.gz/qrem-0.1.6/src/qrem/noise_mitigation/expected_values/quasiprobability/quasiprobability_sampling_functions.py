f1d829adaee25a452806c556ab7cf776dd0106e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 03:04:42 +0000

    Add package qrem/0.1.6/qrem-0.1.6.tar.gz

---
+++ packages/qrem/qrem-0.1.6.tar.gz/qrem-0.1.6/src/qrem/noise_mitigation/expected_values/quasiprobability/quasiprobability_sampling_functions.py
@@ -0,0 +1,4 @@
+"""
+@author: Jan Tuziemski, Filip Maciejewski, Joanna Majsak, Oskar Słowik, Marcin Kotowski, Katarzyna Kowalczyk-Murynka, Paweł Przewłocki, Piotr Podziemski, Michał Oszmaniec
+@contact: filip.b.maciejewski@gmail.com
+"""
---
+++ packages/qrem/qrem-0.1.6.tar.gz/qrem-0.1.6/src/qrem/noise_visualization/NoiseVisualizationHandler.py
@@ -0,0 +1,6 @@
+"""
+@author: Jan Tuziemski, Filip Maciejewski, Joanna Majsak, Oskar Słowik, Marcin Kotowski, Katarzyna Kowalczyk-Murynka, Paweł Przewłocki, Piotr Podziemski, Michał Oszmaniec
+@contact: filip.b.maciejewski@gmail.com
+"""
+#TODO FBM: think whether do something here. For now, check noise_characterization.data_analysis.InitialNoiseAnalyzer
+#ORGANIZE - this file seams obsolete (MO)
