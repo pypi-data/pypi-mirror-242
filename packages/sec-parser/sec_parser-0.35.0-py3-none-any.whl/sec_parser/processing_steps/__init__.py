f4e62a35611ac34dada21eb9b8473a66ab535ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 04:04:32 +0000

    Add package sec-parser/0.35.0/sec_parser-0.35.0-py3-none-any.whl

---
+++ packages/sec-parser/sec_parser-0.35.0-py3-none-any.whl/sec_parser/processing_steps/__init__.py
@@ -0,0 +1,46 @@
+"""
+The processing_steps subpackage provides a collection of steps
+designed to work with parser engines from the parsing_engine
+subpackage. These steps carry out specific tasks such as
+section identification, title parsing, and text extraction, etc.
+"""
+
+from sec_parser.processing_steps.abstract_classes.abstract_elementwise_processing_step import (
+    AbstractElementwiseProcessingStep,
+)
+from sec_parser.processing_steps.abstract_classes.abstract_processing_step import (
+    AbstractProcessingStep,
+)
+from sec_parser.processing_steps.image_classifier import ImageClassifier
+from sec_parser.processing_steps.individual_semantic_element_extractor.individual_semantic_element_extractor import (
+    IndividualSemanticElementExtractor,
+)
+from sec_parser.processing_steps.irrelevant_element_classifier import (
+    IrrelevantElementClassifier,
+)
+from sec_parser.processing_steps.pre_top_level_section_pruner import (
+    PreTopLevelSectionPruner,
+)
+from sec_parser.processing_steps.supplementary_text_classifier import (
+    SupplementaryTextClassifier,
+)
+from sec_parser.processing_steps.table_classifier import TableClassifier
+from sec_parser.processing_steps.text_classifier import TextClassifier
+from sec_parser.processing_steps.title_classifier import TitleClassifier
+from sec_parser.processing_steps.top_level_section_title_classifier import (
+    TopLevelSectionTitleClassifier,
+)
+
+__all__ = [
+    "AbstractProcessingStep",
+    "AbstractElementwiseProcessingStep",
+    "TextClassifier",
+    "TitleClassifier",
+    "ImageClassifier",
+    "TableClassifier",
+    "IndividualSemanticElementExtractor",
+    "SupplementaryTextClassifier",
+    "TopLevelSectionTitleClassifier",
+    "IrrelevantElementClassifier",
+    "PreTopLevelSectionPruner",
+]
