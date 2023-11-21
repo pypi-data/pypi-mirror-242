9b36d0a6855ae38af1c5303e516d439119a67adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 16:03:46 +0000

    Add package DeepChat-AI/1.1.2/DeepChat_AI-1.1.2.tar.gz

---
+++ packages/DeepChat-AI/DeepChat_AI-1.1.2.tar.gz/DeepChat_AI-1.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DeepChat_AI',
+    version='1.1.2',
+    packages=find_packages(),
+    description='''
+        MLChatbotAI is a Python package that provides tools for building, 
+        training, and deploying machine learning-based chatbots. 
+        It includes a flexible training module that allows users to preprocess 
+        and train chatbot models using custom or inbuilt intents data. 
+        The package offers easy-to-use functionality for developers to create 
+        and integrate chatbot capabilities into their applications.
+    ''',
+    author='Shibam Das',
+    author_email='shibomdas121@gmail.com',
+    license='MIT',
+    url='https://github.com/ShibamDas007/python-Bot_AI-Lab.git'
+)
+
+
