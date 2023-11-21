fe5b3c93890268d5b719417bf31f8b69518913cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 14:03:28 +0000

    Add package bibt-qualys/0.0.1/bibt_qualys-0.0.1.tar.gz

---
+++ packages/bibt-qualys/bibt_qualys-0.0.1.tar.gz/bibt_qualys-0.0.1/test.py
@@ -0,0 +1,22 @@
+from bibt.qualys import Client
+import os
+
+
+def test_client():
+    c = Client(
+        os.environ["QUALYS_USER"], os.environ["QUALYS_PASS"], os.environ["QUALYS_URL"]
+    )
+    assert c.session
+    c.close()
+    assert not c.session
+
+
+def test_list_scan_schedules():
+    c = Client(
+        os.environ["QUALYS_USER"], os.environ["QUALYS_PASS"], os.environ["QUALYS_URL"]
+    )
+    scans = c.list_scan_schedules()
+    for scan in scans:
+        print(scan)
+
+    assert len(scans) > 0
