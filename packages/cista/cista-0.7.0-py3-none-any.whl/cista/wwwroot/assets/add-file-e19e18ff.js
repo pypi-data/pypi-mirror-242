f252da2205e901102b4fb0ae4ceb06fc0dd8eca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 14:03:27 +0000

    Add package cista/0.7.0/cista-0.7.0-py3-none-any.whl

---
+++ packages/cista/cista-0.7.0-py3-none-any.whl/cista/wwwroot/assets/add-file-e19e18ff.js
@@ -0,0 +1,1 @@
+import{o as e,c as t,a as o}from"./index-c828fba8.js";const s={xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 28 28"},c=o("path",{d:"M19.2 2.6H6.1V29h19.8V9.3l-6.7-6.7zM18.5 16v7.1h-5.3V16H8.7l7.1-7.1L23 16h-4.5z"},null,-1),n=[c];function a(r,d){return e(),t("svg",s,[...n])}const h={render:a};export{h as default,a as render};
