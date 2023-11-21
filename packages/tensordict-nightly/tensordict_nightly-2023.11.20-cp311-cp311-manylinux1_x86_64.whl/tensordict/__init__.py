3cb33c828a27e8e167f2ec20d7f0797b1f834167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 04:04:27 +0000

    Add package tensordict-nightly/2023.11.20/tensordict_nightly-2023.11.20-cp311-cp311-manylinux1_x86_64.whl

---
+++ packages/tensordict-nightly/tensordict_nightly-2023.11.20-cp311-cp311-manylinux1_x86_64.whl/tensordict/__init__.py
@@ -0,0 +1,54 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from tensordict.memmap import MemoryMappedTensor
+from tensordict.memmap_deprec import MemmapTensor, set_transfer_ownership
+from tensordict.persistent import PersistentTensorDict
+from tensordict.tensorclass import tensorclass
+from tensordict.tensordict import (
+    dense_stack_tds,
+    is_batchedtensor,
+    is_memmap,
+    is_tensor_collection,
+    LazyStackedTensorDict,
+    make_tensordict,
+    merge_tensordicts,
+    pad,
+    pad_sequence,
+    SubTensorDict,
+    TensorDict,
+    TensorDictBase,
+)
+from tensordict.utils import is_tensorclass
+
+try:
+    from tensordict.version import __version__
+except ImportError:
+    __version__ = None
+
+from tensordict._pytree import *
+
+from tensordict._tensordict import unravel_key, unravel_key_list
+
+__all__ = [
+    "LazyStackedTensorDict",
+    "MemmapTensor",
+    "SubTensorDict",
+    "TensorDict",
+    "TensorDictBase",
+    "merge_tensordicts",
+    "set_transfer_ownership",
+    "pad_sequence",
+    "make_tensordict",
+    "is_memmap",
+    "is_batchedtensor",
+    "is_tensor_collection",
+    "pad",
+    "PersistentTensorDict",
+    "tensorclass",
+    "dense_stack_tds",
+]
+
+# from tensordict._pytree import *
