c1b8180a2459dafa3bab18dee970b02f4227b123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 16:03:50 +0000

    Add package scippneutron/23.11.0/scippneutron-23.11.0-py3-none-any.whl

---
+++ packages/scippneutron/scippneutron-23.11.0-py3-none-any.whl/scippneutron/__init__.py
@@ -0,0 +1,37 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+# @author Simon Heybrock
+
+# flake8: noqa
+
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version(__package__ or __name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
+
+from .beamline_components import (
+    position,
+    source_position,
+    sample_position,
+    incident_beam,
+    scattered_beam,
+    Ltotal,
+    L1,
+    L2,
+    two_theta,
+)
+from .core import convert
+from .mantid import (
+    from_mantid,
+    array_from_mantid,
+    to_mantid,
+    load_with_mantid,
+    load,
+    fit,
+)
+from .instrument_view import instrument_view
+from .io.nexus.load_nexus import load_nexus, load_nexus_json
+from .data_streaming.data_stream import data_stream
+from . import data
---
+++ packages/scippneutron/scippneutron-23.11.0-py3-none-any.whl/scippneutron/tof/__init__.py
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+# @author Simon Heybrock
+
+# flake8: noqa
+r"""
+Specifics for time-of-flight neutron-scattering data reduction, including coordinate transformations.
+"""
+
+from .diagram import TimeDistanceDiagram
+from .frames import unwrap_frames
