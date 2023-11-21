cb931f96805576f81a5b929b3b4b58998b9f4d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 14:04:25 +0000

    Add package agstoolbox/0.4.1/agstoolbox-0.4.1.tar.gz

---
+++ packages/agstoolbox/agstoolbox-0.4.1.tar.gz/agstoolbox-0.4.1/src/agstoolbox/core/ags/ags_local_run.py
@@ -0,0 +1,24 @@
+from __future__ import annotations  # for python 3.8
+
+import os
+from subprocess import Popen
+
+from agstoolbox.core.ags.ags_editor import LocalAgsEditor
+from agstoolbox.core.ags.game_project import GameProject
+from agstoolbox.core.utils.file import get_absolute_path
+from agstoolbox.core.utils.run import run_exe_params
+
+
+def start_ags_editor(editor: LocalAgsEditor, block: bool = False):
+    run_exe_params(editor.path, block)
+
+
+def ags_editor_load_project(editor: LocalAgsEditor, project: GameProject, block: bool = False):
+    project_path = get_absolute_path(project.path)
+    run_exe_params(editor.path, block, [project_path], )
+
+
+def ags_editor_build_project(editor: LocalAgsEditor, project: GameProject, block: bool = False):
+    project_path = get_absolute_path(project.path)
+    run_exe_params(editor.path, block, ['/compile', project_path])
+
