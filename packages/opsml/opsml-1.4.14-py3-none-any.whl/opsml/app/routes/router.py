74f368b7448f24885c40d4d36bde320a13514279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 15:04:40 +0000

    Add package opsml/1.4.14/opsml-1.4.14-py3-none-any.whl

---
+++ packages/opsml/opsml-1.4.14-py3-none-any.whl/opsml/app/routes/router.py
@@ -0,0 +1,15 @@
+# Copyright (c) Shipt, Inc.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+from fastapi import APIRouter
+
+from opsml.app.routes import cards, data, files, healthcheck, models, registry, settings
+
+api_router = APIRouter(responses={404: {"description": "Not found"}})
+api_router.include_router(healthcheck.router, tags=["health"], prefix="/opsml")
+api_router.include_router(settings.router, tags=["base"], prefix="/opsml")
+api_router.include_router(cards.router, tags=["cards"], prefix="/opsml")
+api_router.include_router(models.router, tags=["model"], prefix="/opsml")
+api_router.include_router(files.router, tags=["file"], prefix="/opsml")
+api_router.include_router(data.router, tags=["data"], prefix="/opsml")
+api_router.include_router(registry.router, tags=["registry"], prefix="/opsml")
