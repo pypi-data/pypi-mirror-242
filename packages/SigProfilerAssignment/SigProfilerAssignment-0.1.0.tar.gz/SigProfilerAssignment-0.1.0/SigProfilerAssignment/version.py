39208cedf5ce9418fa60a0dc1391a6f7463ec979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 14:03:30 +0000

    Add package SigProfilerAssignment/0.1.0/SigProfilerAssignment-0.1.0.tar.gz

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.1.0.tar.gz/SigProfilerAssignment-0.1.0/SigProfilerAssignment/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.1.0'
+version = '0.1.0'
+Update = 'v0.1.0: Update SigProfilerAssignment with COSMIC v3.4 signatures.'
+
+    
+    
