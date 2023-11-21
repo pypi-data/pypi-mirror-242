f5369651325b912c11525aba496fe98430668e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 16:04:13 +0000

    Add package RefractDQ/1.1.8/RefractDQ-1.1.8.tar.gz

---
+++ packages/RefractDQ/RefractDQ-1.1.8.tar.gz/RefractDQ-1.1.8/data_quality/init_script.sh
@@ -0,0 +1,5 @@
+# echo "Running init_script.sh, installing dependencies";
+# pip install snowflake-snowpark-python;
+# pip install snowflake-snowpark-python[pandas];
+pip install feast --no-deps
+# echo "Done with dependency installtion!";
