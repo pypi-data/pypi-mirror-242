61d53e2e2dfdef1377a1bcfac8b149fedcd23266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 16:03:49 +0000

    Add package vembed/0.22/vembed-0.22-py3-none-any.whl

---
+++ packages/vembed/vembed-0.22-py3-none-any.whl/vembed/__init__.py
@@ -0,0 +1,9 @@
+from .string_to_vector.mpnet_paraphrase import (
+    string_to_embedding,
+    strings_to_embeddings,
+)
+from .similarity.get_similarity import (
+    calculate_similarities,
+    print_formatted_results,
+    plot_similarities,
+)
