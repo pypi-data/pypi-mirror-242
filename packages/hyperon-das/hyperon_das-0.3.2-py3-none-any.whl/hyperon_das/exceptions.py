39ae8a355764604fb2e8616c4b6ba564817a257c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 04:04:30 +0000

    Add package hyperon-das/0.3.2/hyperon_das-0.3.2-py3-none-any.whl

---
+++ packages/hyperon-das/hyperon_das-0.3.2-py3-none-any.whl/hyperon_das/exceptions.py
@@ -0,0 +1,34 @@
+class BaseException(Exception):
+    """
+    Base class to exceptions
+    """
+
+    def __init__(self, message: str, details: str = ""):
+        self.message = message
+        self.details = details
+
+        super().__init__(self.message, self.details)
+
+
+class DatabaseTypeException(BaseException):
+    ...  # pragma no cover
+
+
+class MethodNotAllowed(BaseException):
+    ...  # pragma no cover
+
+
+class QueryParametersException(BaseException):
+    ...  # pragma no cover
+
+
+class UnexpectedQueryFormat(BaseException):
+    ...  # pragma no cover
+
+
+class InvalidAssignment(BaseException):
+    ...  # pragma no cover
+
+
+class InitializeServerException(BaseException):
+    ...  # pragma no cover
---
+++ packages/hyperon-das/hyperon_das-0.3.2-py3-none-any.whl/hyperon_das/factory.py
@@ -0,0 +1,63 @@
+from abc import abstractmethod
+from enum import Enum
+from typing import Optional, Protocol
+
+from hyperon_das_atomdb import IAtomDB
+from hyperon_das_atomdb.adapters import InMemoryDB, RedisMongoDB, ServerDB
+
+
+class DatabaseType(Enum):
+    REDIS_MONGO = 'redis_mongo'
+    RAM_ONLY = 'ram_only'
+    SERVER = 'server'
+
+    @classmethod
+    def values(cls):
+        return [choices.value for choices in cls]
+
+
+class IDatabaseFactory(Protocol):
+    @abstractmethod
+    def create_redis_mongo_database(self):
+        ...  # pragma no cover
+
+    @abstractmethod
+    def create_ram_only_database(self):
+        ...  # pragma no cover
+
+    @abstractmethod
+    def create_server_database(self):
+        ...  # pragma no cover
+
+
+class DatabaseFactory:
+    def __init__(self, database_name: DatabaseType) -> None:
+        self.name = database_name
+
+    def create_redis_mongo_database(self):
+        return RedisMongoDB
+
+    def create_ram_only_database(self):
+        return InMemoryDB
+
+    def create_server_database(self):
+        return ServerDB
+
+
+def database_factory(
+    factory: IDatabaseFactory,
+    host: Optional[str] = None,
+    port: Optional[str] = None,
+) -> IAtomDB:
+    redis_mongo_database = factory.create_redis_mongo_database()
+    ram_only_database = factory.create_ram_only_database()
+    server_database = factory.create_server_database()
+
+    if factory.name == DatabaseType.REDIS_MONGO.value:
+        return redis_mongo_database()
+
+    if factory.name == DatabaseType.RAM_ONLY.value:
+        return ram_only_database()
+
+    if factory.name == DatabaseType.SERVER.value:
+        return server_database(host=host, port=port)
