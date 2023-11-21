dfbfcbcc0b9fe84ca8aae8fe934518607cbb0817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 14:03:30 +0000

    Add package NVRChannel/0.1.2/NVRChannel-0.1.2.tar.gz

---
+++ packages/NVRChannel/NVRChannel-0.1.2.tar.gz/NVRChannel-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='NVRChannel',
+    version='0.1.2',
+    author="Daniel Dunn",
+    author_email="dannydunn@eternityforest.com",
+    packages=['NVRChannel',"NVRChannel.NVRChannel"],
+    package_data={'':["**/*.tflite", '**/*.txt', '**.*.json', "*.json","*.md",]},
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/EternityForest/iot_devices",
+    dependencies = [
+    	"cv2",
+    	"Pillow",
+    	"tflite-runtime",
+        "scullery",
+    ]
+)
