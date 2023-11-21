977c4d254cc42ec55967785f40ef6e812f145576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 14:04:33 +0000

    Add package cfbd-json-py/0.0.15/cfbd_json_py-0.0.15.tar.gz

---
+++ packages/cfbd-json-py/cfbd_json_py-0.0.15.tar.gz/cfbd_json_py-0.0.15/cfbd_json_py/teams.py
@@ -0,0 +1,79 @@
+# Creation Date: 08/30/2023 01:13 EDT
+# Last Updated Date: 10/06/2023 07:54 PM EDT
+# Author: Joseph Armstrong (armstrongjoseph08@gmail.com)
+# File Name: teams.py
+# Purpose: Houses functions pertaining to CFB team data within the CFBD API.
+####################################################################################################
+
+def get_cfbd_team_information(
+        api_key: str = None,
+        api_key_dir: str = None,
+        conference_abv: str = None,
+
+        return_as_dict: bool = False):
+    """
+
+    """
+    raise NotImplementedError(
+        'This function has yet to be implemented by this version.'
+    )
+
+
+def get_cfbd_fbs_team_list(
+        api_key: str = None,
+        api_key_dir: str = None,
+        season: int = None,
+
+        return_as_dict: bool = False):
+    """
+
+    """
+    raise NotImplementedError(
+        'This function has yet to be implemented by this version.'
+    )
+
+
+def get_cfbd_team_rosters(
+        api_key: str = None,
+        api_key_dir: str = None,
+        team: str = None,
+        season: int = None,
+
+        return_as_dict: bool = False):
+    """
+
+    """
+    raise NotImplementedError(
+        'This function has yet to be implemented by this version.'
+    )
+
+
+def get_cfbd_team_talent_rankings(
+        api_key: str = None,
+        api_key_dir: str = None,
+        season: int = None,
+
+        return_as_dict: bool = False):
+    """
+
+    """
+    raise NotImplementedError(
+        'This function has yet to be implemented by this version.'
+    )
+
+
+def get_cfbd_team_matchup_history(
+        team_1: str,
+        team_2: str,
+        api_key: str = None,
+        api_key_dir: str = None,
+        min_season: int = None,
+        max_season: int = None,
+
+        return_as_dict: bool = False):
+    """
+
+    """
+    raise NotImplementedError(
+        'This function has yet to be implemented by this version.'
+    )
