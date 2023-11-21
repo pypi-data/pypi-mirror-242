c00641db0561d555cd4bc4722c4b4e1b88d151d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 14:03:28 +0000

    Add package xprompt-common/0.1.2/xprompt_common-0.1.2.tar.gz

---
+++ packages/xprompt-common/xprompt_common-0.1.2.tar.gz/xprompt_common-0.1.2/xprompt_common/prompt_parser.py
@@ -0,0 +1,45 @@
+from typing import Any
+
+from bs4 import BeautifulSoup, Tag
+from pydantic import BaseModel, Extra
+
+from xprompt_common.base_service import BaseService
+
+
+class PromptParser(BaseModel):
+    prompt: str
+    soup: BeautifulSoup = None
+    tags: list[Tag] = None
+
+    tag_to_service: dict[str, BaseService.__class__] = None
+
+    class Config:
+        """Configuration for this pydantic object."""
+
+        extra = Extra.forbid
+        arbitrary_types_allowed = True
+
+    def __init__(self, **kwargs: Any):
+        super().__init__(**kwargs)
+        self.soup = BeautifulSoup(self.prompt, "html.parser")
+        self.tags = self.soup.find_all()
+
+    def get_service(self, tag: Tag) -> BaseService:
+        return self.tag_to_service[tag.name].create_from_tag(tag)
+
+    @staticmethod
+    def handle_missing_tag(tag: Tag):
+        pass
+
+    def run_services(self):
+        # TODO: running services for parsing tags concurrently
+        for tag in self.tags:
+            if tag.name in self.tag_to_service:
+                service: BaseService = self.get_service(tag=tag)
+                text = service.run()
+                tag.replace_with(text)
+
+            else:
+                self.handle_missing_tag(tag=tag)
+
+        return self.soup.prettify()
