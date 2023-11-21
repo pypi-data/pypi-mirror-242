9c3981612c382f249692b1c70909b8ca648c9531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 16:04:12 +0000

    Add package apsimNGpy/1.0/apsimNGpy-1.0.tar.gz

---
+++ packages/apsimNGpy/apsimNGpy-1.0.tar.gz/apsimNGpy-1.0/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import sys
+import os
+VERSION = '0.0.3beta'
+DESCRIPTION = 'apsimx next generation package interface'
+LONG_DESCRIPTION = 'run, edit, download soils and weather and interact with the apsimx file'
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+setup(
+    name='apsimNGpy',
+    version='1.0',
+    url='https://github.com/MAGALA-RICHARD/apsimNGpy.git',
+    license='MIT',
+    author='Richard Magala',
+    author_email='magalarich20@gmail.com',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe = True,
+    package_data={'': ['basefiles/*.apsimx', 'basefiles/*.met', 'examples/*.png']},
+     keywords=['python', 'apsim'],
+     classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: Microsoft :: Windows",
+        ],
+     install_requires=[
+        'numpy>=1.20.0',
+        'scipy>=1.7',
+         'xmltodict >=0.13.0',
+         'requests >=2.31.0',
+         'pythonnet >= 3.0.1',
+         'rasterio >=1.3.8',
+         'shapely >=2.0.1',
+         'clr >= 1.0.3',
+         'geopandas >=0.13.2',
+         'pandas >=2.0.3',
+         
+
+    ]
+)
+
