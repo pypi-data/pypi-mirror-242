7bd15d1a84e9b89ccec6294c55d3cb74ade9e0cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 04:03:50 +0000

    Add package pybop/23.11/pybop-23.11.tar.gz

---
+++ packages/pybop/pybop-23.11.tar.gz/pybop-23.11/pybop/plotting/quick_plot.py
@@ -0,0 +1,22 @@
+class QuickPlot:
+    """
+
+    Class to generate plots with standard variables and formatting.
+
+    Plots
+    --------------
+    Observability
+    if method == parameterisation
+
+        Comparison of fitting data with optimised forward model
+
+    elseif method == optimisation
+
+        Pareto front
+        Alternative solutions
+        Initial value compared to optimal
+
+    """
+
+    def __init__(self):
+        self.name = "Quick Plot"
