c1f3b2024b6280a194497e76057c77b7fb922ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 03:05:22 +0000

    Add package pdb-numpy/0.0.2/pdb_numpy-0.0.2.tar.gz

---
+++ packages/pdb-numpy/pdb_numpy-0.0.2.tar.gz/pdb_numpy-0.0.2/src/pdb_numpy/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import logging
+import os
+import sys
+
+# Autorship information
+__author__ = "Samuel Murail"
+__copyright__ = "Copyright 2022, RPBS"
+__credits__ = ["Samuel Murail"]
+__license__ = "GNU General Public License v2.0"
+__version__ = "0.0.2"
+__maintainer__ = "Samuel Murail"
+__email__ = "samuel.murail@u-paris.fr"
+__status__ = "Beta"
+
+# Logging
+logger = logging.getLogger(__name__)
+
+
+def show_log():
+    logger.setLevel(logging.INFO)
+    # logger.addHandler(logging.StreamHandler(sys.stdout))
+
+
+from .coor import Coor
+from .model import Model
