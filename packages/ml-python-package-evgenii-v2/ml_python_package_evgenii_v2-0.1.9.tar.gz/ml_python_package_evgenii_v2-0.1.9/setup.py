e97b6d775e21ac83bd60d6503471aa2d7e8ab07b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 14:03:34 +0000

    Add package ml-python-package-evgenii-v2/0.1.9/ml_python_package_evgenii_v2-0.1.9.tar.gz

---
+++ packages/ml-python-package-evgenii-v2/ml_python_package_evgenii_v2-0.1.9.tar.gz/ml_python_package_evgenii_v2-0.1.9/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="ml_python_package_evgenii_v2",
+    packages=find_packages(),
+    version="0.1.9",
+    description="Training stuff",
+    author="Evgenii",
+    install_requires=["scikit-learn==0.24.1", "pandas==1.1.5",],
+    license="MIT",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
