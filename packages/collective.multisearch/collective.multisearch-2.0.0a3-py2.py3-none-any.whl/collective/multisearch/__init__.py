9977a5e2dbf865d05e37fb6da632cb0b23f7f544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 16:04:00 +0000

    Add package collective.multisearch/2.0.0a3/collective.multisearch-2.0.0a3-py2.py3-none-any.whl

---
+++ packages/collective.multisearch/collective.multisearch-2.0.0a3-py2.py3-none-any.whl/collective/multisearch/__init__.py
@@ -0,0 +1,8 @@
+from zope.i18nmessageid import MessageFactory
+
+
+MultiSearchMessageFactory = MessageFactory(u"collective.multisearch")
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
