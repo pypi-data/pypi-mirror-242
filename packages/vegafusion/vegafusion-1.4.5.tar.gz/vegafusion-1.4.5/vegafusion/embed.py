60bb5d44163d7835f71842d5fa960d084e4ff37a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 03:03:46 +0000

    Add package vegafusion/1.4.5/vegafusion-1.4.5.tar.gz

---
+++ packages/vegafusion/vegafusion-1.4.5.tar.gz/vegafusion-1.4.5/vegafusion/embed.py
@@ -0,0 +1,10 @@
+# VegaFusion
+# Copyright (C) 2022, Jon Mease
+#
+# This program is distributed under multiple licenses.
+# Please consult the license documentation provided alongside
+# this program the details of the active license.
+try:
+    from vegafusion_embed import *
+except ImportError:
+    raise ImportError("Please install the vegafusion-python-embed package")
