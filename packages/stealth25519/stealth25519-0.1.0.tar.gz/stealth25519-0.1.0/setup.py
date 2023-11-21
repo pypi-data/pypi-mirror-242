d7b123d591be82d5b332dd01826bbffa1636eda1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 17:03:51 +0000

    Add package stealth25519/0.1.0/stealth25519-0.1.0.tar.gz

---
+++ packages/stealth25519/stealth25519-0.1.0.tar.gz/stealth25519-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stealth25519',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    tests_require=[],
+    entry_points={
+        'console_scripts': [],
+    },
+)
