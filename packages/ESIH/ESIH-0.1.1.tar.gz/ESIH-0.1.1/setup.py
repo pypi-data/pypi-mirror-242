ec6a619d5e0fe202c2b1c5355e7f6411b8793b45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 06:04:09 +0000

    Add package ESIH/0.1.1/ESIH-0.1.1.tar.gz

---
+++ packages/ESIH/ESIH-0.1.1.tar.gz/ESIH-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ESIH',
+    version='0.1.1',
+    packages=find_packages(),
+    description='Un Programme qui annalyse les fichiers CSV et qui donne des statistiques avec Class',
+    long_description=open('README.md').read(),
+    author='EXALENT/AMAZAN',
+    author_email='emaxwisken@gmail.com',    
+)
