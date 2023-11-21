2d0063af8b86f110445f08f70ad98ba3687b3fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 15:04:21 +0000

    Add package rouskinhf/0.3.2/rouskinhf-0.3.2.tar.gz

---
+++ packages/rouskinhf/rouskinhf-0.3.2.tar.gz/rouskinhf-0.3.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="rouskinhf",
+    packages=["rouskinhf"],
+    license="MIT",
+    install_requires=[
+        'pytest',
+        'pytest_cov',
+        'numpy',
+        'pyarrow',
+        'pandas',
+        'huggingface',
+        'huggingface_hub',
+        'jupyter',
+        'ipykernel',
+        'ipython',
+        'scipy',
+        'pydantic',
+        'pytest-dotenv',
+        'python-dotenv',
+        'tqdm',
+        'pytest-env',
+        'scikit-learn'
+        ],
+)
