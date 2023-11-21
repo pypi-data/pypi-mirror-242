cab6a2b3206c8d046ef4f25e933d3f075cf835d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 05:03:51 +0000

    Add package stochasticprocesses/0.7.0/StochasticProcesses-0.7.0.tar.gz

---
+++ packages/stochasticprocesses/StochasticProcesses-0.7.0.tar.gz/StochasticProcesses-0.7.0/Readme.md
@@ -0,0 +1,18 @@
+
+# About the package
+
+This is a package dedicated to simulation of stochastic processes. In it you will find some simulations of trajectories of various stochastic processes:
+
+- Simple Random Walk,
+- Pearson Random Walk,
+- Poisson Process,
+- Brownian Motion,
+- Brownian Bridge,
+- Brownian Motion with drift and volatility coefficient, and
+- Geometric Brownian Motion
+
+
+# References
+
+For further reference : [Stochastic Simulation](https://github.com/JulioCesarMS/StochasticModels/blob/main/Some_Stochastic_Models.ipynb)
+
---
+++ packages/stochasticprocesses/StochasticProcesses-0.7.0.tar.gz/StochasticProcesses-0.7.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("Readme.md", "r") as f:
+    long_description = f.read()
+    
+setup (
+    author= "Julio César Martínez",
+    description="A package to simulate stochastic proccesses",
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    name='StochasticProcesses',
+    version='0.7.0',
+    packages=find_packages(include=["numpy", "pandas"]),
+    author_email="julio.martinez@gmail.com.mx",
+    install_requires=["pandas >= 1.5.3", "numpy >= 1.23.5"],
+    python_requires=">= 3.9.4",
+    url='https://github.com/JulioCesarMS/StochasticModels.git'
+    ),
+
+
+# pip install -e .
