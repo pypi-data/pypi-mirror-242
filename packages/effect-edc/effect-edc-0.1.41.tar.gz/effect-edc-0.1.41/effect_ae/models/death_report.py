db94baeff7d47e97472cb2e407c7555cd70c53b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 05:03:52 +0000

    Add package effect-edc/0.1.41/effect-edc-0.1.41.tar.gz

---
+++ packages/effect-edc/effect-edc-0.1.41.tar.gz/effect-edc-0.1.41/effect_ae/models/death_report.py
@@ -0,0 +1,14 @@
+from django.db import models
+from edc_adverse_event.model_mixins import DeathReportModelMixin
+from edc_model.models import BaseUuidModel
+
+from ..model_mixins import DeathReportModelMixin as EffectDeathReportModelMixin
+
+# TODO: make this EFFECT specific
+
+
+class DeathReport(EffectDeathReportModelMixin, DeathReportModelMixin, BaseUuidModel):
+    study_day = models.IntegerField(default=0, editable=False, help_text="not used")
+
+    class Meta(DeathReportModelMixin.Meta, BaseUuidModel.Meta):
+        pass
