be1d72922ca657a1f7da4f4eeb66e63397999289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 04:03:12 +0000

    Add package compmech/0.8.0/compmech-0.8.0-cp310-cp310-win_amd64.whl

---
+++ packages/compmech/compmech-0.8.0-cp310-cp310-win_amd64.whl/compmech/panel/__init__.py
@@ -0,0 +1,12 @@
+r"""
+==========================================================
+Semi-analytical Models for Panels (:mod:`compmech.panel`)
+==========================================================
+
+.. currentmodule:: compmech.panel
+
+.. autoclass:: compmech.panel.Panel
+    :members:
+
+"""
+from . _panel import Panel
