9623c8aa8d936a320ddb47158aa8e88209c3287c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 03:04:48 +0000

    Add package redturtle.prenotazioni/2.2.0/redturtle.prenotazioni-2.2.0.tar.gz

---
+++ packages/redturtle.prenotazioni/redturtle.prenotazioni-2.2.0.tar.gz/redturtle.prenotazioni-2.2.0/src/redturtle/prenotazioni/events/prenotazione.py
@@ -0,0 +1,198 @@
+# -*- coding: utf-8 -*-
+import hashlib
+from email.utils import formataddr, parseaddr
+from logging import getLogger
+
+from plone import api
+from plone.app.event.base import default_timezone
+from plone.registry.interfaces import IRegistry
+from plone.stringinterp.interfaces import IStringSubstitution
+from Products.CMFPlone.interfaces.controlpanel import IMailSchema
+from zope.component import getAdapter, getMultiAdapter, getUtility
+from zope.i18n import translate
+
+from redturtle.prenotazioni import _, is_migration
+from redturtle.prenotazioni.adapters.booker import IBooker
+from redturtle.prenotazioni.interfaces import IPrenotazioneEmailMessage
+
+logger = getLogger(__name__)
+
+
+def reallocate_gate(obj):
+    """
+    We have to reallocate the gate for this object
+
+    Skip this step if we have a form.gate parameter in the request
+
+    DISABLED: SEEMS ONLY GENERATES PROBLEMS
+    """
+    context = obj.object
+
+    if context.REQUEST.form.get("form.gate", "") and getattr(context, "gate", ""):
+        return
+
+    container = context.getPrenotazioniFolder()
+    booking_date = context.getBooking_date()
+    new_gate = IBooker(container).get_available_gate(booking_date)
+    if new_gate:
+        context.gate = new_gate
+
+
+def reallocate_container(obj):
+    """
+    If we moved Prenotazione to a new week we should move it
+    """
+    container = obj.object.getPrenotazioniFolder()
+    IBooker(container).fix_container(obj.object)
+
+
+def notify_on_after_transition_event(context, event):
+    """The messages are being send only if the following flags on the PrenotazioniFolder are set"""
+    if is_migration():
+        return
+
+    booking_folder = context.getPrenotazioniFolder()
+    flags = {
+        i: getattr(booking_folder, f"notify_on_{i}", False)
+        for i in ("confirm", "submit", "refuse")
+    }
+    if flags["confirm"] and flags["submit"]:
+        flags["submit"] = False
+
+    if flags.get(event.transition and event.transition.__name__ or "", False):
+        if not getattr(context, "email", ""):
+            # booking does not have an email set
+            return
+        adapter = getMultiAdapter(
+            (context, event),
+            IPrenotazioneEmailMessage,
+            name=event.transition.__name__,
+        )
+
+        if adapter:
+            if adapter.message:
+                send_email(adapter.message)
+
+
+def autoconfirm(booking, event):
+    if api.content.get_state(obj=booking, default=None) == "pending":
+        if getattr(booking.getPrenotazioniFolder(), "auto_confirm", False):
+            api.content.transition(obj=booking, transition="confirm")
+            booking.reindexObject(idxs="review_state")
+
+
+def notify_on_move(booking, event):
+    if not getattr(booking.getPrenotazioniFolder(), "notify_on_move", False):
+        return
+    if not getattr(booking, "email", ""):
+        # booking does not have an email set
+        return
+    adapter = getMultiAdapter((booking, event), IPrenotazioneEmailMessage)
+    if adapter:
+        if adapter.message:
+            send_email(adapter.message)
+
+
+def send_email(msg):
+    if not msg:
+        logger.error("Could not send email due to no message was provided")
+        return
+
+    host = api.portal.get_tool(name="MailHost")
+    registry = getUtility(IRegistry)
+    encoding = registry.get("plone.email_charset", "utf-8")
+
+    host.send(msg, charset=encoding)
+
+
+def get_mail_from_address():
+    registry = getUtility(IRegistry)
+    mail_settings = registry.forInterface(IMailSchema, prefix="plone", check=False)
+    from_address = mail_settings.email_from_address
+    from_name = mail_settings.email_from_name
+
+    if not from_address:
+        return ""
+    from_address = from_address.strip()
+    mfrom = formataddr((from_name, from_address))
+    if parseaddr(mfrom)[1] != from_address:
+        mfrom = from_address
+    return mfrom
+
+
+def send_email_to_managers(booking, event):
+    # skip email for vacation/out-of-office
+    if is_migration():
+        return
+
+    if booking.isVacation():
+        return
+
+    booking_folder = booking.getPrenotazioniFolder()
+
+    booking_operator_url = getAdapter(
+        booking, IStringSubstitution, "booking_operator_url"
+    )()
+
+    email_list = getattr(booking_folder, "email_responsabile", "")
+    if email_list:
+        mail_template = api.content.get_view(
+            name="manager_notification_mail",
+            context=booking,
+            request=booking.REQUEST,
+        )
+        booking_date = getattr(booking, "booking_date", None)
+        parameters = {
+            "company": getattr(booking, "company", ""),
+            "booking_folder": booking_folder.title,
+            "booking_url": booking_operator_url,
+            "booking_date": booking_date.astimezone(
+                default_timezone(as_tzinfo=True)
+            ).strftime("%d/%m/%Y"),
+            "booking_hour": booking_date.astimezone(
+                default_timezone(as_tzinfo=True)
+            ).strftime("%H:%M"),
+            "booking_expiration_date": getattr(booking, "booking_expiration_date", ""),
+            "description": getattr(booking, "description", ""),
+            "email": getattr(booking, "email", ""),
+            "fiscalcode": getattr(booking, "fiscalcode", ""),
+            "gate": getattr(booking, "gate", ""),
+            "phone": getattr(booking, "phone", ""),
+            "staff_notes": getattr(booking, "staff_notes", ""),
+            "booking_type": getattr(booking, "booking_type", ""),
+            "title": getattr(booking, "title", ""),
+        }
+        mail_text = mail_template(**parameters)
+
+        mailHost = api.portal.get_tool(name="MailHost")
+        subject = translate(
+            _(
+                "new_booking_admin_notify_subject",
+                default="New booking for ${context}",
+                mapping={"context": booking_folder.title},
+            ),
+            context=booking.REQUEST,
+        )
+        for mail in email_list:
+            if mail:
+                mailHost.send(
+                    mail_text,
+                    mto=mail,
+                    mfrom=get_mail_from_address(),
+                    subject=subject,
+                    charset="utf-8",
+                    msg_type="text/html",
+                )
+
+
+def set_booking_code(booking, event):
+    """
+    set booking code. skip if we are importing old booking
+    """
+    if is_migration():
+        return
+
+    hash_obj = hashlib.blake2b(bytes(booking.UID(), encoding="utf8"), digest_size=3)
+    hash_value = hash_obj.hexdigest().upper()
+    setattr(booking, "booking_code", hash_value)
+    return
---
+++ packages/redturtle.prenotazioni/redturtle.prenotazioni-2.2.0.tar.gz/redturtle.prenotazioni-2.2.0/src/redturtle/prenotazioni/tests/test_prenotazione_events.py
@@ -0,0 +1,211 @@
+# -*- coding: UTF-8 -*-
+import email
+import unittest
+from datetime import date, datetime, timedelta
+
+import pytz
+from plone import api
+from plone.app.testing import TEST_USER_ID, setRoles
+from zope.event import notify
+from zope.interface import implementer
+from zope.interface.interfaces import IObjectEvent
+
+from redturtle.prenotazioni.adapters.booker import IBooker
+from redturtle.prenotazioni.prenotazione_event import MovedPrenotazione
+from redturtle.prenotazioni.testing import REDTURTLE_PRENOTAZIONI_FUNCTIONAL_TESTING
+
+
+@implementer(IObjectEvent)
+class DummyEvent(object):
+    def __init__(self, object):
+        self.object = object
+
+
+class TestSPrenotazioneEvents(unittest.TestCase):
+    layer = REDTURTLE_PRENOTAZIONI_FUNCTIONAL_TESTING
+    maxDiff = None
+    timezone = "Europe/Rome"
+
+    def dt_local_to_utc(self, value):
+        return pytz.timezone(self.timezone).localize(value).astimezone(pytz.utc)
+
+    def setUp(self):
+        self.app = self.layer["app"]
+        self.portal = self.layer["portal"]
+        self.mailhost = self.portal.MailHost
+        self.email_subject = "Testing subject"
+        self.email_message = "Testing message"
+
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+
+        self.folder_prenotazioni = api.content.create(
+            container=self.portal,
+            type="PrenotazioniFolder",
+            title="Prenota foo",
+            description="",
+            daData=date.today(),
+            gates=["Gate A"],
+        )
+
+        api.content.create(
+            type="PrenotazioneType",
+            title="Type A",
+            duration=30,
+            container=self.folder_prenotazioni,
+            gates=["all"],
+        )
+
+        self.today_8_0 = self.dt_local_to_utc(
+            datetime.now().replace(hour=8, minute=0, second=0, microsecond=0)
+        )
+        self.tomorrow_8_0 = self.today_8_0 + timedelta(1)
+        week_table = self.folder_prenotazioni.week_table
+        for data in week_table:
+            data["morning_start"] = "0700"
+            data["morning_end"] = "1000"
+        self.folder_prenotazioni.week_table = week_table
+
+        api.portal.set_registry_record(
+            "plone.portal_timezone",
+            self.timezone,
+        )
+
+    def create_booking(self, booking_date=None):
+        booker = IBooker(self.folder_prenotazioni)
+        if booking_date is None:
+            booking_date = self.tomorrow_8_0
+        return booker.create(
+            {
+                "booking_date": booking_date,
+                "booking_type": "Type A",
+                "title": "foo",
+                "email": "jdoe@redturtle.it",
+            }
+        )
+
+    def test_email_sent_on_submit(self):
+        self.folder_prenotazioni.notify_on_submit = True
+        self.folder_prenotazioni.notify_on_submit_subject = self.email_subject
+        self.folder_prenotazioni.notify_on_submit_message = self.email_message
+
+        self.assertFalse(self.mailhost.messages)
+
+        self.create_booking()
+
+        self.assertEqual(len(self.mailhost.messages), 1)
+
+        mail = email.message_from_bytes(self.mailhost.messages[0])
+
+        self.assertTrue(mail.is_multipart())
+
+        self.assertIn(
+            self.email_subject,
+            "".join([i for i in mail.values()]),
+        )
+        self.assertIn(
+            self.email_message,
+            mail.get_payload()[0].get_payload(),
+        )
+
+    def test_email_send_on_confirm(self):
+        self.folder_prenotazioni.notify_on_confirm = True
+        self.folder_prenotazioni.notify_on_confirm_subject = self.email_subject
+        self.folder_prenotazioni.notify_on_confirm_message = self.email_message
+
+        self.assertFalse(self.mailhost.messages)
+
+        booking = self.create_booking()
+
+        api.content.transition(booking, "confirm")
+
+        self.assertEqual(len(self.mailhost.messages), 1)
+
+        mail = email.message_from_bytes(self.mailhost.messages[0])
+
+        self.assertTrue(mail.is_multipart())
+
+        self.assertIn(
+            self.email_subject,
+            "".join([i for i in mail.values()]),
+        )
+        self.assertIn(
+            self.email_message,
+            mail.get_payload()[0].get_payload(),
+        )
+
+    def test_email_send_on_reject(self):
+        self.folder_prenotazioni.notify_on_refuse = True
+        self.folder_prenotazioni.notify_on_refuse_subject = self.email_subject
+        self.folder_prenotazioni.notify_on_refuse_message = self.email_message
+
+        self.assertFalse(self.mailhost.messages)
+
+        booking = self.create_booking()
+
+        api.content.transition(booking, "refuse")
+
+        self.assertEqual(len(self.mailhost.messages), 1)
+
+        mail = email.message_from_bytes(self.mailhost.messages[0])
+
+        self.assertTrue(mail.is_multipart())
+
+        self.assertIn(
+            self.email_subject,
+            "".join([i for i in mail.values()]),
+        )
+        self.assertIn(
+            self.email_message,
+            mail.get_payload()[0].get_payload(),
+        )
+
+    def test_email_not_sent_on_submit_if_on_confirm_true(self):
+        self.folder_prenotazioni.notify_on_submit = True
+        self.folder_prenotazioni.notify_on_confirm = True
+        self.folder_prenotazioni.notify_on_confirm_subject = self.email_subject
+        self.folder_prenotazioni.notify_on_confirm_message = self.email_message
+
+        self.assertFalse(self.mailhost.messages)
+
+        booking = self.create_booking()
+
+        api.content.transition(booking, "confirm")
+
+        self.assertEqual(len(self.mailhost.messages), 1)
+
+        mail = email.message_from_bytes(self.mailhost.messages[0])
+
+        self.assertTrue(mail.is_multipart())
+
+        self.assertIn(
+            self.email_subject,
+            "".join([i for i in mail.values()]),
+        )
+        self.assertIn(
+            self.email_message,
+            mail.get_payload()[0].get_payload(),
+        )
+
+    def test_email_send_on_prenotazione_move(self):
+        self.folder_prenotazioni.notify_on_move = True
+        self.folder_prenotazioni.notify_on_move_subject = self.email_subject
+        self.folder_prenotazioni.notify_on_move_message = self.email_message
+
+        self.assertFalse(self.mailhost.messages)
+
+        notify(MovedPrenotazione(self.create_booking()))
+
+        self.assertEqual(len(self.mailhost.messages), 1)
+
+        mail = email.message_from_bytes(self.mailhost.messages[0])
+
+        self.assertTrue(mail.is_multipart())
+
+        self.assertIn(
+            self.email_subject,
+            "".join([i for i in mail.values()]),
+        )
+        self.assertIn(
+            self.email_message,
+            mail.get_payload()[0].get_payload(),
+        )
