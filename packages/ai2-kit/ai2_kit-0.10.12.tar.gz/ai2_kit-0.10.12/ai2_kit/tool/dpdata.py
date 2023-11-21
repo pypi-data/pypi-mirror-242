3f8b31c0ce38eb9a1f28bd6966962c866322911d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 14:03:32 +0000

    Add package ai2-kit/0.10.12/ai2_kit-0.10.12.tar.gz

---
+++ packages/ai2-kit/ai2_kit-0.10.12.tar.gz/ai2_kit-0.10.12/ai2_kit/tool/dpdata.py
@@ -0,0 +1,66 @@
+from ai2_kit.core.util import ensure_dir
+from ai2_kit.core.log import get_logger
+
+import dpdata
+import glob
+
+logger = get_logger(__name__)
+
+class DpdataHelper:
+
+    def __init__(self, label: bool = True):
+        self._systems = []
+        self._label = label
+
+    def read(self, *file_path_or_glob: str,**kwargs):
+        kwargs.setdefault('fmt', 'deepmd/npy')
+        files = []
+        for file_path in file_path_or_glob:
+            files += sorted(glob.glob(file_path, recursive=True)) if '*' in file_path else [file_path]
+
+        if len(files) == 0:
+            raise FileNotFoundError(f'No file found for {file_path_or_glob}')
+        for file in files:
+            self._read(file, **kwargs)
+        return self
+
+    def filter(self, lambda_expr: str):
+        fn = eval(lambda_expr)
+        self._systems = [ system for system in self._systems if fn(system.data)]
+        return self
+
+    @property
+    def merge_write(self):
+        logger.warn('merge_write is deprecated, use write instead')
+        return self.write
+
+    def write(self, out_path: str, fmt='deepmd/npy', merge: bool = True):
+        ensure_dir(out_path)
+        if len(self._systems) == 0:
+            raise ValueError('No data to merge')
+
+        if merge:
+            systems = dpdata.MultiSystems(self._systems[0])
+        else:
+            if self._label:
+                systems = dpdata.LabeledSystem(self._systems[0])
+            else:
+                systems = dpdata.System(self._systems[0])
+
+        for system in self._systems[1:]:
+            systems.append(system)
+
+        if fmt == 'deepmd/npy':
+            systems.to_deepmd_npy(out_path)  # type: ignore
+        elif fmt == 'deepmd/raw':
+            systems.to_deepmd_raw(out_path)  # type: ignore
+        elif fmt == 'deepmd/hdf5':
+            systems.to_deepmd_hdf5(out_path)  # type: ignore
+        else:
+            raise ValueError(f'Unknown fmt {fmt}')
+
+    def _read(self, file: str, **kwargs):
+        if self._label:
+            self._systems.extend(dpdata.LabeledSystem(file, **kwargs))
+        else:
+            self._systems.extend(dpdata.System(file, **kwargs))
