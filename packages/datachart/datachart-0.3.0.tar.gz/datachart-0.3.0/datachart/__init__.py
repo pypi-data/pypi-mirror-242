d4d49916dfae2bd156d386139b50feb50a079e51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 05:03:43 +0000

    Add package datachart/0.3.0/datachart-0.3.0.tar.gz

---
+++ packages/datachart/datachart-0.3.0.tar.gz/datachart-0.3.0/datachart/__init__.py
@@ -0,0 +1,20 @@
+"""
+datachart
+=========
+
+Provides
+  1. Data visualization tools
+  2. Visualization customization
+
+How to use the documentation
+----------------------------
+Documentation is available in two forms: docstrings provided
+with the code and a loose standing reference guide, available
+from `the datachart homepage <https://eriknovak.github.io/datachart>`.
+
+"""
+
+from .charts import line_chart, bar_chart, hist_chart
+from . import config
+from . import schema
+from . import utils
