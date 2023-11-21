e9294eb9d837547bc211a0944f483d4a8365b4a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 16:03:48 +0000

    Add package pollinations.ai/0.0.1a0/pollinations.ai-0.0.1a0.tar.gz

---
+++ packages/pollinations.ai/pollinations.ai-0.0.1a0.tar.gz/pollinations.ai-0.0.1a0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+path_absolute = Path(__file__).parent.absolute()
+
+setup(
+    name="pollinations.ai",
+    version="0.0.1a",
+    description="pollinations.ai package api wrapper",
+    long_description=Path(f"{path_absolute}/README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    url="https://pollinations.ai/",
+    author="tlkr.",
+    author_email="toolkitr.dev@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "pollinations=pollinations.__init__:main",
+        ]
+    },
+    python_requires=">=3.7",    
+    keyphrases=['pollinations', 'pollinations.ai', 'pollinations-ai', 'pollinations_ai'],
+)
