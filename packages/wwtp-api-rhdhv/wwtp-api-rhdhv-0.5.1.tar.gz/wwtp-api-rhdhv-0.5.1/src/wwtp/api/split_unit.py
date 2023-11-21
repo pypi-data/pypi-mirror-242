898cb2768dd8c33e8445aa058939bd5cf6d4dc4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 03:04:31 +0000

    Add package wwtp-api-rhdhv/0.5.1/wwtp-api-rhdhv-0.5.1.tar.gz

---
+++ packages/wwtp-api-rhdhv/wwtp-api-rhdhv-0.5.1.tar.gz/wwtp-api-rhdhv-0.5.1/src/wwtp/api/split_unit.py
@@ -0,0 +1,13 @@
+from abc import ABC
+from typing import TypeVar
+
+from wwtp.api.settings import IScenarioSettings
+
+SS = TypeVar('SS', bound=IScenarioSettings)
+
+
+class SplitUnit(ABC):
+    def __init__(
+            self,
+            scenario_settings: SS,
+    ): self.scenario_settings = scenario_settings
