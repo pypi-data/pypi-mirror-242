10705890bead1da26d8e003a77738ba119e9ee0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 06:04:12 +0000

    Add package csvTofile/0.1/csvTofile-0.1.tar.gz

---
+++ packages/csvTofile/csvTofile-0.1.tar.gz/csvTofile-0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='csvTofile',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={},
+    # Autres métadonnées (auteur, description, licence, etc.)
+    author='Wilkens Badio && Clivince Estinvil',
+    author_email='wbadio693@mail.com',
+    description='Un fichier csv',
+    long_description='Un package Python pour des fichiers csv.',
+    url='https://github.com/Wilkens1-debug/Wilkens_b.git',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords='Data Analyze security',
+)
