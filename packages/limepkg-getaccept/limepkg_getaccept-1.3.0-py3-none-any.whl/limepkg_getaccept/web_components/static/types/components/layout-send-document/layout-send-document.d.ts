9603d7c726a594b707f34d317a973c5387d6ec47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 04:03:09 +0000

    Add package limepkg-getaccept/1.3.0/limepkg_getaccept-1.3.0-py3-none-any.whl

---
+++ packages/limepkg-getaccept/limepkg_getaccept-1.3.0-py3-none-any.whl/limepkg_getaccept/web_components/static/types/components/layout-send-document/layout-send-document.d.ts
@@ -0,0 +1,34 @@
+import { EventEmitter } from '../../stencil-public-runtime';
+import { IDocument } from '../../types/Document';
+import { IListItem } from '../../types/ListItem';
+import { LimeWebComponentPlatform } from '@limetech/lime-web-components';
+import { ISession } from '../../types/Session';
+export declare class LayoutSendDocument {
+  document: IDocument;
+  template: IListItem;
+  limeDocument: IListItem;
+  platform: LimeWebComponentPlatform;
+  session: ISession;
+  setNewDocumentName: EventEmitter<string>;
+  setDocumentValue: EventEmitter<number>;
+  setIsSmsSending: EventEmitter<boolean>;
+  setSmartReminder: EventEmitter<boolean>;
+  private documentName;
+  private value;
+  private smartReminder;
+  private sendLinkBySms;
+  private documentVideo;
+  changeView: EventEmitter;
+  removeVideo: EventEmitter;
+  componentWillLoad(): void;
+  componentDidUpdate(): void;
+  constructor();
+  render(): any[];
+  private fileName;
+  private handleChangeDocumentName;
+  private handleChangeValue;
+  private handleChangeSmartReminder;
+  private handleChangeSendLinkBySms;
+  private handleAddVideo;
+  private handleRemoveVideo;
+}
