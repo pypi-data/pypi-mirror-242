e66ab55ef93f9ce8beba6c0e151ca9c0092c5606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 04:03:42 +0000

    Add package angorapy/0.10.0/angorapy-0.10.0.tar.gz

---
+++ packages/angorapy/angorapy-0.10.0.tar.gz/angorapy-0.10.0/angorapy/__init__.py
@@ -0,0 +1,16 @@
+from angorapy import agent
+from angorapy import common
+from angorapy import models
+from angorapy import tasks
+from angorapy import utilities
+from angorapy import analysis
+
+from angorapy.tasks.registration import make_task
+from angorapy.tasks.registration import make_task as make_env  # For backwards compatibility
+
+from angorapy.agent import Agent
+
+# Builtin models
+from angorapy.models import simple
+from angorapy.models import shadow
+from angorapy.models import shadow_v2
