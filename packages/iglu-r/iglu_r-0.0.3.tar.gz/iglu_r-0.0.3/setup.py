bf3692d1a7be8c08c42b78c933b64bf9a9cee27a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 15:04:19 +0000

    Add package iglu-r/0.0.3/iglu_r-0.0.3.tar.gz

---
+++ packages/iglu-r/iglu_r-0.0.3.tar.gz/iglu_r-0.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Python wrapper of R package `iglu` for continuous glucose monitoring data analysis. Wraps the R functions, thus making them accessible in Python.'
+
+# Setting up
+setup(
+        name='iglu-r', # name must match the folder name where code lives
+        version=VERSION,
+        author='Lizzie Chun, Nathaniel J. Fernandes, Irina Gaynanova',
+        author_email='lizzie_chun1@tamu.edu, njfernandes24@tamu.edu, irinagn@umich.edu', 
+        description=DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['rpy2==3.5.13', 'pandas==2.1.2'], # we've validated functionality with these package versions.     
+        keywords=['iglu', 'Continuous Glucose Monitoring analysis software', 'diabetes'],
+        include_package_data=True
+)
