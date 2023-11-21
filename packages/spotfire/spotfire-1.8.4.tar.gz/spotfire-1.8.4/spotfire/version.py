3aae5b91232e7a26d066fa800c4bfd1367ed6507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 14:04:06 +0000

    Add package spotfire/1.8.4/spotfire-1.8.4.tar.gz

---
+++ packages/spotfire/spotfire-1.8.4.tar.gz/spotfire-1.8.4/spotfire/version.py
@@ -0,0 +1,6 @@
+# Copyright © 2021. TIBCO Software Inc.
+# This file is subject to the license terms contained
+# in the license file that is distributed with this file.
+
+# pylint: skip-file
+__version__="1.8.4"
