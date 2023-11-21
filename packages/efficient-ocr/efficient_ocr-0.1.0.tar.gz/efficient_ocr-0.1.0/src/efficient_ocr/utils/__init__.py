f2f560fe85317f609e5d1051c6f68acc8af2678b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 04:03:41 +0000

    Add package efficient-ocr/0.1.0/efficient_ocr-0.1.0.tar.gz

---
+++ packages/efficient-ocr/efficient_ocr-0.1.0.tar.gz/efficient_ocr-0.1.0/src/efficient_ocr/utils/__init__.py
@@ -0,0 +1,15 @@
+from .ops import yolov8_non_max_suppression, yolov5_non_max_suppression, letterbox, get_onnx_input_name, create_batches, make_coco_from_effocr_result, visualize_effocr_result
+from .ops import DEFAULT_MEAN, DEFAULT_STD
+from .onnx import initialize_onnx_model
+from .text import en_preprocess
+from .transforms import get_transform
+from .yolo import create_yolo_training_data, create_yolo_yaml
+from .misc import dictmerge, get_path, dir_is_empty, all_but_last_in_path, last_in_path
+from .default_config import DEFAULT_CONFIG
+
+from .recognition.custom_schedulers import *
+from .recognition.transforms import *
+from .recognition.datasets import *
+from .recognition.samplers import *
+from .recognition.synth_crops import *
+from .recognition.encoders import *
