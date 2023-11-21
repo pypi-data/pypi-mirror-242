0710f3898ec27a8b49c851812ec43037951380ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 04:03:36 +0000

    Add package onec-checkgrammar/0.5.4/onec_checkgrammar-0.5.4-py3-none-any.whl

---
+++ packages/onec-checkgrammar/onec_checkgrammar-0.5.4-py3-none-any.whl/kontur/checkgrammar/grammar.py
@@ -0,0 +1,241 @@
+import json
+import os
+import re
+import time
+from datetime import datetime
+from functools import lru_cache
+from typing import List
+
+import junit_xml
+from pyaspeller import YandexSpeller
+from tqdm import tqdm
+from wasabi import Printer
+
+from kontur.checkgrammar import parse
+
+ya_speller = YandexSpeller(lang="ru", ignore_urls=True)
+
+
+def all_lower(arr: List[str]) -> List[str]:
+    return [x.strip().lower() for x in arr]
+
+
+class Error:
+    def __init__(self, form: str, element: str, text: str, errors: List[str]):
+        self.form = form
+        self.element = element
+        self.text = text
+        self.errors = errors
+
+
+class GrammarError:
+    def __init__(self, word: str, variants: List[str]):
+        self.word = word
+        self.variants = variants
+
+    def __str__(self):
+        return f"{self.word} -> {self.variants}"
+
+
+def has_cyrillic(text):
+    return bool(re.search("[а-яА-Я]", text))
+
+
+@lru_cache(maxsize=2048)
+def checkYaSpeller(text: str, our_dict=tuple()) -> List[GrammarError]:
+    # Подготовить текст. Спеллер плохо проверяет слова с цифрами
+    result: List[GrammarError] = []
+    if not has_cyrillic(text):
+        return result
+
+    check = ya_speller.spell(text)
+    for res in check:
+        if (
+            res["s"]
+            and res["word"].lower() not in our_dict
+            and res["word"].lower() not in all_lower(res["s"])
+        ):  # Есть варианты замены
+            result.append(GrammarError(res["word"], res["s"]))
+
+    return result
+
+
+class GrammarCheck:
+    def __init__(self):
+        self._src = []
+        self._dict = []
+        self._result = None
+        self._skip = ""
+
+    def update_dict_from_file(self, path_to_dict: str):
+        """
+        Дополняет словарь исключений
+            :param path_to_dict: путь до файла со словарем
+        """
+        full_path = os.path.abspath(path_to_dict)
+        assert os.path.exists(full_path), f"Не найден словарь {full_path}"
+
+        with open(path_to_dict, "r", encoding="utf-8") as f:
+            our_dict = all_lower(f.readlines())
+
+        self._dict += our_dict
+
+    def update_dict_from_bsl(self, bsl_settings="./.bsl-language-server.json"):
+        """
+        Дополняет словарь исключений из настроек bsl
+            :param bsl_settings: путь до настроек .bsl-language-server.json
+        """
+        full_path = os.path.abspath(bsl_settings)
+        assert os.path.exists(
+            full_path
+        ), f"Не найдены настройки bsl language server {full_path}"
+
+        with open(full_path, "r", encoding="utf-8") as json_file:
+            data = json.load(json_file)
+
+        user_words_to_ignore = (
+            data.get("diagnostics", {})
+            .get("parameters", {})
+            .get("Typo", {})
+            .get("userWordsToIgnore", "")
+        )
+        assert (
+            user_words_to_ignore
+        ), "Не найден параметр diagnostics.parameters.Typo.userWordsToIgnore"
+
+        our_dict = [x.strip().lower() for x in user_words_to_ignore.split(",")]
+
+        self._dict += our_dict
+
+    def add_src(self, src: str):
+        """
+        Добавить каталог с исходниками для обработки
+            :param src: путь до каталога исходников
+        """
+        full_path = os.path.abspath(src)
+        assert os.path.exists(full_path), f"Не найден каталог исходников {full_path}"
+
+        self._src.append(src)
+
+    def add_skip_pattern(self, pattern: str):
+        """
+        Добавить паттерн для исключения некоторых форм
+
+        :param pattern: паттерн в формате glob
+        """
+        assert pattern, "Паттерн не должен быть пустым"
+        self._skip = f"[!{pattern}]"
+
+    def run(self):
+        """
+        Запуск проверки орфографии
+        """
+        assert self._src, "Не указаны каталоги для проверки"
+
+        result = {}
+        our_dict = tuple(self._dict)
+
+        start_time = datetime.now()
+        for src in self._src:
+            objects = parse.parseSrc(src, self._skip)
+
+            for obj, elements in objects.items():
+                if len(self._src) == 1:
+                    key = obj
+                else:
+                    key = f"{src}->{obj}"
+                for element, text in tqdm(elements.items(), desc=obj):
+                    if len(text) <= 3:
+                        continue
+
+                    text_to_check = " ".join(
+                        [re.sub(r"[0-9]+", "", x) for x in text.split(" ")]
+                    )
+
+                    errors = checkYaSpeller(text_to_check, our_dict)
+
+                    if errors:
+                        if obj not in result:
+                            result[key] = []
+                        result[key].append(Error(obj, element, text, errors))
+        msg = Printer()
+        msg.divider("СТАТИСТИКА", char="*")
+        msg.info(f"Время {datetime.now() - start_time}")
+        msg.info(f"Обнаружено ошибок {sum([len(x) for x in result.items()])}")
+        msg.info(f"{checkYaSpeller.cache_info()}")
+        msg.divider("=", char="*")
+
+        self._result = result
+
+    def dump_junit(self, path_to_xml: str):
+        """
+        Формирует отчет в формате JUnit
+            :param path_to_xml: Путь до выгружаемого отчета
+        """
+
+        assert self._result is not None, "Проверка еще не была выполнена"
+
+        full_path = os.path.abspath(path_to_xml)
+        ts = []
+        for obj, details in self._result.items():
+            test_cases = []
+            for data in details:
+                test_case = junit_xml.TestCase(
+                    data.element,
+                    classname=obj,
+                    stderr=f"Возможно опечатка в тексте: {data.text}",
+                )
+
+                for error in data.errors:
+                    test_case.add_failure_info(error, failure_type="WARNING")
+                test_cases.append(test_case)
+
+            ts.append(junit_xml.TestSuite(obj, test_cases))
+
+        with open(full_path, "w", encoding="utf-8") as f:
+            junit_xml.TestSuite.to_file(f, ts, encoding="utf-8")
+
+    def print(self):
+        """
+        Вывод на экран результатов проверки
+        """
+        assert self._result is not None, "Проверка еще не была выполнена"
+        msg = Printer()
+        for obj, details in self._result.items():
+            msg.divider(obj)
+            for data in details:
+                msg.warn(data.element)
+                for error in data.errors:
+                    msg.fail(error)
+
+    def dump_dict(self, path: str):
+        """
+        Выгрузка отчета в файл. Чтобы было проще готовить файл исключений
+            :param path: путь для выгрузки отчета
+        """
+        full_path = os.path.abspath(path)
+        result = set()
+        for obj, details in self._result.items():
+            for data in details:
+                for error in data.errors:
+                    result.add(error.word.lower())
+
+        with open(full_path, "w", encoding="utf-8") as f:
+            for s in result:
+                f.write(s + "\n")
+            timeout = 30
+            while not os.path.exists(full_path):
+                time.sleep(1)
+                timeout -= 1
+                if timeout == 0:
+                    print("Не удалось сохранить файл")
+
+    @property
+    def has_errors(self) -> bool:
+        """
+        Были ошибки при проверке
+            :return: bool
+        """
+        assert self._result is not None, "Проверка еще не была выполнена"
+
+        return bool(self._result)
---
+++ packages/onec-checkgrammar/onec_checkgrammar-0.5.4-py3-none-any.whl/kontur/checkgrammar/parse.py
@@ -0,0 +1,137 @@
+import glob
+import os
+import re
+from typing import Dict, Union
+from xml.etree import ElementTree as Et
+
+namespaces = {
+    "logform": "http://v8.1c.ru/8.3/xcf/logform",
+    "core": "http://v8.1c.ru/8.1/data/core",
+    "xr": "http://v8.1c.ru/8.3/xcf/readable",
+}
+
+
+def getRuContent(obj: Et.Element) -> Union[str, None]:
+    """
+    Получает русское наименование элемента
+        :param obj: Параметр элемента формы: Заголовок, Подсказка, Расширенная подсказка
+        :return: Строку или None если не нашел
+    """
+    if obj is None:
+        return None
+    elements = obj.findall("core:item", namespaces)
+    for element in elements:
+        lang = element.find("core:lang", namespaces)
+        if lang is not None and lang.text == "ru":
+            content = element.find("core:content", namespaces)
+            if content is not None:
+                return content.text
+
+    return None
+
+
+def getChildItems(obj: Et.Element) -> Dict[str, str]:
+    """
+    Перебирает подчиненные элементы: кнопки, надписи, поля ввода и тд. Выделяет их заголовок, подсказку или
+    расширенную подсказку. Рекурсивно вызывает обработку вложенных элементов.
+    Например: Страница(Заголовок) - Командная панель - Кнопки(Заголовок, Подсказки)
+        :param obj: Элемент формы
+        :return: Словарь НазваниеЭлемента.Поле - Текст
+    """
+    result: Dict[str, str] = {}
+    if obj is None:
+        return result
+    all_elements = obj.find("logform:ChildItems", namespaces)
+    if all_elements is None:
+        return result
+    for element in all_elements:
+        name = element.attrib.get("name")
+        tag = element.tag[element.tag.find("}") + 1 :]
+        title = element.find("logform:Title", namespaces)
+        if title is not None:
+            # Проверим виден ли заголовок
+            title_location = element.find("logform:TitleLocation", namespaces)
+            if (
+                tag in ("InputField", "RadioButtonField", "CheckBoxField")
+                and title_location is not None
+                and title_location.text == "None"
+            ):
+                pass
+            elif tag == "PictureDecoration":
+                pass
+            else:
+                content = getRuContent(title)
+                if content is None:
+                    continue
+                if title.attrib.get("formatted") == "true":
+                    content = re.sub("(\<.*?\>)", "", content)
+                result[f"{name}.Заголовок"] = content
+
+        tooltip = element.find("logform:ToolTip", namespaces)
+        if tooltip is not None:
+            content = getRuContent(tooltip)
+            if content is not None:
+                result[f"{name}.Подсказка"] = content
+
+        ext_tooltip = element.find("logform:ExtendedTooltip/logform:Title", namespaces)
+        if ext_tooltip is not None:
+            content = getRuContent(ext_tooltip)
+            if content is not None:
+                result[f"{name}.РасшПодсказка"] = content
+
+        choice_list = element.findall("logform:ChoiceList/*", namespaces)
+        if choice_list:
+            num = 0
+            for choice in choice_list:
+                choice_value = choice.find("xr:Value", namespaces)
+                if choice_value is None:
+                    continue
+                presentation = choice_value.find("logform:Presentation", namespaces)
+                value = choice_value.find("logform:Value", namespaces)
+                if presentation is not None and presentation.text is not None:
+                    result[f"{name}.List{num}.Представление"] = presentation.text
+                elif value is not None and value.text is not None:
+                    result[f"{name}.List{num}.Значение"] = value.text
+                num += 1
+
+        result.update(getChildItems(element))
+    return result
+
+
+def parseForm(path_to_form: str) -> Dict[str, str]:
+    """
+    Разбор формы на элементы
+            :param path_to_form: путь до файла формы
+            :return: dict с элеметами формы и их текстами
+    """
+    form = Et.parse(path_to_form).getroot()
+
+    bar = form.find("logform:AutoCommandBar", namespaces)
+    buttons: Dict[str, str] = {}
+    if bar is not None:
+        buttons = getChildItems(bar)
+
+    elements = getChildItems(form)
+
+    return dict(elements, **buttons)
+
+
+def parseSrc(path_to_src: str, skip_pattern="") -> Dict[str, Dict[str, str]]:
+    """
+    Разбор каталога исходников на формы и элементы
+        :param path_to_src: путь до каталога исходников
+        :param skip_pattern: паттерн для пропуска форм
+        :return: dict с формами и элементами
+    """
+    full_path = os.path.abspath(path_to_src)
+    files = glob.glob(
+        f"{full_path}/**/Forms/{skip_pattern}*/Ext/Form.xml", recursive=True
+    )
+    result: Dict[str, Dict[str, str]] = {}
+    for form_file in files:
+        temp = form_file.split(os.path.sep)
+        obj = temp[-5]
+        form = temp[-3]
+        result[f"{obj}.{form}"] = parseForm(form_file)
+
+    return result
