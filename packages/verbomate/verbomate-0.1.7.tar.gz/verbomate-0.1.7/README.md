cb68fc376d7f79f665e01fd7fdf2e7c544448281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 06:03:36 +0000

    Add package verbomate/0.1.7/verbomate-0.1.7.tar.gz

---
+++ packages/verbomate/verbomate-0.1.7.tar.gz/verbomate-0.1.7/README.md
@@ -0,0 +1,15 @@
+# Verbomate
+Generate and run python from terminal.
+
+## Examples
+
+- `verbomate "create hello.txt"`
+
+- `verbomate "kill port 3000"`
+
+- `verbomate "append 01_, 02_, 03_ etc to all files in this directory"`
+
+## Installation
+
+1. Install: `pip3 install verbomate`
+2. Set [OpenAI Key](https://platform.openai.com/api-keys): `verbomate -key <API_KEY>`
---
+++ packages/verbomate/verbomate-0.1.7.tar.gz/verbomate-0.1.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='verbomate',  # Replace with your chosen package name
+    version='0.1.7',  # Initial version number
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'verbomate=verbomate.main:main',
+        ],
+    },
+    # Additional metadata like author, description, license, classifiers, etc.
+    author='Gabriel Gordbegli',
+    author_email='gabe@gordbegli.com',
+    description='Generate and run python scripts from your command line',
+    license='MIT',  # Choose the appropriate license
+    url='https://github.com/gordbegli/verbomate',  # Optional: Link to your project's repository
+    install_requires=[
+        'requests',  
+        'openai', 
+        'argparse',
+    ],
+)
+
