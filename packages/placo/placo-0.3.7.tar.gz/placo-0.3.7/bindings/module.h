a26c11d3fb7a8cdc0c218eac250bfcb0786b1a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 07:03:02 +0000

    Add package placo/0.3.7/placo-0.3.7.tar.gz

---
+++ packages/placo/placo-0.3.7.tar.gz/placo-0.3.7/bindings/module.h
@@ -0,0 +1,13 @@
+#pragma once
+
+void exposeTools();
+void exposeDynamics();
+void exposeEigen();
+void exposeFootsteps();
+void exposeProblem();
+void exposeRobotWrapper();
+void exposeParameters();
+void exposeKinematics();
+void exposeWalkPatternGenerator();
+void exposeTrajectory();
+void exposeRegistry();
---
+++ packages/placo/placo-0.3.7.tar.gz/placo-0.3.7/src/placo/trajectory/foot_trajectory.cpp
@@ -0,0 +1,8 @@
+#include "placo/trajectory/foot_trajectory.h"
+
+namespace placo::trajectory
+{
+FootTrajectory::~FootTrajectory()
+{
+}
+}  // namespace placo::trajectory
