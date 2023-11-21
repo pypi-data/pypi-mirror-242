c4c631cb5fda42d4a3abd49085fa6c7f1dd1a1b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 05:03:49 +0000

    Add package pyaslengine/0.1.2/pyaslengine-0.1.2-py3-none-any.whl

---
+++ packages/pyaslengine/pyaslengine-0.1.2-py3-none-any.whl/pyaslengine/log.py
@@ -0,0 +1,16 @@
+"""pyaslengine.log"""
+
+import logging
+
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.propagate = False  # prevents double logging if root logger is configured
+    return logger
