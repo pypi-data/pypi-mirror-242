a2e5d439f7bb4731df1c11bfd1dff660b670f614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 03:05:43 +0000

    Add package atopy/0.6.0/atopy-0.6.0-py3-none-any.whl

---
+++ packages/atopy/atopy-0.6.0-py3-none-any.whl/atopy/pydantic.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BModel = BaseModel
+
+
+class BSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
