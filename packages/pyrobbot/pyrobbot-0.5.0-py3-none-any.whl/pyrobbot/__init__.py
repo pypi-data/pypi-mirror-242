e4653f03abc73e311610505de00e6e5126b3b126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 16:04:11 +0000

    Add package pyrobbot/0.5.0/pyrobbot-0.5.0-py3-none-any.whl

---
+++ packages/pyrobbot/pyrobbot-0.5.0-py3-none-any.whl/pyrobbot/__init__.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+"""Unnoficial OpenAI API UI and CLI tool."""
+import hashlib
+import os
+import sys
+import tempfile
+import uuid
+from dataclasses import dataclass
+from importlib.metadata import metadata, version
+from pathlib import Path
+
+import ipinfo
+from loguru import logger
+from openai import OpenAI, OpenAIError
+
+logger.remove()
+logger.add(
+    sys.stderr,
+    level=os.environ.get("LOGLEVEL", os.environ.get("LOGURU_LEVEL", "INFO")),
+)
+
+
+@dataclass
+class GeneralDefinitions:
+    """General definitions for the package."""
+
+    # Keep track of the OpenAI API key available to the package at initialization
+    SYSTEM_ENV_OPENAI_API_KEY: str = None
+
+    # Main package info
+    RUN_ID = uuid.uuid4().hex
+    PACKAGE_NAME = __name__
+    VERSION = version(__name__)
+    PACKAGE_DESCRIPTION = metadata(__name__)["Summary"]
+
+    # Main package directories
+    PACKAGE_DIRECTORY = Path(__file__).parent
+    _PACKAGE_TMPDIR = tempfile.TemporaryDirectory()
+    PACKAGE_TMPDIR = Path(_PACKAGE_TMPDIR.name)
+
+    # Constants related to the app
+    APP_NAME = "pyRobBot"
+    APP_DIR = PACKAGE_DIRECTORY / "app"
+    APP_PATH = APP_DIR / "app.py"
+    PARSED_ARGS_FILE = PACKAGE_TMPDIR / f"parsed_args_{RUN_ID}.pkl"
+
+    # Location info
+    IPINFO = ipinfo.getHandler().getDetails().all
+
+    @staticmethod
+    def openai_key_hash():
+        """Return a hash of the OpenAI API key."""
+        try:
+            client = OpenAI()
+        except OpenAIError:
+            api_key = None
+        else:
+            api_key = client.api_key
+        if api_key is None:
+            return "demo"
+        return hashlib.sha256(api_key.encode("utf-8")).hexdigest()
+
+    @property
+    def package_cache_directory(self):
+        """Return the path to the package's general cache directory."""
+        return Path.home() / ".cache" / self.PACKAGE_NAME
+
+    @property
+    def current_user_cache_dir(self):
+        """Return the directory where cache info for the current user is stored."""
+        return self.package_cache_directory / f"user_{self.openai_key_hash()}"
+
+    @property
+    def chats_storage_dir(self):
+        """Return the directory where the current user's chats are stored."""
+        return self.current_user_cache_dir / "chats"
+
+
+GeneralConstants = GeneralDefinitions(
+    SYSTEM_ENV_OPENAI_API_KEY=os.environ.get("OPENAI_API_KEY")
+)
+
+# Initialize the OpenAI API client
---
+++ packages/pyrobbot/pyrobbot-0.5.0-py3-none-any.whl/pyrobbot/command_definitions.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""Commands supported by the package's script."""
+import subprocess
+
+from loguru import logger
+
+from . import GeneralConstants
+from .chat import Chat
+from .chat_configs import ChatOptions
+from .openai_utils import CannotConnectToApiError
+from .voice_chat import VoiceChat
+
+
+def voice_chat(args):
+    """Run the chat on the terminal."""
+    chat = VoiceChat.from_cli_args(cli_args=args)
+    try:
+        chat.start()
+    except CannotConnectToApiError as error:
+        logger.error("API connection problems: {}\nExiting.", error)
+        raise SystemExit(1) from error
+
+
+def browser_chat(args):
+    """Run the chat on the browser."""
+    ChatOptions.from_cli_args(args).export(fpath=GeneralConstants.PARSED_ARGS_FILE)
+    try:
+        subprocess.run(
+            [  # noqa: S603, S607
+                "streamlit",
+                "run",
+                GeneralConstants.APP_PATH.as_posix(),
+                "--",
+                GeneralConstants.PARSED_ARGS_FILE.as_posix(),
+            ],
+            cwd=GeneralConstants.APP_DIR.as_posix(),
+            check=True,
+        )
+    except (KeyboardInterrupt, EOFError):
+        logger.info("Exiting.")
+
+
+def terminal_chat(args):
+    """Run the chat on the terminal."""
+    chat = Chat.from_cli_args(cli_args=args)
+    chat.start()
+    if args.report_accounting_when_done:
+        chat.report_token_usage(report_general=True)
+
+
+def accounting_report(args):
+    """Show the accumulated costs of the chat and exit."""
+    chat = Chat.from_cli_args(cli_args=args)
+    # Prevent chat from creating entry in the cache directory
+    chat.private_mode = True
+    chat.report_token_usage(report_general=True, report_current_chat=False)
