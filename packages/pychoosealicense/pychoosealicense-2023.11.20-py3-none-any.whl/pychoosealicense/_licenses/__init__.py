511f982eb2d65d6ffb503a729347c4bbf44995f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 06:03:26 +0000

    Add package pychoosealicense/2023.11.20/pychoosealicense-2023.11.20-py3-none-any.whl

---
+++ packages/pychoosealicense/pychoosealicense-2023.11.20-py3-none-any.whl/pychoosealicense/_licenses/__init__.py
@@ -0,0 +1,17 @@
+"""
+License metadata used by ``pychoosealicense``.
+"""
+
+CHOOSEALICENSE_COMMIT_INFO = """
+commit 005b3dfd323c1e57d0fd0eab7c41b16126f6eb93
+Merge: 080f59b 510b7f2
+Author: Mike Linksvayer <mlinksva@github.com>
+Date:   Mon Oct 30 18:23:18 2023 -0700
+
+    Merge pull request #1162 from vorburger/patch-1
+
+    Fix Code Search link in CONTRIBUTING
+"""
+
+CHOOSEALICENSE_COMMIT_HASH = "005b3dfd323c1e57d0fd0eab7c41b16126f6eb93"
+CHOOSEALICENSE_COMMIT_SHORTHASH = "005b3df"
