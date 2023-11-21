38fb28c8ca45fb2f30edb2c4b32dbe2c4d71a864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 03:04:05 +0000

    Add package spaceone-dashboard/2.0.dev1/spaceone-dashboard-2.0.dev1.tar.gz

---
+++ packages/spaceone-dashboard/spaceone-dashboard-2.0.dev1.tar.gz/spaceone-dashboard-2.0.dev1/spaceone/dashboard/service/__init__.py
@@ -0,0 +1,3 @@
+from spaceone.dashboard.service.domain_dashboard_service import DomainDashboardService
+from spaceone.dashboard.service.project_dashboard_service import ProjectDashboardService
+from spaceone.dashboard.service.custom_widget_service import CustomWidgetService
