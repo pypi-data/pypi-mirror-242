5ac6a024996e06c109298bf284811d057b5da5ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 15:04:37 +0000

    Add package deepepochs/0.4.19/deepepochs-0.4.19-py3-none-any.whl

---
+++ packages/deepepochs/deepepochs-0.4.19-py3-none-any.whl/deepepochs/__init__.py
@@ -0,0 +1,14 @@
+"""
+@author: hitlic
+TODO:
+    混合精度
+    分布式
+"""
+__version__ = '0.4.19'
+
+from .loops import *
+from .trainer import Trainer, TrainerBase, EpochTask
+from .tools import *
+from .callbacks import *
+from .optimizer import Optimizer, Optimizers
+from .patches import PatchBase, ValuePatch, TensorPatch, MeanPatch, ConfusionPatch
---
+++ packages/deepepochs/deepepochs-0.4.19-py3-none-any.whl/deepepochs/callbacks/__init__.py
@@ -0,0 +1,6 @@
+from .callback import Callback, CallbackPool, DefaultCallback, CallbackException
+from .check import CheckCallback
+from .log import LogCallback
+from .lr_find import LRFindCallback
+from .analyze import AnalyzeCallback
+from .interprete import InterpreteCallback
