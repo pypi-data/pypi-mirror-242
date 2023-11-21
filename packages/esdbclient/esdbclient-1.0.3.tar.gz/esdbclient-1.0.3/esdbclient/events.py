12d72385caadf6fba698758b1db70c9408b86748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 15:03:55 +0000

    Add package esdbclient/1.0.3/esdbclient-1.0.3.tar.gz

---
+++ packages/esdbclient/esdbclient-1.0.3.tar.gz/esdbclient-1.0.3/esdbclient/events.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from dataclasses import dataclass, field
+from typing import Optional
+from uuid import UUID, uuid4
+
+from typing_extensions import Literal
+
+ContentType = Literal["application/json", "application/octet-stream"]
+
+
+@dataclass(frozen=True)
+class NewEvent:
+    """
+    Encapsulates event data to be recorded in EventStoreDB.
+    """
+
+    type: str
+    data: bytes
+    metadata: bytes = b""
+    content_type: ContentType = "application/json"
+    id: UUID = field(default_factory=uuid4)
+
+
+@dataclass(frozen=True)
+class RecordedEvent:
+    """
+    Encapsulates event data that has been recorded in EventStoreDB.
+    """
+
+    type: str
+    data: bytes
+    metadata: bytes
+    content_type: str
+    id: UUID
+    stream_name: str
+    stream_position: int
+    commit_position: Optional[int]
+    retry_count: Optional[int] = None
+
+
+class Checkpoint(RecordedEvent):
+    CHECKPOINT_ID = UUID("00000000-0000-0000-0000-000000000000")
+
+    def __init__(self, commit_position: int):
+        super().__init__(
+            id=self.CHECKPOINT_ID,
+            type="",
+            data=b"",
+            content_type="",
+            metadata=b"",
+            stream_name="",
+            stream_position=0,
+            commit_position=commit_position,
+        )
