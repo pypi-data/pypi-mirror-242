336335c86417b9afd858fc092f7b7922f84320ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 06:04:11 +0000

    Add package Misago/0.38.1/Misago-0.38.1-py3-none-any.whl

---
+++ packages/Misago/Misago-0.38.1-py3-none-any.whl/misago/categories/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.urls import reverse
+
+from .serializers import CategoryWithPosterSerializer as CategorySerializer
+from .utils import get_categories_tree
+
+
+def categories(request):
+    categories_tree = get_categories_tree(request, join_posters=True)
+
+    request.frontend_context.update(
+        {
+            "CATEGORIES": CategorySerializer(
+                categories_tree,
+                context={"settings": request.settings},
+                many=True,
+            ).data,
+            "CATEGORIES_API": reverse("misago:api:category-list"),
+        }
+    )
+
+    return render(
+        request, "misago/categories/list.html", {"categories": categories_tree}
+    )
---
+++ packages/Misago/Misago-0.38.1-py3-none-any.whl/misago/hooks.py
@@ -0,0 +1,14 @@
+apipatterns = []
+urlpatterns = []
+context_processors = []
+
+new_registrations_validators = []
+
+oauth2_user_data_filters = []
+oauth2_validators = []
+
+post_search_filters = []
+post_validators = []
+
+markdown_extensions = []
+parsing_result_processors = []
