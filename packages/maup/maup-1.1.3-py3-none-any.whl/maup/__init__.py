2ba2089a1f4a9b144bb2d8d2f8be0fc4cad990b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 15:03:57 +0000

    Add package maup/1.1.3/maup-1.1.3-py3-none-any.whl

---
+++ packages/maup/maup-1.1.3-py3-none-any.whl/maup/__init__.py
@@ -0,0 +1,34 @@
+import geopandas
+from .adjacencies import adjacencies
+from .assign import assign
+from .indexed_geometries import IndexedGeometries
+from .intersections import intersections, prorate
+from .repair import close_gaps, autorepair, snap_to_grid, crop_to, doctor, resolve_overlaps
+from .normalize import normalize
+from .progress_bar import progress
+
+
+# warn about https://github.com/geopandas/geopandas/issues/2199
+if geopandas.options.use_pygeos:
+    raise ImportError(
+        "GerryChain cannot use GeoPandas when PyGeos is enabled. Disable or "
+        "uninstall PyGeos. You can disable PyGeos in GeoPandas by setting "
+        "`geopandas.options.use_pygeos = False` before importing your shapefile."
+    )
+
+__version__ = "1.1.3"
+__all__ = [
+    "adjacencies",
+    "assign",
+    "IndexedGeometries",
+    "intersections",
+    "prorate",
+    "close_gaps",
+    "autorepair",
+    "resolve_overlaps",
+    "snap_to_grid",
+    "crop_to",
+    "doctor",
+    "normalize",
+    "progress"
+]
