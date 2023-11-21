2aadbbdecbf2e277e05068f830624490735ec2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 14:04:09 +0000

    Add package orb-billing/1.31.1/orb_billing-1.31.1-py3-none-any.whl

---
+++ packages/orb-billing/orb_billing-1.31.1-py3-none-any.whl/orb/types/metric_fetch_response.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from typing import Dict, Optional
+from typing_extensions import Literal
+
+from .item import Item
+from .._models import BaseModel
+
+__all__ = ["MetricFetchResponse"]
+
+
+class MetricFetchResponse(BaseModel):
+    id: str
+
+    description: Optional[str]
+
+    item: Item
+    """The Item resource represents a sellable product or good.
+
+    Items are associated with all line items, billable metrics, and prices and are
+    used for defining external sync behavior for invoices and tax calculation
+    purposes.
+    """
+
+    metadata: Dict[str, str]
+    """User specified key-value pairs.
+
+    If not provided, this defaults to an empty dictionary.
+    """
+
+    name: str
+
+    status: Literal["active", "draft", "archived"]
