b5bde0b4a75c17dc78660f22d4ec97fca6339905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 05:03:56 +0000

    Add package mcbcdc/0.3/mcbcdc-0.3.tar.gz

---
+++ packages/mcbcdc/mcbcdc-0.3.tar.gz/mcbcdc-0.3/setup.py
@@ -0,0 +1,16 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mcbcdc',
+    version='0.3',  # Update the version number
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        # list your dependencies here
+    ],
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='Description of your library',
+)
