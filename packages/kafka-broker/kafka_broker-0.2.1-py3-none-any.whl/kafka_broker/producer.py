601430d8cbb5ddf451f669baedcda87a7436e56a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 03:04:22 +0000

    Add package kafka-broker/0.2.1/kafka_broker-0.2.1-py3-none-any.whl

---
+++ packages/kafka-broker/kafka_broker-0.2.1-py3-none-any.whl/kafka_broker/producer.py
@@ -0,0 +1,37 @@
+import logging
+from typing import Any, Callable
+from confluent_kafka import Producer
+
+from .classes import EventObject
+
+
+def default_callback(err, msg):
+    if err:
+        logging.error("Message failed delivery: {}".format(err))
+
+
+def produce(
+    config: dict,
+    logger: logging.Logger,
+    topic: str,
+    event_object: EventObject,
+    callback: Callable = default_callback
+):
+    """Produce an event to the kafka message queue."""
+    kafka_config = config["kafka.default"]
+    kafka_config.update(config["kafka.producer"])
+
+    producer = Producer(kafka_config)
+
+    event_object.add_audit_log(config["general"]["current_location"])
+    event_dict = event_object.encode()
+
+    producer.produce(
+        topic,
+        key=event_dict["correlation_id"],
+        value=event_dict,
+        callback=callback,
+    )
+
+    producer.poll(100)
+    producer.flush()
