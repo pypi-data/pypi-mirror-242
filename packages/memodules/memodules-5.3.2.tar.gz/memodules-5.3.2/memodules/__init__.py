0264be49cbd6d1c625cd513f5b142605146f1d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 16:03:36 +0000

    Add package memodules/5.3.2/memodules-5.3.2.tar.gz

---
+++ packages/memodules/memodules-5.3.2.tar.gz/memodules-5.3.2/memodules/__init__.py
@@ -0,0 +1,20 @@
+"""
+This package is designed for creator-use only
+"""
+"""import e_typing
+from .log import *
+from .cslapp import *
+from .inicon import (
+    Encoding,
+    AbsTrue,
+    AbsFalse,
+    AbsTrueFalse,
+    INIsNotFound,
+    INI,
+    str_bool,
+    StrBool,
+)
+from .error_lib import *
+from .xl_for_python import *
+from .cr_exchange_lib import *
+from .alphabet_to_integer import *"""
