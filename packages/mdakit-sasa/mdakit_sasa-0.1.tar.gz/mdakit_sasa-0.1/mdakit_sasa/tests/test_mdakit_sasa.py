be34a41eacfcdd2ed9b54da3b4fb2c95a6bf607c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 05:03:14 +0000

    Add package mdakit-sasa/0.1/mdakit_sasa-0.1.tar.gz

---
+++ packages/mdakit-sasa/mdakit_sasa-0.1.tar.gz/mdakit_sasa-0.1/mdakit_sasa/tests/test_mdakit_sasa.py
@@ -0,0 +1,12 @@
+"""
+Unit and regression test for the mdakit_sasa package.
+"""
+
+import mdakit_sasa
+import pytest
+import sys
+
+
+def test_mdakit_sasa_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "mdakit_sasa" in sys.modules
