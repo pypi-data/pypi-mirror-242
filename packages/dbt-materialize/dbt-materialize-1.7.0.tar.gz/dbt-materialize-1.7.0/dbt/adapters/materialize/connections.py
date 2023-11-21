230d581d9a6c94f6f36c370da659e0b6427dc4f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 03:04:41 +0000

    Add package dbt-materialize/1.7.0/dbt-materialize-1.7.0.tar.gz

---
+++ packages/dbt-materialize/dbt-materialize-1.7.0.tar.gz/dbt-materialize-1.7.0/dbt/adapters/materialize/connections.py
@@ -0,0 +1,135 @@
+# Copyright 2020 Josh Wills. All rights reserved.
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License in the LICENSE file at the
+# root of this repository, or online at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dataclasses import dataclass
+from typing import Optional
+
+import psycopg2
+
+import dbt.exceptions
+from dbt.adapters.postgres import PostgresConnectionManager, PostgresCredentials
+from dbt.events import AdapterLogger
+from dbt.semver import versions_compatible
+
+# If you bump this version, bump it in README.md too.
+SUPPORTED_MATERIALIZE_VERSIONS = ">=0.68.0"
+
+logger = AdapterLogger("Materialize")
+
+
+@dataclass
+class MaterializeCredentials(PostgresCredentials):
+    cluster: Optional[str] = "default"
+
+    @property
+    def type(self):
+        return "materialize"
+
+    def _connection_keys(self):
+        return (
+            "host",
+            "port",
+            "user",
+            "database",
+            "schema",
+            "cluster",
+            "sslmode",
+            "keepalives_idle",
+            "connect_timeout",
+            "search_path",
+            "retries",
+        )
+
+
+class MaterializeConnectionManager(PostgresConnectionManager):
+    TYPE = "materialize"
+
+    @classmethod
+    def open(cls, connection):
+        connection = super().open(connection)
+
+        # Prevents psycopg connection from automatically opening transactions.
+        # More info: https://www.psycopg.org/docs/usage.html#transactions-control
+        connection.handle.autocommit = True
+
+        cursor = connection.handle.cursor()
+
+        # Check for the current DB version using the "mz_introspection" cluster in case "default"
+        # doesn't exist.
+        cursor.execute("SHOW mz_version")
+        mz_version = cursor.fetchone()[0].split()[0].strip("v")
+
+        if not versions_compatible(mz_version, SUPPORTED_MATERIALIZE_VERSIONS):
+            raise dbt.exceptions.DbtRuntimeError(
+                f"Detected unsupported Materialize version {mz_version}\n"
+                f"  Supported versions: {SUPPORTED_MATERIALIZE_VERSIONS}"
+            )
+
+        # Make sure 'auto_route_introspection_queries' is enabled.
+        var_name = "auto_route_introspection_queries"
+        logger.debug(f"Enabling {var_name}")
+        cursor.execute(f"SET {var_name} = true")
+
+        return connection
+
+    def cancel(self, connection):
+        # The PostgreSQL implementation calls `pg_terminate_backend` from a new
+        # connection to terminate `connection`. At the time of writing,
+        # Materialize doesn't support `pg_terminate_backend`, so we implement
+        # cancellation by calling `close` on the connection.
+        #
+        # NOTE(benesch): I'm not entirely sure why the PostgreSQL implementation
+        # uses `pg_terminate_backend`. I suspect that disconnecting the network
+        # connection by calling `connection.handle.close()` is not immediately
+        # noticed by the PostgreSQL server, and so the queries running on that
+        # connection may continue executing to completion. Materialize, however,
+        # will quickly notice if the network socket disconnects and cancel any
+        # queries that were initiated by that connection.
+
+        connection_name = connection.name
+        try:
+            logger.debug("Closing connection '{}' to force cancellation")
+            connection.handle.close()
+        except psycopg2.InterfaceError as exc:
+            # if the connection is already closed, not much to cancel!
+            if "already closed" in str(exc):
+                logger.debug(f"Connection {connection_name} was already closed")
+                return
+            # probably bad, re-raise it
+            raise
+
+    # Disable transactions. Materialize transactions do not support arbitrary
+    # queries in transactions and therefore many of dbt's internal macros
+    # produce invalid transactions.
+    #
+    # Disabling transactions has precedent in dbt-snowflake and dbt-biquery.
+    # See, for example:
+    # https://github.com/dbt-labs/dbt-snowflake/blob/ffbb05391/dbt/adapters/snowflake/connections.py#L359-L374
+
+    def add_begin_query(self, *args, **kwargs):
+        pass
+
+    def add_commit_query(self, *args, **kwargs):
+        pass
+
+    def begin(self):
+        pass
+
+    def commit(self):
+        pass
+
+    def clear_transaction(self):
+        pass
---
+++ packages/dbt-materialize/dbt-materialize-1.7.0.tar.gz/dbt-materialize-1.7.0/dbt/adapters/materialize/impl.py
@@ -0,0 +1,180 @@
+# Copyright 2020 Josh Wills. All rights reserved.
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License in the LICENSE file at the
+# root of this repository, or online at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dataclasses import dataclass
+from typing import Any, Dict, List, Optional
+
+import dbt.exceptions
+from dbt.adapters.base.impl import AdapterConfig, ConstraintSupport
+from dbt.adapters.base.meta import available
+from dbt.adapters.capability import (
+    Capability,
+    CapabilityDict,
+    CapabilitySupport,
+    Support,
+)
+from dbt.adapters.materialize.connections import MaterializeConnectionManager
+from dbt.adapters.materialize.relation import MaterializeRelation
+from dbt.adapters.postgres import PostgresAdapter
+from dbt.adapters.sql.impl import LIST_RELATIONS_MACRO_NAME
+from dbt.contracts.graph.nodes import (
+    ColumnLevelConstraint,
+    ConstraintType,
+)
+from dbt.dataclass_schema import ValidationError, dbtClassMixin
+
+
+# types in ./misc/dbt-materialize need to import generic types from typing
+@dataclass
+class MaterializeIndexConfig(dbtClassMixin):
+    columns: Optional[List[str]] = None
+    default: Optional[bool] = False
+    name: Optional[str] = None
+    cluster: Optional[str] = None
+
+    @classmethod
+    def parse(cls, raw_index) -> Optional["MaterializeIndexConfig"]:
+        if raw_index is None:
+            return None
+        try:
+            cls.validate(raw_index)
+            return cls.from_dict(raw_index)
+        except ValidationError as exc:
+            msg = dbt.exceptions.validator_error_message(exc)
+            dbt.exceptions.CompilationError(f"Could not parse index config: {msg}")
+        except TypeError:
+            dbt.exceptions.CompilationError(
+                "Invalid index config:\n"
+                f"  Got: {raw_index}\n"
+                '  Expected a dictionary with at minimum a "columns" key'
+            )
+
+
+@dataclass
+class MaterializeConfig(AdapterConfig):
+    cluster: Optional[str] = None
+
+
+class MaterializeAdapter(PostgresAdapter):
+    ConnectionManager = MaterializeConnectionManager
+    Relation = MaterializeRelation
+
+    AdapterSpecificConfigs = MaterializeConfig
+
+    # NOTE(morsapaes): Materialize supports enforcing the NOT NULL constraint
+    # for materialized views (via the ASSERT NOT NULL clause) and tables. As a
+    # reminder, tables are modeled as static materialized views (see #5266).
+    CONSTRAINT_SUPPORT = {
+        ConstraintType.check: ConstraintSupport.NOT_SUPPORTED,
+        ConstraintType.not_null: ConstraintSupport.ENFORCED,
+        ConstraintType.unique: ConstraintSupport.NOT_SUPPORTED,
+        ConstraintType.primary_key: ConstraintSupport.NOT_SUPPORTED,
+        ConstraintType.foreign_key: ConstraintSupport.NOT_SUPPORTED,
+    }
+
+    # NOTE(morsapaes): Materialize supports the functionality required to enable
+    # metadata source freshness checks, but the value of this feature in a data
+    # warehouse built for real-time is limited, so we do not implement it.
+    _capabilities = CapabilityDict(
+        {
+            Capability.SchemaMetadataByRelations: CapabilitySupport(
+                support=Support.NotImplemented
+            ),
+            Capability.TableLastModifiedMetadata: CapabilitySupport(
+                support=Support.NotImplemented
+            ),
+        }
+    )
+
+    @classmethod
+    def is_cancelable(cls) -> bool:
+        return True
+
+    def _link_cached_relations(self, manifest):
+        # NOTE(benesch): this *should* reimplement the parent class's method
+        # for Materialize, but presently none of our tests care if we just
+        # disable this method instead.
+        #
+        # [0]: https://github.com/dbt-labs/dbt-core/blob/13b18654f03d92eab3f5a9113e526a2a844f145d/plugins/postgres/dbt/adapters/postgres/impl.py#L126-L133
+        pass
+
+    def verify_database(self, database):
+        pass
+
+    def parse_index(self, raw_index: Any) -> Optional[MaterializeIndexConfig]:
+        return MaterializeIndexConfig.parse(raw_index)
+
+    def list_relations_without_caching(
+        self, schema_relation: MaterializeRelation
+    ) -> List[MaterializeRelation]:
+        kwargs = {"schema_relation": schema_relation}
+        results = self.execute_macro(LIST_RELATIONS_MACRO_NAME, kwargs=kwargs)
+
+        relations = []
+        quote_policy = {"database": True, "schema": True, "identifier": True}
+
+        columns = ["database", "schema", "name", "type"]
+        for _database, _schema, _identifier, _type in results.select(columns):
+            try:
+                _type = self.Relation.get_relation_type(_type.lower())
+            except ValueError:
+                _type = self.Relation.get_relation_type.View
+            relations.append(
+                self.Relation.create(
+                    database=_database,
+                    schema=_schema,
+                    identifier=_identifier,
+                    quote_policy=quote_policy,
+                    type=_type,
+                )
+            )
+
+        return relations
+
+    # NOTE(morsapaes): Materialize doesn't inline not_null constraints
+    # in the DDL in the same way as other adapters, so we override the
+    # default constraint rendering functions.
+    @classmethod
+    def render_column_constraint(cls, constraint: ColumnLevelConstraint) -> str:
+        if constraint.type == ConstraintType.not_null:
+            rendered_column_constraint = None
+            rendered_column_constraint = "assert not null"
+
+            return rendered_column_constraint
+        else:
+            return ""
+
+    @available
+    @classmethod
+    def render_raw_columns_constraints(
+        cls, raw_columns: Dict[str, Dict[str, Any]]
+    ) -> List:
+        rendered_column_constraints = []
+
+        for v in raw_columns.values():
+            if v.get("constraints"):
+                col_name = cls.quote(v["name"]) if v.get("quote") else v["name"]
+                rendered_column_constraint = [col_name]
+                for con in v.get("constraints", None):
+                    constraint = cls._parse_column_constraint(con)
+                    c = cls.process_parsed_constraint(
+                        constraint, cls.render_column_constraint
+                    )
+                    if c is not None:
+                        rendered_column_constraint.insert(0, c)
+                rendered_column_constraints.append(" ".join(rendered_column_constraint))
+
+        return rendered_column_constraints
