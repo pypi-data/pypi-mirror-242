4e8810e897c900b3fcc4141f7d1132c74822dabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 04:03:56 +0000

    Add package azure-data-factory-testing-framework/0.0.0a31/azure_data_factory_testing_framework-0.0.0a31-py3-none-any.whl

---
+++ packages/azure-data-factory-testing-framework/azure_data_factory_testing_framework-0.0.0a31-py3-none-any.whl/azure_data_factory_testing_framework/models/activities/activity_dependency.py
@@ -0,0 +1,15 @@
+from typing import Any, List, Union
+
+from azure_data_factory_testing_framework.state.dependency_condition import DependencyCondition
+
+
+class ActivityDependency:
+    def __init__(self, **kwargs: Any) -> None:  # noqa: ANN401
+        """ActivityDependency.
+
+        Args:
+            **kwargs: ActivityDependency properties coming directly from the json representation of the activity.
+        """
+        self.kwargs = kwargs
+        self.activity: str = kwargs["activity"]
+        self.dependency_conditions: List[Union[str, DependencyCondition]] = kwargs["dependencyConditions"]
