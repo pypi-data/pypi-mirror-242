ce528e1cb855180998d2d75a6c6649e865bd0998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 03:04:42 +0000

    Add package pyliara/1.3.0/pyliara-1.3.0.tar.gz

---
+++ packages/pyliara/pyliara-1.3.0.tar.gz/pyliara-1.3.0/pyliara/__init__.py
@@ -0,0 +1,5 @@
+from .liara import Liara
+
+
+__version__ = "1.3.0"
+__author__ = "MohammadRezaFirouzi"
---
+++ packages/pyliara/pyliara-1.3.0.tar.gz/pyliara-1.3.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+requirements = ['requests']
+
+with open("README.md", encoding="UTF-8") as f:
+    readme = f.read()
+
+setup(
+    name = 'pyliara',
+    version = '1.3.0',
+    author='MohammadRezaFirouzi',
+    author_email = 'mrfirouziii@gmail.com',
+    description = 'This is an unofficial library for Liara Host',
+    long_description = readme,
+    python_requires="~=3.7",
+    long_description_content_type = 'text/markdown',
+    url = 'https://rubika.ir/Mohamadreza_firouzi',
+    packages = find_packages(),
+    install_requires = requirements,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Internet',
+    ],
+)
