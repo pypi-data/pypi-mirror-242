8664eebe41c7da521d1d8d42d7e6ad65ea85c363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 15:04:12 +0000

    Add package Moby-Land/0.1.1/Moby_Land-0.1.1.tar.gz

---
+++ packages/Moby-Land/Moby_Land-0.1.1.tar.gz/Moby_Land-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Moby_Land',
+    version='0.1.1',
+    author='Adam Boesky',
+    author_email='apboesky@gmail.com',
+    description='A place for all of Adam\' gadgets.',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.0',
+)
+
+# To republish:
+# 1. bump version nuber
+# 2. python setup.py sdist
+# 3. twine upload dist/*
