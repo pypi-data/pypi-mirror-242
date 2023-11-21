78fdba3b007c781acc424231c40a219c2e2af0e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 04:03:09 +0000

    Add package onestep/0.3.4/onestep-0.3.4-py3-none-any.whl

---
+++ packages/onestep/onestep-0.3.4-py3-none-any.whl/onestep/broker/cron.py
@@ -0,0 +1,42 @@
+"""
+使用CRON表达式触发任务执行
+"""
+import logging
+import threading
+from datetime import datetime
+
+from croniter import croniter
+
+from .base import BaseLocalBroker, BaseLocalConsumer
+
+logger = logging.getLogger(__name__)
+
+
+class CronBroker(BaseLocalBroker):
+    _thread = None
+
+    def __init__(self, cron, name=None, middlewares=None, **kwargs):
+        super().__init__(name=name, middlewares=middlewares)
+        self.cron = cron
+        self.itr = croniter(cron, datetime.now())
+        self.next_fire_time = self.itr.get_next(datetime)
+        self.kwargs = kwargs
+
+    def _scheduler(self):
+        if self.next_fire_time <= datetime.now():
+            self.next_fire_time = self.itr.get_next(datetime)
+            self.publish(self.kwargs)
+
+        self._thread = threading.Timer(interval=1, function=self._scheduler)
+        self._thread.start()
+
+    def consume(self):
+        self._scheduler()
+        return CronConsumer(self.queue)
+
+    def shutdown(self):
+        self._thread.cancel()
+
+
+class CronConsumer(BaseLocalConsumer):
+    ...
