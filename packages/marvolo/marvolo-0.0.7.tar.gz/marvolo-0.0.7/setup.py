512d700514647627864f87be885365db73899d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 15:04:02 +0000

    Add package marvolo/0.0.7/marvolo-0.0.7.tar.gz

---
+++ packages/marvolo/marvolo-0.0.7.tar.gz/marvolo-0.0.7/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='marvolo',
+    version='v0.0.7',
+    description='Some useful tools about CV develeped by Marvolo',
+    py_modules=['marvolo'],
+    platforms=['all'],
+    author='Marvolo',
+    author_email='18377221@buaa.edu.cn',
+    python_requires='>=3.6',
+    url='',
+    install_requires=['opencv-python','imageio', 'colorlog'],
+    license='MIT'
+)
