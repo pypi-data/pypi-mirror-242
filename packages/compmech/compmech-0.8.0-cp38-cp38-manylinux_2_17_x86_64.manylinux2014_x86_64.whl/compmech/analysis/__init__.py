04acbe4f5948f40d569915ea5d5ac627b4687e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 05:03:16 +0000

    Add package compmech/0.8.0/compmech-0.8.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/compmech/compmech-0.8.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/compmech/analysis/__init__.py
@@ -0,0 +1,21 @@
+r"""
+==========================================
+Analysis Module (:mod:`compmech.analysis`)
+==========================================
+
+.. currentmodule:: compmech.analysis
+
+.. autoclass:: Analysis
+    :members:
+
+.. autofunction:: freq
+
+.. autofunction:: lb
+
+.. autofunction:: static
+
+"""
+from .analysis import Analysis
+from .freq import freq
+from .linear_buckling import lb
+from .static import static
