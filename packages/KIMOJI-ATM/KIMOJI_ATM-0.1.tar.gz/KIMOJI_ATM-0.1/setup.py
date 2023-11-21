882e0e05bf97d20fdf85e6ac545cf44fd59227b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 05:03:47 +0000

    Add package KIMOJI-ATM/0.1/KIMOJI_ATM-0.1.tar.gz

---
+++ packages/KIMOJI-ATM/KIMOJI_ATM-0.1.tar.gz/KIMOJI_ATM-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='KIMOJI_ATM',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # 列出你的包的依赖
+    ],
+    # 其他元数据：作者、许可证、长描述等
+)
