9919700ecb49059e6954a3557de21fa95a5bcca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 05:03:57 +0000

    Add package penn/0.0.13/penn-0.0.13.tar.gz

---
+++ packages/penn/penn-0.0.13.tar.gz/penn-0.0.13/test/test_core.py
@@ -0,0 +1,16 @@
+import penn
+
+
+###############################################################################
+# Test core.py
+###############################################################################
+
+
+def test_infer(audio):
+    """Test that inference produces the correct shape"""
+    pitch, periodicity = penn.from_audio(
+        audio,
+        penn.SAMPLE_RATE,
+        center='half-hop')
+    shape = (1, audio.shape[1] // penn.HOPSIZE)
+    assert pitch.shape == periodicity.shape == shape
