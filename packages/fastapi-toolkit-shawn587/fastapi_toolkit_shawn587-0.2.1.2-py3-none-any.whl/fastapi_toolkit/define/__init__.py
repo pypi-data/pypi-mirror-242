fcfa185bdec8bd7251f5775dd58ee0935ed931b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 03:04:28 +0000

    Add package fastapi-toolkit-shawn587/0.2.1.2/fastapi_toolkit_shawn587-0.2.1.2-py3-none-any.whl

---
+++ packages/fastapi-toolkit-shawn587/fastapi_toolkit_shawn587-0.2.1.2-py3-none-any.whl/fastapi_toolkit/define/__init__.py
@@ -0,0 +1,10 @@
+from .model import ModelManager, BaseModel, Field
+from .link import OneManyLink, ManyManyLink
+
+__all__ = [
+    'ModelManager',
+    'BaseModel',
+    'Field',
+    'OneManyLink',
+    'ManyManyLink',
+]
