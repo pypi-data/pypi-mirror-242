3c1a21ad081224c041d384f0b04c21cdc4090e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 14:04:00 +0000

    Add package Gifine/0.0.1/Gifine-0.0.1.tar.gz

---
+++ packages/Gifine/Gifine-0.0.1.tar.gz/Gifine-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Gifine',
+    version = '0.0.1',
+    author = 'Authentique',
+    author_email = 'berlinpierre007@gmail.com',
+    packages= find_packages(),
+    url=  'https://github.com/BPone007/giff.git',
+)
