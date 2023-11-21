4a379b5eee39ff3453e45ead123ecf002803662d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 03:04:32 +0000

    Add package inpinitiFinance/1.0.7/inpinitiFinance-1.0.7.tar.gz

---
+++ packages/inpinitiFinance/inpinitiFinance-1.0.7.tar.gz/inpinitiFinance-1.0.7/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name ='inpinitiFinance',
+    version = '1.0.7',
+    description = 'finance data',
+    author = 'inpiniti',
+    author_email = 'younginpiniti@gmail.com',
+    url='https://github.com/inpiniti/inpinitiFinance',
+    install_requires=['OpenDartReader', 'pandas', 'requests', 'keras', 'sklearn'],
+    py_modules = ['ifinance', 'ai'],
+    keywords = ['finance', 'stock', 'data', 'inpiniti', 'dart', 'krx'],
+)
