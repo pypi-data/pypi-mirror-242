9802a2ab8881d8abb5f13426a372178965448867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 14:03:28 +0000

    Add package Gifwine-2/0.1.1/Gifwine_2-0.1.1.tar.gz

---
+++ packages/Gifwine-2/Gifwine_2-0.1.1.tar.gz/Gifwine_2-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Gifwine_2',
+    version = '0.1.1',
+    author = 'Authentique',
+    author_email = 'berlinpierre007@gmail.com',
+    packages= find_packages(),
+    url= 'https://github.com/BPone007/giff_version2/tree/9941213526fcd65cd91b2d53154646772bcac33b/Prjt_1',
+)
