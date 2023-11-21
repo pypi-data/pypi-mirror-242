854d8259b76620b61de3666e7bd28a55d2a76b9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 03:04:31 +0000

    Add package umap-project/1.11.0/umap_project-1.11.0-py3-none-any.whl

---
+++ packages/umap-project/umap_project-1.11.0-py3-none-any.whl/umap/tests/settings.py
@@ -0,0 +1,22 @@
+import os
+
+from umap.settings.base import *  # pylint: disable=W0614,W0401
+
+SECRET_KEY = "justfortests"
+COMPRESS_ENABLED = False
+FROM_EMAIL = "test@test.org"
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+
+if "TRAVIS" in os.environ:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.contrib.gis.db.backends.postgis",
+            "NAME": "umap",
+            "PORT": 5433,
+            "USER": "travis",
+        }
+    }
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.MD5PasswordHasher",
+]
