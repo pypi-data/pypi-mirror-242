54999a8d33688fb913f499d1ed61b97ee586d1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 03:04:42 +0000

    Add package nbdime/4.0.0rc1/nbdime-4.0.0rc1-py3-none-any.whl

---
+++ packages/nbdime/nbdime-4.0.0rc1-py3-none-any.whl/nbdime/_version.py
@@ -0,0 +1,23 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+import re
+from collections import namedtuple
+
+VersionInfo = namedtuple("VersionInfo", ["major", "minor", "micro", "releaselevel", "serial"])
+
+_specifier_ = {"a": "alpha", "b": "beta", "rc": "candidate", "": "final"}
+
+__version__ = "4.0.0rc1"
+
+parser = re.compile(r"^(?P<major>\d+)\.(?P<minor>\d+)\.(?P<micro>\d+)((?P<releaselevel>a|b|rc)(?P<serial>\d+))?$")
+
+parsed_version = parser.match(__version__)
+groups = parsed_version.groupdict()
+version_info = VersionInfo(
+    int(groups["major"]),
+    int(groups["minor"]),
+    int(groups["micro"]),
+    _specifier_[groups.get("releaselevel", "")],
+    groups.get("serial", ""),
+)
