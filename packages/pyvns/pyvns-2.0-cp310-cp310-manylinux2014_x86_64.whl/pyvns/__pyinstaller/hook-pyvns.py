87df2381154e4e86f25f1e8ba5b60733c3cd04a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 15:04:05 +0000

    Add package pyvns/2.0/pyvns-2.0-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/pyvns/pyvns-2.0-cp310-cp310-manylinux2014_x86_64.whl/pyvns/__pyinstaller/hook-pyvns.py
@@ -0,0 +1,28 @@
+import os, pyvns
+
+__PATH: str = pyvns.__path__[0]
+__NAME: str = "pyvns"
+
+# no hidden import
+
+datas: list[tuple[str, str]] = []
+ignores: tuple[str, ...] = ("__pyinstaller", "__pycache__", ".git")
+
+for file_name in os.listdir(__PATH):
+    # 文件夹
+    if os.path.isdir(os.path.join(__PATH, file_name)):
+        ignore_this_folder: bool = False
+        for folder_name_t in ignores:
+            if folder_name_t in file_name:
+                ignore_this_folder = True
+                break
+        if not ignore_this_folder:
+            datas.append(
+                (
+                    os.path.join(__PATH, file_name),
+                    os.path.join(__NAME, file_name),
+                )
+            )
+    # 文件
+    elif "gitignore" not in file_name:
+        datas.append((os.path.join(__PATH, file_name), os.path.join(__NAME)))
