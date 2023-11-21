9e87438c81baecd334f94b1cf3d9d18bcef445af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 04:04:24 +0000

    Add package makda/0.1/makda-0.1-py3-none-any.whl

---
+++ packages/makda/makda-0.1-py3-none-any.whl/lean/lean.py
@@ -0,0 +1,7 @@
+# my_shortcuts/shortcuts.py
+
+def read_file(file_name):
+    file_path = f'data/{file_name}'
+    with open(file_path, 'r') as file:
+        content = file.read()
+    return content
