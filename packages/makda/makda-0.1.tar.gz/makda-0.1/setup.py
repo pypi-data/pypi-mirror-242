0217fd4fa3e54e33701f4522363872f3740abcfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 04:04:24 +0000

    Add package makda/0.1/makda-0.1.tar.gz

---
+++ packages/makda/makda-0.1.tar.gz/makda-0.1/setup.py
@@ -0,0 +1,16 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='makda',
+    version='0.1',  # Update the version number
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        # list your dependencies here
+    ],
+    author='yk2',
+    author_email='kkwieer@yahoo.com',
+    description='Description of your library',
+)
