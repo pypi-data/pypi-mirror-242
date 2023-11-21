3404bd25b335b17b0e6687599ee6d1411e784b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 17:03:51 +0000

    Add package cacheline/0.0.2.dev6/cacheline-0.0.2.dev6-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/cacheline/cacheline-0.0.2.dev6-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/cacheline/_debug.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+import logging
+import sys
+
+
+def setup_debug_logging(log_filepath: str | None = None):
+    import colorlog
+
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+
+    stream_handler = colorlog.StreamHandler(sys.stdout)
+    stream_handler.setLevel(logging.DEBUG)
+    stream_handler.setFormatter(
+        colorlog.ColoredFormatter(
+            "%(asctime)s %(log_color)s%(levelname)-8s%(reset)s %(message_log_color)s%(message)s %(reset)s%(pathname)s:%(lineno)d ",
+            datefmt=None,
+            reset=True,
+            log_colors={
+                "DEBUG": "cyan",
+                "INFO": "green",
+                "WARNING": "yellow",
+                "ERROR": "red",
+                "CRITICAL": "red,bg_white",
+            },
+            secondary_log_colors={
+                "message": {
+                    "DEBUG": "cyan",
+                    "INFO": "green",
+                    "WARNING": "yellow",
+                    "ERROR": "red",
+                    "CRITICAL": "red,bg_white",
+                }
+            },
+            style="%",
+        )
+    )
+    root.addHandler(stream_handler)
+
+    if log_filepath:
+        file_handler = logging.FileHandler(log_filepath)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(
+            logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - 🌟 %(message)s - %(pathname)s:%(lineno)d")
+        )
+        root.addHandler(file_handler)
