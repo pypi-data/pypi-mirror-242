a3535d585d6aaec32a990de5c1ff4d2729d443b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 03:03:42 +0000

    Add package imio.smartweb.core/1.2.5/imio.smartweb.core-1.2.5.tar.gz

---
+++ packages/imio.smartweb.core/imio.smartweb.core-1.2.5.tar.gz/imio.smartweb.core-1.2.5/src/imio/smartweb/core/browser/static/src/edit.js
@@ -0,0 +1,18 @@
+import './edit.less'
+jQuery(document).ready(function ($) {
+  // Hide / show editor's tools & messages when clicking on "Preview" in Plone toolbar
+  $("#contentview-preview a").click(function(e){
+    $(".hide-in-preview, #section-byline, #global_statusmessage").toggle("fast");
+    e.preventDefault();
+  });
+});
+
+jQuery(window).on("load", function(e) {
+  // Move authentic sources menu just before user/personaltools-menulink in Plone toolbar
+  var auth_sources = $("#plone-authentic-sources-menu").wrap("<ul class='plonetoolbar-authentic-sources-menu'>").parent();
+  $(".personaltools-wrapper").prepend(auth_sources);
+
+  // Move smartweb help menu just before user/personaltools-menulink in Plone toolbar
+  var smartweb_help = $("#plone-smartweb-help-menu").wrap("<ul class='plonetoolbar-smartweb-help-menu'>").parent();
+  $(".personaltools-wrapper").prepend(smartweb_help);
+});
