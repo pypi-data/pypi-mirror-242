4c5246c7fff2ac5eebaf1e9dab491cd737a75fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 14:03:34 +0000

    Add package tantivy/0.21.0/tantivy-0.21.0.tar.gz

---
+++ packages/tantivy/tantivy-0.21.0.tar.gz/tantivy-0.21.0/src/query.rs
@@ -0,0 +1,21 @@
+use pyo3::prelude::*;
+use tantivy as tv;
+
+/// Tantivy's Query
+#[pyclass(frozen)]
+pub(crate) struct Query {
+    pub(crate) inner: Box<dyn tv::query::Query>,
+}
+
+impl Query {
+    pub(crate) fn get(&self) -> &dyn tv::query::Query {
+        &self.inner
+    }
+}
+
+#[pymethods]
+impl Query {
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!("Query({:?})", self.get()))
+    }
+}
