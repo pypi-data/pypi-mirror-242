f5a4815d654bacad56f9c9f6ee8cd2b134d5837c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 05:03:09 +0000

    Add package fhdw-modelling/0.1.0/fhdw_modelling-0.1.0-py3-none-any.whl

---
+++ packages/fhdw-modelling/fhdw_modelling-0.1.0-py3-none-any.whl/fhdw/modelling/base.py
@@ -0,0 +1,14 @@
+"""Modelling pipeline Script utilizing pycaret.
+
+It models the diffrent targets for the myNeo webapp and saved the models to the local
+models folder.
+"""
+
+
+def hello_world():
+    """Define sample function."""
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    print("nothing done")
