6c7856eeeac308408e10a32f4499ac23619a7e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 07:03:09 +0000

    Add package join-iterables/0.2/join-iterables-0.2.tar.gz

---
+++ packages/join-iterables/join-iterables-0.2.tar.gz/join-iterables-0.2/join_iterables/__init__.py
@@ -0,0 +1,1 @@
+from join_iterables.join_iterables import generic_join
---
+++ packages/join-iterables/join-iterables-0.2.tar.gz/join-iterables-0.2/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+V = '0.2'
+
+setup(
+    name = 'join-iterables',
+    packages = ['join_iterables'],
+
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
+    version = V,
+    license='MIT',
+    description='Joins iterables in a streaming fashion while respects repetitions',
+    url = 'https://github.com/andyil/join-iterables',
+    download_url = f'https://github.com/andyil/join-iterables/archive/{V}.tar.gz',
+    keywords = ['python', 'data', 'iterable', 'iterables', 'itertools', 'more-itertools', 'join'],
+    install_requires=['more-itertools'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
