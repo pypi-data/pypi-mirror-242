bb3b851a51d631f96d83b758316a7a1bb9d9a4c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 06:03:50 +0000

    Add package stochasticprocesses/0.10.0/stochasticprocesses-0.10.0.tar.gz

---
+++ packages/stochasticprocesses/stochasticprocesses-0.10.0.tar.gz/stochasticprocesses-0.10.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("Readme.md", "r") as f:
+    long_description = f.read()
+    
+setup (
+    author= "Julio César Martínez",
+    description="A package to simulate stochastic proccesses",
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    name='stochasticprocesses',
+    version='0.10.0',
+    packages=find_packages(include=["numpy", "pandas"]),
+    author_email="julio.martinez@gmail.com.mx",
+    install_requires=["pandas >= 1.0.0", "numpy >= 1.15.0"],
+    python_requires=">= 3.7.3",
+    url='https://github.com/JulioCesarMS/StochasticModels.git'
+    ),
+
+
+# pip install -e .
