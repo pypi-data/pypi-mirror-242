7b3dafb0f68df9b3d3b08ec044bfdcaa9a686baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 07:03:07 +0000

    Add package stochasticprocesses/0.12.0/stochasticprocesses-0.12.0.tar.gz

---
+++ packages/stochasticprocesses/stochasticprocesses-0.12.0.tar.gz/stochasticprocesses-0.12.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import io
+import os
+
+
+def read(file_name):
+    """Read a text file and return the content as a string."""
+    with io.open(os.path.join(os.path.dirname(__file__), file_name),
+                 encoding='utf-8') as f:
+        return f.read()
+
+setup(
+    name='stochasticprocesses',
+    description='A package to simulate stochastic processes',
+    author='Julio César Martínez',
+    author_email='julio.martinez@gmail.com.mx',
+    version='0.12.0',
+    long_description=read('Readme.md'),
+    long_description_content_type='text/markdown',
+    #packages=['simulation'],
+    packages=find_packages(include=["numpy", "pandas", "random"]),
+    install_requires=["pandas >= 1.0.0", "numpy >= 1.15.0"],
+    python_requires=">= 3.7.3",
+    url='https://github.com/JulioCesarMS/StochasticModels.git'
+    # package_data={
+    #     'magic': ['py.typed', '*.pyi', '**/*.pyi'],
+    # },
+    
+    
+)
+
+# pip install -e .
