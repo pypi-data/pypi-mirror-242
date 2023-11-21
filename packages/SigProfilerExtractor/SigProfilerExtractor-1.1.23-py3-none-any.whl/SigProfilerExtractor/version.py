c11c88f68773d4252142576eb9ff6359b1d0bd72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 15:03:54 +0000

    Add package SigProfilerExtractor/1.1.23/SigProfilerExtractor-1.1.23-py3-none-any.whl

---
+++ packages/SigProfilerExtractor/SigProfilerExtractor-1.1.23-py3-none-any.whl/SigProfilerExtractor/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEREXTRACTOR SETUP.PY
+short_version = '1.1.23'
+version = '1.1.23'
+Update = 'Upgrade v1.1.23: Update to use the latest SigProfilerAssignment with COSMIC v3.4 signatures.'
+    
+    
