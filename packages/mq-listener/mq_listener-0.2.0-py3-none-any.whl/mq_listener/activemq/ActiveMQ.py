240823dae6baf78eef651bf5dc88697321e7bd9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 15:04:03 +0000

    Add package mq-listener/0.2.0/mq_listener-0.2.0-py3-none-any.whl

---
+++ packages/mq-listener/mq_listener-0.2.0-py3-none-any.whl/mq_listener/activemq/ActiveMQ.py
@@ -0,0 +1,59 @@
+from stompest.config import StompConfig
+from stompest.sync import Stomp
+from stompest.protocol import StompSpec
+import random
+import string
+import uuid
+import json
+from mq_listener.activemq.BaseAction import BaseAction
+class ActiveMQ:
+    def __init__(self) -> None:
+        self.CONFIG = StompConfig(uri='ssl://'+'localhost'+':61613',version='1.2')
+
+    def message_listener(self,queue_name,id,actionobj :BaseAction):
+        queue =  '/queue/' + queue_name
+        client = Stomp(self.CONFIG)
+        client.connect(heartBeats=(0,1000))
+        token =client.subscribe(queue,{StompSpec.ACK_HEADER: StompSpec.ACK_CLIENT_INDIVIDUAL,"id":id,"activemq.prefetchSize": 1})
+
+        frame = None
+        random_uuid = None
+        input_json = ""
+        while True:
+            random_uuid = self.gen_random_uuid()
+            frame= client.receiveFrame()
+            input_json = self.fetch_input_message(frame)
+            status = actionobj.trigger_action(message_data=input_json) 
+            #if status :
+                #client.ack(frame)
+
+    
+    def generate_random_pk(self,no_of_characters = 6):
+        try:
+            res = ''.join(random.choices(string.ascii_uppercase +
+                                string.digits, k = no_of_characters))
+            return res
+        except Exception as err:
+            raise Exception(err)
+    
+
+    def gen_random_uuid(self) -> str:
+        """
+        This method help to produce random uuid number
+        @return: random uuid number
+        """
+        random_uuid = uuid.uuid4()
+        return str(random_uuid).replace('-', '')
+    
+    def fetch_input_message(self, frame):
+        input_json = json.loads(frame.body.decode())
+        return input_json 
+
+# class TestListener(BaseAction.BaseAction):
+#     def trigger_action(self, message_data: object):
+#         return super().trigger_action(message_data)
+
+
+# if  __name__ == '__main__':
+#     objMq = ActiveMQ()
+#     objMq.message_listener('convert-images',4400,TestListener())
