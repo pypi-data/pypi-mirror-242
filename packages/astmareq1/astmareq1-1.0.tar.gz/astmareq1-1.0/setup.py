f0c137f0caf6aa4cae34a84e9d2c3862b748165c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 06:04:06 +0000

    Add package astmareq1/1.0/astmareq1-1.0.tar.gz

---
+++ packages/astmareq1/astmareq1-1.0.tar.gz/astmareq1-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="astmareq1",
+      version="1.0",
+      description="package with astma codes",
+      author="Black_shark",
+      packages=['astmareq1'],
+      install_requires=[])
