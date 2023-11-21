0a0ea03a098bfdb21f0113545ede9cf8414c4c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 14:03:34 +0000

    Add package dsjobs/0.1.5/dsjobs-0.1.5-py3-none-any.whl

---
+++ packages/dsjobs/dsjobs-0.1.5-py3-none-any.whl/dsjobs/__init__.py
@@ -0,0 +1,19 @@
+"""
+`dsjobs` is a library that simplifies the process of submitting, running, and monitoring [TAPIS v2 / AgavePy](https://agavepy.readthedocs.io/en/latest/index.html) jobs on [DesignSafe](https://designsafe-ci.org) via [Jupyter Notebooks](https://jupyter.designsafe-ci.org).
+
+
+## Features
+
+* Simplified TAPIS v2 Calls: No need to fiddle with complex API requests. `dsjobs` abstracts away the complexities.
+
+* Seamless Integration with DesignSafe Jupyter Notebooks: Launch DesignSafe applications directly from the Jupyter environment.
+
+## Installation
+
+```shell
+pip3 install dsjobs
+```
+
+"""
+from .dir import get_ds_path_uri
+from .jobs import get_status, runtime_summary, generate_job_info, get_archive_path
