e67c10d59aafd213176fd5d31a2286a8386956b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 04:04:31 +0000

    Add package peaq-py/0.1.1/peaq_py-0.1.1-py3-none-any.whl

---
+++ packages/peaq-py/peaq_py-0.1.1-py3-none-any.whl/peaq/storage.py
@@ -0,0 +1,39 @@
+from peaq.utils import ExtrinsicBatch
+
+
+def storage_add_payload(batch, item_type, item):
+    batch.compose_call(
+        'PeaqStorage',
+        'add_item',
+        {
+            'item_type': item_type,
+            'item': item,
+        })
+
+
+def storage_add(substrate, kp_src, item_type, item):
+    batch = ExtrinsicBatch(substrate, kp_src)
+    storage_add_payload(batch, item_type, item)
+    return batch.execute()
+
+
+def storage_update_payload(batch, item_type, item):
+    batch.compose_call(
+        'PeaqStorage',
+        'update_item',
+        {
+            'item_type': item_type,
+            'item': item,
+        })
+
+
+def storage_update(substrate, kp_src, item_type, item):
+    batch = ExtrinsicBatch(substrate, kp_src)
+    storage_update_payload(batch, item_type, item)
+    return batch.execute()
+
+
+def storage_rpc_read(substrate, addr, item_type):
+    data = substrate.rpc_request('peaqstorage_readAttribute', [
+                                 addr, item_type])
+    return data['result']
