f36032021cfeaf2f1ba8053f03062a81eea90a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 16:03:36 +0000

    Add package binfootprint/1.2.2/binfootprint-1.2.2.tar.gz

---
+++ packages/binfootprint/binfootprint-1.2.2.tar.gz/binfootprint-1.2.2/README.md
@@ -0,0 +1,438 @@
+# binfootprint - unique serialization of python objects
+
+[![PyPI version](https://badge.fury.io/py/binfootprint.svg)](https://badge.fury.io/py/binfootprint)
+
+## Why unique serialization
+
+When caching computationally expansive function calls, the input arguments (*args, **kwargs)
+serve as key to look up the result of the function.
+To perform efficient lookups these keys (often a large number of nested python objects) needs to be hashable.
+Since python's build-in hash function is randomly seeded (and applies to a few data types only) it is not
+suited for persistent caching.
+Alternatively, standard hash functions, as provided by the 
+[hashlib library](https://docs.python.org/3/library/hashlib.html), can be used.
+As they relay on  byte sequences as input, python objects need to be converted to such a sequence first.
+Surely, python's pickle module provides such a serialization which, for our purpose, has the drawback that
+the byte sequence is not guaranteed to be unique (e.g., a dictionary can be stored as different byte sequences,
+as the order of the (key, value) pairs is irrelevant).
+
+The binfootprint module fills that gap.
+It guarantees that a particular python object will have a unique binary representation which 
+can serve as input for any hash function.  
+
+## Quick start
+
+`binfootprint.dump(data)` generate a unique binary representation 
+(binary footprint) of `data`.
+```python
+b = binfootprint.dump(['hallo', 42])
+```
+
+Its output can serve as suitable input for a hash function.
+```python
+hashlib.sha256(b).hexdigest()
+```
+
+`binfootprint.load(data)` reconstructs the original python object.
+```python
+ob = binfootprint.load(b)
+```
+
+Numpy array can be serialized.
+```python
+a = numpy.asarray([0, 2.3, 4])
+b = binfootprint.dump(a)
+```
+
+Classes which implement `__getstate__` (pickle interface) or `__bfkey__` can be
+serialized too.
+
+```python
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __getstate__(self):
+        return [self.x, self.y]
+        
+ob = Point(4, -2)
+b = binfootprint.dump(ob)
+```
+If `__bfkey__` is implemented, it is used over `__getstate__`.
+
+*New since version 1.2.0:* 
+[`functools.partial`](https://docs.python.org/3/library/functools.html?highlight=partial#functools.partial) 
+objects can now be serialized too. This allows to cache a function which takes a `functools.partial`
+as argument.
+
+```python
+def gaussian(x, a, sigma, x0):
+    return a * math.exp(-(x-x0)**2 / 2 / sigma**2)
+
+@binfootprint.util.ShelveCacheDec()
+def quad(f, x_min, x_max, dx):
+    r = 0
+    x = x_min
+    while x < x_max:
+        r += f(x)
+        x += dx
+    return dx*r
+
+g = functools.partial(gaussian, a=1, sigma=1, x0=-2.34)
+quad(g, x_min=-10, x_max=10, dx=0.001)
+
+```
+
+### cache decorator 
+
+Utilizing the unique binary representation of python objects, a persistent 
+cache for quite general functions is provided by the `ShelveCache` class.
+The decorator `ShelveCacheDec` makes it really easy to use: 
+
+```python
+@binfootprint.ShelveCacheDec(path='.cache')
+def area(p):
+    return p.x * p.y
+```
+
+### parameter base class
+
+To conveniently organize a set of parameters suitable as key for caching 
+you can subclass `ABCParameter`. Why should you do that?
+
+- The `__bfkey__` method of `ABCParameter` ignores parameters that are `None`.
+  This allows to extend your function interface without loosing access to 
+  cached results from earlier stages.
+- You can add informative information to the `__non_key__` member which
+  are not included in the binary representation of the parameter class.
+
+```python
+class Param(bf.ABCParameter):
+    __slots__ = ["x", "y", "__non_key__"]
+
+    def __init__(self, x, y, msg=""):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.__non_key__ = dict()
+        self.__non_key__["msg"] = msg
+```
+
+
+## Which data types can be serialized
+
+Python's **fundamental data types** are supported
+* integer 
+* float (64bit)
+* complex (128bit)
+* strings
+* byte arrays
+* special build-in constants: `True`, `False`, `None`
+
+as well as their **nested combination** by means of the **native data structures**
+- tuple
+- list
+- dictionary
+- namedtuple.
+
+In addition, the following types are supported:
+- numpy `ndarray`: 
+  The serialization makes use of numpy's 
+  [format.write_array()](https://numpy.org/devdocs/reference/generated/numpy.lib.format.write_array.html) 
+  function using version 1.0.
+- `functools.partial` objects (*new since version 1.2.0*)
+
+ Furthermore, any class that implements 
+
+- `__getstate__` (python's pickle interface)
+
+can be serialized as well, given that the returned data from `__getstate__` can be serialized
+**and the returned data is not `None`**
+Distinction between objects is realized by adding the class name and the name of the module which defines 
+the class to the binary data.
+This in turn allows to also reconstruct the original object by means of the `__setstate__` method.
+
+In case the `__getstate__` method is not suitable, you can implement
+
+- `__bfkey__`
+
+which should return the necessary data to distinguish different objects.
+The spirit of `__bfkey__` is very similar to that of `__getstate__`, although it is meant
+for serialization only, and to for reconstruction the original object.
+
+Note that, if `__bfkey__` is implemented it will be used, regardless of `__getstate__`.
+
+Note: dumping older version is not supported anymore. If backwards compatibility is needed check out older
+code from git. If needed converters should/will be written.
+
+### be carefull with functions
+
+Since a function objects seem to implement `__getstate__` which, however, returns `None`, 
+dumping a function will fail.
+**Whether this makes sense, can be discussed.**
+Implementing your own callable ore using `partioal` objects can circumvent this.
+
+## Installation
+
+### pip
+install the latest version using pip
+
+    pip install binfootprint
+
+### poetry
+Using poetry allows you to include this package in your project as a dependency.
+
+### git
+check out the code from github
+
+    git clone https://github.com/richard-hartmann/binfootprint.git
+
+### dependencies
+
+- python3
+- numpy
+
+## How to use the binfootprint module
+
+### data serialization
+
+Generating the binary footprint is done using the `dump(obj)` method.
+
+#### very simple
+```python
+import binfootprint as bf
+bf.dump(['hallo', 42])
+```
+
+#### more complex
+```python
+import hashlib
+import binfootprint as bf
+
+SIGMA_Z = 0x34
+data = {
+    'Færøerne': {
+        'area': (1399, 'km^2'),
+        'population': 54000
+    },
+    SIGMA_Z: [[-1, 0],
+              [0, 1]],
+    'usefulness': None
+}
+b = bf.dump(data)
+print("MD5 check sum:", hashlib.md5(b).hexdigest())
+```
+
+### reconstruct serialized data
+
+Although the primary focus of this module is the binary representation,
+for reasons of convenience or debugging it might be useful restore the original
+python object from the binary data. 
+Calling the `load(bin_data)` function achieves that task. 
+  
+```python
+import binfootprint as bf
+
+data = ['hallo', 42]
+b = bf.dump(data)
+data_prime = bf.load(b)
+print(data_prime)
+```
+
+### python objects - `__getstate__`
+
+Since `__getstate__` is assumed to uniquely represent the state of an
+object by means of the returned data, it can be used to generate a unique binary
+representation.
+
+```python
+import binfootprint as bf
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __getstate__(self):
+        return [self.x, self.y]
+    def __setstate__(self, state):
+        self.x = state[0]
+        self.y = state[1]
+
+ob = Point(4, -2)
+b = bf.dump(ob)
+```
+Since `__setstate__` is implemented as well, the original object can be
+reconstructed.
+```python
+ob_prime = bf.load(b)
+print("type:", type(ob_prime))
+# type: <class '__main__.Point'>
+print("member x:", ob_prime.x)
+# member x: 4
+print("member y:", ob_prime.y)
+# member y: -2
+```
+
+### implement `__bfkey__` if `__getstate__` is not suited
+
+In case `__getstate__` returns data which is not sufficient to uniquely label
+an object or if the data cannot be serialized by the binaryfootprint module,
+the method `__bfkey__` should be implemented.
+It is expected to return serializable data which uniquely identifies the state
+of the object.
+Note that, if `__bfkey__` is present, `__getstate__` is ignored.
+
+**Importantly**, when deserializing the binary data from an object implementing 
+`__bfkey__`, the python object **is not returned**, since there is no 
+`__setstate__`equivalent. Instead, the class name, the name of the module defining 
+the class and the data returned by `__bfkey__` is recovered.
+This should not pose a problem, since the main focus of the binfootprint module is
+the unique binary serialization of an object.
+To ensure deserialization use python's `pickle` module.
+
+```python
+class Point2(Point):
+    def __bfkey__(self):
+        return {'x': self.x, 'y': self.y}
+
+ob = Point2(5, 3)
+b = bf.dump(ob)
+
+ob_prime = bf.load(b)
+print("load on bfkey:", ob_prime)
+# load on bfkey: ('Point2', '__main__', {'x': 5, 'y': 3})
+```
+
+### numpy ndarrays
+
+Numpy's `ndarray` are supported by relaying on numpy's binary serialization 
+using [format.write_array()](https://numpy.org/devdocs/reference/generated/numpy.lib.format.write_array.html).
+
+```python
+import binfootprint as bf
+import numpy as np
+
+a = np.asarray([0, 1, 1, 0])
+b1 = bf.dump(a)
+```
+
+As expected, changing the shape or data type yield a different binary representation
+
+```python
+a2 = a1.reshape(2,2)
+b2 = bf.dump(a2)
+a3 = np.asarray(a1, dtype=np.complex128)
+b3 = bf.dump(a3)
+print("            MD5 of int array :", hashlib.md5(b1).hexdigest())
+# 949bfba1237c48007a066398f744a161
+print("MD5 of int array shape (2,2) :", hashlib.md5(b2).hexdigest())
+# e9049a19f82c6f282d65466a72360cd8
+print("        MD5 of complex array :", hashlib.md5(b3).hexdigest())
+# 2274ea54925d88ec4d53853050e55a82
+```
+
+# caching
+
+With the binaryfootprint module, caching function calls is straight forward.
+An implementation of such a cache using python's `shelve` for persistent storage
+is provided by the `ShelveCacheDec` class.
+
+```python
+@binfootprint.ShelveCacheDec()
+def area(p):
+    print(" * f(p(x={},y={})) called".format(p.x, p.y))
+    return p.x * p.y
+```
+
+It is safe to use the `ShelveCacheDec` with the same data location (`path`)   
+on different functions, since the name of the function and the name of the 
+module defining the function determined the name of the underlying database.  
+
+In addition to caching the decorator extends the function signature by the 
+kwarg `_cache_flag` which modifies the caching behavior as follows:
+
+- `_cache_flag = 'no_cache'`: Simple call of `fnc` with no caching.
+- `_cache_flag = 'update'`: Call `fnc` and update the cache with recent return value.
+- `_cache_flag = 'has_key'`: Return `True` if the call has already been cached, otherwise `False`.
+- `_cache_flag = 'cache_only'`: Raises a `KeyError` if the result has not been cached yet.
+
+```python
+p = Point(10, 10)
+print("first call results in")
+print(area(p))
+# * f(p(x=10,y=10)) called
+# 100
+
+print("second call results in")
+print(area(p))
+# 100
+p = Point(10, 11)
+
+print("f(p(10, 11)) is in cache?")
+print(area(p, _cache_flag='has_key'))
+# False
+```
+
+# pitfalls
+
+### ints and floats
+
+Since the binary representation between ints and floats is different, `1` and `1.0`
+will be treated as different things.
+This means that the cached value of a function call with an argument being `1` is
+not found when passing `1.0` as argument.
+Although the result of the function will most likely be the same.
+Obviously, the same holds true for numpy array of different `dtype`.
+
+# Parameter class
+
+Tha abstract base class `ABCParameter` allows to conveniently manage a set 
+of parameters.
+
+Relevant parameters, explicitly specified as data member via `__slots__` 
+mechanism, are returned by `__bfkey__` method (see above).
+Their order in the `__slots__` definition is irrelevant.
+**Importantly**, class members are included only if they are not `None`.
+In this way a parameter class definition can be extended while still being 
+able to reproduce the binary footprint of an older class definition.
+
+If present, the class member `__non_key__` has a special meaning.
+It is not included in the parameter-values list returned by `__bfkey__`.
+It is expected to be dictionary-like and allows storing 
+additional / informative information.
+This is also reflected by the string representation of the class.
+
+```python
+class Param(binfootprint.ABCParameter):
+    __slots__ = ["x", "y", "__non_key__"]
+
+    def __init__(self, x, y, msg=""):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.__non_key__ = dict()
+        self.__non_key__['msg'] = msg
+
+
+p = Param(3, 4.5)
+bfp = binfootprint.dump(p)
+print("{}\n has hex hash value {}...".format(
+    p, binfootprint.hash_hex_from_bin_data(bfp)[:6])
+)
+# x : 3
+# y : 4.5
+# --- extra info ---
+# msg : 
+# has hex hash value 38dbe8...
+
+p = Param(3, 4.5, msg="I told you, don't use x=3!")
+bfp = binfootprint.dump(p)
+print("{}\n has hex hash value {}...".format(
+    p, binfootprint.hash_hex_from_bin_data(bfp)[:6])
+)
+# x : 3
+# y : 4.5
+# --- extra info ---
+# msg : I told you, don't use x=3!
+# has hex hash value 38dbe8...
+```
---
+++ packages/binfootprint/binfootprint-1.2.2.tar.gz/binfootprint-1.2.2/binfootprint/binfootprint.py
@@ -0,0 +1,505 @@
+# python imports
+import functools
+import inspect
+from collections import namedtuple
+import io
+from math import ceil
+import struct
+import sys
+from types import FunctionType
+
+# third party imports
+import numpy as np
+from numpy.lib import format as np_format
+
+
+##########################################
+#   define constants
+##########################################
+
+_spec_types = (bool, type(None))
+
+_SPEC = 0x00  # True, False, None
+_INT = 0x01
+_FLOAT = 0x02
+_COMPLEX = 0x03
+_STR = 0x04
+_BYTES = 0x05  # only for python3, as bytes and str are equivalent in python2
+# _INT = 0x06
+_TUPLE = 0x07
+_NAMEDTUPLE = 0x08
+_NPARRAY = 0x09
+_LIST = 0x0A
+_GETSTATE = 0x0B  # only used when __bfkey__ is not present
+_DICT = 0x0C
+# _INT_NEG = 0x0D
+_BFKEY = 0x0E  # a special BF-Key member __bfkey__ is used if implemented, uses __getstate__ as fallback
+# _SP_CSC_MAT = 0x0F  # scipy csc sparse matrix
+_PARTIAL = 0x0F  # functools.partial
+
+# _VERS = 0x90        # use pickle to binfootprint numpy arrays / this breaks backwards compatibility
+_VERS = 0x91  # unify ints / this breaks backwards compatibility
+
+__max_int32 = +2147483647
+__min_int32 = -2147483648
+
+
+def get_version():
+    """return the current version used for dump"""
+    return _VERS
+
+
+class BFLoadError(Exception):
+    pass
+
+
+def _dump_spec(ob):
+    """serialize special object 'ob' (True, False, None)"""
+    if ob is True:
+        b = bytes([_SPEC, ord("T")])
+    elif ob is False:
+        b = bytes([_SPEC, ord("F")])
+    elif ob is None:
+        b = bytes([_SPEC, ord("N")])
+    else:
+        raise RuntimeError("object is not of 'special' kind!")
+    return b
+
+
+def _load_spec(b):
+    """convert bytes 'b' to special object (True, False, None)"""
+    assert b[0] == _SPEC
+    if b[1] == ord("T"):
+        return True, 2
+    elif b[1] == ord("F"):
+        return False, 2
+    elif b[1] == ord("N"):
+        return None, 2
+    else:
+        raise BFLoadError("internal error (unknown code for 'special' {})".format(b[1]))
+
+
+def _dump_int(ob):
+    """serialize an integer"""
+    ob_bytes = ob.to_bytes(ceil((ob.bit_length() + 1) / 8), "big", signed=True)
+    num_bytes = len(ob_bytes)
+
+    b = bytes([_INT])
+    b += struct.pack(">I", num_bytes)
+    b += ob_bytes
+    return b
+
+
+def _load_int(b):
+    """converts bytes 'b' to integer"""
+    assert b[0] == _INT
+    num_bytes = struct.unpack(">I", b[1:5])[0]
+    b_ = b[5 : 5 + num_bytes]
+    i = int.from_bytes(b_, byteorder="big", signed=True)
+    return i, 5 + num_bytes
+
+
+def _dump_float(ob):
+    """serialize 32bit float (double)"""
+    b = bytes([_FLOAT])
+    b += struct.pack(">d", ob)
+    return b
+
+
+def _load_float(b):
+    """convert bytes 'b' to 32bit float (double)"""
+    assert b[0] == _FLOAT
+    f = struct.unpack(">d", b[1:9])[0]
+    return f, 9
+
+
+def _dump_complex(ob):
+    """serialize 32bit complex (2x double)"""
+    b = bytes([_COMPLEX])
+    b += struct.pack(">d", ob.real)
+    b += struct.pack(">d", ob.imag)
+    return b
+
+
+def _load_complex(b):
+    """convert bytes 'b' to 32bit complex"""
+    assert b[0] == _COMPLEX
+    re = struct.unpack(">d", b[1:9])[0]
+    im = struct.unpack(">d", b[9:17])[0]
+    return re + 1j * im, 13
+
+
+def _dump_str(ob):
+    """serialize a string"""
+    b = bytes([_STR])
+    str_bytes = bytes(ob, "utf8")
+    num_bytes = len(str_bytes)
+    b += struct.pack(">I", num_bytes)
+    b += str_bytes
+    return b
+
+
+def _load_str(b):
+    """convert bytes 'b' to string"""
+    assert b[0] == _STR
+    num_bytes = struct.unpack(">I", b[1:5])[0]
+    s = str(b[5 : 5 + num_bytes], "utf8")
+    return s, 5 + num_bytes
+
+
+def _dump_bytes(ob):
+    """serialize a byte array"""
+    b = bytes([_BYTES])
+    num_bytes = len(ob)
+    b += struct.pack(">I", num_bytes)
+    b += ob
+    return b
+
+
+def _load_bytes(b):
+    """convert bytes 'b' to a byte array"""
+    assert b[0] == _BYTES
+    num_bytes = struct.unpack(">I", b[1:5])[0]
+    b_ = b[5 : 5 + num_bytes]
+    return b_, 5 + num_bytes
+
+
+def _dump_tuple(t):
+    """serialize a tuple"""
+    b = bytes([_TUPLE])
+    size = len(t)
+    b += struct.pack(">I", size)
+    for ti in t:
+        b += _dump(ti)
+    return b
+
+
+def _load_tuple(b):
+    """convert bytes 'b' to tuple"""
+    assert b[0] == _TUPLE
+    size = struct.unpack(">I", b[1:5])[0]
+    idx = 5
+    t = []
+    for i in range(size):
+        ob, len_ob = _load(b[idx:])
+        t.append(ob)
+        idx += len_ob
+    return tuple(t), idx
+
+
+def _dump_namedtuple(t):
+    """serialize a namedtuple"""
+    b = bytes([_NAMEDTUPLE])
+    size = len(t)
+    b += struct.pack(">I", size)
+    b += _dump(t.__class__.__name__)
+    for i in range(size):
+        b += _dump(t._fields[i])
+        b += _dump(t[i])
+    return b
+
+
+def _load_namedtuple(b):
+    """convert bytes 'b' to namedtuple"""
+    assert b[0] == _NAMEDTUPLE
+    size = struct.unpack(">I", b[1:5])[0]
+    class_name, len_ob = _load_str(b[5:])
+    idx = 5 + len_ob
+    t = []
+    fields = []
+    for i in range(size):
+        ob, len_ob = _load(b[idx:])
+        fields.append(ob)
+        idx += len_ob
+
+        ob, len_ob = _load(b[idx:])
+        t.append(ob)
+        idx += len_ob
+
+    np_class = namedtuple(class_name, fields)
+    np_obj = np_class(*t)
+
+    return np_obj, idx
+
+
+def _dump_list(t):
+    """serialize a list"""
+    b = bytes([_LIST])
+    size = len(t)
+    b += struct.pack(">I", size)
+    for ti in t:
+        b += _dump(ti)
+    return b
+
+
+def _load_list(b):
+    """convert bytes 'b' to list"""
+    assert b[0] == _LIST
+    size = struct.unpack(">I", b[1:5])[0]
+    idx = 5
+    t = []
+    for i in range(size):
+        ob, len_ob = _load(b[idx:])
+        t.append(ob)
+        idx += len_ob
+    return t, idx
+
+
+def _dump_np_array(np_array):
+    """
+    Serialize a numpy array - relays on numpy's 'format.write_array()' which implements the '.npy' file format.
+    Here we use version 1.0.
+
+    In the doc it says:
+
+        The ``.npy`` format is the standard binary file format in NumPy for
+        persisting a *single* arbitrary NumPy array on disk. The format stores all
+        of the shape and dtype information necessary to reconstruct the array
+        correctly even on another machine with a different architecture.
+        The format is designed to be as simple as possible while achieving
+        its limited goals.
+
+    so it should be suited for our purpose.
+    """
+    b = bytes([_NPARRAY])
+    nparray_bytes_io = io.BytesIO()
+    np_format.write_array(nparray_bytes_io, np_array, version=(1, 0))
+    nparray_bytes = nparray_bytes_io.getvalue()
+    size = len(nparray_bytes)
+    b += struct.pack(">I", size)
+    b += nparray_bytes
+    return b
+
+
+def _load_np_array(b):
+    """convert bytes 'b' to numpy array"""
+    assert b[0] == _NPARRAY
+    size = struct.unpack(">I", b[1:5])[0]
+    nparray_bytesIO = io.BytesIO(b[5 : size + 5])
+    npa = np_format.read_array(nparray_bytesIO)
+    return npa, size + 5
+
+
+def _dump_bf_key(ob):
+    """
+    serialize an object based on the '__bfkey__' member
+
+    This is similar to the __getstate__ member used to pickle python objects.
+    So in order to work, the returned object of __bfkey__() needs to be compatible
+    with the binaryfootprint serialization.
+    To uniquely identify objects, their class name and the name of the module which defines
+    the class is serialized as well.
+
+    New in version 0.3: add name of the module to serialization
+    """
+    b = bytes([_BFKEY])
+    bf_key = ob.__bfkey__()
+    cls_name = ob.__class__.__name__
+    cls_module = ob.__class__.__module__
+    b += _dump(str(cls_name))
+    b += _dump(str(cls_module))
+    b += _dump(bf_key)
+    return b
+
+
+def _load_bf_key(b):
+    """convert bytes 'b' to (class_name, class_module, bf_key)"""
+    assert b[0] == _BFKEY
+    cls_name, l_cls_name = _load_str(b[1:])
+    cls_module, l_cls_module = _load_str(b[l_cls_name + 1 :])
+    bf_key, l_state = _load(b[l_cls_name + l_cls_module + 1 :])
+    return (cls_name, cls_module, bf_key), l_cls_name + l_cls_module + l_state + 1
+
+
+def _dump_getstate(ob):
+    """
+    serialize an object based on the '__getstate__' member used by pickle
+
+    To uniquely identify objects, their class name and the name of the module which defines
+    the class is serialized as well.
+
+    New in version 0.3: add name of the module to serialization
+    """
+    b = bytes([_GETSTATE])
+    state = ob.__getstate__()
+    cls_name = ob.__class__.__name__
+    cls_module = ob.__class__.__module__
+    b += _dump(str(cls_name))
+    b += _dump(str(cls_module))
+    b += _dump(state)
+    return b
+
+
+def _load_getstate(b):
+    """
+    convert bytes 'b' to an object
+
+    Use __new__ constructor and __setstate__ to initialize the class, which is then returned.
+    Note that the module which defined the class needs to be loaded in order to
+    construct the object from binary data (similar to pickle.load()).
+    """
+    assert b[0] == _GETSTATE
+    cls_name, l_cls_name = _load_str(b[1:])
+    cls_module, l_cls_module = _load_str(b[l_cls_name + 1 :])
+    state, l_state = _load(b[l_cls_name + l_cls_module + 1 :])
+    m = sys.modules[cls_module]
+    cls = getattr(m, cls_name)
+    obj = cls.__new__(cls)
+    obj.__setstate__(state)
+    return obj, l_cls_name + l_cls_module + l_state + 1
+
+
+def _dump_dict(ob):
+    """
+    serialize a dictionary
+
+    Sorting the tuple containing the binary key and the binary value yields a unique
+    representation for the whole dictionary.
+    """
+    b = bytes([_DICT])
+    keys = ob.keys()
+    bin_keys = []
+    for k in keys:
+        try:
+            bin_keys.append((_dump(k), _dump(ob[k])))
+        except:
+            print("failed to dump key '{}'".format(k))
+            raise
+    b += _dump_list(sorted(bin_keys))
+    return b
+
+
+def _load_dict(b):
+    """convert bytes 'b' to dictionary"""
+    assert b[0] == _DICT
+    sorted_keys_value, l = _load_list(b[1:])
+    res_dict = {}
+    for i in range(len(sorted_keys_value)):
+        key = _load(sorted_keys_value[i][0])[0]
+        value = _load(sorted_keys_value[i][1])[0]
+        res_dict[key] = value
+
+    return res_dict, l + 1
+
+
+def _dump_partial(ob):
+    """
+    serialize a functools.partial function
+
+    we use __reduce__ to obtain the necessary information such as
+        - the original function and
+        - the given parameters
+    """
+    b = bytes([_PARTIAL])
+    if not isinstance(ob.func, FunctionType):
+        raise TypeError(
+            f"Function of partial is '{type(ob.func)}'. It needs to be a generic function (type FunctionType). "
+            + "Bounded (class) methods, Types (class constructors) or callable classes are not supported."
+        )
+    func = ob.func
+    qualname = func.__qualname__
+    module = func.__module__
+    s = inspect.signature(ob.func)
+    ba = s.bind_partial(*ob.args, **ob.keywords)
+    ba.apply_defaults()
+
+    state = (module, qualname, ba.arguments)
+    b += _dump_tuple(state)
+    return b
+
+
+def _load_partial(b):
+    """convert bytes 'b' to functools.partial object"""
+    assert b[0] == _PARTIAL
+    (module, qualname, bounded_arguments), l = _load_tuple(b[1:])
+    return (module, qualname, bounded_arguments), l + 1
+
+
+def _dump(ob):
+    if isinstance(ob, _spec_types):
+        return _dump_spec(ob)
+    elif isinstance(ob, int):
+        return _dump_int(ob)
+    elif isinstance(ob, float):
+        return _dump_float(ob)
+    elif isinstance(ob, complex):
+        return _dump_complex(ob)
+    elif isinstance(ob, str):
+        return _dump_str(ob)
+    elif isinstance(ob, bytes):
+        return _dump_bytes(ob)
+    elif isinstance(ob, tuple):
+        if hasattr(ob, "_fields"):
+            return _dump_namedtuple(ob)
+        else:
+            return _dump_tuple(ob)
+    elif isinstance(ob, list):
+        return _dump_list(ob)
+    elif isinstance(ob, np.ndarray):
+        return _dump_np_array(ob)
+    elif isinstance(ob, dict):
+        return _dump_dict(ob)
+    elif hasattr(ob, "__bfkey__"):
+        return _dump_bf_key(ob)
+    elif isinstance(ob, functools.partial):
+        return _dump_partial(ob)
+    elif hasattr(ob, "__getstate__") and ob.__getstate__() is not None:
+        return _dump_getstate(ob)
+    else:
+        raise TypeError("unsupported type for dump '{}' ({})".format(type(ob), ob))
+
+
+def _load(b):
+    identifier = b[0]
+    if isinstance(identifier, str):
+        identifier = ord(identifier)
+    if identifier == _SPEC:
+        return _load_spec(b)
+    elif identifier == _INT:
+        return _load_int(b)
+    elif identifier == _FLOAT:
+        return _load_float(b)
+    elif identifier == _COMPLEX:
+        return _load_complex(b)
+    elif identifier == _STR:
+        return _load_str(b)
+    elif identifier == _BYTES:
+        return _load_bytes(b)
+    elif identifier == _TUPLE:
+        return _load_tuple(b)
+    elif identifier == _NAMEDTUPLE:
+        return _load_namedtuple(b)
+    elif identifier == _LIST:
+        return _load_list(b)
+    elif identifier == _NPARRAY:
+        return _load_np_array(b)
+    elif identifier == _DICT:
+        return _load_dict(b)
+    elif identifier == _BFKEY:
+        return _load_bf_key(b)
+    elif identifier == _PARTIAL:
+        return _load_partial(b)
+    elif identifier == _GETSTATE:
+        return _load_getstate(b)
+    else:
+        raise BFLoadError(
+            "internal error (unknown identifier '{}')".format(hex(identifier))
+        )
+
+
+def dump(ob):
+    """
+    returns the binary footprint of the object 'ob' as bytes
+    """
+    return bytes([_VERS]) + _dump(ob)
+
+
+def load(b):
+    """
+    reconstruct the object from the binary footprint given as bytes 'b'
+    """
+    vers = b[0]
+    if vers != _VERS:
+        raise BFLoadError("wrong version (converter needed!)")
+
+    return _load(b[1:])[0]
