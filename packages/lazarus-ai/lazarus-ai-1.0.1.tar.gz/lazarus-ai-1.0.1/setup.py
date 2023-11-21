41e20553d330f45d2a3b44182b0a210a6d72ccea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 05:03:19 +0000

    Add package lazarus-ai/1.0.1/lazarus-ai-1.0.1.tar.gz

---
+++ packages/lazarus-ai/lazarus-ai-1.0.1.tar.gz/lazarus-ai-1.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+setup(
+  name = 'lazarus-ai',
+  packages = ['lazarus_ai', 'errors', 'utils'],
+  version = '1.0.1',
+  license='MIT',
+  description = 'Lazarus AI Python Library',
+  author = 'Caroline Abel, KK Barrows, Alvin Xie',
+  author_email = 'caroline@lazarus.enterprises, kathleen@lazarus.enterprises, alvin@lazarus.enterprises',
+  url = 'https://www.lazarusforms.com/',
+  download_url = 'https://github.com/Lazarus-AI/lazarus-ai-python/archive/refs/tags/v1.0.1.tar.gz',
+  install_requires=[
+          'requests',
+          'stringcase',
+  ],
+  project_urls={
+    "Bug Tracker": "https://github.com/Lazarus-AI/lazarus-ai-python/issues",
+    "Documentation": "https://lazarus.stoplight.io/docs/lazarus-forms/welcome",
+    "Source Code": "https://github.com/Lazarus-AI/lazarus-ai-python",
+  },
+  classifiers=[
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
