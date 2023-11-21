3e3a5c14f8b883f5e47b5c4236c116e8a038fbbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-21 03:04:23 +0000

    Add package noos-inv/0.0.14/noos_inv-0.0.14.tar.gz

---
+++ packages/noos-inv/noos_inv-0.0.14.tar.gz/noos_inv-0.0.14/src/noos_inv/local.py
@@ -0,0 +1,22 @@
+from invoke import Collection, task
+
+from . import utils
+
+
+# Local development workflow
+
+
+@task(help={"force": "Whether to destroy the existing file first"})
+def dotenv(ctx, template="./dotenv.tpl", target="./.env", force=False):
+    """Create local dotenv file."""
+    utils.check_path(template)
+    try:
+        utils.check_path(target)
+        if force:
+            raise utils.PathNotFound
+    except utils.PathNotFound:
+        ctx.run(f"cp {template} {target}")
+
+
+ns = Collection("local")
+ns.add_task(dotenv)
