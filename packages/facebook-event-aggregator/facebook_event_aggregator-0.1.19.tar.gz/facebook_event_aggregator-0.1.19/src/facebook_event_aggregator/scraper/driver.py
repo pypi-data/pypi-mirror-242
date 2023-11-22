a802b61d5bf55a1c9389f016794a3db3c56d55bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 04:04:35 +0000

    Add package facebook-event-aggregator/0.1.19/facebook_event_aggregator-0.1.19.tar.gz

---
+++ packages/facebook-event-aggregator/facebook_event_aggregator-0.1.19.tar.gz/facebook_event_aggregator-0.1.19/src/facebook_event_aggregator/scraper/driver.py
@@ -0,0 +1,48 @@
+# Built-in imports
+from platform import system, machine
+
+# Package imports
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Setup Driver
+def setup_driver(chromedriver_path: str=None, headless=False, remote_debugging_port = 0, extra_opts: list[str] = []) -> webdriver.Chrome:
+    options = Options()
+
+    if (headless):
+        headless_opts = [
+            "--headless=new",
+            "--disable-gpu",
+            "--window-size=1920,1200",
+            "--ignore-certificate-errors",
+            "--disable-extensions",
+            #"--no-sandbox",
+            #"--disable-dev-shm-usage",
+            "--remote-debugging-port=" + str(remote_debugging_port),
+            #"--disable-setuid-sandbox"
+        ]
+        for opt in headless_opts:
+            options.add_argument(opt)
+            #print(opt)
+        for opt in extra_opts:
+            options.add_argument("--" + opt)
+    
+    # Much thanks to https://stackoverflow.com/a/71042821
+    try:
+        if not chromedriver_path:
+            service = Service(ChromeDriverManager().install())
+        else:
+            #display = Display(visible=0, size=(1920,1200))
+            #display.start()
+            service = Service(chromedriver_path)
+            #options.binary_location = raspbian_chromium
+    except:
+        # Attempt selenium fallback
+        service = Service()
+                
+
+    return webdriver.Chrome(service=service, options=options)
+
+
---
+++ packages/facebook-event-aggregator/facebook_event_aggregator-0.1.19.tar.gz/facebook_event_aggregator-0.1.19/src/facebook_event_aggregator/utils/fb_regexes.py
@@ -0,0 +1,59 @@
+# Built-in imports
+import re
+
+""" Regexes to parse input from Facebook """
+
+"""
+Example:
+    NOV
+    11
+    Text
+
+    Returns: 
+        Nov
+        11
+"""
+re_three_letter_two_digit_date = r"^[a-zA-Z]{3}\W\d{1,2}$"
+
+"""
+Example:
+    Fri 19:00 UTC+01 · 84 guests
+
+    returns: Fri 19:00 UTC+01
+"""
+re_utc_time = r".*UTC\+\d{2}"
+
+"""
+Example:
+    NOV
+    19
+    Name - 32 guests
+
+    Returns: Name - 32 guests 
+"""
+re_guests = r".*guest.*"
+
+# Matches any line with 1 or more characters
+re_line_with_characters = r"^.{1,}$"
+
+"""
+Example:
+    WED, 5 NOV AT 08:30 UTC+01 AND 1 MORE
+
+    Returns: UTC+01 AND 1 MORE
+
+"""
+re_utc_and_more = r"UTC\+\d* AND \d{1,} MORE"
+
+def regex_in(data, pattern):
+    return bool(re.search(pattern, data))
+
+def find_and_remove(data, pattern):
+    try:
+        found = re.search(pattern, data, flags=re.MULTILINE).group()
+        data = data.replace(found, "")
+    except AttributeError as e:
+        print("Error during {0} Regex search".format(pattern))
+        found = ""
+    finally:
+        return data, found
