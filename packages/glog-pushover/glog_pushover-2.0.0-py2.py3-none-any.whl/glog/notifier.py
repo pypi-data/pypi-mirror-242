16700ade9a6f4236cce29de9f19ad813d0e47390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 03:05:02 +0000

    Add package glog-pushover/2.0.0/glog_pushover-2.0.0-py2.py3-none-any.whl

---
+++ packages/glog-pushover/glog_pushover-2.0.0-py2.py3-none-any.whl/glog/notifier.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""This script handles the Pushover integration"""
+import requests
+from pydantic import BaseModel, StrictStr, validator
+
+
+class ConfigData(BaseModel):
+    """This class holds the configuration data for the Pushover integration"""
+    token: StrictStr
+    user: StrictStr
+    message: StrictStr
+    service: StrictStr = 'pushover'
+    nfty_host: StrictStr = 'https://ntfy.sh'
+
+    @validator('message')
+    def reformat_message(cls, value):
+        """
+        This is the normal format for the message
+        logname date time [level  ] message
+
+        This function replaces that format with
+        [logname - level] message
+        """
+        value = value.split(' ')
+        logname = value[0]
+        level = value[3].replace('[', '')
+        message = ' '.join(value[4:])
+        return_message = f'[{logname} - {level}] {message}'
+        return_message = return_message.replace('    ]', '').replace('  ]', '')
+        return return_message
+    
+    @validator('service')
+    def check_service(cls, value):
+        """
+        This function checks that the service is either pushover or nfty
+        """
+        if value not in ['pushover', 'nfty']:
+            raise ValueError('service must be either pushover or nfty')
+        return value
+
+
+class Notifier():
+    """This is the main class of the module"""
+
+    def __init__(self, config_data):
+        self.config_data = ConfigData(**config_data)
+        if self.config_data.service == 'pushover':
+            self.send_pushover_message()
+        elif self.config_data.service == 'nfty':
+            self.send_nfty_message()
+        else:
+            raise ValueError('service must be either pushover or nfty')
+
+    def send_pushover_message(self):
+        """This function sends the message via pushover"""
+        try:
+            requests.post(
+                'https://api.pushover.net/1/messages.json',
+                files={
+                    'token': (None, self.config_data.token),
+                    'user': (None, self.config_data.user),
+                    'message': (None, self.config_data.message),
+                },
+                timeout=300
+            )
+        except requests.exceptions.RequestException as error:
+            print(error)
+
+    def send_nfty_message(self):
+        """This function sends the message via nfty"""
+        try:
+            requests.post(
+                f'{self.config_data.nfty_host}',
+                data=self.config_data.message.encode(encoding='utf-8'),
+                timeout=300
+            )
+        except requests.exceptions.RequestException as error:
+            print(error)
