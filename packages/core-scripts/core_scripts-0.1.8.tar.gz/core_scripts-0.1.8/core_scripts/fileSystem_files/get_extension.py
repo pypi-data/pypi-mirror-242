9ccc29ad8845af65eba886e017b4c4256f14ff4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 04:04:21 +0000

    Add package core-scripts/0.1.8/core_scripts-0.1.8.tar.gz

---
+++ packages/core-scripts/core_scripts-0.1.8.tar.gz/core_scripts-0.1.8/core_scripts/fileSystem_files/get_extension.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+    pass
+    # from command_cmd import command_cmd
+else:
+    pass
+    # from .command_cmd import command_cmd
+
+
+from os.path import splitext
+
+def get_extension(file):
+    fileExtencion = splitext(file)[1].partition(".")[2]
+    return fileExtencion
+"optiene la extencion del archivo"
+
+print(get_extension("get_extension.py"))
