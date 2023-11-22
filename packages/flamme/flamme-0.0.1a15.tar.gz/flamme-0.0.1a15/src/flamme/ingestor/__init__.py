1f4fd3c0c2931103714bcdd66951fdb266fc26f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 16:05:35 +0000

    Add package flamme/0.0.1a15/flamme-0.0.1a15.tar.gz

---
+++ packages/flamme/flamme-0.0.1a15.tar.gz/flamme-0.0.1a15/src/flamme/ingestor/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+__all__ = [
+    "BaseIngestor",
+    "CsvIngestor",
+    "ParquetIngestor",
+    "PreprocessorIngestor",
+    "is_ingestor_config",
+    "setup_ingestor",
+]
+
+from flamme.ingestor.base import BaseIngestor, is_ingestor_config, setup_ingestor
+from flamme.ingestor.csv import CsvIngestor
+from flamme.ingestor.parquet import ParquetIngestor
+from flamme.ingestor.preprocessor import PreprocessorIngestor
