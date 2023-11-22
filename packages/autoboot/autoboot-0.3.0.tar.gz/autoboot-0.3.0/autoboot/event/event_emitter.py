f19c7b2bb7511dc6ae3cb8673e5d3f79b4a58257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 16:06:00 +0000

    Add package autoboot/0.3.0/autoboot-0.3.0.tar.gz

---
+++ packages/autoboot/autoboot-0.3.0.tar.gz/autoboot-0.3.0/autoboot/event/event_emitter.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+from typing import TypeVar, Generic, Callable
+from autoboot.util.type_reflect import get_generic_type
+
+T = TypeVar('T')
+
+@dataclass
+class Event(Generic[T]):
+  data: T
+  source: any = None
+  
+
+class EventEmitter(object):
+  """
+  EventEmitter is a class that allows you to register event listeners and
+  emit events.
+  """
+
+  def __init__(self):
+    self._listeners = {}
+
+  def on(self, action="default"):
+    """
+    Register a callback for an event.
+    """
+    def decorator(fn: Callable[..., None]) -> None:
+      self._listeners.setdefault(action, []).append(fn)
+    return decorator
+
+  def emit(self, event: Event[T], action = "default"):
+    """
+    Emit event with action
+    """
+    
+    check_type = action == "default"
+    for f in self._listeners.get(action, []):
+      if not check_type:
+        f(event)
+      else:
+        generic_type = get_generic_type(f, "event", 0)
+        if generic_type and isinstance(event.data, generic_type):
+          f(event)
+      
+  def clear(self):
+    self._listeners.clear()
