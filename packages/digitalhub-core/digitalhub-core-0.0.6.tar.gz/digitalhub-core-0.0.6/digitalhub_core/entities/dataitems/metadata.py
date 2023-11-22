53ee7e8b1a7a13fcfea541da9634f370833cce21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 03:04:11 +0000

    Add package digitalhub-core/0.0.6/digitalhub-core-0.0.6.tar.gz

---
+++ packages/digitalhub-core/digitalhub-core-0.0.6.tar.gz/digitalhub-core-0.0.6/digitalhub_core/entities/dataitems/metadata.py
@@ -0,0 +1,46 @@
+"""
+Dataitem metadata module.
+"""
+from __future__ import annotations
+
+from digitalhub_core.entities._base.metadata import Metadata
+
+
+class DataitemMetadata(Metadata):
+    """
+    A class representing Dataitem metadata.
+    """
+
+    def __init__(
+        self,
+        project: str,
+        name: str,
+        version: str,
+        created: str | None = None,
+        updated: str | None = None,
+        description: str | None = None,
+        embedded: bool = False,
+    ) -> None:
+        """
+        Constructor.
+
+        Parameters
+        ----------
+        name : str
+            Name of the object.
+        version : str
+            Version of the object.
+        description : str
+            Description of the entity.
+        embedded : bool
+            If True, embed object in backend.
+
+        See Also
+        --------
+        Metadata.__init__
+        """
+        super().__init__(project, created, updated)
+        self.name = name
+        self.version = version
+        self.description = description
+        self.embedded = embedded
