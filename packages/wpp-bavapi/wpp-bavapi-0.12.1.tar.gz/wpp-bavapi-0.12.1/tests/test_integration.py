a0cc8ba59f291cc36556009a8e99408105cee59f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 06:04:17 +0000

    Add package wpp-bavapi/0.12.1/wpp-bavapi-0.12.1.tar.gz

---
+++ packages/wpp-bavapi/wpp-bavapi-0.12.1.tar.gz/wpp-bavapi-0.12.1/tests/test_integration.py
@@ -0,0 +1,95 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring
+# pylint: disable=redefined-outer-name, protected-access
+
+import os
+import ssl
+from typing import Any, Callable, Dict, Generator, List
+
+import pandas as pd
+import pytest
+from dotenv import load_dotenv
+
+import bavapi
+from bavapi import filters
+from bavapi.query import Query
+
+
+@pytest.fixture(scope="module", autouse=True)
+def load_env() -> Generator[None, None, None]:
+    load_dotenv()
+    yield
+
+
+@pytest.mark.e2e
+def test_raw_query():
+    result: List[Dict[str, Any]] = []
+    for _ in range(5):
+        try:
+            result = bavapi.raw_query(
+                os.environ["BAV_API_KEY"],
+                "countries",
+                Query(filters={"is_active": 1}, include="region", max_pages=2),
+            )
+            break
+        except ssl.SSLError:
+            print("Failed due to SSL error...")
+            continue
+
+    assert result
+    assert len(result) == 200
+    assert "region" in result[0]
+
+
+@pytest.mark.e2e
+def test_with_filters_one_page():
+    result = pd.DataFrame()
+    for _ in range(5):
+        try:
+            result = bavapi.studies(
+                os.environ["BAV_API_KEY"],
+                filters=filters.StudiesFilters(active=1),
+                include="country",
+                page=1,
+                per_page=25,
+                max_pages=1,
+            )
+            break
+        except ssl.SSLError:
+            print("Failed due to SSL error...")
+            continue
+
+    assert "country_id" in result
+    assert result["is_active"].unique()[0] == 1
+    assert result.shape[0] == 25
+
+
+@pytest.mark.e2e
+@pytest.mark.parametrize(
+    ("endpoint", "filters"),
+    (
+        ("audiences", {}),
+        ("brand_metric_groups", {}),
+        ("brand_metrics", {}),
+        ("brands", {}),
+        ("brandscape_data", {"studies": 546}),
+        ("categories", {}),
+        ("collections", {}),
+        ("sectors", {}),
+        ("studies", {}),
+    ),
+)
+def test_endpoints(endpoint: str, filters: Dict[str, Any]):
+    func: Callable[..., pd.DataFrame] = getattr(bavapi, endpoint)
+    result = pd.DataFrame()
+    for _ in range(5):
+        try:
+            result = func(
+                os.environ["BAV_API_KEY"], filters=filters, max_pages=2, per_page=25
+            )
+            break
+        except ssl.SSLError:
+            print("Failed due to SSL error...")
+            continue
+
+    assert 0 < result.shape[0] <= 50
+    assert "id" in result
---
+++ packages/wpp-bavapi/wpp-bavapi-0.12.1.tar.gz/wpp-bavapi-0.12.1/tests/test_query.py
@@ -0,0 +1,73 @@
+# pylint: disable=missing-function-docstring, missing-module-docstring
+
+import pytest
+
+from bavapi import filters
+from bavapi.query import Query
+
+
+def test_with_page():
+    assert Query(filters={"audience": 1}).with_page(2, 25) == Query(
+        page=2, per_page=25, filters={"audience": 1}
+    )
+
+
+def test_paginated():
+    paginated = tuple(Query(filters={"audience": 1}).paginated(10, 100))
+
+    assert len(paginated) == 10
+    assert [p.page for p in paginated] == list(range(1, 11))
+
+
+def test_to_params_no_filters_no_fields():
+    assert Query(id=1).to_params("test") == {}
+
+
+@pytest.mark.parametrize(
+    "query",
+    (
+        Query(id=1, filters={"name": 1}, fields="metric"),
+        Query(
+            id=1,
+            filters=filters.FountFilters(**{"name": 1}),  # type: ignore[arg-type]
+            fields="metric",
+        ),
+    ),
+)
+def test_to_params(query: Query):
+    assert query.to_params("test") == {
+        "filter[name]": 1,
+        "fields[test]": "metric",
+    }
+
+
+def test_to_params_dict_filters():
+    query = Query(id=1, filters={"name": 1})
+    query.filters = {"name": 1}
+
+    assert query.to_params("test") == {"filter[name]": 1}
+
+
+@pytest.mark.parametrize(
+    "kwargs",
+    (
+        {"page": 2},
+        {"max_pages": 1},
+    ),
+)
+def test_is_single_page(kwargs: dict):
+    query = Query(**kwargs)
+    assert query.is_single_page()
+
+
+@pytest.mark.parametrize(
+    "kwargs",
+    (
+        {},
+        {"per_page": 10, "max_pages": 10},
+        {"page": 2, "per_page": 10, "max_pages": 10},
+    ),
+)
+def test_is_not_single_page(kwargs: dict):
+    query = Query(**kwargs)
+    assert not query.is_single_page()
