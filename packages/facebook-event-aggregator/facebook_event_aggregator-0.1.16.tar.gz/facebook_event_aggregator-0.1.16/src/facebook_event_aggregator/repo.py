05ca6fed41be52ef13474b4703b8a2b8d1c4bb3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 04:04:03 +0000

    Add package facebook-event-aggregator/0.1.16/facebook_event_aggregator-0.1.16.tar.gz

---
+++ packages/facebook-event-aggregator/facebook_event_aggregator-0.1.16.tar.gz/facebook_event_aggregator-0.1.16/src/facebook_event_aggregator/repo.py
@@ -0,0 +1,42 @@
+# Built-in imports
+from subprocess import run
+from os.path import realpath, isdir, join, exists
+from datetime import datetime
+
+from git import Repo 
+
+"""This module handles Git repo stuff"""
+
+def _normalize_path(dir) -> str:
+    return realpath(dir)
+
+def _get_repo_path(parent_dir_path, repo_path) -> str:
+    repo_path = _normalize_path(join(str(parent_dir_path), str(repo_path)))
+    return repo_path
+
+def _generate_commit_message():
+    current_date = datetime.now().strftime("%d %m %Y")
+    commit_msg = "Update from {}".format(current_date)
+    return commit_msg
+
+
+def clone_or_pull_repo(parent_dir_path, clone_dirname, repo_url: str):
+    repo_path = _get_repo_path(parent_dir_path, clone_dirname)
+
+    if not exists(repo_path):
+        return Repo.clone_from(repo_url, repo_path)
+    else:
+        return Repo(repo_path).remote("origin").pull()
+
+    
+def update_repo(parent_dir_path, clone_dirname, commit_msg=None):
+    repo_path = _get_repo_path(parent_dir_path, clone_dirname)
+    
+    if commit_msg is None:
+        commit_msg = _generate_commit_message()
+    
+    repo = Repo(repo_path)
+    
+    repo.index.add(".")
+    repo.index.commit(commit_msg)
+    repo.remote("origin").push()
---
+++ packages/facebook-event-aggregator/facebook_event_aggregator-0.1.16.tar.gz/facebook_event_aggregator-0.1.16/src/tests/export/test_export_html.py
@@ -0,0 +1,31 @@
+
+from pathlib import Path
+
+from src.facebook_event_aggregator.Event import Event
+from src.facebook_event_aggregator.export.to_html import events_to_html
+
+from .helpers import example, check_event_contents_in
+
+def test_events_to_html(tmp_path: Path):
+    events_to_html(
+        events=example["events"],
+        absolute_img_dir=tmp_path.joinpath("images"),
+        relative_from_html_img_dir="images/",
+        page_urls=example["page_urls"],
+        output_dir=tmp_path,
+        title=example["title"],
+        host_domain=example["host_domain"],
+        timezone=example["timezone"]
+    )
+
+    with open(tmp_path.joinpath("index.html"), encoding="UTF-8") as file:
+        data = file.read()
+        for item in example.values():
+            if type(item) == list:
+                for value in item:
+                    if not isinstance(value, Event):
+                        assert value in data
+                    else:
+                        check_event_contents_in(value, data)
+            else:
+                assert item in data
