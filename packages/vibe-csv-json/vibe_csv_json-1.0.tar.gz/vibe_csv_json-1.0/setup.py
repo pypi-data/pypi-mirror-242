e79f07522815f6a5c7bf7372ad7da98997d5eb86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 03:04:09 +0000

    Add package vibe-csv-json/1.0/vibe_csv_json-1.0.tar.gz

---
+++ packages/vibe-csv-json/vibe_csv_json-1.0.tar.gz/vibe_csv_json-1.0/setup.py
@@ -0,0 +1,16 @@
+
+
+from setuptools import setup, find_packages
+
+setup(name = 'vibe_csv_json',
+      version = 1.0,
+      author = 'NegreMan-001, Mardodo',
+      author_email = 'danydaniedany1@gmail.com, mardocheulysse54@gmail.com',
+      maintainer = 'NegreMan-001, Mardodo',
+      maintainer_email = 'danydaniedany1@gmail.com, mardocheulysse54@gmail.com',
+      classifiers = ['Topic :: Education', 'Topic :: Documentation'],
+      packages = find_packages(),
+      description = 'demonstration de creation d\'un package Python',
+      license = 'GPL V3',
+      platforms = 'ALL',
+     )
