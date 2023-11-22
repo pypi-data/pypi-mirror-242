03452e7210f2a6f39c2184bab0e63135c83cfe4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 15:05:00 +0000

    Add package nettoolkit-common/0.0.3/nettoolkit_common-0.0.3.tar.gz

---
+++ packages/nettoolkit-common/nettoolkit_common-0.0.3.tar.gz/nettoolkit_common-0.0.3/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+docs_extras = [
+    'Sphinx >= 3.0.0',  # Force RTD to use >= 3.0.0
+    'docutils',
+    'pylons-sphinx-themes >= 1.0.8',  # Ethical Ads
+    'pylons_sphinx_latesturl',
+    'repoze.sphinx.autointerface',
+    'sphinx-copybutton',
+    'sphinxcontrib-autoprogram',
+]
+
+setuptools.setup(
+    name="nettoolkit_common",
+    version="0.0.3",
+    author="ALIASGAR - ALI",
+    author_email="aholo2000@gmail.com",
+    description="Networking Toolkit common Functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aliasgar1978/nettoolkit_common",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires= ['pandas', ],
+    # extras_require={'docs': docs_extras},
+)
+
