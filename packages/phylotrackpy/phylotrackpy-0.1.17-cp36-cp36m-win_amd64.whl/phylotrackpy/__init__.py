1d582b873e3f8948bc5c00d11656d83401c6d8a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 14:04:03 +0000

    Add package phylotrackpy/0.1.17/phylotrackpy-0.1.17-cp36-cp36m-win_amd64.whl

---
+++ packages/phylotrackpy/phylotrackpy-0.1.17-cp36-cp36m-win_amd64.whl/phylotrackpy/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for phylotrackpy."""
+
+__author__ = """Emily Dolson"""
+__email__ = 'emilydolson@gmail.com'
+__version__ = '0.1.17'
+
+import phylotrackpy.systematics
+
+__all__ = [
+    "systematics",
+]
