fe74d10f6be5a2330a47a00410fb2cd2c6bb2ea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 04:04:21 +0000

    Add package faststream/0.2.15/faststream-0.2.15.tar.gz

---
+++ packages/faststream/faststream-0.2.15.tar.gz/faststream-0.2.15/faststream/__about__.py
@@ -0,0 +1,8 @@
+"""Simple and fast framework to create message brokers based microservices"""
+__version__ = "0.2.15"
+
+
+INSTALL_YAML = """
+To generate YAML documentation, please install dependencies:\n
+pip install PyYAML
+"""
