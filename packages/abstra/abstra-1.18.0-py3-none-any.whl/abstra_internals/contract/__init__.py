67e7f193c631f271a8587e26a979840d9dad3450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 05:04:12 +0000

    Add package abstra/1.18.0/abstra-1.18.0-py3-none-any.whl

---
+++ packages/abstra/abstra-1.18.0-py3-none-any.whl/abstra_internals/contract/__init__.py
@@ -0,0 +1,7 @@
+from . import forms as forms_contract  # exported
+from . import common as common_contract  # exported
+from .common import Message, StdioMessage  # exported
+
+
+def should_send(msg: Message, is_preview: bool) -> bool:
+    return is_preview or not isinstance(msg, StdioMessage)
