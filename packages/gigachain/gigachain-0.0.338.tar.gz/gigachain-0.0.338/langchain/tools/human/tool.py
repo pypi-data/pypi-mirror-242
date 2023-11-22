4f0913c128bc5012e2275d165137da6b290d086e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 03:04:01 +0000

    Add package gigachain/0.0.338/gigachain-0.0.338.tar.gz

---
+++ packages/gigachain/gigachain-0.0.338.tar.gz/gigachain-0.0.338/langchain/tools/human/tool.py
@@ -0,0 +1,34 @@
+"""Tool for asking human input."""
+
+from typing import Callable, Optional
+
+from langchain.callbacks.manager import CallbackManagerForToolRun
+from langchain.pydantic_v1 import Field
+from langchain.tools.base import BaseTool
+
+
+def _print_func(text: str) -> None:
+    print("\n")
+    print(text)
+
+
+class HumanInputRun(BaseTool):
+    """Tool that asks user for input."""
+
+    name: str = "human"
+    description: str = (
+        "Ты можешь попросить человека о помощи, когда ты думаешь, что "
+        "застрял или не уверен, что делать дальше. "
+        "Ввод должен быть вопросом для человека."
+    )
+    prompt_func: Callable[[str], None] = Field(default_factory=lambda: _print_func)
+    input_func: Callable = Field(default_factory=lambda: input)
+
+    def _run(
+        self,
+        query: str,
+        run_manager: Optional[CallbackManagerForToolRun] = None,
+    ) -> str:
+        """Use the Human input tool."""
+        self.prompt_func(query)
+        return self.input_func()
