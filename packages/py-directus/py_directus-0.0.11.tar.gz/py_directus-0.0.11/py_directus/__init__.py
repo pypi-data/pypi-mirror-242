23d6dfe1cb2fa7cb6f2f72f876391b26efff7f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 03:03:43 +0000

    Add package py-directus/0.0.11/py_directus-0.0.11.tar.gz

---
+++ packages/py-directus/py_directus-0.0.11.tar.gz/py_directus-0.0.11/py_directus/__init__.py
@@ -0,0 +1,9 @@
+from .directus import Directus
+from .filter import F
+
+try:
+    from .fast_api.auth import HeaderAndCookieBearer
+    from .fast_api.globals import directus_admin, directus_session, directus_auth
+    from .fast_api import globals
+except ImportError:
+    pass
