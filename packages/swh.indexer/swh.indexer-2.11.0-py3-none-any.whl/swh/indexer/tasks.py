b865f3316844c18b190f60f20142063d5e41b934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 04:04:39 +0000

    Add package swh.indexer/2.11.0/swh.indexer-2.11.0-py3-none-any.whl

---
+++ packages/swh.indexer/swh.indexer-2.11.0-py3-none-any.whl/swh/indexer/tasks.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2016-2023  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+
+from celery import shared_task
+
+from .fossology_license import FossologyLicenseIndexer
+from .metadata import OriginMetadataIndexer
+from .mimetype import MimetypeIndexer
+from .rehash import RecomputeChecksums
+
+
+@shared_task(name=__name__ + ".OriginMetadata")
+def origin_metadata(*args, **kwargs):
+    return OriginMetadataIndexer().run(*args, **kwargs)
+
+
+@shared_task(name=__name__ + ".ContentFossologyLicense")
+def fossology_license(*args, **kwargs):
+    return FossologyLicenseIndexer().run(*args, **kwargs)
+
+
+@shared_task(name=__name__ + ".RecomputeChecksums")
+def _recompute_checksums(*args, **kwargs):
+    return RecomputeChecksums().run(*args, **kwargs)
+
+
+@shared_task(name=__name__ + ".ContentMimetype")
+def mimetype(*args, **kwargs):
+    return MimetypeIndexer().run(*args, **kwargs)
