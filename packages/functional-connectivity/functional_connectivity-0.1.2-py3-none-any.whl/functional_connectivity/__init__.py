0a16954e1178610eaba221f92711e1a2a352c287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 16:05:42 +0000

    Add package functional-connectivity/0.1.2/functional_connectivity-0.1.2-py3-none-any.whl

---
+++ packages/functional-connectivity/functional_connectivity-0.1.2-py3-none-any.whl/functional_connectivity/__init__.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# functional-connectivity -- Sensing functional connectivity in the brain, in Python
+#
+# Copyright (C) 2023-2024 Tzu-Chi Yen <tzuchi.yen@colorado.edu>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+from .utils import *
+
+__package__ = 'functional_connectivity'
+__title__ = 'functional_connectivity: sensing functional connectivity in the brain, in Python.'
+__description__ = ''
+__copyright__ = 'Copyright (C) 2023-2024 Tzu-Chi Yen'
+__license__ = "LGPL version 3 or above"
+__author__ = """\n""".join([
+    'Tzu-Chi Yen <tzuchi.yen@colorado.edu>',
+])
+__URL__ = ""
+__version__ = '0.1.1'
+__release__ = '0.1'
+
+
+__all__ = [
+    "__author__",
+    "__URL__",
+    "__version__",
+    "__copyright__",
+]
