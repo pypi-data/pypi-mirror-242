1946916b8b2c134576819f55f32f909e0fb06dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 03:04:43 +0000

    Add package fastapi-cachette/0.1.6/fastapi_cachette-0.1.6-py3-none-any.whl

---
+++ packages/fastapi-cachette/fastapi_cachette-0.1.6-py3-none-any.whl/fastapi_cachette/codecs/vanilla.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# coding:utf-8
+# Copyright (C) 2022-2023, All rights reserved.
+# FILENAME:  codecs/vanilla.py
+# VERSION: 	 0.1.6
+# CREATED: 	 2022-04-06 15:38
+# AUTHOR: 	 Sitt Guruvanich <aekazitt@gmail.com>
+# DESCRIPTION:
+#
+# HISTORY:
+# *************************************************************
+### Standard Packages ###
+from typing import Any
+
+### Local Modules ##
+from fastapi_cachette.codecs import Codec
+
+
+class VanillaCodec(Codec):
+    def dumps(self, obj: Any) -> bytes:
+        return str(obj).encode("utf-8")
+
+    def loads(self, data: bytes) -> Any:
+        return data.decode("utf-8")
