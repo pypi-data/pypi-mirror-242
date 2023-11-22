038aebc4c756475170ce722d4a65c8d5069ee65a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 03:04:11 +0000

    Add package caplena/1.2.0/caplena-1.2.0.tar.gz

---
+++ packages/caplena/caplena-1.2.0.tar.gz/caplena-1.2.0/caplena/resources/__init__.py
@@ -0,0 +1,13 @@
+from caplena.endpoints.projects_endpoint import (
+    ListedProject,
+    ProjectDetail,
+    Row,
+    RowsAppend,
+)
+
+__all__ = [
+    "ProjectDetail",
+    "ListedProject",
+    "RowsAppend",
+    "Row",
+]
