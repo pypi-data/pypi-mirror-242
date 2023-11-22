28de6c36c0e3bf972dd4cc8b58f47292aaab8682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 06:04:20 +0000

    Add package dolfin-adjoint/2020.0.0/dolfin_adjoint-2020.0.0-py3-none-any.whl

---
+++ packages/dolfin-adjoint/dolfin_adjoint-2020.0.0-py3-none-any.whl/fenics_adjoint/types/genericvector.py
@@ -0,0 +1,13 @@
+import backend
+from dolfin_adjoint_common import compat
+compat = compat.compat(backend)
+
+__all__ = []
+
+
+@staticmethod
+def _ad_to_list(self):
+    return compat.gather(self).tolist()
+
+
+backend.GenericVector._ad_to_list = _ad_to_list
