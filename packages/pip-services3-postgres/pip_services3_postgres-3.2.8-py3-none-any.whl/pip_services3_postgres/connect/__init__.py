a6f97a4ee55edbb8d8485676b97eb030b077d2f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 06:04:32 +0000

    Add package pip-services3-postgres/3.2.8/pip_services3_postgres-3.2.8-py3-none-any.whl

---
+++ packages/pip-services3-postgres/pip_services3_postgres-3.2.8-py3-none-any.whl/pip_services3_postgres/connect/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+__all__ = ['PostgresConnection', 'PostgresConnectionResolver']
+
+from .PostgresConnection import PostgresConnection
+from .PostgresConnectionResolver import PostgresConnectionResolver
