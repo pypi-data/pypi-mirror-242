2fdf30e3b13335e7a3c482b1e3348df6e9a23b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 15:05:01 +0000

    Add package redisvl/0.0.5/redisvl-0.0.5-py3-none-any.whl

---
+++ packages/redisvl/redisvl-0.0.5-py3-none-any.whl/redisvl/utils/connection.py
@@ -0,0 +1,42 @@
+import os
+from typing import Optional
+
+# TODO: handle connection errors.
+
+
+def get_redis_connection(url: Optional[str] = None, **kwargs):
+    from redis import Redis
+
+    if url:
+        client = Redis.from_url(url, **kwargs)
+    else:
+        try:
+            client = Redis.from_url(get_address_from_env())
+        except ValueError:
+            raise ValueError("No Redis URL provided and REDIS_URL env var not set")
+    return client
+
+
+def get_async_redis_connection(url: Optional[str] = None, **kwargs):
+    from redis.asyncio import Redis as ARedis
+
+    if url:
+        client = ARedis.from_url(url, **kwargs)
+    else:
+        try:
+            client = ARedis.from_url(get_address_from_env())
+        except ValueError:
+            raise ValueError("No Redis URL provided and REDIS_URL env var not set")
+    return client
+
+
+def get_address_from_env():
+    """Get a redis connection from environment variables.
+
+    Returns:
+        str: Redis URL
+    """
+    addr = os.getenv("REDIS_URL", None)
+    if not addr:
+        raise ValueError("REDIS_URL env var not set")
+    return addr
