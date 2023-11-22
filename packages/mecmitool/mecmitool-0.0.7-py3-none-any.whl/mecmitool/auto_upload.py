334fc14f5145da87d9f1460b59e68e91f79a8e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 05:04:07 +0000

    Add package mecmitool/0.0.7/mecmitool-0.0.7-py3-none-any.whl

---
+++ packages/mecmitool/mecmitool-0.0.7-py3-none-any.whl/mecmitool/auto_upload.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import build.__main__
+from mecmitool.path_checker import check_inpath
+import mecmitool.twine.__main__
+
+
+def main(path=check_inpath(sys.path[0])):
+    files_before = []
+    for root, dirs, files in os.walk(path, topdown=False):
+        for file in files:
+            file_path = os.path.join(root, file)
+            print(file_path)
+            if file_path not in files_before:
+                files_before.append(file_path)
+    build.__main__.main([path], "python -m build")
+    mecmitool.twine.__main__.main(["upload", path + "dist/*"])
+    for root, dirs, files in os.walk(path, topdown=False):
+        for file in files:
+            file_path = os.path.join(root, file)
+            if file_path not in files_before:
+                os.remove(file_path)
+    for root, dirs, files in os.walk(path, topdown=False):
+        for dir in dirs:
+            dir_path = os.path.join(root, dir)
+            if not os.listdir(dir_path):
+                os.rmdir(dir_path)
+
+
+if __name__ == "__main__":
+    main()
