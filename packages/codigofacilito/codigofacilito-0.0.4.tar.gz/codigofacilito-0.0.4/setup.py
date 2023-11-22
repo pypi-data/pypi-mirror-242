7a2458e17627ca27c35cede4a1ce4e7b68d2e80b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 14:04:11 +0000

    Add package codigofacilito/0.0.4/codigofacilito-0.0.4.tar.gz

---
+++ packages/codigofacilito/codigofacilito-0.0.4.tar.gz/codigofacilito-0.0.4/setup.py
@@ -0,0 +1,42 @@
+from pathlib import Path # > 3.6
+from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf8")
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Permite consumir el API de CódigoFacilito'
+PACKAGE_NAME = 'codigofacilito'
+AUTHOR = 'Ange1D'
+EMAIL = 'ange1d.contact0@gmail.com'
+GITHUB_URL = 'https://github.com/Ange1D/codigofacilito_package/'
+
+setup(
+    name = PACKAGE_NAME,
+    packages = [PACKAGE_NAME],
+    entry_points={
+        "console_scripts":
+            ["pycody=codigofacilito.__main__:main"]
+    },
+    version = VERSION,
+    license='MIT',
+    description = DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    author = AUTHOR,
+    author_email = EMAIL,
+    url = GITHUB_URL,
+    keywords = [
+        'codigofacilito'
+    ],
+    install_requires=[ 
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
