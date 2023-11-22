66458eaf73a33b0f8dec7bf94e1f9b3591835002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 04:03:53 +0000

    Add package g2pk3/1.4/g2pk3-1.4.tar.gz

---
+++ packages/g2pk3/g2pk3-1.4.tar.gz/g2pk3-1.4/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+setuptools.setup(
+    name="g2pk3",
+    version="1.4",
+    license='MIT',
+    author="kdr",
+    author_email="kdrhacker1234@gmail.com",
+    description="",
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kdrkdrkdr/g2pk3",
+    packages=setuptools.find_packages(),
+    package_data={'': ['*.txt', '*.csv']},
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=[
+        'jamo',
+        'g2p_en',
+    ],
+    dependency_links=["git+https://github.com/kdrkdrkdr/pyopenjtalk.git@main#egg=pyopenjtalk"],
+    py_modules = ['g2pk3']
+)
