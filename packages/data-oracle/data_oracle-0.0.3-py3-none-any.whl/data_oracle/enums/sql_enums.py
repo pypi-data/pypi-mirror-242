c3c97894ed4ee66e4388dcd8d02da1b0a2bd13f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 16:05:59 +0000

    Add package data-oracle/0.0.3/data_oracle-0.0.3-py3-none-any.whl

---
+++ packages/data-oracle/data_oracle-0.0.3-py3-none-any.whl/data_oracle/enums/sql_enums.py
@@ -0,0 +1,31 @@
+from .base_enum import BaseEnum
+
+
+
+
+
+class Sql_Alchemy_Types(BaseEnum):
+    MYSQL = 'MySQL'
+    POSTGRESQL = 'PostgreSQL'
+    ORACLE = 'Oracle'
+    MICROSOFTSQLSERVER = 'Microsoft SQL Server'
+    SQLITE = 'SQLite'
+
+class Sql_Types(BaseEnum):
+    MYSQL = 'MySQL'
+    POSTGRESQL = 'PostgreSQL'
+    ORACLE = 'Oracle'
+    MICROSOFTSQLSERVER = 'Microsoft SQL Server'
+    SQLITE = 'SQLite'
+    BIGQUERY = 'BigQuery'
+
+class data_types(BaseEnum):
+    INTEGER = "INT"
+    VARCHAR = "VARCHAR"
+    DATE = "DATE"
+
+class Data_Table_Type(BaseEnum):
+    TABLE = "Table"
+    VIEW = "View"
+
+
