2b700dbc570d1ce9375f966e1cd72fa4fd07e739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 03:04:00 +0000

    Add package nettoolkit/1.0.0/nettoolkit-1.0.0.tar.gz

---
+++ packages/nettoolkit/nettoolkit-1.0.0.tar.gz/nettoolkit-1.0.0/nettoolkit/__init__.py
@@ -0,0 +1,8 @@
+__doc__ = '''Networking ToolSet
+'''
+
+__version__ = "1.0.0"
+
+
+def version():
+	return __version__
---
+++ packages/nettoolkit/nettoolkit-1.0.0.tar.gz/nettoolkit-1.0.0/nettoolkit/facts_finder/__init__.py
@@ -0,0 +1,22 @@
+"""generate Network Devices (Switch/Router) facts from its configuration outputs.
+"""
+
+# ------------------------------------------------------------------------------
+
+from .generators.merger import device
+from .generators.merger import DeviceDB
+from .rearrange import rearrange_tables
+from .generators import get_necessary_cmds, get_absolute_command
+
+from .clean import CleanFacts
+
+
+
+__all__ = [ 
+	'device', 'DeviceDB',
+	'CleanFacts',
+	'rearrange_tables',
+	'get_necessary_cmds', 'get_absolute_command',
+	]
+
+__ver__ = "0.1.2"
