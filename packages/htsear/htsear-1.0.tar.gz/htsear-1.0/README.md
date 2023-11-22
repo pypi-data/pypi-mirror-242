964580f7856e209b13d2b6498313a1307c2b494f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 05:04:06 +0000

    Add package htsear/1.0/htsear-1.0.tar.gz

---
+++ packages/htsear/htsear-1.0.tar.gz/htsear-1.0/README.md
@@ -0,0 +1,15 @@
+# htsear
+
+htsear is a python word function, 
+
+`pip install htsear`
+
+# Usage
+
+| default python   | htsear         |
+| ---------------- | -------------- |
+| `print()`      | `sear()`     |
+| `len()`        | `sear_ln()`  |
+| `upper()`      | `sear_upr()` |
+| `lower()`      | `sear_lwr()` |
+| `capitalize()` | `sear_cap()` |
---
+++ packages/htsear/htsear-1.0.tar.gz/htsear-1.0/htsear/htsear.py
@@ -0,0 +1,19 @@
+##htsear version 1.0 Beta
+
+def sear(text):
+    text = print(text)
+    return text
+
+def sear_upr(text):
+    text = print(text.upper())
+    return text
+
+def sear_lwr(text):
+    text = print(text.lower())
+    
+def sear_cap(text):
+    text = print(text.capitalize())
+    
+def sear_ln(text):
+    text = print(len(text))
+    return text
