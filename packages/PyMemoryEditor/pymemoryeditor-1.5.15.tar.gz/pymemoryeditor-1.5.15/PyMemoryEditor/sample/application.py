c120b6a112afe13e0dac51d400c1d51d8dd03b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 15:05:09 +0000

    Add package PyMemoryEditor/1.5.15/pymemoryeditor-1.5.15.tar.gz

---
+++ packages/PyMemoryEditor/pymemoryeditor-1.5.15.tar.gz/pymemoryeditor-1.5.15/PyMemoryEditor/sample/application.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from .main_application_window import ApplicationWindow
+from .open_process_window import OpenProcessWindow
+
+
+def main(*args, **kwargs):
+    open_process_window = OpenProcessWindow()
+    process = open_process_window.get_process()
+
+    if not process: return
+
+    try: ApplicationWindow(process)
+    finally: process.close()
+
+
+if __name__ == "__main__":
+    main()
