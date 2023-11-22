984f4d9641592aaa080ad872591ae70c95174149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 04:04:36 +0000

    Add package idr-torch/2.0.0/idr_torch-2.0.0-py3-none-any.whl

---
+++ packages/idr-torch/idr_torch-2.0.0-py3-none-any.whl/idr_torch/api/modifiers/undistributed_warning.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import warnings
+from functools import wraps
+
+from ...utils import IdrTorchWarning
+from .. import API
+from .decorate_methods import decorate_methods
+
+
+def warn(func):
+    @wraps(func)
+    def wrapper(self):
+        warnings.warn(
+            message=(
+                "Calling idr_torch only makes sense within a distributed execution "
+                "but none was detected. You may have forgotten to use a launcher "
+                "(such as srun). Back to default (non distributed) values"
+            ),
+            category=IdrTorchWarning,
+            stacklevel=4,
+        )
+        return func(self)
+
+    return wrapper
+
+
+def UndistributedWarning(cls: API) -> API:
+    return decorate_methods(cls, func_to_apply=warn)
