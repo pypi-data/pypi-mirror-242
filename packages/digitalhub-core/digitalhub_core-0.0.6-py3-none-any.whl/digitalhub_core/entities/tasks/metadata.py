6ee743a87f5765648f8cc8b508da452f2492989e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 03:04:11 +0000

    Add package digitalhub-core/0.0.6/digitalhub_core-0.0.6-py3-none-any.whl

---
+++ packages/digitalhub-core/digitalhub_core-0.0.6-py3-none-any.whl/digitalhub_core/entities/tasks/metadata.py
@@ -0,0 +1,34 @@
+"""
+Task metadata module.
+"""
+from __future__ import annotations
+
+from digitalhub_core.entities._base.metadata import Metadata
+
+
+class TaskMetadata(Metadata):
+    """
+    A class representing Task metadata.
+    """
+
+    def __init__(
+        self,
+        project: str,
+        name: str,
+        created: str | None = None,
+        updated: str | None = None,
+    ) -> None:
+        """
+        Constructor.
+
+        Parameters
+        ----------
+        name : str
+            Name (UUID) of the object.
+
+        See Also
+        --------
+        Metadata.__init__
+        """
+        super().__init__(project, created, updated)
+        self.name = name
