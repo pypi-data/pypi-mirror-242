4dec506eab4948bcf412d60a7d279f581162276d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 17:04:13 +0000

    Add package models-for-plutus/0.1.0/models_for_plutus-0.1.0.tar.gz

---
+++ packages/models-for-plutus/models_for_plutus-0.1.0.tar.gz/models_for_plutus-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="models_for_plutus",
+    version="0.1.0",
+    description="models for plutus app",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
