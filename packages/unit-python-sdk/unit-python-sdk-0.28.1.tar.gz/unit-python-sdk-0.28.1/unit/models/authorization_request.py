7e22c5ec5cb4094ccbd14ae80c622b66145f0e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 03:04:08 +0000

    Add package unit-python-sdk/0.28.1/unit-python-sdk-0.28.1.tar.gz

---
+++ packages/unit-python-sdk/unit-python-sdk-0.28.1.tar.gz/unit-python-sdk-0.28.1/unit/models/authorization_request.py
@@ -0,0 +1,94 @@
+import json
+try:
+    from typing import Optional, Literal
+except ImportError:
+    from typing import Optional
+    from typing_extensions import Literal
+from unit.models import *
+from unit.utils import date_utils
+
+PurchaseAuthorizationRequestStatus = Literal["Pending", "Approved", "Declined"]
+DeclineReason = Literal["AccountClosed", "CardExceedsAmountLimit", "DoNotHonor", "InsufficientFunds", "InvalidMerchant",
+                        "ReferToCardIssuer", "RestrictedCard", "Timeout", "TransactionNotPermittedToCardholder"]
+
+
+class PurchaseAuthorizationRequestDTO(object):
+    def __init__(self, id: str, created_at: datetime, amount: int, status: PurchaseAuthorizationRequestStatus,
+                 partial_approval_allowed: str, approved_amount: Optional[int], decline_reason: Optional[DeclineReason],
+                 merchant_name: str, merchant_type: int, merchant_category: str, merchant_location: Optional[str],
+                 recurring: bool, tags: Optional[Dict[str, str]], relationships: Optional[Dict[str, Relationship]],
+                 merchant_id: Optional[str]):
+        self.id = id
+        self.type = "purchaseAuthorizationRequest"
+        self.attributes = {"createdAt": created_at, "amount": amount, "status": status,
+                           "partialApprovalAllowed": partial_approval_allowed, "approvedAmount": approved_amount,
+                           "declineReason": decline_reason, "merchant": { "name": merchant_name, "type": merchant_type,
+                                                                          "category": merchant_category,
+                                                                          "location": merchant_location,
+                                                                          "id": merchant_id},
+                           "recurring": recurring, "tags": tags}
+        self.relationships = relationships
+
+    @staticmethod
+    def from_json_api(_id, _type, attributes, relationships):
+        return PurchaseAuthorizationRequestDTO(_id, date_utils.to_datetime(attributes["createdAt"]),
+                                               attributes["amount"], attributes["status"],
+                                               attributes.get("partialApprovalAllowed"),
+                                               attributes.get("approvedAmount"), attributes.get("declineReason"),
+                                               attributes["merchant"]["name"], attributes["merchant"]["type"],
+                                               attributes["merchant"]["category"],
+                                               attributes["merchant"].get("location"), attributes["recurring"],
+                                               attributes.get("tags"), relationships, attributes["merchant"].get("id"))
+
+
+class ListPurchaseAuthorizationRequestParams(object):
+    def __init__(self, limit: int = 100, offset: int = 0, account_id: Optional[str] = None,
+                 customer_id: Optional[str] = None):
+        self.limit = limit
+        self.offset = offset
+        self.account_id = account_id
+        self.customer_id = customer_id
+
+    def to_dict(self) -> Dict:
+        parameters = {"page[limit]": self.limit, "page[offset]": self.offset}
+        if self.customer_id:
+            parameters["filter[customerId]"] = self.customer_id
+        if self.account_id:
+            parameters["filter[accountId]"] = self.account_id
+        return parameters
+
+
+class ApproveAuthorizationRequest(UnitRequest):
+    def __init__(self, authorization_id: str, amount: Optional[int] = None, tags: Optional[Dict[str, str]] = None,
+                 funding_account: Optional[str] = None):
+        self.authorization_id = authorization_id
+        self.amount = amount
+        self.tags = tags
+        self.funding_account = funding_account
+
+    def to_json_api(self) -> Dict:
+        return super().to_payload("approveAuthorizationRequest", ignore=["authorization_id"])
+
+    def __repr__(self):
+        return json.dumps(self.to_json_api())
+
+
+class DeclineAuthorizationRequest(object):
+    def __init__(self, authorization_id: str, reason: DeclineReason):
+        self.authorization_id = authorization_id
+        self.reason = reason
+
+    def to_json_api(self) -> Dict:
+        payload = {
+            "data": {
+                "type": "declineAuthorizationRequest",
+                "attributes": {
+                    "reason": self.reason
+                }
+            }
+        }
+
+        return payload
+
+    def __repr__(self):
+        return json.dumps(self.to_json_api())
