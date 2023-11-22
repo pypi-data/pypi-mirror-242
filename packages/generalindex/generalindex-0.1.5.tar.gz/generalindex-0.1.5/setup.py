38d03496470d6e3371c6caedd2d4d708c255f531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 03:04:01 +0000

    Add package generalindex/0.1.5/generalindex-0.1.5.tar.gz

---
+++ packages/generalindex/generalindex-0.1.5.tar.gz/generalindex-0.1.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="generalindex",
+    version="0.1.5",
+    author="General Index",
+    author_email="info@general-index.com",
+    description="Python SDK for the General Index platform",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.general-index.com/",
+    packages=setuptools.find_packages(),
+    classifiers=["Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent"
+                 ],
+    install_requires=[
+        'requests>=2.27.1',
+        'urllib3>=1.26.9',
+        'deprecated==1.2.14'
+    ],
+    python_requires='>=3.6',
+)
