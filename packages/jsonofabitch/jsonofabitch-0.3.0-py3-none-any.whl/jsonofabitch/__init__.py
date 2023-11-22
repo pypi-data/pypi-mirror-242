7d33a0d8c0abbdc035a4ccc68135b984bea30c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 03:04:08 +0000

    Add package jsonofabitch/0.3.0/jsonofabitch-0.3.0-py3-none-any.whl

---
+++ packages/jsonofabitch/jsonofabitch-0.3.0-py3-none-any.whl/jsonofabitch/__init__.py
@@ -0,0 +1,13 @@
+"""
+JSONofabitch
+============
+
+sloppy json parser. messy json parser. error correcting json parser. standarization of low standards
+
+"""
+
+__version__: str = "0.3.0"
+
+
+from .jsonofabitch import __all__
+from .jsonofabitch import *
