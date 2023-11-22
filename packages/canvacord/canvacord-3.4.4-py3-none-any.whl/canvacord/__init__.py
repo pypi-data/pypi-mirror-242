b1287fa7cc2487c9b858379702648ea3037502ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 06:04:14 +0000

    Add package canvacord/3.4.4/canvacord-3.4.4-py3-none-any.whl

---
+++ packages/canvacord/canvacord-3.4.4-py3-none-any.whl/canvacord/__init__.py
@@ -0,0 +1,17 @@
+__version__ = "3.4.4"
+
+from .generators import rankcard
+from .generators import trigger
+from .generators import communism
+from .generators import gay
+from .generators import jail
+from .generators import hitler
+from .generators import spank
+from .generators import aborted
+from .generators import affect
+from .generators import airpods
+from .generators import america
+from .generators import bed
+from .generators import wanted
+from .generators import jokeoverhead
+from .generators import avatar
