20bee8cc16e71a9cd3b0225977f0ef3882e83ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 14:04:06 +0000

    Add package apsimNGpy/1.1/apsimNGpy-1.1.tar.gz

---
+++ packages/apsimNGpy/apsimNGpy-1.1.tar.gz/apsimNGpy-1.1/optimizer/__init__.py
@@ -0,0 +1,6 @@
+from os.path import dirname, basename, isfile, join
+import glob
+from .optimizer import process_model, optimization_function, algarithms, Optimize, substract
+
+__all__ = ["process_model", "optimization_function", "algarithms", "Optimize", "substract"]
+
