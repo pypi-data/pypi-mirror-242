6ef0cc5a638a257d5ef5f601d8adc7d7d508093e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 04:03:55 +0000

    Add package jmcfunction/1.2.16/jmcfunction-1.2.16.tar.gz

---
+++ packages/jmcfunction/jmcfunction-1.2.16.tar.gz/jmcfunction-1.2.16/jmc/compile/command/flow_control.py
@@ -0,0 +1,26 @@
+from typing import Callable
+from ._flow_control import (
+    if_,
+    else_,
+    while_,
+    do,
+    switch,
+    for_
+)
+
+from ..tokenizer import Token, Tokenizer
+from ..datapack import DataPack
+
+FLOW_CONTROL_COMMANDS: dict[str, Callable[
+    [
+        list[Token],
+        DataPack,
+        Tokenizer,
+    ], str | None]] = {
+    "while": while_,
+    "do": do,
+    "if": if_,
+    "else": else_,
+    "switch": switch,
+    "for": for_,
+}
---
+++ packages/jmcfunction/jmcfunction-1.2.16.tar.gz/jmcfunction-1.2.16/jmc/config.py
@@ -0,0 +1,2 @@
+VERSION = "v1.2.16"
+CONFIG_FILE_NAME = "jmc_config.json"
