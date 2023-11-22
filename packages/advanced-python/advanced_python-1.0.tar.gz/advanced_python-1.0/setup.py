9fb03bdbc73c2a50d0d4630918dae6f2b53ddc88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 17:04:22 +0000

    Add package advanced-python/1.0/advanced_python-1.0.tar.gz

---
+++ packages/advanced-python/advanced_python-1.0.tar.gz/advanced_python-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='advanced_python',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        # list your dependencies here
+    ],
+)
