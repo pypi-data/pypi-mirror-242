45e513ea5791891ac777f3ed0038b34d2bdc6941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 14:04:18 +0000

    Add package chitter/0.1.0/chitter-0.1.0.tar.gz

---
+++ packages/chitter/chitter-0.1.0.tar.gz/chitter-0.1.0/chitter/__init__.py
@@ -0,0 +1,46 @@
+"""
+
+The :code:`iterchain` module is callable, this means we can do this:
+::
+
+    >>> import iterchain
+    >>> iterchain([1, 2, 3]).map(lambda x: x**2)
+
+instead of:
+::
+
+    >>> import iterchain
+    >>> iterchain.Iterator([1, 2, 3]).map(lambda x: x**2)
+
+|
+
+Overview
+````````
+
+**Generators**
+  - :meth:`generators.count`
+  - :meth:`generators.repeat`
+  - ...
+
+
+**Chainable operations**
+  - :meth:`Iterator.map`
+  - :meth:`Iterator.flat_map`
+  - :meth:`Iterator.filter`
+  - ...
+
+**Reduction operators**
+  - :meth:`Iterator.reduce`
+  - :meth:`Iterator.all`
+  - :meth:`Iterator.sum`
+  - ...
+
+**Consumers / access operators**
+  - :meth:`Iterator.to_list`
+  - :meth:`Iterator.first`
+  - :meth:`Iterator.last`
+  - ...
+"""
+
+# simplify public interface
+from .core import ChainableIter
