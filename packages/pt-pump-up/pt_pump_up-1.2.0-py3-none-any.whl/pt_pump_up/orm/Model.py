aeaaa505359506078a9d24bc94a1b47efd7431e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 03:03:46 +0000

    Add package pt-pump-up/1.2.0/pt_pump_up-1.2.0-py3-none-any.whl

---
+++ packages/pt-pump-up/pt_pump_up-1.2.0-py3-none-any.whl/pt_pump_up/orm/Model.py
@@ -0,0 +1,30 @@
+from beanie import Document, Link
+from typing import Optional, List
+from pt_pump_up.orm.Conference import Conference
+from pt_pump_up.orm.Language import Language
+from pydantic import BaseModel
+from pt_pump_up.orm.Hrefs import Hrefs
+from pt_pump_up.orm.Status import Status
+from pt_pump_up.orm.Author import Author
+from pt_pump_up.orm.License import License
+from pt_pump_up.orm.Architecture import Architecture
+from pt_pump_up.orm.Benchmark import Benchmark
+
+
+class ModelStats(BaseModel):
+    languages: List[Link[Language]]
+    architecture: Architecture
+    number_parameters: Optional[int] = 0
+    size_MB: Optional[int] = 0
+
+
+class Model(Document):
+    name: str
+    model_stats: ModelStats
+    conference: Optional[Conference]
+    hrefs: Hrefs
+    year: int
+    status: Status
+    authors: List[Link[Author]]
+    license: Optional[Link[License]] = None
+    benchmarks: List[Link[Benchmark]]
