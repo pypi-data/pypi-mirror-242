7b200b41711c72b29f9b8d433b36dd09edbe4696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 04:03:55 +0000

    Add package seidr/3.0.0/seidr-3.0.0-py3-none-any.whl

---
+++ packages/seidr/seidr-3.0.0-py3-none-any.whl/seidr/templates/template.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stdio.h>
+#include <math.h>
+#include <map>
+#include <set>
+#include <stack>
+#include <climits>
+using namespace std;
+/*
+$prompt
+*/
+int main() {
---
+++ packages/seidr/seidr-3.0.0-py3-none-any.whl/seidr/templates/template.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import numpy as np
+import math
+import datetime
+import collections
+import itertools
+import queue
+import re
+"""
+$prompt
+"""
+if __name__ == '__main__':
