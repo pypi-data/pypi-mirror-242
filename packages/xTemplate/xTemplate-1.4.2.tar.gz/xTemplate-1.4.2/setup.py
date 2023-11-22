8cf2114fa2a7f2439371f69babe578bda1c41bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 14:04:11 +0000

    Add package xTemplate/1.4.2/xTemplate-1.4.2.tar.gz

---
+++ packages/xTemplate/xTemplate-1.4.2.tar.gz/xTemplate-1.4.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='xTemplate',
+    version='1.4.2',
+    description='A simple Template for create python project',
+    author='ks lim',
+    author_email='kianseng.lim@sony.com',
+    packages=['python'],
+)
