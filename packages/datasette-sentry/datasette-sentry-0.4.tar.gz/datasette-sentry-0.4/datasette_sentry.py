fda22b8f5d4a252ab1f708c089a045bf827dd8f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 05:04:07 +0000

    Add package datasette-sentry/0.4/datasette-sentry-0.4.tar.gz

---
+++ packages/datasette-sentry/datasette-sentry-0.4.tar.gz/datasette-sentry-0.4/datasette_sentry.py
@@ -0,0 +1,49 @@
+from datasette import hookimpl
+from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
+import sentry_sdk
+
+
+@hookimpl
+def asgi_wrapper(datasette):
+    config = datasette.plugin_config("datasette-sentry") or {}
+    dsn = config.get("dsn")
+    if dsn is not None:
+        kwargs = dict(dsn=dsn)
+        if config.get("capture_events"):
+            kwargs["transport"] = CaptureTransport(datasette)
+        if config.get("sample_rate") is not None:
+            kwargs["sample_rate"] = config["sample_rate"]
+        if config.get("enable_tracing"):
+            kwargs["enable_tracing"] = True
+            kwargs["traces_sample_rate"] = (
+                config["traces_sample_rate"] if "traces_sample_rate" in config else 1.0
+            )
+        sentry_sdk.init(**kwargs)
+
+    def wrap_with_class(app):
+        if dsn is None:
+            return app
+        else:
+            return SentryAsgiMiddleware(app)
+
+    return wrap_with_class
+
+
+@hookimpl
+def handle_exception(exception):
+    sentry_sdk.capture_exception(exception)
+
+
+class CaptureTransport(sentry_sdk.transport.Transport):
+    "Transport that captures events in a list, mainly for testing"
+
+    def __init__(self, datasette):
+        self.datasette = datasette
+        self.datasette._datasette_sentry_events = []
+        self.datasette._datasette_sentry_envelopes = []
+
+    def capture_event(self, event):
+        self.datasette._datasette_sentry_events.append(event)
+
+    def capture_envelope(self, envelope):
+        self.datasette._datasette_sentry_envelopes.append(envelope)
---
+++ packages/datasette-sentry/datasette-sentry-0.4.tar.gz/datasette-sentry-0.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import os
+
+VERSION = "0.4"
+
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
+
+setup(
+    name="datasette-sentry",
+    description="Datasette plugin for configuring Sentry",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Simon Willison",
+    url="https://github.com/simonw/datasette-sentry",
+    license="Apache License, Version 2.0",
+    version=VERSION,
+    entry_points={"datasette": ["sentry = datasette_sentry"]},
+    py_modules=["datasette_sentry"],
+    install_requires=["sentry-sdk", "datasette>=0.62"],
+    extras_require={"test": ["pytest", "pytest-asyncio"]},
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
