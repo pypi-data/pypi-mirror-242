95bd7bdb4d7001da86b2a7a2878c7a3b633e2484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 17:04:28 +0000

    Add package acconeer-exptool/7.7.1/acconeer-exptool-7.7.1.tar.gz

---
+++ packages/acconeer-exptool/acconeer-exptool-7.7.1.tar.gz/acconeer-exptool-7.7.1/src/acconeer/exptool/a121/algo/__init__.py
@@ -0,0 +1,32 @@
+# Copyright (c) Acconeer AB, 2022-2023
+# All rights reserved
+
+from ._base import (
+    AlgoBase,
+    AlgoConfigBase,
+    AlgoParamEnum,
+    AlgoProcessorConfigBase,
+    Controller,
+    ExtendedProcessorBase,
+    GenericProcessorBase,
+    ProcessorBase,
+)
+from ._utils import (
+    APPROX_BASE_STEP_LENGTH_M,
+    ENVELOPE_FWHM_M,
+    NOISE_TEMPERATURE_MODEL_PARAMETER,
+    PERCEIVED_WAVELENGTH,
+    SIGNAL_TEMPERATURE_MODEL_PARAMETER,
+    calculate_loopback_peak_location,
+    double_buffering_frame_filter,
+    exponential_smoothing_coefficient,
+    find_peaks,
+    get_approx_fft_vels,
+    get_distance_filter_coeffs,
+    get_distance_filter_edge_margin,
+    get_distance_offset,
+    get_distances_m,
+    get_temperature_adjustment_factors,
+    interpolate_peaks,
+    select_prf,
+)
