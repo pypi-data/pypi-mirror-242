aa354ce1fd44153eb27d3b38b1b60fb09c2cb367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 03:05:02 +0000

    Add package combadge/4.1.0/combadge-4.1.0-py3-none-any.whl

---
+++ packages/combadge/combadge-4.1.0-py3-none-any.whl/combadge/support/soap/response.py
@@ -0,0 +1,28 @@
+"""Response extensions for SOAP."""
+
+from typing import NoReturn, Optional
+
+from combadge.core.response import ErrorResponse
+
+
+class BaseSoapFault(ErrorResponse):
+    """
+    SOAP Fault model.
+
+    Notes:
+        - This class matches the SOAP Fault specification.
+          For custom errors returned in a SOAP response body (such as `<error>` tag),
+          subclass the `ErrorResponse`.
+
+    See Also:
+        - https://www.w3.org/TR/2000/NOTE-SOAP-20000508/#_Toc478383507
+    """
+
+    code: str
+    message: str
+
+    def raise_for_result(self, exception: Optional[BaseException] = None) -> NoReturn:
+        """Raise the derived error for this fault."""
+        if not exception:
+            raise self.Error(self)
+        raise exception from self.Error(self)
