f123299429e3913422e5176a70a3df55a34a00f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 16:06:05 +0000

    Add package xarg-python/0.11b1/xarg_python-0.11b1-py2.py3-none-any.whl

---
+++ packages/xarg-python/xarg_python-0.11b1-py2.py3-none-any.whl/xarg/util.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# coding=utf-8
+
+__package_name__ = "xarg-python"
+__prog_name__ = "xarg"
+__prog_complete__ = "xargcomplete"
+__version__ = "0.11.beta.1"
+
+URL_PROG = "https://github.com/bondbox/xarg-python"
