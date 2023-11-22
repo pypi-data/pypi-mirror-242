725b029b174a9d7325f8dc303796670dd054b92c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 03:04:21 +0000

    Add package netbox-device-support-plugin/0.0.2/netbox-device-support-plugin-0.0.2.tar.gz

---
+++ packages/netbox-device-support-plugin/netbox-device-support-plugin-0.0.2.tar.gz/netbox-device-support-plugin-0.0.2/netbox_device_support_plugin/urls.py
@@ -0,0 +1,56 @@
+from django.urls import path
+from . import views
+
+
+app_name = "netbox_device_support_plugin"
+
+urlpatterns = (
+    # Cisco Device Support
+    path(
+        "device-support/",
+        views.CiscoDeviceSupportListView.as_view(),
+        name="ciscodevicesupport_list",
+    ),
+    path(
+        "device-support/delete/",
+        views.CiscoDeviceSupportBulkDeleteView.as_view(),
+        name="ciscodevicesupport_bulk_delete",
+    ),
+    path(
+        "device-support/<int:pk>/delete/",
+        views.CiscoDeviceSupportDeleteView.as_view(),
+        name="ciscodevicesupport_delete",
+    ),
+    # Cisco Device Type Support
+    path(
+        "device-type-support/",
+        views.CiscoDeviceTypeSupportListView.as_view(),
+        name="ciscodevicetypesupport_list",
+    ),
+    path(
+        "device-type-support/delete/",
+        views.CiscoDeviceTypeSupportBulkDeleteView.as_view(),
+        name="ciscodevicetypesupport_bulk_delete",
+    ),
+    path(
+        "device-type-support/<int:pk>/delete/",
+        views.CiscoDeviceTypeSupportDeleteView.as_view(),
+        name="ciscodevicetypesupport_delete",
+    ),
+    # Fortnet Support
+    path(
+        "fortinet-support/",
+        views.FortinetDeviceSupportListView.as_view(),
+        name="fortinetsupport_list",
+    ),
+    path(
+        "fortinet-support/delete/",
+        views.FortinetDeviceSupportBulkDeleteView.as_view(),
+        name="fortinetsupport_bulk_delete",
+    ),
+    path(
+        "fortinet-support/<int:pk>/delete/",
+        views.FortinetDeviceSupportDeleteView.as_view(),
+        name="fortinetsupport_delete",
+    ),
+)
