eba502a1378b769399a5298828d6ce53d768759c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 03:04:21 +0000

    Add package MyBaleCloud/2.1.3/MyBaleCloud-2.1.3.tar.gz

---
+++ packages/MyBaleCloud/MyBaleCloud-2.1.3.tar.gz/MyBaleCloud-2.1.3/MyBaleCloud/messager.py
@@ -0,0 +1,142 @@
+# {'ok': True, 'result': [{'update_id': 1, 'message': {'message_id': 1, 'from': {'id': 554324725, 'is_bot': False, 'first_name': 'Al3X', 'last_name': ''}, 'date': 1694601472, 'chat': {
+# 'id': 554324725, 'type': 'private', 'first_name': 'Al3X', 'photo': None}, 'text': '/start'}}, {'update_id': 2, 'message': {'message_id': 3, 'from': {'id': 554324725, 'is_bot': False,
+#  'first_name': 'Al3X', 'last_name': ''}, 'date': 1694601512, 'chat': {'id': 554324725, 'type': 'private', 'first_name': 'Al3X', 'photo': None}, 'text': '/ali'}}, {'update_id': 3, 'me
+# ssage': {'message_id': 9, 'from': {'id': 554324725, 'is_bot': False, 'first_name': 'Al3X', 'last_name': ''}, 'date': 1694602000, 'chat': {'id': 554324725, 'type': 'private', 'first_n
+# ame': 'Al3X', 'photo': None}, 'text': '/lady'}}, {'update_id': 4, 'message': {'message_id': 11, 'from': {'id': 554324725, 'is_bot': False, 'first_name': 'Al3X', 'last_name': ''}, 'da
+# te': 1694602091, 'chat': {'id': 554324725, 'type': 'private', 'first_name': 'Al3X', 'photo': None}, 'text': '/start'}}, {'update_id': 5, 'message': {'message_id': 13, 'from': {'id':
+# 554324725, 'is_bot': False, 'first_name': 'Al3X', 'last_name': ''}, 'date': 1694602104, 'chat': {'id': 554324725, 'type': 'private', 'first_name': 'Al3X', 'photo': None}, 'text': '/t
+# imer'}}, {'update_id': 6, 'message': {'message_id': 16, 'from': {'id': 554324725, 'is_bot': False, 'first_name': 'Al3X', 'last_name': ''}, 'date': 1694607514, 'chat': {'id': 55432472
+# 5, 'type': 'private', 'first_name': 'Al3X', 'photo': None}, 'text': '/ali'}}, {'update_id': 7, 'message': {'message_id': 20, 'from': {'id': 554324725, 'is_bot': False, 'first_name':
+# 'Al3X', 'last_name': ''}, 'date': 1694607724, 'chat': {'id': 554324725, 'type': 'private', 'first_name': 'Al3X', 'photo': None}, 'text': '/timer'}}, {'update_id': 8, 'message': {'mes
+# sage_id': 22, 'from': {'id': 225240921, 'is_bot': False, 'first_name': 'سینا', 'last_name': ''}, 'date': 1694607793, 'chat': {'id': 225240921, 'type': 'private', 'first_name': 'سینا'
+# , 'photo': None}, 'text': '/start'}}, {'update_id': 9, 'message': {'message_id': 24, 'from': {'id': 225240921, 'is_bot': False, 'first_name': 'سینا', 'last_name': ''}, 'date': 169460
+# 7822, 'chat': {'id': 225240921, 'type': 'private', 'first_name': 'سینا', 'photo': None}, 'text': 'Dog'}}, {'update_id': 10, 'message': {'message_id': 25, 'from': {'id': 225240921, 'i
+# s_bot': False, 'first_name': 'سینا', 'last_name': ''}, 'date': 1694607864, 'chat': {'id': 225240921, 'type': 'private', 'first_name': 'سینا', 'photo': None}, 'text': '/dog'}}, {'upda
+# te_id': 11, 'message': {'message_id': 27, 'from': {'id': 554324725, 'is_bot': False, 'first_name': 'Al3X', 'last_name': ''}, 'date': 1694607887, 'chat': {'id': 554324725, 'type': 'pr
+# ivate', 'first_name': 'Al3X', 'photo': None}, 'text': '/cat'}}, {'update_id': 12, 'message': {'message_id': 29, 'from': {'id': 225240921, 'is_bot': False, 'first_name': 'سینا', 'last
+# _name': ''}, 'date': 1694607893, 'chat': {'id': 225240921, 'type': 'private', 'first_name': 'سینا', 'photo': None}, 'text': '/ father'}}, {'update_id': 13, 'message': {'message_id':
+# 31, 'from': {'id': 225240921, 'is_bot': False, 'first_name': 'سینا', 'last_name': ''}, 'date': 1694607911, 'chat': {'id': 225240921, 'type': 'private', 'first_name': 'سینا', 'photo':
+#  None}, 'text': '/ kos'}}, {'update_id': 14, 'message': {'message_id': 32, 'from': {'id': 554324725, 'is_bot': False, 'first_name': 'Al3X', 'last_name': ''}, 'date': 1694607923, 'cha
+# t': {'id': 554324725, 'type': 'private', 'first_name': 'Al3X', 'photo': None}, 'text': '/mother'}}, {'update_id': 15, 'message': {'message_id': 35, 'from': {'id': 225240921, 'is_bot'
+# : False, 'first_name': 'سینا', 'last_name': ''}, 'date': 1694607953, 'chat': {'id': 225240921, 'type': 'private', 'first_name': 'سینا', 'photo': None}, 'text': '/cat'}}, {'update_id'
+# : 16, 'message': {'message_id': 37, 'from': {'id': 554324725, 'is_bot': False, 'first_name': 'Al3X', 'last_name': ''}, 'date': 1694607965, 'chat': {'id': 554324725, 'type': 'private'
+# , 'first_name': 'Al3X', 'photo': None}, 'text': '/kali linux'}}, {'update_id': 17, 'message': {'message_id': 38, 'from': {'id': 225240921, 'is_bot': False, 'first_name': 'سینا', 'las
+# t_name': ''}, 'date': 1694607967, 'chat': {'id': 225240921, 'type': 'private', 'first_name': 'سینا', 'photo': None}, 'text': '/ sina'}}, {'update_id': 18, 'message': {'message_id': 4
+# 1, 'from': {'id': 554324725, 'is_bot': False, 'first_name': 'Al3X', 'last_name': ''}, 'date': 1694607976, 'chat': {'id': 554324725, 'type': 'private', 'first_name': 'Al3X', 'photo':
+# None}, 'text': '/black arch'}}, {'update_id': 19, 'message': {'message_id': 43, 'from': {'id': 554324725, 'is_bot': False, 'first_name': 'Al3X', 'last_name': ''}, 'date': 1694607988,
+#  'chat': {'id': 554324725, 'type': 'private', 'first_name': 'Al3X', 'photo': None}, 'text': '/black arch linux'}}, {'update_id': 20, 'message': {'message_id': 45, 'from': {'id': 2252
+# 40921, 'is_bot': False, 'first_name': 'سینا', 'last_name': ''}, 'date': 1694607996, 'chat': {'id': 225240921, 'type': 'private', 'first_name': 'سینا', 'photo': None}, 'text': '/good'
+# }}, {'update_id': 21, 'message': {'message_id': 47, 'from': {'id': 554324725, 'is_bot': False, 'first_name': 'Al3X', 'last_name': ''}, 'date': 1694608001, 'chat': {'id': 554324725, '
+# type': 'private', 'first_name': 'Al3X', 'photo': None}, 'text': '/moai'}}, {'update_id': 22, 'message': {'message_id': 49, 'from': {'id': 554324725, 'is_bot': False, 'first_name': 'A
+# l3X', 'last_name': ''}, 'date': 1694608007, 'chat': {'id': 554324725, 'type': 'private', 'first_name': 'Al3X', 'photo': None}, 'text': '/🗿'}}, {'update_id': 23, 'message': {'message
+# _id': 50, 'from': {'id': 225240921, 'is_bot': False, 'first_name': 'سینا', 'last_name': ''}, 'date': 1694608010, 'chat': {'id': 225240921, 'type': 'private', 'first_name': 'سینا', 'p
+# hoto': None}, 'text': '/mmm'}}, {'update_id': 24, 'message': {'message_id': 53, 'from': {'id': 225240921, 'is_bot': False, 'first_name': 'سینا', 'last_name': ''}, 'date': 1694608031,
+#  'chat': {'id': 225240921, 'type': 'private', 'first_name': 'سینا', 'photo': None}, 'text': '/Big'}}, {'update_id': 25, 'message': {'message_id': 55, 'from': {'id': 225240921, 'is_bo
+# t': False, 'first_name': 'سینا', 'last_name': ''}, 'date': 1694608047, 'chat': {'id': 225240921, 'type': 'private', 'first_name': 'سینا', 'photo': None}, 'text': 'Parmida'}}, {'updat
+# e_id': 26, 'message': {'message_id': 56, 'from': {'id': 225240921, 'is_bot': False, 'first_name': 'سینا', 'last_name': ''}, 'date': 1694608051, 'chat': {'id': 225240921, 'type': 'pri
+# vate', 'first_name': 'سینا', 'photo': None}, 'text': '/Parmida'}}, {'update_id': 27, 'message': {'message_id': 57, 'from': {'id': 225240921, 'is_bot': False, 'first_name': 'سینا', 'l
+# ast_name': ''}, 'date': 1694608054, 'chat': {'id': 225240921, 'type': 'private', 'first_name': 'سینا', 'photo': None}, 'text': '😂😂'}}, {'update_id': 28, 'message': {'message_id': 5
+# 9, 'from': {'id': 225240921, 'is_bot': False, 'first_name': 'سینا', 'last_name': ''}, 'date': 1694608088, 'chat': {'id': 225240921, 'type': 'private', 'first_name': 'سینا', 'photo':
+# None}, 'text': '/ maniya'}}, {'update_id': 29, 'message': {'message_id': 61, 'from': {'id': 225240921, 'is_bot': False, 'first_name': 'سینا', 'last_name': ''}, 'date': 1694798475, 'c
+# hat': {'id': 225240921, 'type': 'private', 'first_name': 'سینا', 'photo': None}, 'text': '/start'}}, {'update_id': 30, 'message': {'message_id': 63, 'from': {'id': 554324725, 'is_bot
+# ': False, 'first_name': 'Al3X', 'last_name': ''}, 'date': 1700209157, 'chat': {'id': 554324725, 'type': 'private', 'first_name': 'Al3X', 'photo': {'small_file_id': '1239109739:314857
+# 9611246665474:1:1f53fb33008f0f25bbef6dd7a1e4ee1c', 'small_file_unique_id': '1239109739:3148579611246665474:1:1f53fb33008f0f25bbef6dd7a1e4ee1c', 'big_file_id': '1239109739:72627748496
+# 71667458:1:1f53fb33008f0f25bbef6dd7a1e4ee1c', 'big_file_unique_id': '1239109739:7262774849671667458:1:1f53fb33008f0f25bbef6dd7a1e4ee1c'}}, 'text': '/start'}}, {'update_id': 31, 'mess
+# age': {'message_id': 65, 'from': {'id': 554324725, 'is_bot': False, 'first_name': 'Al3X', 'last_name': ''}, 'date': 1700209796, 'chat': {'id': 554324725, 'type': 'private', 'first_na
+# me': 'Al3X', 'photo': {'small_file_id': '1239109739:3148579611246665474:1:1f53fb33008f0f25bbef6dd7a1e4ee1c', 'small_file_unique_id': '1239109739:3148579611246665474:1:1f53fb33008f0f2
+# 5bbef6dd7a1e4ee1c', 'big_file_id': '1239109739:7262774849671667458:1:1f53fb33008f0f25bbef6dd7a1e4ee1c', 'big_file_unique_id': '1239109739:7262774849671667458:1:1f53fb33008f0f25bbef6d
+# d7a1e4ee1c'}}, 'text': 'hi'}}, {'update_id': 32, 'message': {'message_id': 66, 'from': {'id': 554324725, 'is_bot': False, 'first_name': 'Al3X', 'last_name': ''}, 'date': 1700209823,
+# 'chat': {'id': 554324725, 'type': 'private', 'first_name': 'Al3X', 'photo': {'small_file_id': '1239109739:3148579611246665474:1:1f53fb33008f0f25bbef6dd7a1e4ee1c', 'small_file_unique_
+# id': '1239109739:3148579611246665474:1:1f53fb33008f0f25bbef6dd7a1e4ee1c', 'big_file_id': '1239109739:7262774849671667458:1:1f53fb33008f0f25bbef6dd7a1e4ee1c', 'big_file_unique_id': '1
+# 239109739:7262774849671667458:1:1f53fb33008f0f25bbef6dd7a1e4ee1c'}}, 'text': '/start'}}, {'update_id': 33, 'message': {'message_id': 68, 'from': {'id': 554324725, 'is_bot': False, 'f
+# irst_name': 'Al3X', 'last_name': ''}, 'date': 1700209833, 'chat': {'id': 554324725, 'type': 'private', 'first_name': 'Al3X', 'photo': {'small_file_id': '1239109739:314857961124666547
+# 4:1:1f53fb33008f0f25bbef6dd7a1e4ee1c', 'small_file_unique_id': '1239109739:3148579611246665474:1:1f53fb33008f0f25bbef6dd7a1e4ee1c', 'big_file_id': '1239109739:7262774849671667458:1:1
+# f53fb33008f0f25bbef6dd7a1e4ee1c', 'big_file_unique_id': '1239109739:7262774849671667458:1:1f53fb33008f0f25bbef6dd7a1e4ee1c'}}, 'text': '/info'}}, {'update_id': 34, 'message': {'messa
+# ge_id': 70, 'from': {'id': 554324725, 'is_bot': False, 'first_name': 'Al3X', 'last_name': ''}, 'date': 1700209846, 'chat': {'id': 554324725, 'type': 'private', 'first_name': 'Al3X',
+# 'photo': {'small_file_id': '1239109739:3148579611246665474:1:1f53fb33008f0f25bbef6dd7a1e4ee1c', 'small_file_unique_id': '1239109739:3148579611246665474:1:1f53fb33008f0f25bbef6dd7a1e4
+# ee1c', 'big_file_id': '1239109739:7262774849671667458:1:1f53fb33008f0f25bbef6dd7a1e4ee1c', 'big_file_unique_id': '1239109739:7262774849671667458:1:1f53fb33008f0f25bbef6dd7a1e4ee1c'}}
+# , 'text': '/wiki iran'}}, 
+
+
+
+#{'update_id': 35, 'message': {'message_id': 75, 'from': {'id': 554324725, 'is_bot': False, 'first_name': 'Al3X', 'last_name': ''}, 'date': 1700209921, 'chat
+# ': {'id': 554324725, 'type': 'private', 'first_name': 'Al3X', 'photo': {'small_file_id': '1239109739:3148579611246665474:1:1f53fb33008f0f25bbef6dd7a1e4ee1c', 'small_file_unique_id':
+# '1239109739:3148579611246665474:1:1f53fb33008f0f25bbef6dd7a1e4ee1c', 'big_file_id': '1239109739:7262774849671667458:1:1f53fb33008f0f25bbef6dd7a1e4ee1c', 'big_file_unique_id': '123910
+# 9739:7262774849671667458:1:1f53fb33008f0f25bbef6dd7a1e4ee1c'}}, 'text': '/wiki iran'}}]}
+# #{'id': 6247247894, 'type': 'group', 'title': 'ربات', 'photo': None}
+# {'id': 554324725, 'type': 'private', 'first_name': 'Al3X', 'photo': {'small_file_id': '1239109739:3148579611246665474:1:1f53fb33008f0f25bbef6dd7a1e4ee1c', 'small_file_unique_id': '12
+# 39109739:3148579611246665474:1:1f53fb33008f0f25bbef6dd7a1e4ee1c', 'big_file_id': '1239109739:7262774849671667458:1:1f53fb33008f0f25bbef6dd7a1e4ee1c', 'big_file_unique_id': '123910973
+# 9:7262774849671667458:1:1f53fb33008f0f25bbef6dd7a1e4ee1c'}}
+class Messenger:
+    def __init__(self, result) -> None:
+        self.results = result
+        
+    @property
+    def text(self):
+        return self.results['result'][-1]['message']['text']
+    
+    @property
+    def status(self):
+        return self.results['ok']
+    
+    @property
+    def result(self):
+        return self.results['result'][-1]
+
+    @property
+    def message(self):
+        return self.results['result'][-1]['message']
+    
+    @property
+    def message_id(self):
+        return self.message['message_id']
+    
+    @property
+    def from_side(self):
+        return self.message['from']
+    
+    @property
+    def from_id(self):
+        return self.from_side['id']
+    
+    @property
+    def from_isbot(self):
+        return self.from_side['is_bot']
+    
+    @property
+    def from_firstname(self):
+        return self.from_side['first_name']
+    
+    @property 
+    def from_lastname(self):
+        return self.from_side['last_name']
+    
+    @property
+    def date(self):
+        return self.message['date']
+    
+    @property
+    def chat_side(self):
+        return self.message['chat']
+    
+    @property
+    def chat_id(self):
+        return self.chat_side['id']
+    
+    @property
+    def chat_type(self):
+        return self.chat_side['type']
+    
+    @property
+    def chat_firstname(self):
+        if self.chat_type == 'private':
+            return self.chat_side['first_name']
+        else:
+            return False
+    
+    @property
+    def chat_title(self):
+        if self.chat_type == 'group':
+            return self.chat_side['title']
+        else:
+            return False
