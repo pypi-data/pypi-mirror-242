fd68cab54152e2d4ba2c61ddfe2bcf155452381a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 06:04:28 +0000

    Add package dataframe-api-compat/0.1.34/dataframe_api_compat-0.1.34.tar.gz

---
+++ packages/dataframe-api-compat/dataframe_api_compat-0.1.34.tar.gz/dataframe_api_compat-0.1.34/tests/namespace/convert_to_standard_column_test.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+import pandas as pd
+import polars as pl
+import pytest
+
+
+@pytest.mark.skipif(
+    tuple(int(v) for v in pl.__version__.split(".")) < (0, 19, 0)
+    or tuple(int(v) for v in pd.__version__.split(".")) < (2, 1, 0),
+    reason="before consortium standard in polars/pandas",
+)
+def test_convert_to_std_column() -> None:
+    s = pl.Series([1, 2, 3]).__column_consortium_standard__()
+    assert float(s.mean().persist()) == 2
+    s = pl.Series("bob", [1, 2, 3]).__column_consortium_standard__()
+    assert float(s.mean().persist()) == 2
+    s = pd.Series([1, 2, 3]).__column_consortium_standard__()
+    assert float(s.mean().persist()) == 2
+    s = pd.Series([1, 2, 3], name="alice").__column_consortium_standard__()
+    assert float(s.mean().persist()) == 2
