6742366832ad7f89215b7b64f508a6fafe7e19b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 03:04:08 +0000

    Add package aws-ssh-tunnel/2.3.0/aws-ssh-tunnel-2.3.0.tar.gz

---
+++ packages/aws-ssh-tunnel/aws-ssh-tunnel-2.3.0.tar.gz/aws-ssh-tunnel-2.3.0/setup.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="aws-ssh-tunnel",
+    version="2.3.0",
+    author="Daniel Molenaars",
+    author_email="danielmolenaars@binx.io",
+    description="CLI for port forwarding sessions with private AWS RDS and EC2 instances.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=["aws_ssh_tunnel"],
+    install_requires=[
+        "boto3",
+        "click",
+    ],
+    entry_points={
+        "console_scripts": [
+            "aws-ssh-tunnel=aws_ssh_tunnel:main",
+        ],
+    },
+)
