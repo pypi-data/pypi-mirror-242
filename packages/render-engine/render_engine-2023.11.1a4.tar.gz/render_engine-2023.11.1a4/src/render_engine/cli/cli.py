746794e42ad49aa4b63b928900669a7e46cbae95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 03:04:17 +0000

    Add package render-engine/2023.11.1a4/render_engine-2023.11.1a4.tar.gz

---
+++ packages/render-engine/render_engine-2023.11.1a4.tar.gz/render_engine-2023.11.1a4/src/render_engine/cli/cli.py
@@ -0,0 +1,307 @@
+# ruff: noqa: UP007
+
+import pathlib
+import typing
+
+import dtyper
+import typer
+from rich.console import Console
+from rich.progress import Progress
+
+from render_engine.cli.event import RegExHandler, get_app
+from render_engine.engine import engine
+from render_engine.site import Site
+
+app = typer.Typer()
+
+
+def _create_folder(*, folder: pathlib.Path, overwrite: bool) -> pathlib.Path:
+    """Create a folder if it doesn't exist or if overwrite is True"""
+    folder.mkdir(parents=True, exist_ok=overwrite)
+    return folder
+
+
+CREATE_APP_PY_TEMPLATE = engine.get_template("create_app_py.txt")
+
+
+def _create_templates_folder(
+    *templates,
+    project_folder: pathlib.Path,
+    templates_folder_name: pathlib.Path,
+    exists_ok: bool,
+) -> None:
+    """Create a folder for templates and optionally create an index.html file"""
+    path = project_folder.joinpath(templates_folder_name)
+    path.mkdir(
+        exist_ok=exists_ok,
+    )
+
+    for template in templates:
+        path.joinpath(template).write_text(engine.get_template(template).render())
+
+
+def _create_site_with_vars(
+    *,
+    site_title: typing.Optional[str] = None,  # noqa: UP007
+    site_url: typing.Optional[str] = None,
+    site_description: typing.Optional[str] = None,
+    site_author: typing.Optional[str] = None,
+    collection_path: typing.Optional[str] = None,
+) -> Site:
+    """Create a new site from a template"""
+    site = Site()
+    potential_site_vars = {
+        "site_title": site_title,
+        "site_url": site_url,
+        "site_author": site_author,
+        "site_description": site_description,
+        "collections_path": str(collection_path),
+    }
+    site_vars = {key: value for key, value in potential_site_vars.items() if value}
+    site.site_vars.update(site_vars)
+    return site
+
+
+@dtyper.function
+@app.command()
+def init(
+    collection_path: pathlib.Path = typer.Option(
+        pathlib.Path("pages"),
+        help="create your content folder in a custom location",
+        rich_help_panel="Path Options",
+    ),
+    force: bool = typer.Option(
+        False,
+        "--force",
+        "-f",
+        help="Force overwrite of existing files",
+        rich_help_panel="Flags",
+    ),
+    output_path: pathlib.Path = typer.Option(
+        "output",
+        help="custom output folder location.",
+        rich_help_panel="Path Attributes",
+    ),
+    project_path_name: pathlib.Path = typer.Option(
+        "app.py",
+        help="name of render_engine app name",
+        rich_help_panel="Path Attributes",
+    ),
+    project_folder: pathlib.Path = typer.Option(
+        pathlib.Path("./"),
+        help="path to create the project in",
+        rich_help_panel="Path Attributes",
+    ),
+    owner_name: typing.Optional[str] = typer.Option(
+        ...,
+        prompt="Owner Name",
+        help="Site Owners's Name",
+        rich_help_panel="Site Vars",
+    ),
+    owner_email: typing.Optional[str] = typer.Option(
+        ...,
+        prompt="Owner Email",
+        help="Email of Site's Owner",
+        rich_help_panel="Site Vars",
+    ),
+    site_description: typing.Optional[str] = typer.Option(
+        None,
+        help="(Optional): Site Description",
+        rich_help_panel="Site Vars",
+    ),
+    site_title: typing.Optional[str] = typer.Option(
+        None,
+        "--title",
+        "-t",
+        help="title of the site",
+        rich_help_panel="Site Vars",
+        show_default=False,
+    ),
+    site_url: typing.Optional[str] = typer.Option(
+        None,
+        "--url",
+        "-u",
+        help="URL for the site",
+        rich_help_panel="Site Vars",
+        show_default=False,
+    ),
+    skip_collection: bool = typer.Option(
+        False,
+        "--skip-collection",
+        "-C",
+        help="Skip creating the content folder and a collection",
+        rich_help_panel="Flags",
+    ),
+    skip_static: bool = typer.Option(
+        False,
+        "--skip-static",
+        "-S",
+        help="Skip copying static files",
+        rich_help_panel="Flags",
+    ),
+    static_path: pathlib.Path = typer.Option(
+        pathlib.Path("static"),
+        help="custom static folder",
+        rich_help_panel="Path Attributes",
+    ),
+    templates_path: pathlib.Path = typer.Option(
+        pathlib.Path("templates"),
+        "--templates-path",
+        help="custom templates folder",
+    ),
+):
+    """
+    CLI for creating a new site configuration.
+
+    Params:
+        collection_path: create your content folder in a custom location
+        force: Force overwrite of existing files
+        output_path: custom output folder location
+        project_path_name: name of render_engine app name
+        project_folder: path to create the project
+        owner:  owner of the site
+        site_description: Site Description
+        site_title: title of the site
+        site_url: URL for the site
+        skip_collection: Skip creating the content folder and a collection
+        skip_static: Skip copying static files
+        static_path: custom static folder
+        templates_path: custom templates folder
+    """
+    # creating the site object and site_vars
+
+    pathlib.Path(project_folder)
+    with Progress() as progress:
+        progress.console.rule("[green][bold]Creating Project")
+        # creating the app.py file from the template
+        project_config_path = pathlib.Path(project_folder).joinpath(project_path_name).with_suffix(".py")
+        task_generate_project_path = progress.add_task(f"Generating App File: [blue]{project_config_path}", total=1)
+
+        project_config_path.write_text(
+            CREATE_APP_PY_TEMPLATE.render(
+                site_title=site_title,
+                site_url=site_url,
+                site_description=site_description,
+                owner={"name": owner_name, "email": owner_email},
+                output_path=output_path,
+                skip_static=skip_static,
+                static_path=static_path,
+                collection_path=collection_path,
+                skip_collection=skip_collection,
+            )
+        )
+        progress.update(task_generate_project_path, advance=1)
+
+        # Create the templates folder and the index.html file
+        task_templates = progress.add_task(f"Creating Templates Folder: [blue]{templates_path}", total=1)
+        templates = ["index.html"]
+        _create_templates_folder(
+            *templates,
+            project_folder=project_folder,
+            templates_folder_name=templates_path,
+            exists_ok=force,
+        )
+
+        progress.update(task_templates, advance=1)
+
+        # Create the collection
+        if not skip_collection:
+            task_create_collection = progress.add_task(f"Creating Collection: [blue]{collection_path}", total=1)
+            _collection_path = pathlib.Path(project_folder).joinpath(collection_path)
+            _collection_path.mkdir(exist_ok=force)
+            _collection_path.joinpath("sample_page.md").write_text(
+                engine.get_template("base_collection_path.md").render()
+            )
+
+            progress.update(task_create_collection, advance=1)
+
+
+@app.command()
+def build(site_module: str):
+    """
+    CLI for creating a new site
+
+    Params:
+        site_module: module and class name of the site
+
+    """
+    app = get_app(site_module)
+    app.render()
+
+
+@app.command()
+def serve(
+    module_site: typing.Optional[str] = typer.Option(
+        None,
+        "--build",
+        "-b",
+        help="module:site for Build the site prior to serving",
+    ),
+    reload: typing.Optional[bool] = typer.Option(
+        None,
+        "--reload",
+        "-r",
+        help="Reload the server when files change",
+    ),
+    directory: typing.Optional[str] = typer.Option(
+        None,
+        "--directory",
+        "-d",
+        help="Directory to serve",
+        show_default=False,
+    ),
+    port: int = typer.Option(
+        8000,
+        "--port",
+        "-p",
+        help="Port to serve on",
+        show_default=False,
+    ),
+):
+    """
+    Create an HTTP server to serve the site at `localhost`.
+
+    !!! warning
+        this is only for development purposes and should not be used in production.
+
+    Params:
+        module_site: Python module and initialize Site class
+        reload: Use to reload server on file change
+        build: flag to build the site prior to serving the app
+        directory: Directory to serve. If `module_site` is provided, this will be the `output_path` of the site.
+        port: Port to serve on
+    """
+
+    if module_site:
+        app = get_app(module_site)
+        app.render()
+
+    if not directory:
+        if module_site:
+            directory = app.output_path
+        else:
+            directory = "output"
+
+    server_address = ("127.0.0.1", port)
+
+    handler = RegExHandler(
+        server_address=server_address,
+        dir_to_serve=directory,
+        app=app,
+        module_site=module_site,
+        patterns=None,
+        ignore_patterns=[r".*output\\*.+$", r"\.\\\..+$", r".*__.*+$"],
+    )
+
+    console = Console()
+
+    if not reload:
+        console.print(f"[bold green]Starting server on http://{server_address[0]}:{server_address[1]}[/bold green]")
+        handler._server(server_address=server_address, directory=directory).serve_forever()
+    else:
+        console.print("Watching for changes...")
+        handler.watch()
+
+
+def cli():
+    app()
---
+++ packages/render-engine/render_engine-2023.11.1a4.tar.gz/render_engine-2023.11.1a4/src/render_engine/site.py
@@ -0,0 +1,273 @@
+import logging
+import pathlib
+import typing
+from collections import defaultdict
+
+import pluggy
+from jinja2 import Environment, FileSystemLoader
+from rich.progress import Progress
+
+from .collection import Collection
+from .engine import engine
+from .hookspecs import _PROJECT_NAME, SiteSpecs
+from .page import Page
+from .utils.themes import Theme, ThemeManager
+
+
+class Site(ThemeManager):
+    """
+    The site stores your pages and collections to be rendered.
+
+    Attributes:
+        engine: Jinja2 Environment used to render pages
+        output_path:
+            path to write rendered content
+        partial:
+            if True, only render pages that have been modified. Uses gitPython to check for changes.
+        plugins:
+            list of plugins that will be loaded and passed into each object
+        static_paths:
+            list of paths for static folders. This will get copied to the output folder. Folders are recursive.
+        site_vars:
+            dictionary that will be passed into page template
+        site_settings:
+            settings that will be passed into pages and collections but not into templates
+    """
+
+    _pm: pluggy.PluginManager
+    partial: bool = False
+    site_settings: dict = {"plugins": {}}
+    site_vars: dict = {
+        "SITE_TITLE": "Untitled Site",
+        "SITE_URL": "http://localhost:8000/",
+        "DATETIME_FORMAT": "%d %b %Y %H:%M %Z",
+        "head": set(),
+        "theme": {},
+    }
+    engine: Environment = engine
+    template_path: str = "templates"
+
+    def __init__(
+        self,
+    ) -> None:
+        self.route_list = dict()
+        self.subcollections = defaultdict(lambda: {"pages": []})
+        self.engine.globals.update(self.site_vars)
+
+        if self.template_path:
+            self.engine.loader.loaders.insert(0, FileSystemLoader(self.template_path))
+
+        # Manage Plugins
+        self._pm = pluggy.PluginManager(project_name=_PROJECT_NAME)
+        self._pm.add_hookspecs(SiteSpecs)
+
+    def update_site_vars(self, **kwargs) -> None:
+        self.site_vars.update(**kwargs)
+        self.engine.globals.update(self.site_vars)
+
+    def register_plugins(self, *plugins, **settings: dict[str, typing.Any]) -> None:
+        """Register plugins with the site
+
+        parameters:
+            plugins: list of plugins to register
+            settings: settings to pass into the plugins
+                settings keys are the plugin names as strings.
+        """
+
+        for plugin in plugins:
+            self._pm.register(plugin)
+            self.site_settings["plugins"][plugin.__name__] = getattr(plugin, "default_settings", {})
+
+        self._pm.hook.add_default_settings(
+            site=self,
+            custom_settings=settings,
+        )
+        self.site_settings["plugins"].update(**settings)
+
+    @property
+    def plugins(self):
+        return self._pm.get_plugins()
+
+    def register_theme(self, theme: Theme):
+        """Overrides the ThemeManager register_theme method to add plugins to the site"""
+        super().register_theme(theme)
+
+        if theme.plugins:
+            self.register_plugins(*theme.plugins)
+
+    def update_theme_settings(self, **settings):
+        for key, value in settings.items():
+            self.site_vars["theme"].update({key: value})
+
+    def collection(self, Collection: type[Collection]) -> Collection:
+        """
+        Add the collection to the route list to be rendered later.
+
+        This is the primary way to add a collection to the site and
+        can either be called on an uninstantiated class or on the class definition as a decorator.
+
+        In most cases. You should use the decorator method.
+
+        ```python
+        from render_engine import Site, Collection
+
+        site = Site()
+
+        @site.collection # works
+        class Pages(Collection):
+            pass
+
+
+        class Posts(Collection):
+            pass
+
+        site.collection(Posts) # also works
+        ```
+        """
+        _Collection = Collection()
+        self.register_themes(*getattr(_Collection, "required_themes", []))
+        plugins = [*self.plugins, *getattr(_Collection, "plugins", [])]
+
+        for plugin in getattr(_Collection, "ignore_plugins", []):
+            plugins.remove(plugin)
+        _Collection.register_plugins(plugins)
+
+        self._pm.hook.pre_build_collection(
+            collection=_Collection,
+            settings=self.site_settings.get("plugins", {}),
+        )  # type: ignore
+        self.route_list[_Collection._slug] = _Collection
+        return _Collection
+
+    def page(self, Page: Page) -> Page:
+        """
+        Add the page to the route list to be rendered later.
+        Also remaps `title` in case the user wants to use it in the template rendering.
+
+        This is the primary way to add a page to the site and can either be called
+        on an uninstantiated class or on the class definition as a decorator.
+
+        In most cases. You should use the decorator method.
+
+        ```python
+
+        from render_engine import Site, Page
+
+        site = Site()
+
+        @site.page # works
+        class Home(Page):
+            pass
+
+        class About(Page):
+            pass
+
+        site.page(About) # also works
+        ```
+        """
+        page = Page()
+        page.title = page._title  # Expose _title to the user through `title`
+
+        # copy the plugin manager, removing any plugins that the page has ignored
+        page._pm = self._pm
+
+        for plugin in getattr(page, "ignore_plugins", []):
+            page._pm.unregister(plugin)
+
+        self.route_list[getattr(page, page._reference)] = page
+
+    def _render_output(self, route: str, page: Page):
+        """writes the page object to disk"""
+        path = pathlib.Path(self.output_path) / pathlib.Path(route) / pathlib.Path(page.path_name)
+        path.parent.mkdir(parents=True, exist_ok=True)
+        settings = {**self.site_settings.get("plugins", {}), **{"route": route}}
+        self._pm.hook.render_content(page=page, settings=settings)
+        page.rendered_content = page._render_content(engine=self.engine)
+        # pass the route to the plugin settings
+        self._pm.hook.post_render_content(page=page.__class__, settings=settings, site=self)
+
+        return path.write_text(page.rendered_content)
+
+    def _render_partial_collection(self, collection: Collection) -> None:
+        """Iterate through the Changed Pages and Check for Collections and Feeds"""
+        for entry in collection._generate_content_from_modified_pages():
+            for route in collection.routes:
+                self._render_output(route, entry)
+
+        if getattr(collection, "has_archive", False):
+            for archive in collection.archives:
+                logging.debug("Adding Archive: %s", archive.__class__.__name__)
+
+                self._render_output(collection.routes[0], archive)
+
+        if hasattr(collection, "Feed"):
+            self._render_output("./", collection.feed)
+
+    def _render_full_collection(self, collection: Collection) -> None:
+        """Iterate through Pages and Check for Collections and Feeds"""
+
+        for entry in collection:
+            for route in collection.routes:
+                self._render_output(route, entry)
+
+        if getattr(collection, "has_archive", False):
+            for archive in collection.archives:
+                logging.debug("Adding Archive: %s", archive.__class__.__name__)
+
+                for route in collection.routes:
+                    self._render_output(collection.routes[0], archive)
+
+        if hasattr(collection, "Feed"):
+            self._render_output("./", collection.feed)
+
+    def render(self) -> None:
+        """
+        Render all pages and collections.
+
+        These are pages and collections that have been added to the site using
+        the [`Site.page`][src.render_engine.Site.page]
+        and [`Site.collection`][src.render_engine.Site.collection] methods.
+
+        Render should be called after all pages and collections have been added to the site.
+
+        You can choose to call it manually in your file or
+        use the CLI command [`render-engine build`][src.render_engine.cli.build]
+        """
+
+        with Progress() as progress:
+            pre_build_task = progress.add_task("Loading Pre-Build Plugins", total=1)
+            self._pm.hook.pre_build_site(site=self, settings=self.site_settings.get("plugins", {}))  # type: ignore
+
+            self.engine.globals.update(self.site_vars)
+            # Parse Route List
+            task_add_route = progress.add_task("[blue]Adding Routes", total=len(self.route_list))
+
+            self._render_static()
+
+            self.engine.globals["site"] = self
+            self.engine.globals["routes"] = self.route_list
+
+            for slug, entry in self.route_list.items():
+                progress.update(task_add_route, description=f"[blue]Adding[gold]Route: [blue]{slug}")
+                if isinstance(entry, Page):
+                    if getattr(entry, "collection", None):
+                        self._pm.hook.render_content(Page=entry, settings=self.site_settings.get("plugins", None))
+                    for route in entry.routes:
+                        progress.update(
+                            task_add_route,
+                            description=f"[blue]Adding[gold]Route: [blue]{entry._slug}",
+                        )
+                        self._render_output(route, entry)
+
+                if isinstance(entry, Collection):
+                    if not self.partial:
+                        self._render_full_collection(entry)
+                    else:
+                        self._render_partial_collection(entry)
+
+            progress.add_task("Loading Post-Build Plugins", total=1)
+            self._pm.hook.post_build_site(
+                site=self,
+                settings=self.site_settings.get("plugins", {}),
+            )
+            progress.update(pre_build_task, advance=1)
