a476d17185de6614227b0e59341fae0546125d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 03:04:12 +0000

    Add package async-processor/0.1.12/async_processor-0.1.12-py3-none-any.whl

---
+++ packages/async-processor/async_processor-0.1.12-py3-none-any.whl/async_processor/sidecar/sidecar.py
@@ -0,0 +1,66 @@
+import asyncio
+from typing import Optional
+
+import aiohttp
+
+from async_processor import (
+    AsyncProcessor,
+    InputMessageInterface,
+    OutputMessage,
+    ProcessStatus,
+)
+from async_processor.logger import logger
+from async_processor.pydantic_v1 import BaseSettings, confloat
+
+
+class Settings(BaseSettings):
+    destination_url: str
+    request_timeout: confloat(gt=0) = 3.0
+
+    class Config:
+        env_prefix = "TFY_ASYNC_PROCESSOR_SIDECAR_"
+
+
+settings = Settings()  # type: ignore
+
+
+class SidecarProcessor(AsyncProcessor):
+    def __init__(self):
+        self._client_session: Optional[aiohttp.ClientSession] = None
+
+    async def init(self):
+        self._client_session = aiohttp.ClientSession()
+        while True:
+            try:
+                async with self._client_session.head(
+                    settings.destination_url,
+                    timeout=settings.request_timeout,
+                ):
+                    ...
+                break
+            except Exception as ex:
+                logger.warning(
+                    "Cannot connect to %s retrying in 1 second. " "%s",
+                    settings.destination_url,
+                    str(ex),
+                )
+                await asyncio.sleep(1.0)
+
+    async def process(self, input_message: InputMessageInterface) -> OutputMessage:
+        async with self._client_session.post(
+            settings.destination_url,
+            json=input_message.get_body(),
+            timeout=settings.request_timeout,
+        ) as response:
+            status = ProcessStatus.SUCCESS if response.ok else ProcessStatus.FAILED
+            return OutputMessage(
+                request_id=input_message.get_request_id(),
+                status=status,
+                body=await response.text(),
+                status_code=str(response.status),
+                content_type=response.headers["content-type"],
+                input_message=input_message if status is ProcessStatus.FAILED else None,
+            )
+
+
+app = SidecarProcessor().build_app()
