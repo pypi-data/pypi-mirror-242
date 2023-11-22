e6625b417d3feeef6298e7efee1cf7de70d30099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-22 03:04:00 +0000

    Add package Slugify-class/0.1.5/Slugify_class-0.1.5.tar.gz

---
+++ packages/Slugify-class/Slugify_class-0.1.5.tar.gz/Slugify_class-0.1.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Slugify_class',
+    version='0.1.5',
+    author=['Marcklens Badio', 'Wendel Hector'],
+    author_email='baddesignhaiti@gmail.com',
+    long_description='Ce package est la pour un devoir de python realiser par Marcklens et Wendel',
+    url='https://github.com/wendoohector/SGLUG_Python.git',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
