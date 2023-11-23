a979e7a613d669d5f9c60b1f37dbf33553db5d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 04:04:36 +0000

    Add package colorem/0.1.0/colorem-0.1.0-py3-none-any.whl

---
+++ packages/colorem/colorem-0.1.0-py3-none-any.whl/colorem/classes.py
@@ -0,0 +1,5 @@
+class Model:
+    def __init__(self):
+        pass
+    def show(self):
+        print("Testing model class")
---
+++ packages/colorem/colorem-0.1.0-py3-none-any.whl/colorem/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+exec(open("colorem/_version.py").read())
+
+setup(
+    version=__version__)
