1a58f37766752b8f66824e8bc178fbe8af1eec7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 14:05:32 +0000

    Add package PromptTrack/0.1.17/PromptTrack-0.1.17.tar.gz

---
+++ packages/PromptTrack/PromptTrack-0.1.17.tar.gz/PromptTrack-0.1.17/README.md
@@ -0,0 +1,27 @@
+## PromptTrack 
+A library for tracking based on your prompt 
+
+# Installation
+pip install PromptTrack
+The package has been implemented for python 3.9 and later might work on 3.8 and 3.7
+You can check our [github repo](https://github.com/ngobibibnbe/PromptTrack)
+
+
+# Usage
+from PromptTrack import PromptTracker
+
+tracker = PromptTracker()
+
+video_file = "[path_to_your_video](https://www.pexels.com/video/penguins-hopping-down-the-stairs-9116156/)"  #[video example](https://www.pexels.com/video/penguins-hopping-down-the-stairs-9116156/)
+
+tracker.detect_objects(video_file, prompt="i am interested in penguin",nms_threshold=0.8) 
+#you can replace the prompt value by a caption of what you are interested in
+
+tracker.process_mot (video_file, fixed_parc=False, track_thresh=0.2, match_thresh=0.8, frame_rate=25,)
+#Default values fixed_parc=True, nbr_items=15, track_thresh=0, match_thresh=1, frame_rate=6, track_buffer=10000, max_time_lost=20000)
+
+tracker.read_video_with_mot(video_file)
+
+# Result
+It will provide you in the video folder, a video with the track and a json file with track in the format {frame_id:{pig_id:{x:"", y:"",width:"",height:""}}}
+
---
+++ packages/PromptTrack/PromptTrack-0.1.17.tar.gz/PromptTrack-0.1.17/setup.py
@@ -0,0 +1,40 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# This call to setup() does all the work
+setup(
+    name="PromptTrack",
+    version="0.1.17",
+    description="Demo library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://PaquetLab_PromptTrack.readthedocs.io/",
+    author="sophie NGO BIBINBE",
+    author_email="anne.ngobibinbe@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent"
+    ],
+    packages=["PromptTrack"],
+    include_package_data=True,
+    install_requires=["numpy","timm==0.4.5","transformers==4.6", "matplotlib", "scikit-image", "opencv-python", "nms==0.1.6","bytetracker"]
+)
