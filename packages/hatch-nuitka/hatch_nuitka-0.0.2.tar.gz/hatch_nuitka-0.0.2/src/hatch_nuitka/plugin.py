c9eb9624705e277ea336b2ca5ebc9d45b55df493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 16:04:45 +0000

    Add package hatch-nuitka/0.0.2/hatch_nuitka-0.0.2.tar.gz

---
+++ packages/hatch-nuitka/hatch_nuitka-0.0.2.tar.gz/hatch_nuitka-0.0.2/src/hatch_nuitka/plugin.py
@@ -0,0 +1,61 @@
+from typing import Any
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+import platform
+
+class NuitkaBuildHook(BuildHookInterface):
+    PLUGIN_NAME = "nuitka"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__config_nuitka_args = None
+        self.__config_options = None
+        self.__config_separation = None
+        self.__config_build_dir = None
+        self.__config_include = None
+        self.__config_exclude = None
+        self.__package_source = None
+        self.__include_spec = None
+        self.__exclude_spec = None
+        self.__included_files = None
+        self.__normalized_included_files = None
+        self.__artifact_globs = None
+        self.__normalized_artifact_globs = None
+        self.__artifact_patterns = None
+        self._on_windows = platform.system() == 'Windows'
+        self.__compiled_extension = '.pyd' if self._on_windows else '.so'
+        
+    @property
+    def config_nuitka(self):
+        if self.__config_nuitka_args is None:
+            nuitka_args = self.config.get('nuitka-args', {})
+            if isinstance(self.__config_nuitka_args, list):
+                for i, argument in enumerate(nuitka_args, 1):
+                    if not isinstance(argument, str):
+                        raise ValueError(f"nuitka-args[{i}] is not a string")
+                    elif not argument:
+                        raise ValueError(f"nuitka-args[{i}] is empty")
+            else:
+                raise ValueError("nuitka-args is not a list")
+            self.__config_nuitka_args = nuitka_args
+        return self.__config_nuitka_args
+    
+    @property
+    def config_options(self):
+        if self.__config_options is None:
+            options = self.config.get('options', {})
+            if not isinstance(options, dict):
+                raise ValueError("options is not a dict")
+            self.__config_options = options
+        return self.__config_options
+    
+    @property
+    def config_separation(self):
+        if self.__config_separation is None:
+            self.__config_separation = self.config.get('separation', False) == True
+        return self.__config_separation
+    
+    
+    def initialize(self, version: str, build_data: dict[str, Any]) -> None:
+        if self.target_name != "wheel":
+            return
+        
