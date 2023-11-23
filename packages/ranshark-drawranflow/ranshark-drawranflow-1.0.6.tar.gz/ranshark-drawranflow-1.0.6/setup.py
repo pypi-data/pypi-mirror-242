fdb01e15e5f84fe6bbeab64b42de7d16ce6ba307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 04:04:49 +0000

    Add package ranshark-drawranflow/1.0.6/ranshark-drawranflow-1.0.6.tar.gz

---
+++ packages/ranshark-drawranflow/ranshark-drawranflow-1.0.6.tar.gz/ranshark-drawranflow-1.0.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+
+setup(
+    install_requires=[
+        'Django~=4.2.7',
+        'celery~=5.3.4',
+        'pandas~=1.3.3',
+        'pyshark~=0.6',
+
+    ],
+)
