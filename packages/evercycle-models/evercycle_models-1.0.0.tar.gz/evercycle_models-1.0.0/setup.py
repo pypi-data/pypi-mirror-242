81146c07e45dac668b7f9c4994974d2f541492bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 14:05:37 +0000

    Add package evercycle-models/1.0.0/evercycle_models-1.0.0.tar.gz

---
+++ packages/evercycle-models/evercycle_models-1.0.0.tar.gz/evercycle_models-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='evercycle_models',  # Replace with your app's name
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',  # Replace with your choice of license
+    description='Everycle Models',
+    url='https://github.com/evercycle-org/evercycle-models',
+    author='Evercycle',
+    author_email='mija@evercycle.io',
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        # Other classifiers...
+    ],
+    install_requires=[
+        'django>=3.0',
+        # Other dependencies...
+    ],
+)
