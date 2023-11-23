a940ce1c337ad5f1c83452a5a831a887f4d0594d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 04:04:16 +0000

    Add package orb-billing/1.32.0/orb_billing-1.32.0-py3-none-any.whl

---
+++ packages/orb-billing/orb_billing-1.32.0-py3-none-any.whl/orb/types/plan_update_params.py
@@ -0,0 +1,20 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from __future__ import annotations
+
+from typing import Dict, Optional
+from typing_extensions import TypedDict
+
+__all__ = ["PlanUpdateParams"]
+
+
+class PlanUpdateParams(TypedDict, total=False):
+    external_plan_id: Optional[str]
+    """
+    An optional user-defined ID for this plan resource, used throughout the system
+    as an alias for this Plan. Use this field to identify a plan by an existing
+    identifier in your system.
+    """
+
+    metadata: Optional[Dict[str, Optional[str]]]
+    """User-specified key/value pairs for the resource."""
