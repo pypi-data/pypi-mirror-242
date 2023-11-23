a521f1f189a19e8e864dc7169fece1b283676434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 04:04:06 +0000

    Add package bonus/1.1.6/bonus-1.1.6.tar.gz

---
+++ packages/bonus/bonus-1.1.6.tar.gz/bonus-1.1.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bonus',
+    version='1.1.6',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='AI University',
+    author_email='falson2007@yandex.ru',
+)
