30490030a7aedfe15a4be25380f8205b66bd85f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 03:04:42 +0000

    Add package nov22lib/1.0/nov22lib-1.0.tar.gz

---
+++ packages/nov22lib/nov22lib-1.0.tar.gz/nov22lib-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nov22lib',
+    version='1.0',
+    Summary = 'Easily download, build, install, upgrade, and uninstall Python packages',
+    Author='Hemalatha',
+    Author_email='hemalatha.chandaka@thundersoft.com' ,
+    Location= 'C:/Users/dell/AppData/Local/Programs/Python/Python311/Lib/site-packages',
+    packages=find_packages(),
+
+install_requires=[
+        # list your dependencies here
+
+    ],
+)
