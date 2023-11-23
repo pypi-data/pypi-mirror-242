582855d7ea745401a302dd067ffc5fc0ccc4f7ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 03:05:34 +0000

    Add package openpicv/1.1.7/openpicv-1.1.7.tar.gz

---
+++ packages/openpicv/openpicv-1.1.7.tar.gz/openpicv-1.1.7/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name="openpicv",  # 模块名称
+    version="1.1.7",  # 当前版本
+    author="coder_zqs",  # 作者
+    author_email="wupeiqi@163.com",  # 作者邮箱
+    description="一个非常NB的包",  # 模块简介
+    long_description=long_description,  # 模块详细介绍,这里可以读取readme文件
+    long_description_content_type="text/markdown",  # 模块详细介绍格式
+    # url="https://github.com/wupeiqi/fucker",  # 模块github地址
+    packages=setuptools.find_packages(),  # 自动找到项目中导入的模块
+    # 模块相关的元数据
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # 依赖模块
+    install_requires=[
+        'opencv-python',
+    ],
+    python_requires='>=3',
+)
+
