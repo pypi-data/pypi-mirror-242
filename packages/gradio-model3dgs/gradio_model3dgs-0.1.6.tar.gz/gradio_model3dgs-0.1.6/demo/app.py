428b8a32f49fb158d8d763bbaf8866f515361a46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 14:05:26 +0000

    Add package gradio-model3dgs/0.1.6/gradio_model3dgs-0.1.6.tar.gz

---
+++ packages/gradio-model3dgs/gradio_model3dgs-0.1.6.tar.gz/gradio_model3dgs-0.1.6/demo/app.py
@@ -0,0 +1,21 @@
+
+import gradio as gr
+from gradio_model3dgs import Model3DGS
+
+
+def load_splat(splat_file_name):
+    return splat_file_name
+
+
+demo = gr.Interface(
+    fn=load_splat,
+    inputs=Model3DGS(),
+    outputs=Model3DGS(),
+    examples=[
+        "https://huggingface.co/datasets/dylanebert/3dgs/resolve/main/bonsai/bonsai-7k.splat"
+    ],
+    cache_examples=True,
+)
+
+
+demo.launch()
