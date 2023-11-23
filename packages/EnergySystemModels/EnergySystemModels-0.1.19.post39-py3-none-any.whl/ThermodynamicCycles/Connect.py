cd1fa874e527e16053723b8a74868409a279bde5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 16:04:50 +0000

    Add package EnergySystemModels/0.1.19.post39/EnergySystemModels-0.1.19.post39-py3-none-any.whl

---
+++ packages/EnergySystemModels/EnergySystemModels-0.1.19.post39-py3-none-any.whl/ThermodynamicCycles/Connect.py
@@ -0,0 +1,9 @@
+from ThermodynamicCycles.FluidPort.FluidPort import FluidPort
+def Fluid_connect( a=FluidPort(),b=FluidPort()):
+    a.fluid=b.fluid
+    a.P=b.P
+    a.h=b.h
+    a.F=b.F
+    a.S=b.S
+    a.T=b.T
+    return "connectés"
