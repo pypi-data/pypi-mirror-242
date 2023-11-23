eafc3f9849cb0d153f91aaab5629d03bb32ab231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 04:04:45 +0000

    Add package serena/0.8.0/serena-0.8.0-py3-none-any.whl

---
+++ packages/serena/serena-0.8.0-py3-none-any.whl/serena/__init__.py
@@ -0,0 +1,30 @@
+import logging
+
+# our public exports, relatively minimal
+from serena.channel import Channel
+from serena.connection import AMQPConnection, open_connection
+from serena.enums import ClassID, ExchangeType, ReplyCode
+from serena.exc import (
+    AMQPError,
+    AMQPStateError,
+    InvalidPayloadTypeError,
+    InvalidProtocolError,
+    MessageReturnedError,
+    UnexpectedCloseError,
+)
+from serena.message import AMQPMessage
+from serena.mixin import ChannelDelegate, ChannelLike
+from serena.payloads.header import BasicHeader
+
+if not hasattr(logging.Logger, "trace"):
+    # TRACE_LEVEL = 5
+    logging.addLevelName(5, "TRACE")
+
+    def trace(self, message, *args, **kws) -> None:  # type: ignore
+        if self.isEnabledFor(5):
+            self._log(5, message, args, **kws)
+
+    logging.Logger.trace = trace  # type: ignore
+    del trace
+
+del logging
