87bf6c9c56520a483ea0507f17274d896a39a024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 15:05:03 +0000

    Add package Bs-Extractor/1.3.4/Bs_Extractor-1.3.4.tar.gz

---
+++ packages/Bs-Extractor/Bs_Extractor-1.3.4.tar.gz/Bs_Extractor-1.3.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("/Users/nabilalasri/new_bsextractor_pkg/requirements.txt") as f:
+    required = f.read().splitlines()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="Bs_Extractor",
+    version="1.3.4",
+    author="Nabila Lasri",
+    author_email="lasri.nabilait@gmail.com",
+    long_description_content_type="text/markdown",
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=required,
+    include_package_data=True,
+)
