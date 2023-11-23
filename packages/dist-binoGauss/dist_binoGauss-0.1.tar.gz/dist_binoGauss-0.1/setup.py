921c1315a4329942d688563715d8806dd4db4c5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 04:04:42 +0000

    Add package dist-binoGauss/0.1/dist_binoGauss-0.1.tar.gz

---
+++ packages/dist-binoGauss/dist_binoGauss-0.1.tar.gz/dist_binoGauss-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_binoGauss',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dist_binoGauss'],
+      author = 'Sharon Olatunji',
+      author_email = 'dolivon555@gmail.com',
+      zip_safe=False)
