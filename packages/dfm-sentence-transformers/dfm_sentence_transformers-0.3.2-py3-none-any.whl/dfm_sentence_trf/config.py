79eda5b50f780d2b625e924a5f91ddfb29381771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 04:04:32 +0000

    Add package dfm-sentence-transformers/0.3.2/dfm_sentence_transformers-0.3.2-py3-none-any.whl

---
+++ packages/dfm-sentence-transformers/dfm_sentence_transformers-0.3.2-py3-none-any.whl/dfm_sentence_trf/config.py
@@ -0,0 +1,17 @@
+from confection import Config
+
+default_config = Config(
+    dict(
+        training=dict(
+            epochs=5,
+            warmup_steps=100,
+            batch_size=30,
+            max_seq_length=128,
+            steps_per_epoch=None,
+        ),
+        model=dict(
+            device="cpu",
+            max_seq_length=128,
+        ),
+    ),
+)
