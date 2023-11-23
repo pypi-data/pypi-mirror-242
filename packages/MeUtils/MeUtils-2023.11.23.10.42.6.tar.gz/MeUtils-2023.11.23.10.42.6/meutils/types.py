82ef173f32e047599534e7c3ce0ad96e02f676b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 15:04:48 +0000

    Add package MeUtils/2023.11.23.10.42.6/MeUtils-2023.11.23.10.42.6.tar.gz

---
+++ packages/MeUtils/MeUtils-2023.11.23.10.42.6.tar.gz/MeUtils-2023.11.23.10.42.6/meutils/types.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : types
+# @Time         : 2023/8/15 12:14
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  :
+from os import PathLike
+
+from meutils.pipe import *
+
+StrPath = Union[str, PathLike]
+
+from typing import List
+
+
+def is_list_of_strings(lst):
+    return isinstance(lst, List) and all(isinstance(item, str) for item in lst)
+
+
+def is_list_of_ints(lst):
+    return isinstance(lst, List) and all(isinstance(item, int) for item in lst)
