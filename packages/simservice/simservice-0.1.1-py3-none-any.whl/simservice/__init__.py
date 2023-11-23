798284f585d7ab256cc454517a0c3c0b63d6a3b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 05:05:10 +0000

    Add package simservice/0.1.1/simservice-0.1.1-py3-none-any.whl

---
+++ packages/simservice/simservice-0.1.1-py3-none-any.whl/simservice/__init__.py
@@ -0,0 +1,48 @@
+"""
+SimService
+-----------
+
+A library for building simulation services in Python.
+
+MIT License
+-----------
+
+Copyright (c) 2022, 2023 T.J. Sego, Ph.D.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+# Access for customizing a service proxy interface on the fly by a service
+from .ServiceFunctionRegistry import service_function
+from .service_factory import registered_services
+from .service_factory import close_service
+from .utils import ExecutionContext
+import os
+
+__version__ = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'VERSION.txt')).readline().strip()
+
+
+def close():
+    """
+    Closes module managers
+
+    :return: None
+    """
+    from .managers import close_services
+    close_services()
