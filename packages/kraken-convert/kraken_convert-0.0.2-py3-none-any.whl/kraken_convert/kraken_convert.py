348f472486fa2aae3ac646ece679b338e73e479f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 04:04:45 +0000

    Add package kraken-convert/0.0.2/kraken_convert-0.0.2-py3-none-any.whl

---
+++ packages/kraken-convert/kraken_convert-0.0.2-py3-none-any.whl/kraken_convert/kraken_convert.py
@@ -0,0 +1,21 @@
+
+from kraken_convert import to_dot
+from kraken_convert import mapping_engine
+from kraken_convert import json_helper as json
+import time
+
+
+
+def convert(records, map):
+    """Given records and a map, converts records
+    """
+
+    # Cycle through records and transform using mapping
+    mapped_records = []
+    for record in records:
+        mapped_record = mapping_engine.mapping_engine(record, map)
+        mapped_record = mapped_record if isinstance(mapped_record, list) else [mapped_record]
+        mapped_records += mapped_record
+       
+    return mapped_records
+    
