7ce2656f9627e765a450e5525ffac394e94dad75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 04:04:57 +0000

    Add package uniboost/0.8.37/uniboost-0.8.37.tar.gz

---
+++ packages/uniboost/uniboost-0.8.37.tar.gz/uniboost-0.8.37/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='uniboost',
+    version='0.8.37',
+    packages=find_packages(),
+    install_requires=[
+        # your dependencies, e.g.,
+        # 'requests',
+    ],
+    # other metadata
+    author='MIT',
+    description='AWS',
+    url='https://github.com/yourusername/your-package-name',  # if applicable
+)
