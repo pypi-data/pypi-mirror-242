2531e61b7a36f67bd1c042e5356cbe5d887dfff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 05:04:34 +0000

    Add package example-py-create-package/0.5.0/example_py_create_package-0.5.0.tar.gz

---
+++ packages/example-py-create-package/example_py_create_package-0.5.0.tar.gz/example_py_create_package-0.5.0/example_py_create_package/__init__.py
@@ -0,0 +1,9 @@
+from .moduleMain import moduleMain
+from .initVacio import init_vacio
+from example_py_create_package.sinInit import sin_init
+from .bienImportado.bien_importado import bienImportado
+import example_py_create_package.indexRelleno
+
+bien_importado = {
+    "bienImportado": bienImportado
+}
