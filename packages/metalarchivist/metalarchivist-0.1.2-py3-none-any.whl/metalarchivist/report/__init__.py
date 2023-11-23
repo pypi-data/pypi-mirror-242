20c7cd76979cdafc347c88d4fe923289836b02ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 14:05:28 +0000

    Add package metalarchivist/0.1.2/metalarchivist-0.1.2-py3-none-any.whl

---
+++ packages/metalarchivist/metalarchivist-0.1.2-py3-none-any.whl/metalarchivist/report/__init__.py
@@ -0,0 +1,5 @@
+from .albums import get_albums
+from .bands import get_bands
+
+__all__ = ['get_albums',
+           'get_bands']
---
+++ packages/metalarchivist/metalarchivist-0.1.2-py3-none-any.whl/metalarchivist/report/albums.py
@@ -0,0 +1,30 @@
+from .. import export
+
+from .bands import get_bands
+
+import pandas as pd
+
+
+def get_albums(range_start=None, range_stop=None, verbose=False):
+    if range_start:
+        release_page = export.Albums.get_range(range_start, range_stop, verbose=verbose)
+    else:
+        release_page = export.Albums.get_upcoming(verbose=verbose)
+
+    release = pd.DataFrame(release_page.data)
+
+    # hoist out the link attributes from each band
+    profile_urls = release['band'].apply(lambda n: n['link'])
+    profile_urls = pd.DataFrame(profile_urls.to_list(), columns=['band_url'])
+
+    band = get_bands(list(profile_urls['band_url'].unique()), verbose=verbose)
+
+    album = pd.DataFrame(list(release['album']))
+    album = album.rename(columns=dict(name='album', link='album_url'))
+
+    band = band.rename(columns=dict(url='band_url'))
+    release = release.drop(columns=['genres', 'band', 'album'])
+    album = pd.concat([album, profile_urls, release], axis=1)
+
+    return pd.merge(left=album, right=band, how='left', 
+                    left_on='band_url', right_on='band_url')
