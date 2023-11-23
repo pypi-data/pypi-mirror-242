9c88b3f40e817c3e530f4ff2f8b77821ac19144d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 14:05:06 +0000

    Add package routing-lib/0.2.3/routing_lib-0.2.3.tar.gz

---
+++ packages/routing-lib/routing_lib-0.2.3.tar.gz/routing_lib-0.2.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+abs_path = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name='routing_lib',
+    version='0.2.3',
+    license='MIT',
+    description = 'Small package with alternative routing algorithms and measures.',
+    long_description = open(os.path.join(abs_path, 'README.rst')).read(),
+    author="Ludovico Lemma",
+    author_email='lwdovico@protonmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/lwdovico/routing-lib',
+    keywords='Utils',
+    install_requires=[
+          'matplotlib',
+          'numpy',
+          'Shapely==1.8.5',
+          'Fiona',
+          'rtree',
+          'pyproj',
+          'pygeos',
+          'scikit-mobility',
+          'geopandas',
+          'compress-json',
+          'tqdm',
+          'igraph',
+          'eclipse-sumo==1.15.0',
+          'sumolib==1.15.0',
+          'libsumo==1.15.0',
+          'seaborn',
+      ],
+
+)
---
+++ packages/routing-lib/routing_lib-0.2.3.tar.gz/routing_lib-0.2.3/src/routing_lib/__init__.py
@@ -0,0 +1,7 @@
+"""Small package with alternative routing algorithms and measures. Author: Ludovico Lemma"""
+
+__version__ = '0.2.3'
+
+from .routing_algorithms import *
+from .routing_measures import *
+from .routing_utils import *
