b4cf5c147a1fbfc19aa99aa7d7b1409f078546a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 04:04:49 +0000

    Add package gempy-plugins/2023.2.0b1/gempy_plugins-2023.2.0b1.tar.gz

---
+++ packages/gempy-plugins/gempy_plugins-2023.2.0b1.tar.gz/gempy_plugins-2023.2.0b1/setup.py
@@ -0,0 +1,32 @@
+﻿# setup.py for gempy_viewer. Requierements are numpy and matplotlib
+
+from setuptools import setup, find_packages
+
+import gempy
+
+version = gempy.__version__
+
+
+def read_requirements(file_name):
+    with open(file_name, "r", encoding="utf-8") as f:
+        return [line.strip() for line in f.readlines()]
+
+
+setup(
+    name='gempy_plugins',
+    version=version,
+    packages=find_packages(),
+    url='',
+    license='EUPL',
+    author='Miguel de la Varga', 
+    author_email="miguel@terranigma-solutions.com",
+    description='Extra plugins for the geological modeling package GemPy',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Scientific/Engineering :: GIS',
+        'Programming Language :: Python :: 3.10'
+    ],
+    python_requires='>=3.10'
+)
