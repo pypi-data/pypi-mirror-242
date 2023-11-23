2083fd097e1e92568f3fad74157149d5b5453c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 03:05:01 +0000

    Add package azure-data-factory-testing-framework/0.0.0a36/azure_data_factory_testing_framework-0.0.0a36.tar.gz

---
+++ packages/azure-data-factory-testing-framework/azure_data_factory_testing_framework-0.0.0a36.tar.gz/azure_data_factory_testing_framework-0.0.0a36/azure_data_factory_testing_framework/data_factory/generated/data_factory_element.py
@@ -0,0 +1,12 @@
+from typing import Generic, TypeVar
+
+T = TypeVar("T")
+
+
+class DataFactoryElement(Generic[T]):
+    expression: str
+    value: T
+
+    def __init__(self, expression: str):
+        self.expression = expression
+
