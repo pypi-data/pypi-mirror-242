2d43a4f86c9d58d18b43c26208590bc34a07f13c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 05:04:51 +0000

    Add package cotd-stats-visualiser/0.0.1a3/cotd-stats-visualiser-0.0.1a3.tar.gz

---
+++ packages/cotd-stats-visualiser/cotd-stats-visualiser-0.0.1a3.tar.gz/cotd-stats-visualiser-0.0.1a3/cotd_stats_visualiser/const.py
@@ -0,0 +1,17 @@
+__all__ = ("path", "headers", "API_URL")
+
+import os
+import pathlib
+
+path = pathlib.Path(os.path.expanduser("~/documents")).absolute()
+if not os.path.exists(path / "cotd_stats_visualiser_cache"):
+    os.mkdir(path / "cotd_stats_visualiser_cache")
+path /= "cotd_stats_visualiser_cache"
+
+headers: dict[str, str] = {
+    "User-Agent": "COTD Stats Visualiser / 0.0.1-alpha.3 A script that generates a visualisation of the COTD "
+    "stats/results for certain players in a given time period. ("
+    "https://github.com/EdVraz/COTDStatsVisualiser) / contact: @edvraz (discord)"
+}
+
+API_URL: str = "https://trackmania.io/api/"
