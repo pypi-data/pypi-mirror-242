0968086fb1d36534ea893801701110f7175da7b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 06:04:21 +0000

    Add package teeplot/0.6.2/teeplot-0.6.2.tar.gz

---
+++ packages/teeplot/teeplot-0.6.2.tar.gz/teeplot-0.6.2/teeplot/teeplot.py
@@ -0,0 +1,152 @@
+import hashlib
+from keyname import keyname as kn
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import pathlib
+from slugify import slugify
+
+def _digest(data):
+
+    # also considered https://newbedev.com/how-to-generate-a-hash-or-checksum-value-on-python-dataframe-created-from-a-fixed-width-file
+    # which is slower
+    if isinstance(data, pd.DataFrame):
+        # adapted from https://stackoverflow.com/a/47800021
+        return hashlib.sha256(
+            pd.util.hash_pandas_object(data, index=True).values
+        ).hexdigest()
+    elif isinstance(data, pd.core.series.Series):
+        # adapted from https://stackoverflow.com/a/47800021
+        return hashlib.sha256(
+            pd.util.hash_pandas_object(data, index=True).values
+        ).hexdigest()
+    elif isinstance(data, np.ndarray):
+        # adapted from https://stackoverflow.com/a/806342
+        view = data.view(np.uint8)
+        return hashlib.sha256(view).hexdigest()
+    else:
+        return hashlib.sha256( data ).hexdigest()
+
+
+def _is_running_on_ci():
+    ci_envs = ['CI', 'TRAVIS', 'GITHUB_ACTIONS', 'GITLAB_CI', 'JENKINS_URL']
+    return any(env in os.environ for env in ci_envs)
+
+
+def tee(
+    plotter,
+    *args,
+    teeplot_outattrs={},
+    teeplot_outdir="teeplots",
+    teeplot_save=None,
+    teeplot_subdir='.',
+    teeplot_transparent=True,
+    **kwargs
+):
+
+    if teeplot_save is None:
+        if "TEEPLOT_DRAFT_MODE" in os.environ:
+            teeplot_save = {}
+        elif _is_running_on_ci():
+            teeplot_save = {".pdf"}
+        else:
+            teeplot_save = {".pdf", ".png"}
+    elif teeplot_save is False:
+        teeplot_save = {}
+    elif teeplot_save is True:
+        if _is_running_on_ci():
+            teeplot_save = {".pdf"}
+        else:
+            teeplot_save = {".pdf", ".png"}
+
+    # enable TrueType fonts
+    # see https://gecco-2021.sigevo.org/Paper-Submission-Instructions
+    matplotlib.rcParams['pdf.fonttype'] = 42
+    matplotlib.rcParams['ps.fonttype'] = 42
+
+    res = plotter(
+        *args,
+        **{
+            k : v
+            for k, v in kwargs.items()
+        }
+    )
+
+    attr_maker = lambda ext: {
+        **{
+            slugify(k) : slugify(v)
+            for k, v in kwargs.items()
+            if isinstance(v, str)
+        },
+        **{
+            'viz' : slugify(plotter.__name__),
+            'ext' : ext,
+        },
+        **{
+            k : v
+            for k, v in teeplot_outattrs.items()
+            if k != '_datafordigest'
+        },
+        **(
+            {'_datadigest' : _digest( kwargs['data'] )[:16]}
+            if 'data' in kwargs
+            else {'_datadigest' : _digest(
+                teeplot_outattrs['_datafordigest']
+            )[:16]}
+            if '_datafordigest' in teeplot_outattrs
+            else {'_datadigest' : _digest( np.concatenate([
+                kwargs.get('x', []),
+                kwargs.get('y', []),
+                kwargs.get('hue', []),
+                kwargs.get('size', []),
+                kwargs.get('style', []),
+            ]) )[:16]}
+            if any(q in kwargs for q in ['x', 'y', 'hue', 'size', 'style'])
+            else {}
+        ),
+
+    }
+    out_filenamer = lambda ext: kn.pack({
+        k : v
+        for k, v in attr_maker(ext).items()
+        if not k.startswith('_')
+    })
+    out_metamaker = lambda ext: kn.pack({
+        k : v
+        for k, v in attr_maker(ext).items()
+        if k.startswith('_')
+    })
+
+    out_folder = f'{teeplot_outdir}/{teeplot_subdir}'
+    pathlib.Path(
+        out_folder,
+    ).mkdir(
+        parents=True,
+        exist_ok=True,
+    )
+
+    for ext, dpi in ('.pdf', 'figure'), ('.png', 300):
+
+        out_path = kn.chop(
+            f'{out_folder}/{out_filenamer(ext)}',
+            mkdir=True,
+        )
+
+        if ext not in teeplot_save:
+            print(f"skipping {out_path}")
+            continue
+
+        print(out_path)
+        plt.savefig(
+            out_path,
+            bbox_inches='tight',
+            transparent=teeplot_transparent,
+            dpi=dpi,
+        )
+
+        with open(f'{out_path}.meta', 'w') as file:
+            file.write( out_metamaker(ext) )
+
+    return res
---
+++ packages/teeplot/teeplot-0.6.2.tar.gz/teeplot-0.6.2/tests/test_tee.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+
+'''
+`tee` tests for `teeplot` package.
+'''
+
+import numpy as np
+from keyname import keyname as kn
+import os
+import pytest
+import seaborn as sns
+
+from teeplot import teeplot as tp
+
+def test():
+
+    tp.tee(
+        sns.lineplot,
+        x='timepoint',
+        y='signal',
+        hue='region',
+        style='event',
+        data=sns.load_dataset('fmri'),
+        teeplot_outattrs={
+          'additional' : 'metadata',
+          'for' : 'output-filename',
+          '_one-for' : 'the-meta',
+        },
+    )
+
+    for ext in '.pdf', '.png':
+        assert os.path.exists(
+            f'teeplots/additional=metadata+for=output-filename+hue=region+style=event+viz=lineplot+x=timepoint+y=signal+ext={ext}',
+        )
+        with open(
+            f'teeplots/additional=metadata+for=output-filename+hue=region+style=event+viz=lineplot+x=timepoint+y=signal+ext={ext}.meta',
+            'r',
+        ) as file:
+            assert '_one-for=the-meta' in file.read()
+        # don't assert the exact digest,
+        # because the fmri dataset appears to vary between versions?
+        with open(
+            f'teeplots/additional=metadata+for=output-filename+hue=region+style=event+viz=lineplot+x=timepoint+y=signal+ext={ext}.meta',
+            'r',
+        ) as file:
+            assert '_datadigest=' in file.read()
+
+
+def test_ndarray():
+
+    # adapted from https://seaborn.pydata.org/generated/seaborn.lineplot.html
+    np.random.seed(1)
+    x, y = np.random.normal(size=(2, 5000)).cumsum(axis=1)
+
+    tp.tee(
+        sns.lineplot,
+        x=x,
+        y=y,
+        sort=False,
+        lw=1,
+    )
+
+    for ext in '.pdf', '.png':
+        assert os.path.exists(
+            f'teeplots/viz=lineplot+ext={ext}',
+        )
+        with open(
+            f'teeplots/viz=lineplot+ext={ext}.meta',
+            'r',
+        ) as file:
+            assert '_datadigest=011789ddade2f359' in file.read()
+
+def test_datafordigest():
+
+    # adapted from https://seaborn.pydata.org/generated/seaborn.lineplot.html
+    np.random.seed(1)
+    x, y = np.random.normal(size=(2, 5000)).cumsum(axis=1)
+
+    tp.tee(
+        sns.lineplot,
+        x=x,
+        y=y,
+        sort=False,
+        lw=1,
+        teeplot_outattrs={
+          'additional' : 'metadata',
+          '_datafordigest' : x,
+        },
+    )
+
+    for ext in '.pdf', '.png':
+        assert os.path.exists(
+            f'teeplots/additional=metadata+viz=lineplot+ext={ext}',
+        )
+
+        with open(
+            f'teeplots/additional=metadata+viz=lineplot+ext={ext}.meta',
+            'r',
+        ) as file:
+            assert '_datadigest=1053487e16d654f6' in file.read()
+
+def test_outpath():
+
+    # adapted from https://seaborn.pydata.org/generated/seaborn.lineplot.html
+    np.random.seed(1)
+    x, y = np.random.normal(size=(2, 5000)).cumsum(axis=1)
+
+    tp.tee(
+        sns.lineplot,
+        x=x,
+        y=y,
+        sort=False,
+        lw=1,
+        teeplot_outattrs={
+          'additional' : 'metadata',
+        },
+        teeplot_subdir='mydirectory',
+    )
+
+    for ext in '.pdf', '.png':
+        assert os.path.exists(
+            f'teeplots/mydirectory/additional=metadata+viz=lineplot+ext={ext}',
+        )
+        assert os.path.exists(
+            f'teeplots/mydirectory/additional=metadata+viz=lineplot+ext={ext}.meta',
+        )
+
+def test_longname():
+
+    # adapted from https://seaborn.pydata.org/generated/seaborn.lineplot.html
+    np.random.seed(1)
+    x, y = np.random.normal(size=(2, 5000)).cumsum(axis=1)
+
+    tp.tee(
+        sns.lineplot,
+        x=x,
+        y=y,
+        sort=False,
+        lw=1,
+        teeplot_outattrs={
+          f'additional{i}' : 'metadata'
+          for i in range(100)
+        },
+    )
+
+    for ext in '.pdf', '.png':
+        assert os.path.exists(
+            kn.chop(
+                f'''teeplots/{
+                    kn.pack({
+                      f'additional{i}' : 'metadata'
+                      for i in range(100)
+                    })
+                }+viz=lineplot+ext={ext}''',
+            )
+        )
