df57729fe7e2597c7bb710b39c37a7f71d898193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 04:04:44 +0000

    Add package cvrmap/2.0.4/CVRmap-2.0.4.tar.gz

---
+++ packages/cvrmap/CVRmap-2.0.4.tar.gz/CVRmap-2.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CVRmap',
+    version='2.0.4',
+    url='https://github.com/ln2t/cvrmap',
+    author='Antonin Rovai',
+    author_email='antonin.rovai@hubruxelles.be',
+    description='CVRmap is an opensource software to compute maps of Cerebro-Vascular Reactivity',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'cvrmap = CVRmap.cvrmap:main',
+        ]}
+)
