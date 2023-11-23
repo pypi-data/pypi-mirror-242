93701f2c42f7a4a05a2faee17a6f4edc84a8ed04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 16:05:32 +0000

    Add package rbx/3.6.3.dev48/rbx-3.6.3.dev48.tar.gz

---
+++ packages/rbx/rbx-3.6.3.dev48.tar.gz/rbx-3.6.3.dev48/rbx/buildtools/cli.py
@@ -0,0 +1,10 @@
+from invoke import Config, Program
+
+from .tasks import deploy
+
+
+class BuildtoolsConfig(Config):
+    prefix = "rbx"
+
+
+program = Program(config_class=BuildtoolsConfig, namespace=deploy.ns, version="3.6.3.dev48")
