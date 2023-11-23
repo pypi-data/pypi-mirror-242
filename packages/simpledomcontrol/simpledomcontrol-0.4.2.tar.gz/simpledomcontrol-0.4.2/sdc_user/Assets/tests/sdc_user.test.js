155e12966ce4820edbd101734d892ebacf012184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 16:05:30 +0000

    Add package simpledomcontrol/0.4.2/simpledomcontrol-0.4.2.tar.gz

---
+++ packages/simpledomcontrol/simpledomcontrol-0.4.2.tar.gz/simpledomcontrol-0.4.2/sdc_user/Assets/tests/sdc_user.test.js
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import {test_utils} from "sdc";
+
+
+describe('SdcNavigator', () => {
+    let controller, Controller;
+
+    beforeEach(async () => {
+        // Create new controller instance based on the standard process.
+        const a = await test_utils.get_controller('sdc_tools',
+            'sdc-navigator',
+            '<div><div class="main-nav-import-container"></div><div class="sdc_detail_view">Loading</div></div>',
+            '<div>Loading</div>');
+
+        controller = a.instance;
+        Controller = a.class;
+    });
+
+    test('Load Content', async () => {
+        const $div = controller.$container.find('div');
+        expect($div.length).toBe(4);
+    });
+
+});
