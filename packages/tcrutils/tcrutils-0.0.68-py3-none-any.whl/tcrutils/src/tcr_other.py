ed9d0c940135f9ae85a0465b93fc7483a1b707d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 05:04:18 +0000

    Add package tcrutils/0.0.68/tcrutils-0.0.68-py3-none-any.whl

---
+++ packages/tcrutils/tcrutils-0.0.68-py3-none-any.whl/tcrutils/src/tcr_other.py
@@ -0,0 +1,48 @@
+"""`other` submodule contains garbage or joke functions that never should be used in real code, just don't use this submodule pls. Why is it there? uhhh... :3."""
+from builtins import hex as sex
+from collections.abc import Callable
+from typing import Literal
+
+
+def oddeven(n: int | str) -> Literal['odd', 'even']:
+  """### Outputs a string `'odd'` or `'even'` based on the supplied int `n`.
+
+  ```py
+  >>> from tcrutils.other import oddeven
+  >>> oddeven(1)
+  'odd'
+  >>> oddeven(2)
+  'even'
+  """
+  n = int(n)
+  return 'eovdedn'[n % 2 :: 2]
+
+
+def hex(number, leading_zeroes=2, *, upper=True):
+  hex_output = sex(number)
+  hex_value = (
+    hex_output[2:].zfill(leading_zeroes).upper() if upper else hex_output[2:].zfill(leading_zeroes)
+  )
+
+  formatted_output = f'0x{hex_value}'
+  if not upper:
+    formatted_output = formatted_output.lower()
+
+  return formatted_output
+
+
+def commafy(text: str | int, splitter: str = ','):
+  text = str(text)
+  temp = ''
+  for i, letter in enumerate(text[::-1]):
+    temp += letter
+    if i % 3 == 2 and i != len(text) - 1:
+      temp += splitter
+  return temp[::-1]
+
+
+def intbool(__o: object, /):
+  return int(bool(__o))
+
+
+fizzbuz: Callable[[int], str] = lambda n: 'Fizz' * (n % 3 == 0) + 'Buzz' * (n % 5 == 0) or str(n)
