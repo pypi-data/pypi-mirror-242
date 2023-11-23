7d6f9dc6c5a3eb8eecc935f481ce64c263fb2fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 16:05:33 +0000

    Add package py-pheno/0.0.10/py_pheno-0.0.10.tar.gz

---
+++ packages/py-pheno/py_pheno-0.0.10.tar.gz/py_pheno-0.0.10/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+import platform
+
+# 获取Python版本信息
+python_version = platform.python_version_tuple()
+
+# 根据Python版本选择对应的pyd文件
+if python_version >= ("3", "9"):
+    pyd_file = "func_preseason_api.cp39-win_amd64.pyd"
+elif python_version >= ("3", "8"):
+    pyd_file = "func_preseason_api.cp38-win_amd64.pyd"
+elif python_version >= ("3", "7"):
+    pyd_file = "func_preseason_api.cp37-win_amd64.pyd"
+else:
+    raise RuntimeError("Unsupported Python version")
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py_pheno",
+    version="0.0.10",
+    author="Shen Pengju",
+    author_email="spjace@sina.com",
+    description="A small package for py_pheno analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/spjace/py_pheno",
+    packages=setuptools.find_packages(),
+    
+    # 将选择的pyd文件添加到package_data
+    package_data={'': [pyd_file]},
+    
+    install_requires=['numpy', 'pandas', 'pingouin', 'scipy'],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+    
