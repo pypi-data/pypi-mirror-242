805483c3c5b4547d316e9810b3a22dca7c408575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 03:05:26 +0000

    Add package gdshoplib/2.3.2/gdshoplib-2.3.2.tar.gz

---
+++ packages/gdshoplib/gdshoplib-2.3.2.tar.gz/gdshoplib-2.3.2/gdshoplib/cli/application.py
@@ -0,0 +1,18 @@
+import typer
+
+from gdshoplib.cli.crm.application import app as crm
+from gdshoplib.cli.finance.application import app as finance
+from gdshoplib.cli.product.application import app as product
+from gdshoplib.cli.service.application import app as service
+from gdshoplib.cli.temporal.application import app as temporal
+
+app = typer.Typer()
+
+app.add_typer(crm, name="crm")
+app.add_typer(temporal, name="temporal")
+app.add_typer(finance, name="finance")
+app.add_typer(product, name="product")
+app.add_typer(service, name="service")
+
+if __name__ == "__main__":
+    app()
