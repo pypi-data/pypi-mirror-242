14dbca00e9d94dc73d2efbb23ba51c65937cee9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 14:05:31 +0000

    Add package pyWBE/0.0.3/pywbe-0.0.3.tar.gz

---
+++ packages/pyWBE/pywbe-0.0.3.tar.gz/pyWBE-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='pyWBE',
+  version='0.0.3',
+  description='A Python Library for wastewater-based epidemiology.',
+  long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Anuj Tiwari',
+  author_email='anujt@uic.edu',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Python_Library', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
