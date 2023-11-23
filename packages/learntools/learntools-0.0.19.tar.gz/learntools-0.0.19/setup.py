a70c38628423e6d2d3817554dd9e34cb95644363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 05:04:18 +0000

    Add package learntools/0.0.19/learntools-0.0.19.tar.gz

---
+++ packages/learntools/learntools-0.0.19.tar.gz/learntools-0.0.19/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.19' 
+DESCRIPTION = 'Reinforcement Learning with Numpy'
+LONG_DESCRIPTION = 'Reinforcement Learning with Numpy'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="learntools", 
+        version=VERSION,
+        author="William Dennis",
+        author_email="wwdennis.home@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['learntools','numpy'], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'neural network','learning'],
+        
+)
