960530b20f18ca530d7b073ee605fdddc8ec4bf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 16:05:30 +0000

    Add package autoed/0.0.2/autoed-0.0.2.tar.gz

---
+++ packages/autoed/autoed-0.0.2.tar.gz/autoed-0.0.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name='autoed',
+    version='0.0.2',
+    entry_points={
+        'console_scripts': [
+            'autoed_watch = autoed.watch:main',
+            'autoed = autoed.autoed:main',
+            'autoed_beam_center = autoed.beam_center:main'
+        ]
+    },
+    setup_requires=['argcomplete'],
+    packages=find_packages(),
+    install_requires=[
+        'watchdog',
+        'numpy>=1.10.0',
+        'nexgen',
+        'h5py',
+        'argparse',
+        'python-daemon',
+        'pathlib',
+        'argcomplete'
+    ]
+)
