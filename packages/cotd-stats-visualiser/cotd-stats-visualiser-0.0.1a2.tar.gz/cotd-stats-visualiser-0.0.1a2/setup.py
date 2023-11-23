76ecd81a892dd8a063d2be7b15864d55373d0331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 05:04:33 +0000

    Add package cotd-stats-visualiser/0.0.1a2/cotd-stats-visualiser-0.0.1a2.tar.gz

---
+++ packages/cotd-stats-visualiser/cotd-stats-visualiser-0.0.1a2.tar.gz/cotd-stats-visualiser-0.0.1a2/setup.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup  # type: ignore[import]
+
+extras_require = {"GUI": ["imgui[full]"]}
+requirements = [
+    "matplotlib",
+    "orjson",
+    "pandas",
+    "ratelimiter",
+    "requests",
+    "setuptools>=61.0",
+]
+
+setup(
+    name="cotd-stats-visualiser",
+    description="A script that generates a visualisation of the Trackmania COTD stats for a given player.",
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    author="EdVraz",
+    author_email="edvraz12@gmail.com",
+    url="https://github.com/EdVraz/COTDStatsVisualiser",
+    version="0.0.1-alpha.2",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.10",
+    install_requires=requirements,  # FIXME go back to automatic reading later
+    license="LICENSE"
+    # extras_require=extras_require,
+)
