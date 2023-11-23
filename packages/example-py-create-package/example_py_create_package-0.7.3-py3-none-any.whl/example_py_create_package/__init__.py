9579cf4e9e90dfc91d657a07a3861e4530dff844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 06:04:10 +0000

    Add package example-py-create-package/0.7.3/example_py_create_package-0.7.3-py3-none-any.whl

---
+++ packages/example-py-create-package/example_py_create_package-0.7.3-py3-none-any.whl/example_py_create_package/__init__.py
@@ -0,0 +1,10 @@
+from .moduleMain import moduleMain
+from .initVacio import init_vacio
+from example_py_create_package.sinInit import sin_init
+from .bienImportado.bien_importado import bienImportado
+from example_py_create_package.indexRelleno import __index__
+from .a import a
+
+bien_importado = {
+    "bienImportado": bienImportado
+}
