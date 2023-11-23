b029997b16932841cd87ce76be5ff6481a2064d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 05:04:42 +0000

    Add package ragpy/0.0.1/ragpy-0.0.1.tar.gz

---
+++ packages/ragpy/ragpy-0.0.1.tar.gz/ragpy-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="ragpy",
+    version="0.0.1",
+    description="Will be soon",
+    author="Ruslan Dev",
+    author_email="ruslanperesy@outlook.com",
+    project_urls={
+        "Source": "https://github.com/RuslanPeresy/ragpy"
+    }
+)
