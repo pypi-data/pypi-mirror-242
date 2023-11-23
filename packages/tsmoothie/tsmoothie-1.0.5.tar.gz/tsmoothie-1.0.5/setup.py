c0ca482766d4133438ed8379c7619c048ad5d6da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 16:04:47 +0000

    Add package tsmoothie/1.0.5/tsmoothie-1.0.5.tar.gz

---
+++ packages/tsmoothie/tsmoothie-1.0.5.tar.gz/tsmoothie-1.0.5/setup.py
@@ -0,0 +1,35 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '1.0.5'
+PACKAGE_NAME = 'tsmoothie'
+AUTHOR = 'Marco Cerliani'
+AUTHOR_EMAIL = 'cerlymarco@gmail.com'
+URL = 'https://github.com/cerlymarco/tsmoothie'
+
+LICENSE = 'MIT'
+DESCRIPTION = 'A python library for timeseries smoothing and outlier detection in a vectorized way.'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'numpy',
+      'scipy',
+      'simdkalman'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      python_requires='>=3',
+      packages=find_packages()
+      )
