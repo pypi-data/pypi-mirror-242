75b3c5dfd4f1e21170bffc7a9d78d77b3dd01090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 16:05:15 +0000

    Add package firesoft/0.0.8/firesoft-0.0.8.tar.gz

---
+++ packages/firesoft/firesoft-0.0.8.tar.gz/firesoft-0.0.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='firesoft',
+    version='0.0.8',
+    packages=['firesoft', 'firesoft.utils'],
+    install_requires=[
+        "segno==1.5.3",
+        "qrcode-artistic==3.0.0",
+        "Pillow==10.1.0",
+        "setuptools==68.2.2",
+        "python-barcode==0.15.1",
+        "pandas==2.0.3",
+        "reportlab==4.0.6"
+    ],
+    url='',
+    license='',
+    author='Salah Anwer',
+    author_email='salahanwer.dev@gmail.com',
+    description='',
+    python_requires=">=3",
+)
