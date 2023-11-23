825fdad825e004277df50399bcf6851f33118218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 06:04:34 +0000

    Add package simplehtmltopdf/0.0.4/simplehtmltopdf-0.0.4-py3-none-any.whl

---
+++ packages/simplehtmltopdf/simplehtmltopdf-0.0.4-py3-none-any.whl/simplehtmltopdf/convert.py
@@ -0,0 +1,52 @@
+import os
+import time
+from base64 import b64decode
+from contextlib import AbstractContextManager
+from pathlib import Path
+from typing import Self
+
+from selenium import webdriver
+
+from simplehtmltopdf.compress import compress
+
+
+class WebDriverWrapper(AbstractContextManager):
+    def __init__(self,
+                 wait_before_print: int = 2,
+                 implicit_wait: int = 10):
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless=new")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--no-sandbox")
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.implicitly_wait(implicit_wait)
+        self.wait_before_print = wait_before_print
+
+    def __enter__(self) -> Self:
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.driver.quit()
+
+    def html_to_pdf(self,
+                    url: str,
+                    save_path: str | os.PathLike,
+                    compression: int = None,
+                    ghostscript_command: str = None) -> None:
+        save_path = Path(save_path)
+        self.driver.get(url)
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(self.wait_before_print)
+        b64 = self.driver.print_page()
+        bites = b64decode(b64, validate=True)
+        if compression is not None:
+            compress(source=bites,
+                     save_path=save_path,
+                     power=compression,
+                     ghostscript_command=ghostscript_command)
+        else:
+            with save_path.open(mode='wb') as f:
+                f.write(bites)
+
+    def quit(self):
+        self.driver.quit()
