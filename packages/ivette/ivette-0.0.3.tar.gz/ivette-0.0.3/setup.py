2174db19fe3f8ea1ffbffc429fb237cb2f10eb70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 04:04:50 +0000

    Add package ivette/0.0.3/ivette-0.0.3.tar.gz

---
+++ packages/ivette/ivette-0.0.3.tar.gz/ivette-0.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+# Read deps
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='ivette',
+    version='0.0.3',
+    description='Python client for Ivette Computational chemistry and Bioinformatics project',
+    author='Eduardo Bogado',
+    py_modules=['ivette', 'package.IO_module', 'package.load_module', 'package.run_module', 'package.supabase_module'],  # Include 'ivette.py' as a module
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'ivette=ivette:main',
+        ],
+    },
+)
