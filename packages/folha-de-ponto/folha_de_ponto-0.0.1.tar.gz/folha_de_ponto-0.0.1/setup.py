d422676966533cb3bb7928f59fcc354d0d5c7a6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 04:04:32 +0000

    Add package folha-de-ponto/0.0.1/folha_de_ponto-0.0.1.tar.gz

---
+++ packages/folha-de-ponto/folha_de_ponto-0.0.1.tar.gz/folha_de_ponto-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='folha_de_ponto',
+      version='0.0.1',
+      description='Folha de ponto Python package',
+      author='Victor Cruz',
+      author_email='gil.victor1998@gmail.com',
+      license='MIT',
+      packages=['folha_de_ponto'],
+      zip_safe=False)
