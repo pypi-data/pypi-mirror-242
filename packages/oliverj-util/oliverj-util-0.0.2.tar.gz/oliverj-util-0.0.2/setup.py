e3ed66658f8f06f08031b9625160f2d85ef26b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 14:06:30 +0000

    Add package oliverj-util/0.0.2/oliverj-util-0.0.2.tar.gz

---
+++ packages/oliverj-util/oliverj-util-0.0.2.tar.gz/oliverj-util-0.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setuptools.setup(
+    name="oliverj-util",
+    version="0.0.2",
+    author="Olliejohnson",
+    author_email="contact@oliverj.io",
+    packages=["oliverj_utils"],
+    description="A utility package",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/oliverj-util",
+    license="MIT",
+    python_requires=">=3.8",
+    install_requires=[]
+)
