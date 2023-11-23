b9646800d6f5ac9767224cc9d5b8d4e1b49048ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 05:04:32 +0000

    Add package cdlclient/0.5.0/cdlclient-0.5.0-py3-none-any.whl

---
+++ packages/cdlclient/cdlclient-0.5.0-py3-none-any.whl/cdlclient/widgets/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+DataLab Simple Client Widgets
+-----------------------------
+
+This module requires `guidata`_ and `qtpy`_ packages.
+
+.. _guidata: https://pypi.org/project/guidata/
+.. _qtpy: https://pypi.org/project/QtPy/
+
+"""
+
+# pylint: disable=unused-import
+from cdlclient.widgets.connection import ConnectionDialog
