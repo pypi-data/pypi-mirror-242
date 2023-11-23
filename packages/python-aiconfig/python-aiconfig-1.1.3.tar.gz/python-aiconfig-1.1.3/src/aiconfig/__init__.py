ca8a2a1020ebf072a0f57f725abc6f69aa2e8efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 05:04:18 +0000

    Add package python-aiconfig/1.1.3/python-aiconfig-1.1.3.tar.gz

---
+++ packages/python-aiconfig/python-aiconfig-1.1.3.tar.gz/python-aiconfig-1.1.3/src/aiconfig/__init__.py
@@ -0,0 +1,35 @@
+# Core Data Classes
+from .schema import (
+    AIConfig,
+    ConfigMetadata,
+    ExecuteResult,
+    JSONObject,
+    ModelMetadata,
+    Output,
+    Prompt,
+    PromptInput,
+    PromptMetadata,
+    SchemaVersion,
+)
+
+# ModelParser Utilities
+from .model_parser import ModelParser, InferenceOptions
+from .default_parsers.parameterized_model_parser import ParameterizedModelParser
+from .util.params import resolve_prompt
+from .util.config_utils import get_api_key_from_environment
+from .registry import ModelParserRegistry
+
+# Model Parsers
+from .default_parsers.palm import PaLMTextParser, PaLMChatParser
+from .default_parsers.openai import OpenAIInference, DefaultOpenAIParser
+
+# Callback Utilities
+from .callback import (
+    Callback,
+    CallbackEvent,
+    CallbackManager,
+    create_logging_callback,
+)
+
+# The AIConfigRuntime class. This is the main class that you will use to run your AIConfig.
+from .Config import AIConfigRuntime
