565e002d5294c74a8d36847d635e327aea52747e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 04:04:23 +0000

    Add package wml-ai-model-managers/0.0.3/wml_ai_model_managers-0.0.3.tar.gz

---
+++ packages/wml-ai-model-managers/wml_ai_model_managers-0.0.3.tar.gz/wml_ai_model_managers-0.0.3/wml_ai_model_managers/text_model_manager_zero/multi_head_attention.py
@@ -0,0 +1,23 @@
+from torch import nn
+import torch
+from torch.nn import functional as F
+
+from wml_ai_model_managers.text_model_manager_zero.head import WMLHead
+
+
+class WMLMultiHeadAttention(nn.Module):
+    """ multiple heads of self-attention in parallel """
+
+    def __init__(self, num_heads, head_size,dropout,n_embd,block_size):
+        super().__init__()
+        self.heads = nn.ModuleList(
+          [WMLHead(n_embd,head_size,block_size,dropout) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size * num_heads, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1) # (B, T, F) -> (B, T, [h1, h1, h1, h1, h2, h2, h2, h2, h3, h3, h3, h3])
+        out = self.dropout(self.proj(out))
+        return out
+
+
