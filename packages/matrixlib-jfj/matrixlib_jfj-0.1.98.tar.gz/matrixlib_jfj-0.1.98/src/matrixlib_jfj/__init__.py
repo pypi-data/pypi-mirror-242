92e495e73acf3c7694c2d42a62fcf5f29695c5eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 06:04:21 +0000

    Add package matrixlib-jfj/0.1.98/matrixlib_jfj-0.1.98.tar.gz

---
+++ packages/matrixlib-jfj/matrixlib_jfj-0.1.98.tar.gz/matrixlib_jfj-0.1.98/src/matrixlib_jfj/__init__.py
@@ -0,0 +1,48 @@
+"""
+Matrix Library
+
+A Python library for working with matrices and performing matrix operations.
+
+Usage
+----------
+	import matrixlib
+
+	# Example Usage
+ 
+	matrix = matrixlib.matrix.Matrix((3, 3)) # Result: [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+ 
+Key Classes/Functions
+----------
+	- Matrix : Represents a matrix and provides numerous operations for manipulating matrices.
+
+Examples
+----------
+	from matrixlib.matrix import Matrix
+
+	matrix = Matrix((3, 3))
+
+	matrix.set_value(1, 3, 6) # Result: [[0, 0, 6], [0, 0, 0], [0, 0, 0]]
+
+ 	transposed = matrix.transpose()
+
+ 	print(transposed) # Output:
+
+ 	0 0 0\n
+	0 0 0\n
+ 	6 0 0
+
+Version: 0.1.98
+
+Author: Jemma Starecki
+
+Made By <a href="https://github.com/JemmaFromJupiter" target="_blank">JemmaFromJupiter</a> On Github.
+"""
+from __future__ import annotations
+from . import matrix
+from . import vector
+from . import utils
+
+__version__ = "0.1.98"
+__author__ = "JemmaFromJupiter"
+
+__all__ = ["matrix", "vector", "utils"]
