84ba75978a33d924f2fb0a91a91f11650ad10eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 05:05:02 +0000

    Add package kisstdlib/0.0.1/kisstdlib-0.0.1-py3-none-any.whl

---
+++ packages/kisstdlib/kisstdlib-0.0.1-py3-none-any.whl/kisstdlib/io/encoder/__init__.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2023 Jan Malakhovski <oxij@oxij.org>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# This file is a part of kisstdlib project.
+#
+# This file can be distributed under the terms of the above MIT license or
+# Python Software Foundation License version 2 (PSF-2.0) as published by Python
+# Software Foundation.
+
+from .base import *
+from .pyrepr import *
