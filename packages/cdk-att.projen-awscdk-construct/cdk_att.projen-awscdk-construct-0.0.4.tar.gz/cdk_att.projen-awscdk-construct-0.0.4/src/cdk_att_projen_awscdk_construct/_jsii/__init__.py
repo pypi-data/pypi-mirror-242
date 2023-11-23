dcaec86e0f1e4c81962f5e361fa123dafe189893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-23 04:04:45 +0000

    Add package cdk-att.projen-awscdk-construct/0.0.4/cdk_att.projen-awscdk-construct-0.0.4.tar.gz

---
+++ packages/cdk-att.projen-awscdk-construct/cdk_att.projen-awscdk-construct-0.0.4.tar.gz/cdk_att.projen-awscdk-construct-0.0.4/src/cdk_att_projen_awscdk_construct/_jsii/__init__.py
@@ -0,0 +1,32 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import aws_cdk.aws_apigatewayv2_alpha._jsii
+import aws_cdk.aws_redshift_alpha._jsii
+import cdk_monitoring_constructs._jsii
+import cdk_nag._jsii
+import constructs._jsii
+import projen._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@cdk-att/projen-awscdk-construct",
+    "0.0.4",
+    __name__[0:-6],
+    "projen-awscdk-construct@0.0.4.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
