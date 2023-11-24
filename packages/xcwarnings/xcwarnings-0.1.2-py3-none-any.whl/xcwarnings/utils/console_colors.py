87027b6b4c94590da2db3d8d80b684438722f2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 04:05:15 +0000

    Add package xcwarnings/0.1.2/xcwarnings-0.1.2-py3-none-any.whl

---
+++ packages/xcwarnings/xcwarnings-0.1.2-py3-none-any.whl/xcwarnings/utils/console_colors.py
@@ -0,0 +1,18 @@
+"""
+@File       console_colors.py
+@Brief      Colors
+@Author     cavacagu
+@Date       13-05-2019
+@copyright  Microsoft Corporation. All rights reserved.
+"""
+
+#pylint: disable = too-few-public-methods
+class ConsoleColors:
+    """Console colors associated with various logging levels"""
+    OK = '\033[92m'
+    VERBOSE = '\033[36m'
+    INFO = '\033[94m'
+    ERROR = '\033[91m'
+    WARNING = '\033[93m'
+    END = '\033[0m'
+#pylint: enable = too-few-public-methods
