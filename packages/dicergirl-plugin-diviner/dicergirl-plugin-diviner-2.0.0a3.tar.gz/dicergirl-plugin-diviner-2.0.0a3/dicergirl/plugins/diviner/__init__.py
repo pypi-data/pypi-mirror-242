88b92f185b12b02b1fbd9bf3d0b71abf412988fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 14:05:23 +0000

    Add package dicergirl-plugin-diviner/2.0.0a3/dicergirl-plugin-diviner-2.0.0a3.tar.gz

---
+++ packages/dicergirl-plugin-diviner/dicergirl-plugin-diviner-2.0.0a3.tar.gz/dicergirl-plugin-diviner-2.0.0a3/dicergirl/plugins/diviner/__init__.py
@@ -0,0 +1,13 @@
+from .nbhandlers import commands
+from .utils import init
+
+
+init()
+
+__version__ = "2.0.0a3"
+
+__type__ = "plugin"
+__name__ = "diviner"
+__nbhandler__ = nbhandlers
+__nbcommands__ = commands
+__description__ = "基于古蓍草占卜算法."
