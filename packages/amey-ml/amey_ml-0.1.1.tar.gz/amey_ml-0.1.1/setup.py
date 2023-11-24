4320b25e895627dd7a30e368f2f17adf8c9a1779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 15:04:48 +0000

    Add package amey-ml/0.1.1/amey_ml-0.1.1.tar.gz

---
+++ packages/amey-ml/amey_ml-0.1.1.tar.gz/amey_ml-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+
+VERSION = '0.1.1'
+DESCRIPTION = 'A Machine Learning Library for the noobies'
+LONG_DESCRIPTION = 'A package that allows to get the classification and test results of machine learning algorithm'
+
+# Setting up
+setup(
+    name="amey_ml",
+    version=VERSION,
+    author="Amey Shinde",
+    author_email="<amey.shinde@bizzencecollab.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['numpy', 'tensorflow', 'pandas','scikit-learn','keras'],
+    keywords=['python', 'machinelearning'],
+    classifiers=[]
+)
