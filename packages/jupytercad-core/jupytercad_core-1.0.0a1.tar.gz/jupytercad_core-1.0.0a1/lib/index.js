4d8d69bd480eee9d4799bf78a677308875df7e7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 15:06:10 +0000

    Add package jupytercad-core/1.0.0a1/jupytercad_core-1.0.0a1.tar.gz

---
+++ packages/jupytercad-core/jupytercad_core-1.0.0a1.tar.gz/jupytercad_core-1.0.0a1/lib/index.js
@@ -0,0 +1,10 @@
+import jcadPlugin from './jcadplugin/plugins';
+import { annotationPlugin, trackerPlugin, workerRegistryPlugin } from './plugin';
+export * from './workerregistry';
+export * from './factory';
+export default [
+    trackerPlugin,
+    annotationPlugin,
+    workerRegistryPlugin,
+    jcadPlugin
+];
