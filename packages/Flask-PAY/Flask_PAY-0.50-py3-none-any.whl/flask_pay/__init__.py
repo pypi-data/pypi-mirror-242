615c5eb550b1452f183e063926147365a84ab213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 14:05:10 +0000

    Add package Flask-PAY/0.50/Flask_PAY-0.50-py3-none-any.whl

---
+++ packages/Flask-PAY/Flask_PAY-0.50-py3-none-any.whl/flask_pay/__init__.py
@@ -0,0 +1,66 @@
+"""
+ Flask-pay
+ # ~~~~~~~~~~~~~~ 
+ flask 支付 扩展
+ Flask SMS extension
+ :copyright: (c) 2023.11 by 浩. 
+ :license: GPL, see LICENSE for more details.
+"""
+
+import os
+import random
+import string
+from datetime import datetime
+
+from wechatpayv3 import WeChatPay, WeChatPayType
+
+
+# 限制短信频率
+
+
+class Pay(object):
+    def __init__(self, app=None, **kwargs):
+        self.app = app
+        if app is not None:
+            self.state = self.init_app(app)
+        else:
+            self.state = None
+        self.wxpay = None
+
+    def init_app(self, app):
+        # getattr 的作用是从对象中获取指定名称的属性。如果属性存在，
+        # 则返回属性的值；如果属性不存在，并且提供了默认值，则返回默认值；
+        # 如果属性不存在且没有提供默认值，则抛出 AttributeError 异常。
+        app.extensions = getattr(app, 'extensions', {})
+
+        state = self.init_pay(app.config)
+        # 在 app 应用中存储所有扩展实例, 可验证扩展是否完成实例化
+        app.extensions['pay'] = state
+
+        # 扩展配置， 初始化后添加到 app.config 中, 以 SHARE_ 开头避免冲突
+
+        return state
+
+
+
+    def init_pay(self, config):
+        pay_type = config.get("PAY_TYPE")
+        if pay_type == "wxpay":
+            self.init_wxpay(config)
+
+    def init_wxpay(self, config):
+
+        wxpay = WeChatPay(
+            wechatpay_type=config.get("WECHATPAY_TYPE"),
+            mchid=config.get("MCHID"),
+            private_key=config.get("PRIVATE_KEY"),
+            cert_serial_no=config.get("CERT_SERIAL_NO"),
+            apiv3_key=config.get("APIV3_KEY"),
+            appid=config.get("APPID"),
+            notify_url=config.get("NOTIFY_URL"),
+            cert_dir=config.get("CERT_DIR"),
+            logger=config.get("LOGGER"),
+            partner_mode=config.get("PARTNER_MODE"),
+            proxy=config.get("PROXY"))
+
+        self.wxpay = wxpay
