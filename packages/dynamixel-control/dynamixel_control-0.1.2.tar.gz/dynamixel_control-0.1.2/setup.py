3198232f87838a9def3a1285d3f0b3f39d53f2ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 14:04:51 +0000

    Add package dynamixel-control/0.1.2/dynamixel_control-0.1.2.tar.gz

---
+++ packages/dynamixel-control/dynamixel_control-0.1.2.tar.gz/dynamixel_control-0.1.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+# with open("README.md", "r", encoding = "utf-8") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name = "dynamixel_control",
+    version = "0.1.2",
+    author = "Kyle DuFrene",
+    author_email = "dufrenekm@gmail.com",
+    description = "Library for communicating with various Dynamixel motors.",
+    # long_description = long_description,
+    # long_description_content_type = "text/markdown",
+    url = "https://github.com/OSUrobotics/dynamixel-control",
+    # project_urls = {
+    #     "Bug Tracker": "package issues URL",
+    # },
+    install_requires=["dynamixel_sdk>=3", "numpy"],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.6"
+)
