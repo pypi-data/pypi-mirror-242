0fb53f35661c11da26f15ff831f73f502dce636a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 14:04:59 +0000

    Add package pysonPostgreSQL/1.0.0/pysonPostgreSQL-1.0.0.tar.gz

---
+++ packages/pysonPostgreSQL/pysonPostgreSQL-1.0.0.tar.gz/pysonPostgreSQL-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pysonPostgreSQL',
+    version='1.0.0',
+    description='A simple library for connecting to PostgreSQL',
+    author='ks lim',
+    author_email='kianseng.lim@sony.com',
+    packages=['python'],
+)
