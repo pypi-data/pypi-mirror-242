38d0caeabff7d7e2249f6e95e29ccfdf0cc8c999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 15:05:50 +0000

    Add package fake-bge-module-latest/20231124/fake_bge_module_latest-20231124-py3-none-any.whl

---
+++ packages/fake-bge-module-latest/fake_bge_module_latest-20231124-py3-none-any.whl/bgui/__init__.py
@@ -0,0 +1,16 @@
+import sys
+import typing
+from . import label
+from . import widget
+from . import system
+from . import text_block
+from . import text_input
+from . import frame_button
+from . import progress_bar
+from . import frame
+from . import image
+from . import list_box
+from . import theme
+from . import image_button
+
+GenericType = typing.TypeVar("GenericType")
---
+++ packages/fake-bge-module-latest/fake_bge_module_latest-20231124-py3-none-any.whl/bl_operators/__init__.py
@@ -0,0 +1,49 @@
+import sys
+import typing
+from . import sequencer
+from . import freestyle
+from . import anim
+from . import object_randomize_transform
+from . import wm
+from . import node
+from . import uvcalc_transform
+from . import assets
+from . import console
+from . import view3d
+from . import bmesh
+from . import screen_play_rendered_anim
+from . import constraint
+from . import object_quick_effects
+from . import uvcalc_lightmap
+from . import object
+from . import mesh
+from . import add_mesh_torus
+from . import vertexpaint_dirt
+from . import object_align
+from . import image
+from . import spreadsheet
+from . import presets
+from . import rigidbody
+from . import uvcalc_follow_active
+from . import clip
+from . import userpref
+from . import file
+from . import geometry_nodes
+
+GenericType = typing.TypeVar("GenericType")
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
