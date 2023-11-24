157bc454db841785cad097416d86315d25153750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 06:04:27 +0000

    Add package pebbleauthclient/0.1.0/pebbleauthclient-0.1.0-py3-none-any.whl

---
+++ packages/pebbleauthclient/pebbleauthclient-0.1.0-py3-none-any.whl/pebbleauthclient/token_data.py
@@ -0,0 +1,44 @@
+from datatypes.AuthenticatedLicenceObject import AuthenticatedLicenceObject
+from datatypes.PebbleTokenData import PebbleTokenData
+from datatypes.UserObject import UserObject
+from models.User import User
+
+
+def get_licence_object_from_token_data(token_data: PebbleTokenData) -> AuthenticatedLicenceObject:
+    user = User(
+        UserObject(
+            username=token_data.sub,
+            roles=token_data.roles,
+            level=token_data.lv,
+            display_name=token_data.name
+        )
+    )
+
+    return AuthenticatedLicenceObject(
+        app=token_data.aud,
+        id=token_data.iss,
+        tenant_id=token_data.tid,
+        user=user
+    )
+
+
+def get_token_data_from_jwt_payload(jwt_payload: dict, token: str) -> PebbleTokenData:
+
+    claims = ('aud', 'iss', 'tid', 'sub', 'roles', 'lv', 'name', 'iat', 'exp')
+
+    for claim in claims:
+        if claim not in jwt_payload:
+            jwt_payload[claim] = None
+
+    return PebbleTokenData(
+        aud=jwt_payload['aud'],
+        iss=jwt_payload['iss'],
+        tid=jwt_payload['tid'],
+        sub=jwt_payload['sub'],
+        roles=jwt_payload['roles'],
+        lv=jwt_payload['lv'],
+        name=jwt_payload['name'],
+        iat=jwt_payload['iat'],
+        exp=jwt_payload['exp'],
+        token=token
+    )
