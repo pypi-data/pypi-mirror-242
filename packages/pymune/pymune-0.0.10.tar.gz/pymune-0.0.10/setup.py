167550266c0a2aee7a233898a4ff7cf3fee79751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 14:05:00 +0000

    Add package pymune/0.0.10/pymune-0.0.10.tar.gz

---
+++ packages/pymune/pymune-0.0.10.tar.gz/pymune-0.0.10/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+install_requires=[
+    'numpy==1.24.3',
+    'pandas==1.5.3',
+    'matplotlib==3.7.2',
+    'scikit-learn==1.2.2',
+    'seaborn==0.12.2',
+    'treelib==1.6.1',
+    ]
+
+)
+
