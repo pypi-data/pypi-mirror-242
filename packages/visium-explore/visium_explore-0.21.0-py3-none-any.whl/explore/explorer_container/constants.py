ccb105b125bfc083a4c0c3e703193bc3e0d63ec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 04:05:13 +0000

    Add package visium-explore/0.21.0/visium_explore-0.21.0-py3-none-any.whl

---
+++ packages/visium-explore/visium_explore-0.21.0-py3-none-any.whl/explore/explorer_container/constants.py
@@ -0,0 +1,9 @@
+"""Constants for the explorer container module."""
+
+
+class PlotTypes:
+    """Plot types available in the explorer container."""
+
+    HISTOGRAM = "Histogram"
+    BOX = "Boxplot"
+    SCATTER = "Scatter"
