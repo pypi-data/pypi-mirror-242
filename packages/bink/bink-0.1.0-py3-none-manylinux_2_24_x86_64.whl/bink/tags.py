890aabfb2e38556ae1b9836cd4adc00a29de2988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 04:05:02 +0000

    Add package bink/0.1.0/bink-0.1.0-py3-none-manylinux_2_24_x86_64.whl

---
+++ packages/bink/bink-0.1.0-py3-none-manylinux_2_24_x86_64.whl/bink/tags.py
@@ -0,0 +1,31 @@
+# pylint: disable=E1101
+
+"""Handle Ink tags."""
+import ctypes
+from bink import LIB, BINK_OK
+
+class Tags:
+    """Contains a list of tags."""
+    def __init__(self, tags, c_len):
+        self._tags = tags
+        self._len = c_len
+
+    def len(self) -> int:
+        """Returns the number of choices."""
+        return self._len
+
+    def get(self, idx) -> str:
+        """Returns the tag text."""
+        tag = ctypes.c_char_p()
+        ret = LIB.bink_choices_get_text(self._tags, idx, ctypes.byref(tag))
+
+        if ret != BINK_OK:
+            raise RuntimeError("Error getting tag, index out of bounds?")
+
+        result = tag.value.decode('utf-8')
+        LIB.bink_cstring_free(tag)
+
+        return result
+
+    def __del__(self):
+        LIB.bink_choices_free(self._tags)
