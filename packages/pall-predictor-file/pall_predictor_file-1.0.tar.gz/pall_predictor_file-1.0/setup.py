5999c62750137d7675871b6b8ba6626e6399f63a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 15:04:55 +0000

    Add package pall-predictor-file/1.0/pall_predictor_file-1.0.tar.gz

---
+++ packages/pall-predictor-file/pall_predictor_file-1.0.tar.gz/pall_predictor_file-1.0/setup.py
@@ -0,0 +1,13 @@
+# import setuptools
+# from distutils import  setup
+from setuptools import setup
+
+packages = ['pall_predict']
+
+setup(
+    name='pall_predictor_file',
+    version='1.0',
+    author='wanggj', 
+    author_email='guijiang_wang@163.com',
+    packages=packages,
+)
