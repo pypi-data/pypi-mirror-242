643526417f1a9300d1a9dcf5830a91942699f698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 03:06:14 +0000

    Add package jjstock/0.0.1/jjstock-0.0.1.tar.gz

---
+++ packages/jjstock/jjstock-0.0.1.tar.gz/jjstock-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="jjstock",
+    version="0.0.1",
+    license='MIT',
+    author="joe",
+    author_email="kim_junhan@naver.com",
+    description="money tree",
+    long_description=open('README.md').read(),
+    url="https://blog.naver.com/joendjoy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # 패키지에 대한 태그
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
