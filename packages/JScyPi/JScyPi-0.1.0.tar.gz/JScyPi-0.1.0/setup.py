caab38ec377eb9a0652e984e233627bfcab7d301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 06:04:28 +0000

    Add package JScyPi/0.1.0/JScyPi-0.1.0.tar.gz

---
+++ packages/JScyPi/JScyPi-0.1.0.tar.gz/JScyPi-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='JScyPi',
+    version='0.1.0',
+    author='Jonios',
+    author_email='giannisvar2015@gmail.com',
+    description='Various mathematical / scientific constants and methods',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
