955f751160ee8e5c24f5d1537893fda558786664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 14:04:40 +0000

    Add package pdm/2.10.4/pdm-2.10.4-py3-none-any.whl

---
+++ packages/pdm/pdm-2.10.4-py3-none-any.whl/pdm/resolver/core.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Dict, cast
+
+from pdm import termui
+from pdm.models.candidates import Candidate
+from pdm.models.repositories import BaseRepository
+from pdm.models.requirements import strip_extras
+from pdm.resolver.providers import BaseProvider
+from pdm.resolver.python import PythonRequirement
+from pdm.utils import normalize_name
+
+if TYPE_CHECKING:
+    from resolvelib.resolvers import Resolver
+
+    from pdm.models.requirements import Requirement
+    from pdm.models.specifiers import PySpecSet
+
+
+def resolve(
+    resolver: Resolver,
+    requirements: list[Requirement],
+    requires_python: PySpecSet,
+    max_rounds: int = 10000,
+    keep_self: bool = False,
+) -> tuple[dict[str, Candidate], dict[tuple[str, str | None], list[Requirement]]]:
+    """Core function to perform the actual resolve process.
+    Return a tuple containing 2 items:
+
+        1. A map of pinned candidates
+        2. A map of resolved dependencies for each dependency group
+    """
+    requirements.append(PythonRequirement.from_pyspec_set(requires_python))
+    provider = cast(BaseProvider, resolver.provider)
+    repository = cast(BaseRepository, provider.repository)
+    result = resolver.resolve(requirements, max_rounds)
+
+    if repository.has_warnings:
+        repository.environment.project.core.ui.echo(
+            "Use `-q/--quiet` to suppress these warnings, or ignore them per-package with "
+            r"`ignore_package_warnings` config in \[tool.pdm] table.",
+            err=True,
+            style="info",
+            verbosity=termui.Verbosity.NORMAL,
+        )
+
+    mapping = cast(Dict[str, Candidate], result.mapping)
+    mapping.pop("python", None)
+
+    local_name = normalize_name(repository.environment.project.name) if repository.environment.project.name else None
+    for key, candidate in list(result.mapping.items()):
+        if key is None:
+            continue
+
+        # For source distribution whose name can only be determined after it is built,
+        # the key in the resolution map should be updated.
+        if key.startswith(":empty:"):
+            new_key = provider.identify(candidate)
+            mapping[new_key] = mapping.pop(key)
+            key = new_key
+
+        if not keep_self and strip_extras(key)[0] == local_name:
+            del mapping[key]
+
+    return mapping, provider.fetched_dependencies
