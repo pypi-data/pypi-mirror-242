76ffee694209458a4bd123609d2791a4afc7a27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 04:04:47 +0000

    Add package stealthbot/0.1.0/stealthbot-0.1.0-py2.py3-none-any.whl

---
+++ packages/stealthbot/stealthbot-0.1.0-py2.py3-none-any.whl/stealthbot/__init__.py
@@ -0,0 +1,11 @@
+"""StealthBot: Un bot Python con tecnologia antidetect.
+
+Questo modulo fornisce funzionalità per automatizzare compiti mantenendo
+un profilo basso e evitando rilevazioni. È ideale per operazioni che richiedono
+discrezione e sofisticatezza nell'elusione dei sistemi di rilevamento.
+"""
+__version__ = "0.1.0"
+from .decorators import RetryException, stealth, request, AsyncQueueResult, AsyncResult
+from .anti_detect_driver import AntiDetectDriver
+from .anti_detect_requests import AntiDetectRequests
+import stealthbot.bt as bt
