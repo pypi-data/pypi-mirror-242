c76b6eab4ee1bebc9097a6c06f8e19fe81f01cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 03:06:20 +0000

    Add package stencila/2.0.0a23/stencila-2.0.0a23-cp38-none-win_amd64.whl

---
+++ packages/stencila/stencila-2.0.0a23-cp38-none-win_amd64.whl/stencila/types/property_value_or_str.py
@@ -0,0 +1,14 @@
+# Generated file; do not edit. See the Rust `schema-gen` crate.
+
+from .prelude import *
+
+PropertyValue = ForwardRef("PropertyValue")
+
+
+PropertyValueOrStr = Union[
+    PropertyValue,
+    str,
+]
+"""
+`PropertyValue` or `str`
+"""
