f5993d47c06c7a9b27b025ca8fa3df40dbe80359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 04:04:51 +0000

    Add package text-ming-group/1.0.0/text_ming_group-1.0.0.tar.gz

---
+++ packages/text-ming-group/text_ming_group-1.0.0.tar.gz/text_ming_group-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='text_ming_group',
+    version='1.0.0',
+    author='Omar Paucar Bautista',
+    author_email='aryan.opb@gmail.com',
+    description='categoriza registros',
+    packages=find_packages(),
+)
