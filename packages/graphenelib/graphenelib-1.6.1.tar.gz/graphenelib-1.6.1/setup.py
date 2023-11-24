620bce6890c0a666aab3af8f3d74a82effdef48b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 05:04:57 +0000

    Add package graphenelib/1.6.1/graphenelib-1.6.1.tar.gz

---
+++ packages/graphenelib/graphenelib-1.6.1.tar.gz/graphenelib-1.6.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+__version__ = "1.6.1"
+
+setup(
+    version=__version__,
+    install_requires=open("requirements.txt").readlines(),
+    tests_require=open("requirements-test.txt").readlines(),
+    include_package_data=True,  # needed for data from manifest
+    # Use git repo data (latest tag, current commit hash, etc) for building a
+    # version number according PEP 440. Conflicts with semantic-release
+    setuptools_git_versioning={
+        "enabled": False,
+    },
+)
