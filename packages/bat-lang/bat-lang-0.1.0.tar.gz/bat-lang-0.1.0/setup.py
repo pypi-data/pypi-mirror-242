ebf033ccb32dc5e6743fb7efc36e03bcf7e84962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 14:05:13 +0000

    Add package bat-lang/0.1.0/bat-lang-0.1.0.tar.gz

---
+++ packages/bat-lang/bat-lang-0.1.0.tar.gz/bat-lang-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='bat-lang',
+    version='0.1.0',
+    packages=['batlang'],
+    install_requires=[
+        # list your dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'bat-lang = batlang.cli:main',
+        ],
+    },
+)
