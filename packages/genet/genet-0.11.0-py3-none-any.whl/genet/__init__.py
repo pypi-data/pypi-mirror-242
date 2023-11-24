f04a654bf226bcbcbed24a25355b664f31bce36a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 15:04:47 +0000

    Add package genet/0.11.0/genet-0.11.0-py3-none-any.whl

---
+++ packages/genet/genet-0.11.0-py3-none-any.whl/genet/__init__.py
@@ -0,0 +1,18 @@
+# from genet.utils import __version__
+
+'''
+genet의 처음 init 부분
+
+'''
+
+
+# for PyPI release and upload source URL
+# python setup.py sdist bdist_wheel
+# twine upload dist/genet-0.5.2-py3-none-any.whl
+
+# How to release using hatch
+# conda activate py38
+# hatch build
+# hatch publish
+
+__version__ = '0.11.0'
---
+++ packages/genet/genet-0.11.0-py3-none-any.whl/genet/predict_dev/__init__.py
@@ -0,0 +1,6 @@
+from genet.predict_dev.functional_dev import *
+from silence_tensorflow import silence_tensorflow
+import warnings
+
+silence_tensorflow()
+warnings.filterwarnings(action='ignore')
