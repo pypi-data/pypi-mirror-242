2408480d048537cfb08df879110bcaacdf0fe3ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 15:04:55 +0000

    Add package casdoor/1.17.2/casdoor-1.17.2.tar.gz

---
+++ packages/casdoor/casdoor-1.17.2.tar.gz/casdoor-1.17.2/src/casdoor/application.py
@@ -0,0 +1,163 @@
+# Copyright 2021 The Casdoor Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+from typing import Dict, List
+
+import requests
+
+from .organization import Organization, ThemeData
+from .provider import Provider
+
+
+class ProviderItem:
+    def __init__(self):
+        self.owner = ""
+        self.name = ""
+        self.canSignUp = False
+        self.canSignIn = False
+        self.canUnlink = False
+        self.prompted = False
+        self.alertType = ""
+        self.rule = ""
+        self.provider = Provider
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def to_dict(self) -> dict:
+        return self.__dict__
+
+
+class SignupItem:
+    def __init__(self):
+        self.name = ""
+        self.visible = False
+        self.required = False
+        self.prompted = False
+        self.rule = ""
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def to_dict(self) -> dict:
+        return self.__dict__
+
+
+class Application:
+    def __init__(self):
+        self.owner = ""
+        self.name = ""
+        self.createdTime = ""
+        self.displayName = ""
+        self.logo = ""
+        self.homepageUrl = ""
+        self.description = ""
+        self.organization = ""
+        self.cert = ""
+        self.enablePassword = False
+        self.enableSignUp = False
+        self.enableSigninSession = False
+        self.enableAutoSignin = False
+        self.enableCodeSignin = False
+        self.enableSamlCompress = False
+        self.enableWebAuthn = False
+        self.enableLinkWithEmail = False
+        self.orgChoiceMode = ""
+        self.samlReplyUrl = ""
+        self.providers = [ProviderItem]
+        self.signupItems = [SignupItem]
+        self.grantTypes = [""]
+        self.organizationObj = Organization
+        self.tags = [""]
+        self.clientId = ""
+        self.clientSecret = ""
+        self.redirectUris = [""]
+        self.tokenFormat = ""
+        self.expireInHours = 0
+        self.refreshExpireInHours = 0
+        self.signupUrl = ""
+        self.signinUrl = ""
+        self.forgetUrl = ""
+        self.affiliationUrl = ""
+        self.termsOfUse = ""
+        self.signupHtml = ""
+        self.signinHtml = ""
+        self.themeData = ThemeData
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def to_dict(self) -> dict:
+        return self.__dict__
+
+
+class _ApplicationSDK:
+    def get_applications(self) -> List[Dict]:
+        """
+        Get the applications from Casdoor.
+
+        :return: a list of dicts containing application info
+        """
+        url = self.endpoint + "/api/get-applications"
+        params = {
+            "owner": "admin",
+            "clientId": self.client_id,
+            "clientSecret": self.client_secret,
+        }
+        r = requests.get(url, params)
+        applications = r.json()
+        return applications
+
+    def get_application(self, application_id: str) -> Dict:
+        """
+        Get the application from Casdoor providing the application_id.
+
+        :param application_id: the id of the application
+        :return: a dict that contains application's info
+        """
+        url = self.endpoint + "/api/get-application"
+        params = {
+            "id": f"{self.org_name}/{application_id}",
+            "clientId": self.client_id,
+            "clientSecret": self.client_secret,
+        }
+        r = requests.get(url, params)
+        application = r.json()
+        return application
+
+    def modify_application(self, method: str, application: Application) -> Dict:
+        url = self.endpoint + f"/api/{method}"
+        application.owner = self.org_name
+        params = {
+            "id": f"{application.owner}/{application.name}",
+            "clientId": self.client_id,
+            "clientSecret": self.client_secret,
+        }
+        application_info = json.dumps(application.to_dict())
+        r = requests.post(url, params=params, data=application_info)
+        response = r.json()
+        return response
+
+    def add_application(self, application: Application) -> Dict:
+        response = self.modify_application("add-application", application)
+        return response
+
+    def update_application(self, application: Application) -> Dict:
+        response = self.modify_application("update-application", application)
+        return response
+
+    def delete_application(self, application: Application) -> Dict:
+        response = self.modify_application("delete-application", application)
+        return response
---
+++ packages/casdoor/casdoor-1.17.2.tar.gz/casdoor-1.17.2/src/casdoor/organization.py
@@ -0,0 +1,150 @@
+# Copyright 2021 The Casdoor Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+from typing import Dict, List
+
+import requests
+
+
+class AccountItem:
+    def __init__(self):
+        self.name = ""
+        self.visible = False
+        self.viewRule = ""
+        self.modifyRule = ""
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def to_dict(self) -> dict:
+        return self.__dict__
+
+
+class ThemeData:
+    def __init__(self):
+        self.themeType = ""
+        self.colorPrimary = ""
+        self.borderRadius = 0
+        self.isCompact = False
+        self.isEnabled = False
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def to_dict(self) -> dict:
+        return self.__dict__
+
+
+class MfaItem:
+    def __init__(self):
+        self.name = ""
+        self.rule = ""
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def to_dict(self) -> dict:
+        return self.__dict__
+
+
+class Organization:
+    def __init__(self):
+        self.owner = ""
+        self.name = ""
+        self.createdTime = ""
+        self.displayName = ""
+        self.websiteUrl = ""
+        self.favicon = ""
+        self.passwordType = ""
+        self.passwordSalt = ""
+        self.passwordOptions = [""]
+        self.countryCodes = [""]
+        self.defaultAvatar = ""
+        self.defaultApplication = ""
+        self.tags = [""]
+        self.languages = [""]
+        self.themeData = ThemeData
+        self.masterPassword = ""
+        self.initScore = 0
+        self.enableSoftDeletion = False
+        self.isProfilePublic = False
+        self.mfaItems = [MfaItem]
+        self.accountItems = [AccountItem]
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def to_dict(self) -> dict:
+        return self.__dict__
+
+
+class _OrganizationSDK:
+    def get_organizations(self) -> List[Dict]:
+        """
+        Get the organizations from Casdoor.
+
+        :return: a list of dicts containing organization info
+        """
+        url = self.endpoint + "/api/get-organizations"
+        params = {
+            "owner": self.org_name,
+            "clientId": self.client_id,
+            "clientSecret": self.client_secret,
+        }
+        r = requests.get(url, params)
+        organizations = r.json()
+        return organizations
+
+    def get_organization(self, organization_id: str) -> Dict:
+        """
+        Get the organization from Casdoor providing the organization_id.
+
+        :param organization_id: the id of the organization
+        :return: a dict that contains organization's info
+        """
+        url = self.endpoint + "/api/get-organization"
+        params = {
+            "id": f"{self.org_name}/{organization_id}",
+            "clientId": self.client_id,
+            "clientSecret": self.client_secret,
+        }
+        r = requests.get(url, params)
+        organization = r.json()
+        return organization
+
+    def modify_organization(self, method: str, organization: Organization) -> Dict:
+        url = self.endpoint + f"/api/{method}"
+        organization.owner = self.org_name
+        params = {
+            "id": f"{organization.owner}/{organization.name}",
+            "clientId": self.client_id,
+            "clientSecret": self.client_secret,
+        }
+        organization_info = json.dumps(organization.to_dict())
+        r = requests.post(url, params=params, data=organization_info)
+        response = r.json()
+        return response
+
+    def add_organization(self, organization: Organization) -> Dict:
+        response = self.modify_organization("add-organization", organization)
+        return response
+
+    def update_organization(self, organization: Organization) -> Dict:
+        response = self.modify_organization("update-organization", organization)
+        return response
+
+    def delete_organization(self, organization: Organization) -> Dict:
+        response = self.modify_organization("delete-organization", organization)
+        return response
