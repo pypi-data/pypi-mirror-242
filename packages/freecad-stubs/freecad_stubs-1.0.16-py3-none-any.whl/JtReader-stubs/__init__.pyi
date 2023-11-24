1402855565d17a36c8a15b72727152d1a62a2567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 14:05:07 +0000

    Add package freecad-stubs/1.0.16/freecad_stubs-1.0.16-py3-none-any.whl

---
+++ packages/freecad-stubs/freecad_stubs-1.0.16-py3-none-any.whl/JtReader-stubs/__init__.pyi
@@ -0,0 +1,11 @@
+# AppJtReaderPy.cpp
+def open(Name: str, /):
+    """open a jt file in a new Document"""
+
+
+def insert(Name: str, DocName: str, /):
+    """isert a jt file in a existing document"""
+
+
+def read(Name: str, /):
+    """Read a Mesh from a jt file and returns a Mesh object."""
