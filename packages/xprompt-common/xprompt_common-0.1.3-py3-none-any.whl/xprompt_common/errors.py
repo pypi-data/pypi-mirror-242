7eaefa78420260e07218ab457abfada91c90919f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 05:04:45 +0000

    Add package xprompt-common/0.1.3/xprompt_common-0.1.3-py3-none-any.whl

---
+++ packages/xprompt-common/xprompt_common-0.1.3-py3-none-any.whl/xprompt_common/errors.py
@@ -0,0 +1,29 @@
+class UserError(Exception):
+    pass
+
+
+class TryAgain(Exception):
+    pass
+
+
+class Timeout(Exception):
+    pass
+
+
+class InvalidTag(UserError):
+    def __init__(self, message):
+        self.message = message
+
+
+class SearchError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class AWSError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class OpenAIAuthenticationError(UserError):
+    pass
