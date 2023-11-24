573f02b4cbad3bebb36cdb60730d0b3cf25f1657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 04:05:16 +0000

    Add package pv-site-prediction/0.1.10/pv_site_prediction-0.1.10-py3-none-any.whl

---
+++ packages/pv-site-prediction/pv_site_prediction-0.1.10-py3-none-any.whl/psp/tests/conftest.py
@@ -0,0 +1,38 @@
+import pytest
+
+from psp.data_sources.nwp import NwpDataSource
+from psp.data_sources.pv import NetcdfPvDataSource
+
+
+@pytest.fixture
+def pv_data_source():
+    return NetcdfPvDataSource(
+        "psp/tests/fixtures/pv_data.nc",
+        id_dim_name="ss_id",
+        timestamp_dim_name="timestamp",
+        rename={"generation_wh": "power"},
+    )
+
+
+@pytest.fixture
+def nwp_data_source(pv_data_source):
+    return NwpDataSource(
+        "psp/tests/fixtures/nwp.zarr",
+        coord_system=27700,
+        time_dim_name="init_time",
+        value_name="UKV",
+        y_is_ascending=False,
+    )
+
+
+@pytest.fixture
+def nwp_data_sources(pv_data_source):
+    return {
+        "UKV": NwpDataSource(
+            "psp/tests/fixtures/nwp.zarr",
+            coord_system=27700,
+            time_dim_name="init_time",
+            value_name="UKV",
+            y_is_ascending=False,
+        ),
+    }
