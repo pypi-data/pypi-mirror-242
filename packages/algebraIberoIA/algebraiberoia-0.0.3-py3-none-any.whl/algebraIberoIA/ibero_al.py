b30b12d7edba021d2b46a2cfcc6b2d6be4b3eb88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 05:04:56 +0000

    Add package algebraIberoIA/0.0.3/algebraiberoia-0.0.3-py3-none-any.whl

---
+++ packages/algebraIberoIA/algebraiberoia-0.0.3-py3-none-any.whl/algebraIberoIA/ibero_al.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+def pedir_matriz(filas, columnas):
+    matriz = []
+    for i in range(filas):
+        fila = []
+        for j in range(columnas):
+            elemento = float(input(f"Ingrese el valor para la fila {i+1}, columna {j+1}: "))
+            fila.append(elemento)
+        matriz.append(fila)
+    matriz_np = np.array(matriz)   
+    return matriz_np
+
+def determinante(matriz, filas, columnas):
+    if filas == columnas:
+        return np.linalg.det(matriz)
+    else:
+        return print("Solo se pueden obtener determinantes de matrices cuadradas")
+
+def producto_escalar(escalar, matriz):
+    matriz2 = matriz[:]
+    matriz2 *= escalar
+    matriz_escalar = np.array(matriz2) 
+    return matriz_escalar
+
+def suma_matrices(matriz1, matriz2):
+    suma = matriz1 + matriz2
+    return suma
+    
+def resta_matrices(matriz1, matriz2):
+    resta = matriz1 - matriz2
+    return resta
