78a4bdf31533c14c7df220cd43b78880db28b4bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 03:06:24 +0000

    Add package FilesNFolders/0.5/FilesNFolders-0.5.tar.gz

---
+++ packages/FilesNFolders/FilesNFolders-0.5.tar.gz/FilesNFolders-0.5/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+  name='FilesNFolders',
+  packages=[],
+  version = '0.5',
+  license='MIT',
+  description = 'Allows the quick generation of files, folders, and their contents.',
+  author = 'KayLa Thomas',
+  author_email = 'kaylathomas.dev@gmail.com',
+  url = 'https://github.com/kaylathomas/FilesNFolders',
+  download_url = 'https://github.com/kaylathomas/FilesNFolders/archive/refs/tags/v_0.5.tar.gz',
+  keywords = ['files', 'folders', 'generation', 'f-string', 'generator'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.11'
+  ]
+)
