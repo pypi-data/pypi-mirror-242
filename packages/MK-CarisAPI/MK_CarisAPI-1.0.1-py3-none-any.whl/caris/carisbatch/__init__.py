58babacc55cbdd5df45fccaaa27382e62b5ed2ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 14:05:14 +0000

    Add package MK-CarisAPI/1.0.1/MK_CarisAPI-1.0.1-py3-none-any.whl

---
+++ packages/MK-CarisAPI/MK_CarisAPI-1.0.1-py3-none-any.whl/caris/carisbatch/__init__.py
@@ -0,0 +1,20 @@
+from .base_editor_processes import *
+from .base_manager_processes import *
+from .compose_processes import *
+from .czmil_processes import *
+from .engineering_analisys_processes import *
+from .feature_editing_processes import *
+from .hips_processes import *
+from .variable_resolution_surface_processes import *
+
+# __all__ = [
+#     "base_editor_processes.pyc",
+#     "base_editor_processes.pyc",
+#     "base_manager_processes.pyc",
+#     "compose_processes.pyc",
+#     "czmil_processes.pyc",
+#     "engineering_analisys_processes.pyc",
+#     "feature_editing_processes.pyc",
+#     "hips_processes.pyc",
+#     "variable_resolution_surface_processes.pyc",
+# ]
