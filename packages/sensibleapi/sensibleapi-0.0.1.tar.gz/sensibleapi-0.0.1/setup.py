11176eb13108861489ef7d615ef14c5017a376b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 04:05:11 +0000

    Add package sensibleapi/0.0.1/sensibleapi-0.0.1.tar.gz

---
+++ packages/sensibleapi/sensibleapi-0.0.1.tar.gz/sensibleapi-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sensibleapi',
+    version='0.0.1',
+    author='Sensible Technologies, Inc',
+    author_email='hello@sensible.so',
+    description='Javascript SDK for Sensible, the developer-first platform for extracting structured data from documents so that you can build document-automation features into your SaaS products',
+    packages=find_packages(),
+)
+
