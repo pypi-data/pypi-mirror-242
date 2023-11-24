535645ff99fb2d663cce6b28ce05fdfe13425b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 04:04:40 +0000

    Add package fjformer/0.0.10/fjformer-0.0.10-py3-none-any.whl

---
+++ packages/fjformer/fjformer-0.0.10-py3-none-any.whl/fjformer/attention/__init__.py
@@ -0,0 +1,3 @@
+from .efficient_attention import efficient_attention
+from .flash_attention_0 import dot_product_attention_multihead, dot_product_attention_multiquery, \
+    dot_product_attention_queries_per_head
