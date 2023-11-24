1de512dc8715e9cd24b577339ab5b2850b8b0261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 15:05:03 +0000

    Add package conoha-client/0.3.5/conoha_client-0.3.5-py3-none-any.whl

---
+++ packages/conoha-client/conoha_client-0.3.5-py3-none-any.whl/conoha_client/features/vm_actions/domain/errors.py
@@ -0,0 +1,33 @@
+"""VM actions error."""
+
+
+class VMDeleteError(Exception):
+    """VMを削除できなかった."""
+
+
+class VMActionTargetNotFoundError(Exception):
+    """VM操作対象が見つからなかったときの共通エラー."""
+
+
+class VMActionConflictingError(Exception):
+    """VM操作が競合した."""
+
+
+class VMShutdownError(Exception):
+    """VMをシャットダウンできなかった."""
+
+
+class VMBootError(Exception):
+    """VMを起動できなかった."""
+
+
+class VMRebootError(Exception):
+    """VMを再起動できなかった."""
+
+
+class VMSnapshotError(Exception):
+    """VMをイメージとして保存できなかった."""
+
+
+class VMResizeError(Exception):
+    """VMのサイズを変更できなかった."""
