815b2c4602bae98475efc0726a7358df429a27fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 04:05:11 +0000

    Add package alpacaz/0.1/alpacaz-0.1-py3-none-any.whl

---
+++ packages/alpacaz/alpacaz-0.1-py3-none-any.whl/alpacaz/image_conversion.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+            
+# -*- coding: utf-8 -*-
+
+'''
+.. module:: image_conversion
+   :platform: Unix, Windows
+   :synopsis: This module performs image conversions.
+
+.. moduleauthor:: Lalith Kumar Shiyam Sundar, Zacharias Chalampalakis <Lalith.shiyamsundar@meduniwien.ac.at>
+
+'''
+
+# Your code here
---
+++ packages/alpacaz/alpacaz-0.1-py3-none-any.whl/alpacaz/resources.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+            
+# -*- coding: utf-8 -*-
+
+'''
+.. module:: resources
+   :platform: Unix, Windows
+   :synopsis: This module manages resources.
+
+.. moduleauthor:: Lalith Kumar Shiyam Sundar, Zacharias Chalampalakis <Lalith.shiyamsundar@meduniwien.ac.at>
+
+'''
+
+# Your code here
