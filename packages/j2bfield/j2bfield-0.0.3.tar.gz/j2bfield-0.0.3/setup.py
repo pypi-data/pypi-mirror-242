2ed741d772bde04c469b744ac41df2821203fe9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 03:04:57 +0000

    Add package j2bfield/0.0.3/j2bfield-0.0.3.tar.gz

---
+++ packages/j2bfield/j2bfield-0.0.3.tar.gz/j2bfield-0.0.3/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='j2bfield',
+      version='0.0.3',
+      description='首次上传',
+      author='aheck',
+      author_email='610993278@qq.com',
+      url='https://gitee.com/aheck',
+      packages=['src',""],
+     )
+
+
