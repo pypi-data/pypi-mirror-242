10a68ab2dd5274d6b46ec9e8c3860d5e5d9ce52d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 03:04:54 +0000

    Add package bmitools/0.1.3/bmitools-0.1.3.tar.gz

---
+++ packages/bmitools/bmitools-0.1.3.tar.gz/bmitools-0.1.3/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+setup(
+    name='bmitools',
+    version='0.1.3',
+    packages=['bmitools'],
+    install_requires=[
+        'numpy',
+        'tqdm',
+        'scipy',
+        'matplotlib',
+        'pyyaml',
+        'networkx',
+        'scikit-learn',
+        'pandas',
+        'opencv-python',
+        'parmap',
+    ],
+	long_description = """
+	BMI utilities and analysis tools
+	=================
+
+	...
+	
+	
+	.. code-block:: bash
+
+	   pip install bmitools
+
+	Usage
+	-----
+
+	To use Binarize2PCalcium, import the package in your Python code:
+
+	.. code-block:: python
+
+	   from bmitools import ... 
+
+
+	""",
+    long_description_content_type='text/markdown',
+)
+
