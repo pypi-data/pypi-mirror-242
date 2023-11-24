d74dfce2caaffbdbd9a191319051731a890c4ad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 15:05:46 +0000

    Add package gptbuilder/1.0.1/gptbuilder-1.0.1.tar.gz

---
+++ packages/gptbuilder/gptbuilder-1.0.1.tar.gz/gptbuilder-1.0.1/gptbuilder/__init__.py
@@ -0,0 +1,9 @@
+from gptbuilder.builder import JsonBuilder, ListBuilder
+from gptbuilder.config import OpenaiConfig, AzureConfig
+
+__version__ = "1.0.1"
+__all__ = [
+    'JsonBuilder',
+    'ListBuilder'
+    'OpenaiConfig',
+    'AzureConfig']
---
+++ packages/gptbuilder/gptbuilder-1.0.1.tar.gz/gptbuilder-1.0.1/gptbuilder/builder/__init__.py
@@ -0,0 +1,6 @@
+from gptbuilder.builder.jsonbuilder import JsonBuilder
+from gptbuilder.builder.listbuilder import ListBuilder
+
+__all__ = [
+    'JsonBuilder',
+    'ListBuilder']
