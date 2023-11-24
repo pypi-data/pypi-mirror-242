35f5088635c3f4334add04426a33d77b9c19320d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 04:05:16 +0000

    Add package synthesized3/3.0/synthesized3-3.0-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/synthesized3/synthesized3-3.0-cp39-cp39-macosx_11_0_arm64.whl/synthesized3/model/models/__init__.py
@@ -0,0 +1,6 @@
+"""Synthesized models."""
+from .deep_table_model import DeepTableModel
+from .enumeration_model import EnumerationModel
+from .sampling_model import SamplingModel
+
+__all__ = ["DeepTableModel", "SamplingModel", "EnumerationModel"]
