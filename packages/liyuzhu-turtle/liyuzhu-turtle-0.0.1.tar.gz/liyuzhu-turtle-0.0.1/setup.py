0a64e2d36116bf93596d811a02e777c660001353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 16:05:01 +0000

    Add package liyuzhu-turtle/0.0.1/liyuzhu-turtle-0.0.1.tar.gz

---
+++ packages/liyuzhu-turtle/liyuzhu-turtle-0.0.1.tar.gz/liyuzhu-turtle-0.0.1/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+'''
+@Author  ：程序员李玉竹
+@WeChat     ：李玉竹
+@Blog      ：www.python-office.com
+@Date    ：2023/11/23 18:00
+@Description     ：记录一下我刚学Python时的创意代码
+
+'''
+
+from setuptools import setup  # 这个包没有的可以pip一下
+import time,turtle
+setup()
+
