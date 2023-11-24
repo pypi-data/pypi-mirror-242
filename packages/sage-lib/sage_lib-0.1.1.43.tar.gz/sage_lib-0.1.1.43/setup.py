aceede6c8b121deb199ffab1e4cdfc0d79778ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 03:04:51 +0000

    Add package sage-lib/0.1.1.43/sage_lib-0.1.1.43.tar.gz

---
+++ packages/sage-lib/sage_lib-0.1.1.43.tar.gz/sage_lib-0.1.1.43/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sage_lib',  # El nombre del paquete
+    version='0.1.1.43',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+    ],
+    entry_points={
+        'console_scripts': [
+            'sage = sage_lib.main:main',  # Establece 'sage' como el comando de consola
+        ],
+    },
+)
+
