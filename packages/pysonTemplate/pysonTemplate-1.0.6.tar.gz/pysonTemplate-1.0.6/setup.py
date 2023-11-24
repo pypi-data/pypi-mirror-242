6a016fc01544d98d8210bcb8c8aa34d19277d6b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 14:04:59 +0000

    Add package pysonTemplate/1.0.6/pysonTemplate-1.0.6.tar.gz

---
+++ packages/pysonTemplate/pysonTemplate-1.0.6.tar.gz/pysonTemplate-1.0.6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pysonTemplate',
+    version='1.0.6',
+    description='A simple Template for create python project',
+    author='ks lim',
+    author_email='kianseng.lim@sony.com',
+    packages=['python'],
+)
