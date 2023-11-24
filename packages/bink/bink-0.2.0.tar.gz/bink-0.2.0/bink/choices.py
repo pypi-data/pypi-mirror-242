7f05f65ec8bacc6cccd2079812376e6964c6df4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 14:05:10 +0000

    Add package bink/0.2.0/bink-0.2.0.tar.gz

---
+++ packages/bink/bink-0.2.0.tar.gz/bink-0.2.0/bink/choices.py
@@ -0,0 +1,31 @@
+# pylint: disable=E1101
+"""Handle Ink Choices."""
+import ctypes
+from bink import LIB, BINK_OK
+
+class Choices:
+    """List of story choices."""
+    def __init__(self, choices, c_len: int):
+        self._choices = choices
+        self._len = c_len
+
+    def __len__(self) -> int:
+        """Returns the number of choices."""
+        return self._len
+
+    def get_text(self, idx) -> str:
+        """Returns the choice text."""
+        text = ctypes.c_char_p()
+        ret = LIB.bink_choices_get_text(self._choices, idx, ctypes.byref(text))
+
+        if ret != BINK_OK:
+            raise RuntimeError(
+                "Error getting choice text, index out of bounds?")
+
+        result = text.value.decode('utf-8')
+        LIB.bink_cstring_free(text)
+
+        return result
+
+    def __del__(self):
+        LIB.bink_choices_free(self._choices)
