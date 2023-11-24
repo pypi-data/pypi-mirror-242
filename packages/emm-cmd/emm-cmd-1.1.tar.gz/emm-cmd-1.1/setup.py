31e8c76110bf53fa908e189979800f02f497db35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 14:05:10 +0000

    Add package emm-cmd/1.1/emm-cmd-1.1.tar.gz

---
+++ packages/emm-cmd/emm-cmd-1.1.tar.gz/emm-cmd-1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='emm-cmd',
+    version='1.1',
+    author="Chunyang",
+    description="AI command line assistant.",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'emm=emm.main:app',
+        ],
+    },
+    install_requires=required,
+    python_requires='>=3.6',
+)
