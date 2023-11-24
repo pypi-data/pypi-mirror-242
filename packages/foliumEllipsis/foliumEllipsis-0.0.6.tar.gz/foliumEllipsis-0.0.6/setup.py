29bcbd20d2a494588362e9f4ee7f0773bfd77c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 03:06:03 +0000

    Add package foliumEllipsis/0.0.6/foliumEllipsis-0.0.6.tar.gz

---
+++ packages/foliumEllipsis/foliumEllipsis-0.0.6.tar.gz/foliumEllipsis-0.0.6/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="foliumEllipsis",
+    version="0.0.6",
+    author="Daniel van der Maas",
+    author_email="daniel@ellipsis-drive.com",
+    description="Package to add Ellipsis Drive layers to folium",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ellipsis-drive/folium",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    install_requires=[
+    'folium',
+    'folium-vectorgrid',
+    ],
+    python_requires='>=3.6',
+)
