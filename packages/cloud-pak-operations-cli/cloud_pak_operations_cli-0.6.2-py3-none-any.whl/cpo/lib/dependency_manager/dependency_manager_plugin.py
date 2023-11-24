671205b82aa8e18ae4e1d6d51cd75a3d0d5643f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 04:05:03 +0000

    Add package cloud-pak-operations-cli/0.6.2/cloud_pak_operations_cli-0.6.2-py3-none-any.whl

---
+++ packages/cloud-pak-operations-cli/cloud_pak_operations_cli-0.6.2-py3-none-any.whl/cpo/lib/dependency_manager/dependency_manager_plugin.py
@@ -0,0 +1,166 @@
+#  Copyright 2021, 2023 IBM Corporation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import json
+import re as regex
+
+from abc import ABC, abstractmethod
+from typing import Optional
+
+import requests
+import semver
+
+from cpo.utils.operating_system import OperatingSystem
+
+
+class AbstractDependencyManagerPlugIn(ABC):
+    """Base class of all dependency manager plug-in classes"""
+
+    @abstractmethod
+    def download_dependency_version(self, version: semver.Version):
+        """Downloads the given version of the dependency
+
+        Parameters
+        ----------
+        version
+            version of the dependency to be downloaded
+        """
+
+        pass
+
+    @abstractmethod
+    def get_dependency_alias(self) -> str:
+        """Returns the alias of the dependency
+
+        The alias is used as a key in ~/.cpo/binaries.json to store the version
+        of the downloaded dependency.
+
+        Example:
+
+        {
+            […]
+            "ibmcloud": "1.2.3",
+            […]
+        }
+
+        Returns
+        -------
+        str
+            alias of the dependency
+        """
+
+        pass
+
+    @abstractmethod
+    def get_dependency_name(self) -> str:
+        """Returns the dependency name
+
+        Returns
+        -------
+        str
+            dependency name
+        """
+
+        pass
+
+    @abstractmethod
+    def get_latest_dependency_version(self) -> semver.Version:
+        """Returns the latest version of the dependency available at the
+        official download location
+
+        Returns
+        -------
+        semver.Version
+            latest version of the dependency available at the official download
+            location
+        """
+
+        pass
+
+    @abstractmethod
+    def is_operating_system_supported(self, operating_system: OperatingSystem) -> bool:
+        pass
+
+    def _get_latest_dependency_version_on_github(self, owner: str, repo: str) -> Optional[semver.Version]:
+        """Returns the latest version of the dependency on GitHub
+
+        This method parses the "name" key of the JSON document returned by the
+        GitHub Releases API, which has the following structure:
+
+        [
+            {
+                "url": […],
+                "html_url": […],
+                "assets_url": […],
+                "upload_url": […],
+                "tarball_url": […],
+                "zipball_url": […],
+                "id": […],
+                "node_id": […],
+                "tag_name": […],
+                "target_commitish": […],
+                "name": "v1.0.0",
+                "body": […],
+                "draft": […],
+                "prerelease": […],
+                "created_at": […],
+                "published_at": […],
+                "author": {
+                    […]
+                },
+                "assets": [
+                    […]
+                ]
+            },
+            {
+                […]
+                "name": "v2.0.0",
+                […]
+            },
+            […]
+        ]
+
+        GitHub Releases API: https://developer.github.com/v3/repos/releases/
+
+        Parameters
+        ----------
+        owner
+            GitHub repository owner
+        repo
+            GitHub repository name
+
+        Returns
+        -------
+        Optional[semver.Version]
+            latest version of the dependency or None if no release was found
+        """
+
+        response = requests.get(f"https://api.github.com/repos/{owner}/{repo}/releases")
+        response.raise_for_status()
+
+        response_json = json.loads(response.content)
+        result: Optional[semver.Version] = None
+
+        for release in response_json:
+            search_result = regex.search(
+                "v(\\d+\\.\\d+\\.\\d+)$",
+                release["name"],
+            )
+
+            if search_result is not None:
+                result = semver.Version.parse(search_result.group(1))
+
+                break
+
+        return result
