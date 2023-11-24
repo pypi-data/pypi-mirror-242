ab51b19d5b1ba7bc3ad0ad8fe622209e84c71352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 15:06:37 +0000

    Add package yaylib/1.3.4/yaylib-1.3.4-py3-none-any.whl

---
+++ packages/yaylib/yaylib-1.3.4-py3-none-any.whl/yaylib/api/__init__.py
@@ -0,0 +1,55 @@
+"""
+
+Yay! (nanameue, Inc.) API Client
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+An API wrapper for Yay! (yay.space) written in Python.
+
+:copyright: (c) 2023-present qvco
+:license: MIT, see LICENSE for more details.
+
+"""
+
+from .api import *
+from .call import *
+from .cassandra import *
+from .chat import *
+from .group import *
+from .login import *
+from .misc import *
+from .post import *
+from .review import *
+from .thread import *
+from .user import *
+from .websocket import *
+
+__all__ = [
+    "API",
+    "block",
+    "call",
+    "cassandra",
+    "chat",
+    "common",
+    "config",
+    "dev",
+    "file_upload",
+    "game",
+    "gift",
+    "group",
+    "hidden",
+    "id_card_check",
+    "internal_id_card_check",
+    "login",
+    "misc",
+    "mute_keyword",
+    "payment",
+    "post",
+    "review",
+    "search",
+    "thread",
+    "twitter",
+    "user_identity",
+    "user",
+    "wallet",
+    "websocket",
+]
