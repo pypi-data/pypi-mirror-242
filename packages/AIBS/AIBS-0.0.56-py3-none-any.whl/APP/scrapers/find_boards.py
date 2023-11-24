ed0ebf3d19d1417731ae27a89427617dfab772d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 03:04:56 +0000

    Add package AIBS/0.0.56/AIBS-0.0.56-py3-none-any.whl

---
+++ packages/AIBS/AIBS-0.0.56-py3-none-any.whl/APP/scrapers/find_boards.py
@@ -0,0 +1,46 @@
+
+import ssl
+from httpx import AsyncClient
+from bs4 import BeautifulSoup
+from random import choice
+from urllib.parse import urljoin
+
+
+from APP.config import base_urls
+from APP.config import headers, timeout
+
+# Pick a random base url to use to avoid hitting the same url over and over. (All urls hit the same server but cloudflare
+# will block you if you hit the same url too many times in a row.) Adding proxies soon.
+base_url = choice(base_urls)
+
+
+async def find_boards(board:str, requests_semaphore):
+    async with requests_semaphore:
+        async with AsyncClient() as client:
+            tried_urls = []
+            r = True
+            while r:
+                try:
+                    possible_urls = [url for url in base_urls if url not in tried_urls]
+                    if len(possible_urls) == 0:
+                        r = False
+                        continue
+                    used_url = choice(possible_urls)
+                    tried_urls.append(used_url)
+                    board_url = urljoin(used_url, board)
+                    response = await client.get(board_url, headers=headers, timeout=timeout, follow_redirects=True)
+                    if not response:
+                        r = False
+                        continue
+                    if response is None or response.status_code != 200:
+                        print(f'Error with {used_url}, status code {response.status_code}')
+                        r = False
+                    soup = BeautifulSoup(response.content, 'html.parser')
+                    boards = soup.find_all(class_="opCell")
+                    return boards
+                except ssl.SSLError:
+                    pass
+
+                except Exception as e:
+                    r = False
+                    raise e
