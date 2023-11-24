387a31765e025feea41ba41ed37de2063ab99553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 05:04:45 +0000

    Add package matrixlib-jfj/0.2.2/matrixlib_jfj-0.2.2-py2.py3-none-any.whl

---
+++ packages/matrixlib-jfj/matrixlib_jfj-0.2.2-py2.py3-none-any.whl/matrixlib_jfj/utils.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from . import matrix, vector
+
+def delete_matrix(mat: matrix.Matrix):
+    matrix = matrix * 0
+    del matrix
+    
+def is_square(mat: matrix.Matrix):
+    shape = mat.get_shape()
+    if shape[0] == shape[1]:
+        return True
+    else:
+        return False
