c2e2c378d1fa35c26971af03c94f836d4da4c91a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 04:04:30 +0000

    Add package monistode-assembler/0.1.12/monistode_assembler-0.1.12-py3-none-any.whl

---
+++ packages/monistode-assembler/monistode_assembler-0.1.12-py3-none-any.whl/monistode_assembler/exceptions.py
@@ -0,0 +1,36 @@
+"""Exceptions for the assembler."""
+
+
+class AssemblerError(Exception):
+    """An error that occurred while assembling a program."""
+
+    def __init__(self, message: str, line_number: int | None = None) -> None:
+        """Initialize the error.
+
+        Args:
+            message: The error message.
+            line_number: The line number where the error occurred.
+        """
+        super().__init__(message)
+        self.line_number = line_number
+        self.message = message
+
+    def __str__(self) -> str:
+        """Return the error message."""
+        return (
+            f"{self.message} (line {self.line_number})"
+            if self.line_number
+            else self.message
+        )
+
+
+class AssemblyError(AssemblerError):
+    """An error that occurred while assembling an assembly source file."""
+
+
+class DisassemblyError(AssemblerError):
+    """An error that occurred while disassembling a binary file."""
+
+
+class ParserError(AssemblerError):
+    """An error that occurred while parsing an assembly source file."""
