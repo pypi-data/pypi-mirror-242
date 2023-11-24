84f17e27335c7a77a6df6c5b6c3c16760c62e4ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 15:05:47 +0000

    Add package qg-botsdk/4.0.1/qg-botsdk-4.0.1.tar.gz

---
+++ packages/qg-botsdk/qg-botsdk-4.0.1.tar.gz/qg-botsdk-4.0.1/setup.py
@@ -0,0 +1,18 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools"
+    ],
+)
