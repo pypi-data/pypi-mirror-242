eadf9b7480a1b0740ee207e38ac8e4182ab8f5a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 14:05:17 +0000

    Add package szCAl/1.0.0/szCAl-1.0.0.tar.gz

---
+++ packages/szCAl/szCAl-1.0.0.tar.gz/szCAl-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='szCAl',
+    version='1.0.0',
+    author='Hiram Jiao',
+    packages=['E:/shuizhiJD/NBshuizhi/Mudushuizhi/moxingjiaohe'],
+    install_requires=[
+
+    ],
+)
+
