9e27972cabc88fed600a22fc3969aed9fa4f465e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 15:05:12 +0000

    Add package qubitmapping/0.1.6/qubitmapping-0.1.6.tar.gz

---
+++ packages/qubitmapping/qubitmapping-0.1.6.tar.gz/qubitmapping-0.1.6/setup.py
@@ -0,0 +1,14 @@
+
+import io
+from setuptools import find_packages, setup
+
+setup(
+	name="qubitmapping",
+	version='0.1.6',
+	description='initial qubit mapping picked randomly',
+	author='Yongsoo Hwang',
+	install_requires=[],
+	packages=find_packages(),
+	zip_safe=False,
+	python_requires='>=3'
+	)
