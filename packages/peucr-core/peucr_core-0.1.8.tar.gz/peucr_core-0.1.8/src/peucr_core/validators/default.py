c90cb59fad386709105ef2cf4ad51f6cd6a93f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 04:05:12 +0000

    Add package peucr-core/0.1.8/peucr_core-0.1.8.tar.gz

---
+++ packages/peucr-core/peucr_core-0.1.8.tar.gz/peucr_core-0.1.8/src/peucr_core/validators/default.py
@@ -0,0 +1,15 @@
+import requests
+from peucr_core.validator import TestValidator
+from peucr_core.exceptions import InvalidDefinitionException
+
+class DefaultValidator(TestValidator):
+
+    def __init__(self):
+        self.labels = ["DEFAULT"]
+
+
+    def apply(self, expectation, response, suite):
+        if "success" not in response:
+            raise InvalidDefinitionException("No \"success\" field in response")
+
+        return {"success": response["success"]}
