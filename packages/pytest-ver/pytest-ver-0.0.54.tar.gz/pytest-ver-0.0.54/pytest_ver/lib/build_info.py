dc3f8ee813246c9c17610d2981f985a44d9deadf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 05:05:01 +0000

    Add package pytest-ver/0.0.54/pytest-ver-0.0.54.tar.gz

---
+++ packages/pytest-ver/pytest-ver-0.0.54.tar.gz/pytest-ver-0.0.54/pytest_ver/lib/build_info.py
@@ -0,0 +1,10 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.10.12'
+    os_name = 'posix'
+    version = '0.0.54'
+    git_sha = '232ca96dd21b9aaeb220a9f32d41e99ae93fa383'
+    git_branch = 'master'
+    git_uncommitted = [
+    ]
+    git_unpushed = [
+    ]
