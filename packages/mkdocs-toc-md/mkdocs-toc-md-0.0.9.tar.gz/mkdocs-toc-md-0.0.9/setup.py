c70dfbd9576458238b43aaf48f6576d68a95856f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-24 03:05:57 +0000

    Add package mkdocs-toc-md/0.0.9/mkdocs-toc-md-0.0.9.tar.gz

---
+++ packages/mkdocs-toc-md/mkdocs-toc-md-0.0.9.tar.gz/mkdocs-toc-md-0.0.9/setup.py
@@ -0,0 +1,40 @@
+import io
+from setuptools import setup
+
+# python setup.py develop
+# python setup.py sdist bdist_wheel
+# twine check dist/mkdocs-toc-md-x.x.x.tar.gz
+# twine upload --repository pypi dist/*
+setup(
+    name='mkdocs-toc-md',
+    version='0.0.9',
+    description='Generate a table of contents markdown file',
+    long_description=io.open('README.md', encoding='utf8').read(),
+    long_description_content_type='text/markdown',
+    keywords='mkdocs plugin toc generator',
+    author='Ryo Tsunoda',
+    author_email='try0.development@gmail.com',
+    url='https://github.com/try0/mkdocs-toc-md',
+    license='MIT',
+    install_requires=[
+        'mkdocs>=1.1',
+        'beautifulsoup4>=4.6.3',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation'
+    ],
+    python_requires='>=3.6',
+    packages=['mkdocs_toc_md'],
+    include_package_data=True,
+    entry_points={
+        'mkdocs.plugins': [
+            'toc-md = mkdocs_toc_md.plugin:TocMdPlugin',
+        ]
+    },
+)
