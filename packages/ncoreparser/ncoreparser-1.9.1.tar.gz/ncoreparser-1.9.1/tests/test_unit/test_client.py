3978aa4101f0c92fc701700060c8b83c98a915db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:05:12 +0000

    Add package ncoreparser/1.9.1/ncoreparser-1.9.1.tar.gz

---
+++ packages/ncoreparser/ncoreparser-1.9.1.tar.gz/ncoreparser-1.9.1/tests/test_unit/test_client.py
@@ -0,0 +1,35 @@
+from unittest.mock import MagicMock
+from ncoreparser.client import Client
+from ncoreparser.error import NcoreConnectionError, NcoreCredentialError
+from ncoreparser.data import URLs
+import requests
+import pytest
+
+
+class TestClient:
+
+    def test_credentials(self, monkeypatch):
+        session = MagicMock()
+        session.return_value.post.return_value.url = URLs.INDEX.value
+
+        monkeypatch.setattr(requests, "session", session)
+        c = Client()
+        c.login("username", "password")
+
+    def test_credentials_con_error(self, monkeypatch):
+        session = MagicMock()
+        session.return_value.post.side_effect = Exception()
+
+        monkeypatch.setattr(requests, "session", session)
+        with pytest.raises(NcoreConnectionError):
+            c = Client()
+            c.login("username", "password")
+
+    def test_invalid_credentials(self, monkeypatch):
+        session = MagicMock()
+        session.return_value.post.return_value.url = URLs.LOGIN.value
+
+        monkeypatch.setattr(requests, "session", session)
+        with pytest.raises(NcoreCredentialError):
+            c = Client()
+            c.login("username", "password")
