c03fef5f684626571ae619c34121af4784be5afd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 04:05:06 +0000

    Add package py-tisgrabber/1.0.1/py-tisgrabber-1.0.1.tar.gz

---
+++ packages/py-tisgrabber/py-tisgrabber-1.0.1.tar.gz/py-tisgrabber-1.0.1/examples/41-qt-triggering.py
@@ -0,0 +1,144 @@
+import sys
+from ctypes import Structure
+
+import cv2
+from PyQt5.QtCore import QObject, pyqtSignal
+from PyQt5.QtWidgets import (
+    QAction,
+    QApplication,
+    QHBoxLayout,
+    QMainWindow,
+    QProgressBar,
+    QVBoxLayout,
+    QWidget,
+)
+
+from tisgrabber.cam import Camera
+from tisgrabber.wrapper import ImageControl
+
+ic = ImageControl()
+
+
+class CallbackSignals(QObject):
+    new_image_ready = pyqtSignal(object)
+
+
+callback_signals = CallbackSignals()
+
+
+class CallbackPayload(Structure):
+    image_data = None
+
+    def __init__(self, index: int):
+        self.index = index
+        print(f"CallbackPayload {index} created")
+
+
+def frame_ready_callback(grabber, ptr, n_frame, payload: CallbackPayload):
+    print(f"Frame ready callback {payload.index}")
+    payload.image_data = cameras[payload.index].get_image_data()
+    callback_signals.new_image_ready.emit(payload)
+
+
+frame_ready_callback_pointer = ic.create_frame_ready_callback(frame_ready_callback)
+
+
+def on_close_button_clicked():
+    for cam in cameras:
+        if cam is not None:
+            cam.stop_live()
+    app.quit()
+
+
+def on_camera_selection_clicked(index):
+    print(f"Camera {index} clicked")
+    grabber = ic.show_device_selection_dialog()
+    if ic.is_dev_valid(grabber):
+        cam = Camera(grabber)
+        cam.set_frame_ready_callback(
+            frame_ready_callback_pointer,
+            callback_payloads[index],
+        )
+        cam.set_window_handle(video_widgets[index].winId())
+        cam.set_continuous_mode(False)
+        cam.start_live()
+        cameras[index] = cam
+
+
+def on_camera_properties_clicked(index):
+    if cameras[index] is not None:
+        cameras[index].show_property_dialog()
+    else:
+        on_camera_selection_clicked(index)
+
+
+def on_new_image(payload: CallbackPayload):
+    print(f"New image received from camera {payload.index}")
+    gray = cv2.cvtColor(payload.image_data, cv2.COLOR_BGR2GRAY)
+    mean = cv2.mean(gray)
+    brightness_bars[payload.index].setValue(int(mean[0]))
+
+
+callback_signals.new_image_ready.connect(on_new_image)
+
+if __name__ == "__main__":
+    n_cameras = max(ic.get_device_count(), 1)
+    cameras = n_cameras * [None]
+
+    app = QApplication(sys.argv)
+
+    main_window = QMainWindow()
+    main_window.resize(1280, 480)
+    main_window.move(300, 300)
+    main_window.setWindowTitle("Trigger Demo")
+
+    main_menu = main_window.menuBar()
+    file_menu = main_menu.addMenu("&File")
+
+    exit_action = QAction("&Exit", app)
+    exit_action.setStatusTip("Exit program")
+    exit_action.triggered.connect(on_close_button_clicked)
+    file_menu.addAction(exit_action)
+
+    camera_menu = main_menu.addMenu("&Cameras")
+    property_menu = main_menu.addMenu("&Properties")
+    callback_payloads = []
+    for i in range(n_cameras):
+        device_selection_action = QAction(f"&Select {i}", app)
+        device_selection_action.triggered.connect(
+            lambda clicked, index=i: on_camera_selection_clicked(index)
+        )
+        camera_menu.addAction(device_selection_action)
+
+        dev_property_action = QAction(f"&Camera {i}", app)
+        dev_property_action.triggered.connect(
+            lambda clicked, index=i: on_camera_properties_clicked(index)
+        )
+        property_menu.addAction(dev_property_action)
+
+        callback_payloads.append(CallbackPayload(i))
+
+    main_widget = QWidget()
+
+    video_widgets = []
+    brightness_bars = []
+    outer_hbox_layout = QHBoxLayout()
+    for _ in range(n_cameras):
+        inner_vbox_layout = QVBoxLayout()
+        video_widget = QWidget()
+        inner_vbox_layout.addWidget(video_widget)
+
+        brightness_bar = QProgressBar()
+        brightness_bar.setRange(0, 256)
+        brightness_bar.setValue(128)
+        inner_vbox_layout.addWidget(brightness_bar)
+
+        outer_hbox_layout.addLayout(inner_vbox_layout)
+        video_widgets.append(video_widget)
+        brightness_bars.append(brightness_bar)
+    main_widget.setLayout(outer_hbox_layout)
+    main_window.setCentralWidget(main_widget)
+
+    main_window.show()
+
+    app.exec()
---
+++ packages/py-tisgrabber/py-tisgrabber-1.0.1.tar.gz/py-tisgrabber-1.0.1/tisgrabber/cam.py
@@ -0,0 +1,223 @@
+from ctypes import Structure
+from typing import Any, Self
+
+import numpy as np
+
+from .enums import CameraProperty, VideoProperty
+from .structs import HGRABBER
+from .wrapper import FRAMEREADYCALLBACK, FilePath, ImageControl
+
+ic = ImageControl()
+
+
+class CameraSetting:
+    def __init__(self, grabber, property: CameraProperty):
+        self._grabber = grabber
+        self._property = property
+        self.is_available = ic.is_camera_property_available(grabber, property)
+        self.auto_available = ic.is_camera_property_auto_available(grabber, property)
+
+    @property
+    def value(self) -> int:
+        if self.is_available:
+            return ic.get_camera_property(self._grabber, self._property)
+        else:
+            raise RuntimeError("Camera property not available.")
+
+    @value.setter
+    def value(self, value: int) -> None:
+        if self.is_available:
+            self.auto = False
+            ic.set_camera_property(self._grabber, self._property, value)
+        else:
+            raise RuntimeError("Camera property not available.")
+
+    @property
+    def setting_range(self) -> tuple[int, int]:
+        if self.is_available:
+            return ic.camera_property_get_range(self._grabber, self._property)
+        else:
+            raise RuntimeError("Camera property not available.")
+
+    @property
+    def auto(self) -> bool:
+        if self.auto_available:
+            return ic.get_auto_camera_property(self._grabber, self._property)
+        else:
+            raise RuntimeError("Auto setting for property is not available.")
+
+    @auto.setter
+    def auto(self, enable: bool) -> None:
+        if self.auto_available:
+            ic.enable_auto_camera_property(self._grabber, self._property, enable)
+        else:
+            raise RuntimeError("Auto setting for property is not available.")
+
+
+class Exposure(CameraSetting):
+    @property
+    def setting(self) -> int:
+        if self.is_available:
+            return ic.get_exp_reg_val(self._grabber)
+        else:
+            raise RuntimeError("Camera property not available.")
+
+    @setting.setter
+    def setting(self, value: int) -> None:
+        if self.is_available:
+            self.auto = False
+            ic.set_exp_reg_val(self._grabber, value)
+        else:
+            raise RuntimeError("Camera property not available.")
+
+    @property
+    def value(self) -> float:
+        return ic.get_property_absolute_value(self._grabber, "Exposure", "Value")
+
+    @value.setter
+    def value(self, value: float) -> None:
+        ic.set_property_absolute_value(self._grabber, "Exposure", "Value", value)
+
+
+class VideoSetting:
+    def __init__(self, grabber, property: VideoProperty):
+        self._grabber = grabber
+        self._property = property
+        self.is_available = ic.is_video_property_available(grabber, property)
+        self.auto_available = ic.is_video_property_auto_available(grabber, property)
+
+    @property
+    def setting(self) -> int:
+        if self.is_available:
+            return ic.get_video_property(self._grabber, self._property)
+        else:
+            raise RuntimeError("Video property not available.")
+
+    @setting.setter
+    def setting(self, value: int) -> None:
+        if self.is_available:
+            self.auto = False
+            ic.set_video_property(self._grabber, self._property, value)
+        else:
+            raise RuntimeError("Video property not available.")
+
+    @property
+    def auto(self) -> bool:
+        if self.auto_available:
+            return ic.get_auto_video_property(self._grabber, self._property)
+        else:
+            raise RuntimeError("Auto setting for property is not available.")
+
+    @auto.setter
+    def auto(self, enable: bool) -> None:
+        if self.auto_available:
+            ic.enable_auto_video_property(self._grabber, self._property, enable)
+        else:
+            raise RuntimeError("Auto setting for property is not available.")
+
+    @property
+    def setting_range(self) -> tuple[int, int]:
+        if self.is_available:
+            return ic.video_property_get_range(self._grabber, self._property)
+        else:
+            raise RuntimeError("Video property not available.")
+
+
+class Camera:
+    def __init__(self, grabber: HGRABBER) -> None:
+        self._grabber = grabber
+
+        self.pan = CameraSetting(self._grabber, CameraProperty.PAN)
+        self.tilt = CameraSetting(self._grabber, CameraProperty.TILT)
+        self.roll = CameraSetting(self._grabber, CameraProperty.ROLL)
+        self.zoom = CameraSetting(self._grabber, CameraProperty.ZOOM)
+        # NOTE: Exposure is a special case with different commands
+        self.exposure = Exposure(self._grabber, CameraProperty.EXPOSURE)
+        self.iris = CameraSetting(self._grabber, CameraProperty.IRIS)
+        self.focus = CameraSetting(self._grabber, CameraProperty.FOCUS)
+        self.brightness = VideoSetting(self._grabber, VideoProperty.BRIGHTNESS)
+        self.contrast = VideoSetting(self._grabber, VideoProperty.CONTRAST)
+        self.hue = VideoSetting(self._grabber, VideoProperty.HUE)
+        self.saturation = VideoSetting(self._grabber, VideoProperty.SATURATION)
+        self.sharpness = VideoSetting(self._grabber, VideoProperty.SHARPNESS)
+        self.gamma = VideoSetting(self._grabber, VideoProperty.GAMMA)
+        self.color_enable = VideoSetting(self._grabber, VideoProperty.COLORENABLE)
+        self.white_balance = VideoSetting(self._grabber, VideoProperty.WHITEBALANCE)
+        self.black_light_compensation = VideoSetting(
+            self._grabber, VideoProperty.BLACKLIGHTCOMPENSATION
+        )
+        self.gain = VideoSetting(self._grabber, VideoProperty.GAIN)
+
+    def __enter__(self) -> Self:
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        ic.release_grabber(self._grabber)
+
+    def release_grabber(self):
+        ic.release_grabber(self._grabber)
+
+    @property
+    def frame_rate(self) -> float:
+        return ic.get_frame_rate(self._grabber)
+
+    @frame_rate.setter
+    def frame_rate(self, value: float) -> None:
+        ic.set_frame_rate(self._grabber, value)
+
+    def start_live(self) -> None:
+        ic.start_live(self._grabber)
+
+    def stop_live(self) -> None:
+        ic.stop_live(self._grabber)
+
+    def snap_image(self, timeout=1000) -> None:
+        ic.snap_image(self._grabber, timeout=timeout)
+
+    def get_image_data(self) -> np.ndarray:
+        return ic.get_image_data(self._grabber)
+
+    def save_device_state_to_file(self, filename: FilePath) -> None:
+        ic.save_device_state_to_file(self._grabber, filename)
+
+    def get_image_description(self) -> tuple[int, int, int, int]:
+        return ic.get_image_description(self._grabber)
+
+    def show_property_dialog(self) -> None:
+        ic.show_property_dialog(self._grabber)
+
+    def set_window_handle(self, handle: Any) -> None:
+        ic.set_hwnd(self._grabber, handle)
+
+    def set_continuous_mode(self, enable: bool) -> None:
+        ic.set_continuous_mode(self._grabber, enable)
+
+    def set_frame_ready_callback(self, callback: FRAMEREADYCALLBACK, data: Structure):
+        """Set a callback function that is called when a new frame is ready."""
+        ic.set_frame_ready_callback(self._grabber, callback, data)
+
+    def enable_trigger(self, enable: bool) -> None:
+        ic.enable_trigger(self._grabber, enable)
+
+    def software_trigger(self) -> None:
+        ic.software_trigger(self._grabber)
+
+    def set_roi(self, top, left, height, width) -> None:
+        filter_handle = ic.create_frame_filter("ROI")
+        ic.add_frame_filter_to_device(self._grabber, filter_handle)
+        ic.frame_filter_set_parameter_int(filter_handle, "Top", top)
+        ic.frame_filter_set_parameter_int(filter_handle, "Left", left)
+        ic.frame_filter_set_parameter_int(filter_handle, "Height", height)
+        ic.frame_filter_set_parameter_int(filter_handle, "Width", width)
+
+
+def main():
+    grabber = ic.show_device_selection_dialog()
+    with Camera(grabber) as cam:
+        cam.start_live()
+        ic.msg_box("Press OK to stop.", "Live image")
+        cam.stop_live()
+
+
+if __name__ == "__main__":
+    main()
