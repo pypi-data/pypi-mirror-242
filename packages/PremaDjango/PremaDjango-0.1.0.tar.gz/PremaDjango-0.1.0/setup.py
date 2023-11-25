1e025038973b1f5af9fc852ca9ce332125388aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:05:42 +0000

    Add package PremaDjango/0.1.0/PremaDjango-0.1.0.tar.gz

---
+++ packages/PremaDjango/PremaDjango-0.1.0.tar.gz/PremaDjango-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PremaDjango',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'Django',  # Add other default libraries here
+    ],
+)
