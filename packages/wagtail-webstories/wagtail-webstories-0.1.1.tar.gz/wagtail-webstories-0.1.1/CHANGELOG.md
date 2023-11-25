e45d33d5ed8350ba92e23fca2af09d8b24545ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 04:04:35 +0000

    Add package wagtail-webstories/0.1.1/wagtail-webstories-0.1.1.tar.gz

---
+++ packages/wagtail-webstories/wagtail-webstories-0.1.1.tar.gz/wagtail-webstories-0.1.1/CHANGELOG.md
@@ -0,0 +1,40 @@
+Changelog
+=========
+
+0.1.1 (2023-11-24)
+------------------
+
+* Address deprecation warnings from Wagtail 5.1 and 5.2
+
+
+0.1 (2023-07-27)
+----------------
+
+* Update for Django 3.2 - 4.2, Wagtail 4.1 - 5.0, Python 3.8 - 3.11
+* Drop support older versions of Django, Wagtail, and Python
+
+
+0.0.4 (2021-05-27)
+------------------
+
+* Fix length of url_hash field
+* Fix packaging to include migrations
+
+
+0.0.3 (2021-03-11)
+------------------
+
+* Add support for embedding and linking external stories without importing
+* Make `BaseWebStoryPage` functionality available through a mixin class `WebStoryPageMixin`
+
+
+0.0.2 (2021-03-02)
+------------------
+
+* Allow disabling HTML cleaning with `WAGTAIL_WEBSTORIES_IMPORT_MODEL`
+
+
+0.0.1 (2020-12-07)
+------------------
+
+* Initial release
---
+++ packages/wagtail-webstories/wagtail-webstories-0.1.1.tar.gz/wagtail-webstories-0.1.1/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="wagtail-webstories",
+    version="0.1.1",
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    include_package_data=True,
+    test_suite="tests",
+    url="https://github.com/torchbox/wagtail-webstories/",
+
+    author="Matt Westcott",
+    author_email="matthew@torchbox.com",
+    description="AMP web story support for Wagtail",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Framework :: Django",
+        "Framework :: Django :: 3",
+        "Framework :: Django :: 4",
+        "Framework :: Wagtail",
+        "Framework :: Wagtail :: 4",
+        "Framework :: Wagtail :: 5",
+        "Topic :: Internet :: WWW/HTTP",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "wagtail>=4.1",
+        "webstories>=0.0.1,<1",
+        "requests>=2.24.0,<3",
+        "beautifulsoup4>=4.6,<5",
+    ],
+    extras_require={
+        "testing": [
+            "responses>=0.12,<1",
+            "Pillow>=4.0.0,<10.0.0",
+            "wagtailmedia>=0.6,<0.15",
+        ]
+    },
+    license="BSD",
+)
