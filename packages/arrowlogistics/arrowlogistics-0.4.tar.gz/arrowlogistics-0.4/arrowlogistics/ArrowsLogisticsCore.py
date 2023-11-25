62268bed57bbf0a5971bfe490a9efbdea42931ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 04:05:03 +0000

    Add package arrowlogistics/0.4/arrowlogistics-0.4.tar.gz

---
+++ packages/arrowlogistics/arrowlogistics-0.4.tar.gz/arrowlogistics-0.4/arrowlogistics/ArrowsLogisticsCore.py
@@ -0,0 +1,191 @@
+# ArrowLogistics by Senko
+
+# Library/high-level programming language for logics-arrows.io. Logic-Arrows developer - Onigiri.
+
+# Thanks Xeonys, KtoTo for parsing the save system.
+
+# You can modify the code and distribute it for free. Selling, issuing an unchanged code for your own is prohibited.
+
+import base64
+
+
+
+
+
+TYPES = {
+    "whitespace": 0,
+    "arrow": 1,
+    "source": 2,
+    "blocker": 3,
+    "delay_arrow": 4,
+    "detector": 5,
+    "splitter_1": 6,
+    "splitter_2": 7,
+    "splitter_3": 8,
+    "pulse": 9,
+    "blue_arrow": 10,
+    "diagonal_arrow": 11,
+    "blue_splitter_1": 12,
+    "blue_splitter_2": 13,
+    "blue_splitter_3": 14,
+    "gate_not": 15,
+    "gate_and": 16,
+    "gate_xor": 17,
+    "latch": 18,
+    "t_flip_flop": 19,
+    "randomizer": 20,
+    "button": 21,
+    "directional_button": 24,
+}
+
+ROTATION = {"up": 0, "right": 1, "down": 2, "left": 3}
+
+
+
+def encode(inp):
+    numbers = list(map(int, inp.split()))
+    print(numbers)
+    byte_array = bytes(numbers)
+    base64_string = base64.b64encode(byte_array).decode('utf-8')
+    return base64_string
+
+
+def setup(n):
+    if int(n) < 256:
+        output_ArrowLogistics = "0 0 " + str(n) + " 0" + " 0 0 0 0"
+        return output_ArrowLogistics
+    else:
+
+        print("Setup(" + str(n) + ")")
+        print("^^^^^^^^^^^^^^^^^^^^")
+        print("cannot create up 256 chunks")
+        exit()
+
+def npr(typearl, pos, rotation):
+    typeArrowLogistics = TYPES[typearl]
+    posArrowLogistics = pos
+    rotationArrowLogistics = ROTATION[rotation]
+
+    output = " " + " " + str(typeArrowLogistics) + " 0 " + str(posArrowLogistics) + " " + str(rotationArrowLogistics)
+
+    return output
+
+def tpr(typearl, pos, rotation):
+    typeArrowLogistics = typearl
+    posArrowLogistics = pos
+    rotationArrowLogistics = rotation
+
+    output = " " + " " + str(typeArrowLogistics) + " 0 " + str(posArrowLogistics) + " " + str(rotationArrowLogistics)
+
+    return output
+
+
+def newChunk(x, y):
+
+    output = f" {x} 0 {y} 0"
+
+    return output
+
+
+def counts(n):
+    a = " " + str(n - 1) + " "
+    return a
+
+
+def ramgen():
+    a = ""
+
+    for i in range(1, 7):
+        if i == 1:
+            for iw in range(0, 16):
+                if iw % 2:
+                    a += tpr(3, iw, 3)
+
+                else:
+                    a += tpr(18, iw, 0)
+        elif i == 2:
+            for iw in range(0, 16):
+                if iw % 2:
+                    a += tpr(13, iw + 16, 3)
+
+                else:
+                    a += tpr(1, iw + 16, 0)
+
+        elif i == 3:
+            for iw in range(0, 16):
+                if iw % 2:
+                    a += tpr(13, iw + 32, 7)
+        elif i == 4:
+            for iw in range(0, 16):
+                if iw % 2:
+                    a += tpr(3, iw + 16 * 3, 3)
+
+                else:
+                    a += tpr(10, iw + 16 * 3, 0)
+        elif i == 5:
+            for iw in range(0, 16):
+                if iw % 2:
+                    pass
+
+                else:
+                    a += tpr(19, iw + 16 * 4, 0)
+        elif i == 6:
+            for iw in range(0, 16):
+                if iw % 2:
+                    pass
+
+                else:
+                    a += tpr(24, iw + 16 * 5, 0)
+
+    return a
+
+
+def peerbuild(filename):
+    file_name = filename
+
+    formatpeer = file_name.split(".")[1]
+
+    outputbytearray = ""
+
+
+
+
+    if formatpeer == "peer":
+        with open(file_name, 'r') as file:
+            lines = [line for line in file]
+
+            for i in lines:
+
+
+
+
+                if "arrow" in i:
+
+                    command = i.replace("("," ").replace(")"," ").replace(",", " ").replace(";", " ").split(" ")
+
+                    commandd = [item for item in command if item != ""]
+                    commandss = [item for item in commandd if item != "\n"]
+                    commands = [int(item) if item.isdigit() else item for item in commandss]
+
+
+
+                    types = commands[1]
+
+                    x = commands[2]
+
+                    y = commands[3]
+
+                    r = commands[4]
+
+                    pos = x + (y*16)
+
+
+
+                    outputbytearray += tpr(types, pos, r)
+
+
+
+    else:
+        print(f"ERROR: It is not possible to open a file with the format '{formatpeer}' try '.peer'")
+
+    return outputbytearray
---
+++ packages/arrowlogistics/arrowlogistics-0.4.tar.gz/arrowlogistics-0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='arrowlogistics',
+      version='0.4',
+      description='ArrowLogics',
+      packages=['arrowlogistics'],
+      author_email='zxctails@gmail.com',
+      zip_safe=False)
+
+
