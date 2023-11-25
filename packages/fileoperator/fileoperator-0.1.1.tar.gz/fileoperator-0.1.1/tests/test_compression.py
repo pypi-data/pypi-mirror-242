392e616ed8460ad8b3aaba6f434274259c465c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 04:05:09 +0000

    Add package fileoperator/0.1.1/fileoperator-0.1.1.tar.gz

---
+++ packages/fileoperator/fileoperator-0.1.1.tar.gz/fileoperator-0.1.1/tests/test_compression.py
@@ -0,0 +1,8 @@
+"""
+import fileoperator
+
+fileoperator.compress_file("testdir/test.txt", "testdir/test.zip")
+fileoperator.decompress_file("testdir/test.zip", "testdir/testzip.zip")
+fileoperator.compress_folder("./testdir/", "./testcompress.zip")
+fileoperator.decompress_file("./testcompress/", "./testdir2.zip")
+"""
---
+++ packages/fileoperator/fileoperator-0.1.1.tar.gz/fileoperator-0.1.1/tests/test_hash.py
@@ -0,0 +1,6 @@
+"""
+import fileoperator
+
+print(fileoperator.hash_file("testdir/test.txt"))
+print(fileoperator.verify_file_checksum("testdir/test.txt", 123))
+"""
