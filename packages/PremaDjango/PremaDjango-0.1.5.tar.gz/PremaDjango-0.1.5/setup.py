cc0c871757559af29c5af2fed769f2621bef6a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:05:18 +0000

    Add package PremaDjango/0.1.5/PremaDjango-0.1.5.tar.gz

---
+++ packages/PremaDjango/PremaDjango-0.1.5.tar.gz/PremaDjango-0.1.5/setup.py
@@ -0,0 +1,23 @@
+# from setuptools import setup, find_packages
+#
+# setup(
+#     name='PremaDjango',
+#     use_scm_version=True,
+#     setup_requires=['setuptools_scm'],
+#     packages=find_packages(),
+#     install_requires=[
+#         # List your dependencies here
+#     ],
+# )
+#
+
+from setuptools import setup, find_packages
+
+setup(
+    name='PremaDjango',
+    version='0.1.5',
+    packages=find_packages(),
+    install_requires=[
+        'Django',  # Add other default libraries here
+    ],
+)
