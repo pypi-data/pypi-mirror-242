a8a5331b51fa664cbb413b7bc720ce545872a2db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:05:02 +0000

    Add package frenetix/0.1.1rc1/frenetix-0.1.1rc1.tar.gz

---
+++ packages/frenetix/frenetix-0.1.1rc1.tar.gz/frenetix-0.1.1rc1/src/strategies/CostFunctions/CalculateDistanceToObstacleCost.cpp
@@ -0,0 +1,27 @@
+#include "CalculateDistanceToObstacleCost.hpp"
+
+#include "CartesianSample.hpp"
+#include "TrajectorySample.hpp"
+
+CalculateDistanceToObstacleCost::CalculateDistanceToObstacleCost(std::string funName, double costWeight, Eigen::Ref<RowMatrixXd> obstacles)
+    : CostStrategy(funName, costWeight)
+    , m_obstacles(obstacles)
+{
+}
+
+void CalculateDistanceToObstacleCost::evaluateTrajectory(TrajectorySample& trajectory)
+{
+    double cost {0};
+
+    Eigen::MatrixXd posMatrix(2, trajectory.m_cartesianSample.x.size());
+    posMatrix.row(0) = trajectory.m_cartesianSample.x;
+    posMatrix.row(1) = trajectory.m_cartesianSample.y;
+
+    for (int i = 0; i < m_obstacles.rows(); ++i) {
+        Eigen::Vector2d obstaclePos(m_obstacles(i, 0), m_obstacles(i, 1));
+        Eigen::MatrixXd diffMatrix = (posMatrix.colwise() - obstaclePos).colwise().norm();
+        cost += (1.0 / (diffMatrix.array() * diffMatrix.array())).sum();
+    }
+
+    trajectory.addCostValueToList(m_functionName, cost, cost*m_costWeight);
+}
---
+++ packages/frenetix/frenetix-0.1.1rc1.tar.gz/frenetix-0.1.1rc1/src/strategies/CostFunctions/CalculateVelocityOffsetCost.cpp
@@ -0,0 +1,28 @@
+#include "CalculateVelocityOffsetCost.hpp"
+
+#include <Eigen/Core>
+#include <cmath>
+
+#include "CartesianSample.hpp"
+#include "TrajectorySample.hpp"
+
+CalculateVelocityOffsetCost::CalculateVelocityOffsetCost(std::string funName, double costWeight, double desiredSpeed)
+    : CostStrategy(funName, costWeight)
+    , m_desiredSpeed(desiredSpeed)
+{
+}
+
+void CalculateVelocityOffsetCost::evaluateTrajectory(TrajectorySample& trajectory)
+{
+    double cost {0};
+
+    const Eigen::VectorXd& vel = trajectory.m_cartesianSample.velocity;
+
+    int half_idx = vel.size() / 2;
+    int half_count = vel.size() - half_idx - 1;
+    cost = (vel.segment(half_idx, half_count) - Eigen::VectorXd::Constant(half_count, m_desiredSpeed)).cwiseAbs().sum();
+    cost += std::pow((vel(vel.size()-1) - m_desiredSpeed), 2);
+
+
+    trajectory.addCostValueToList(m_functionName, cost, cost*m_costWeight);
+}
