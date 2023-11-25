b19d701d14565191d9e10ab9d2d556a4072f0940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:05:32 +0000

    Add package st-microservice/1.2.3/st_microservice-1.2.3-py3-none-any.whl

---
+++ packages/st-microservice/st_microservice-1.2.3-py3-none-any.whl/st_microservice/backend_connector.py
@@ -0,0 +1,18 @@
+from typing import Any
+
+from graphql import GraphQLResolveInfo
+from httpx import AsyncClient
+from st_microservice.request_utils import get_request, get_token
+
+
+async def graphql_call(info: GraphQLResolveInfo, uri: str, query: str, variables: dict[str, Any] | None = None):
+    client: AsyncClient = get_request(info).app.state.httpx_client
+    return await send_graphql_request(client, get_token(info), uri, query, variables)
+
+
+async def send_graphql_request(client: AsyncClient, token: str, uri: str, query: str, variables: dict[str, Any] | None):
+    headers = {'Authorization': 'bearer ' + token}
+    response = await client.post(uri, headers=headers, json={'query': query, 'variables': variables})
+    if response.is_error:
+        raise Exception(response.json()['error'])
+    return response.json()['data']
