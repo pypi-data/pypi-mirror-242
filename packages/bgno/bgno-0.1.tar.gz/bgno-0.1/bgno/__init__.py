5e8b6600a3d3816e650e4ebd1ce01f936643436d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:04:50 +0000

    Add package bgno/0.1/bgno-0.1.tar.gz

---
+++ packages/bgno/bgno-0.1.tar.gz/bgno-0.1/bgno/__init__.py
@@ -0,0 +1,4 @@
+from Conv2DLayer import Conv2DLayer
+from MaxPool2DLayer import MaxPool2DLayer
+from Flatten2D import Flatten2D
+from Dense2D import Dense2D
---
+++ packages/bgno/bgno-0.1.tar.gz/bgno-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='bgno',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['bgno'],
+      author_email='arturgugasian@gmail.com',
+      zip_safe=False)
