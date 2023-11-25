6a15face55f5cfbf828e1c9e697ade97811aa527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:04:41 +0000

    Add package appollo/1.1.1/appollo-1.1.1-py3-none-any.whl

---
+++ packages/appollo/appollo-1.1.1-py3-none-any.whl/appollo/__main__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+def appollo():
+    print("Appollo has been renamed to Odevio.")
+    print("To download it and have access to the latest features type")
+    print("pip install odevio")
+    print("Or go to https://odevio.com to download our new desktop version")
