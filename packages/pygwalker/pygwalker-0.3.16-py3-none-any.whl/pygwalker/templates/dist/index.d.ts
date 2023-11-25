e09d09efabd4f642d6ac26af2a77792f6d2902f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:04:49 +0000

    Add package pygwalker/0.3.16/pygwalker-0.3.16-py3-none-any.whl

---
+++ packages/pygwalker/pygwalker-0.3.16-py3-none-any.whl/pygwalker/templates/dist/index.d.ts
@@ -0,0 +1,6 @@
+import { IAppProps } from './interfaces';
+declare function GWalker(props: IAppProps, id: string): void;
+declare const _default: {
+    GWalker: typeof GWalker;
+};
+export default _default;
