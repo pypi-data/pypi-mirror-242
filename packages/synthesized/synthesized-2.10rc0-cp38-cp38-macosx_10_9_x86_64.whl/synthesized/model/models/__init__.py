761bb34835b3710926224d8c897f11b85ec218dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:05:38 +0000

    Add package synthesized/2.10rc0/synthesized-2.10rc0-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/synthesized/synthesized-2.10rc0-cp38-cp38-macosx_10_9_x86_64.whl/synthesized/model/models/__init__.py
@@ -0,0 +1,20 @@
+from .address import AddressModel, PostcodeModel
+from .bank import BankModel
+from .enumeration import Enumeration
+from .histogram import Histogram
+from .kde import KernelDensityEstimate
+from .person import GenderModel, PersonModel
+from .string import FormattedStringModel, SequentialFormattedString
+
+__all__ = [
+    "AddressModel",
+    "BankModel",
+    "Histogram",
+    "KernelDensityEstimate",
+    "PersonModel",
+    "GenderModel",
+    "PostcodeModel",
+    "FormattedStringModel",
+    "SequentialFormattedString",
+    "Enumeration",
+]
