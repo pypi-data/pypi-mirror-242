f398db22231a0673684ccda818ca4c1c9d79ed68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 04:05:08 +0000

    Add package clisops/0.12.0/clisops-0.12.0.tar.gz

---
+++ packages/clisops/clisops-0.12.0.tar.gz/clisops-0.12.0/clisops/core/__init__.py
@@ -0,0 +1,13 @@
+from .subset import (
+    create_mask,
+    subset_bbox,
+    subset_gridpoint,
+    subset_level,
+    subset_level_by_values,
+    subset_shape,
+    subset_time,
+    subset_time_by_components,
+    subset_time_by_values,
+)
+
+from .regrid import Grid, Weights, regrid, weights_cache_init, weights_cache_flush
