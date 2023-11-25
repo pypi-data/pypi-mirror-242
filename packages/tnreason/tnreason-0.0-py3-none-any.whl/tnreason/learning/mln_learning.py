464de5f4a66e25bc37363b85566283be0e604012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:06:12 +0000

    Add package tnreason/0.0/tnreason-0.0-py3-none-any.whl

---
+++ packages/tnreason/tnreason-0.0-py3-none-any.whl/tnreason/learning/mln_learning.py
@@ -0,0 +1,82 @@
+import tnreason.logic.expression_calculus as ec
+import tnreason.logic.coordinate_calculus as cc
+import tnreason.logic.expression_generation as eg
+
+import tnreason.representation.factdf_to_cores as ftoc
+import tnreason.representation.sampledf_to_cores as stoc
+import tnreason.representation.pairdf_to_cores as ptoc
+
+import tnreason.optimization.weight_estimation as wees
+
+import tnreason.learning.expression_learning as el
+
+from tnreason.model import markov_logic_network as mln
+
+import numpy as np
+
+
+class AtomicMLNLearner:
+    def __init__(self):
+        self.weightedFormulas = []
+        self.sampleDf = None
+        self.atomDict = None
+
+    def load_sampleDf(self, sampleDf):
+        self.sampleDf = sampleDf
+
+        atoms = list(sampleDf.columns)
+        self.atomDict = {
+            atom: cc.CoordinateCore(stoc.sampleDf_to_universal_core(sampleDf, [atom]).flatten(), ["j"])
+            for atom in atoms
+        }
+
+    def learn_independent_weight(self, expression, verbose = False):
+        return wees.calculate_weight(expression, self.atomDict, verbose=verbose)
+
+    def add_independent_formula(self, expression):
+        weight = self.learn_independent_weight(expression)
+        self.weightedFormulas.append([expression, weight])
+        print("The expression {} with weight {} has been added.".format(expression, weight))
+
+    def learn_implication(self, positiveExpression, skeletonExpression, candidatesDict):
+        positiveCore = ec.evaluate_expression_on_sampleDf(self.sampleDf, positiveExpression)
+        negativeCore = positiveCore.negate()
+
+        learnedPremise = self.learn_formula(skeletonExpression, candidatesDict, positiveCore, negativeCore)
+        solutionExpression = eg.generate_from_generic_expression([learnedPremise, "imp", positiveExpression])
+
+        self.add_independent_formula(solutionExpression)
+
+    def learn_equivalence(self, positiveExpression, skeletonExpression, candidatesDict):
+        positiveCore = ec.evaluate_expression_on_sampleDf(self.sampleDf, positiveExpression)
+        negativeCore = positiveCore.negate()
+
+        learnedPremise = self.learn_formula(skeletonExpression, candidatesDict, positiveCore, negativeCore)
+        solutionExpression = eg.generate_from_generic_expression([learnedPremise, "eq", positiveExpression])
+
+        self.add_independent_formula(solutionExpression)
+
+    def learn_tautology(self, skeletonExpression, candidatesDict):
+        sampleNum = self.sampleDf.values.shape[0]
+        positiveCore = cc.CoordinateCore(np.ones(sampleNum),["j"])
+        negativeCore = positiveCore.negate()
+
+        solutionExpression = self.learn_formula(skeletonExpression, candidatesDict, positiveCore, negativeCore)
+
+        self.add_independent_formula(solutionExpression)
+
+    def learn_formula(self, skeletonExpression, candidatesDict, positiveCore, negativeCore):
+        exLearner = el.AtomicLearner(skeletonExpression, candidatesDict)
+
+        exLearner.generate_fixedCores_sampleDf(self.sampleDf)
+        exLearner.generate_target_and_filterCore_from_exampleCores(positiveCore, negativeCore)
+        exLearner.random_initialize_variableCoresDict()
+        exLearner.als(10)
+        exLearner.get_solution()
+
+        return exLearner.solutionExpression
+
+    def generate_mln(self):
+        return mln.MarkovLogicNetwork(expressionsDict=
+                                      {str(i): [self.weightedFormulas[i][0], self.weightedFormulas[i][1]]
+                                       for i in range(len(self.weightedFormulas))})
---
+++ packages/tnreason/tnreason-0.0-py3-none-any.whl/tnreason/optimization/weight_estimation.py
@@ -0,0 +1,86 @@
+from tnreason.logic import satisfaction_counter as sc
+from tnreason.logic import expression_calculus as ec
+
+from tnreason.model import markov_logic_network as mln
+
+import numpy as np
+
+
+def calculate_satRate_bc(expression):
+    return mln.calculate_dangling_basis(expression).count_satisfaction()
+
+def solve_rate_equation(satRate, empRate):
+    return -np.log(((1 - empRate) * satRate) / (empRate * (1 - satRate)))
+
+
+def regularize_empRate(empRate, regFactor):
+    return regFactor * (empRate - 0.5) + 0.5
+
+def cutoff_weight(weight, cutoff):
+    if weight > cutoff:
+        return cutoff
+    else:
+        return  weight
+
+def calculate_empRate(expression, atomDict, filterCore=None):
+    expressionCore = ec.calculate_core(atomDict, expression)
+    if filterCore is None:
+        expressionResults = expressionCore.values.flatten()
+        empSatNum = np.sum(expressionResults)
+        dataNum = expressionResults.shape[0]
+    else:
+        expressionCore = expressionCore.compute_and(filterCore)
+        empSatNum = np.sum(expressionCore.values)
+        dataNum = np.sum(filterCore.values)
+    return empSatNum / dataNum
+
+
+def calculate_satRate(expression):
+    variables = np.unique(ec.get_variables(expression))
+    modelNum = 2 ** len(variables)
+    satNum = sc.count_satisfaction(expression)
+    return satNum / modelNum
+
+
+def calculate_weight(expression, atomDict, filterCore=None, regFactor=1, verbose=False, check=True, cut = 20):
+    satRate = calculate_satRate(expression)
+
+    if check:
+        assert satRate == calculate_satRate_bc(expression), "Saturation Rate does not coincide with Basis Calculus!"
+
+    empRate = regularize_empRate(calculate_empRate(expression, atomDict, filterCore), regFactor)
+    weight = cutoff_weight(solve_rate_equation(satRate, empRate), cut)
+    if verbose:
+        print("## Calculationg the weight of {} ##".format(expression))
+        print("World satisfaction rate: {}".format(satRate))
+        print("Data satifcation rate: {}".format(empRate))
+        print("Calculated weight: {}".format(weight))
+    return weight
+
+
+def partition_function(satRate, w):
+    return (satRate * np.exp(w)) / (1 - satRate + satRate * np.exp(w))
+
+
+if __name__ == "__main__":
+    from tnreason.logic import coordinate_calculus as cc
+
+    atomDict = {
+        "a": cc.CoordinateCore(np.random.binomial(n=1, p=0.4, size=(10, 7, 5)), ["l1", "y", "z"], name="a"),
+        "b": cc.CoordinateCore(np.random.binomial(n=1, p=0.4, size=(10, 7, 5)), ["l2", "q", "z"], name="b"),
+        "c": cc.CoordinateCore(np.random.binomial(n=1, p=0.4, size=(10, 7, 5)), ["l3", "q", "z"], name="c"),
+    }
+
+    expression = ["b", "and", ["a", "and", ["not", "c"]]]
+    filterCore = cc.CoordinateCore(np.random.binomial(n=1, p=0.4, size=(10, 10, 10, 7, 7, 5)),
+                                   ["l1", "l2", "l3", "y", "q", "z"], name="c")
+
+    checkEmpRate = calculate_empRate(expression, atomDict, filterCore)
+    checkSatRate = calculate_satRate(expression)
+    result = calculate_weight(expression, atomDict, filterCore)
+
+    assert np.abs(partition_function(checkSatRate, result) - checkEmpRate) < 0.001, "Weight Calculation failed."
+
+    print(solve_rate_equation(0.65,0.8))
+
+    print(mln.calculate_dangling_basis(["not",["a","and",["not","b"]]]).values.shape)
