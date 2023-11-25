a3b9dd6be1d08257f57bf761b0a3af9ccbd364e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:04:15 +0000

    Add package python-ishare/0.0.1/python-ishare-0.0.1.tar.gz

---
+++ packages/python-ishare/python-ishare-0.0.1.tar.gz/python-ishare-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='python-ishare',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='',
+    author='Topsector Logistiek',
+    author_email='huibert.alblas@topsectorlogistiek.nl',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
