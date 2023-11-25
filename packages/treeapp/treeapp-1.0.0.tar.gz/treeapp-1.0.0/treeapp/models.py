ac3c8ee74f4f00e7721bb5b5360937a54b6f5f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 04:05:18 +0000

    Add package treeapp/1.0.0/treeapp-1.0.0.tar.gz

---
+++ packages/treeapp/treeapp-1.0.0.tar.gz/treeapp-1.0.0/treeapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class TreeNode(models.Model):
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    def __str__(self):
+        return self.name
+
+    # Additional methods can be added as needed
