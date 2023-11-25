c6845897bdb59444b72220673e1136c0003abcd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 04:04:59 +0000

    Add package 42videobricks-python-client/1.2.0/42videobricks_python_client-1.2.0-py3-none-any.whl

---
+++ packages/42videobricks-python-client/42videobricks_python_client-1.2.0-py3-none-any.whl/Api42Vb/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+# import apis into api package
+from Api42Vb.api.data_api import DataApi
+from Api42Vb.api.playlists_api import PlaylistsApi
+from Api42Vb.api.tags_api import TagsApi
+from Api42Vb.api.videos_api import VideosApi
+from Api42Vb.api.webhooks_api import WebhooksApi
+
