9253dd558c28cde382df50f79bb73db6fd7baca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 04:05:07 +0000

    Add package marvlcli/0.0.18/marvlcli-0.0.18.tar.gz

---
+++ packages/marvlcli/marvlcli-0.0.18.tar.gz/marvlcli-0.0.18/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="marvlcli",
+    version='0.0.18',
+    py_modules= ['marvlcli','info','ssh','list', 'create', 'delete', 'client'],
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'pyyaml',
+        'python-novaclient',
+        'python-cinderclient',
+        'python-neutronclient',
+        'python-openstackclient',
+        'ws4py'
+        
+    ],
+    include_package_data=True,
+    package_data={'package_data': ['payloads.yaml', 'myuserdata_marv_default.txt']},
+    entry_points='''
+    [console_scripts]
+    marvl=marvlcli:marvlcli
+    
+    
+    '''
+    
+)
+
